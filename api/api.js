2cf39acc1b25cc08322493f53f34d4b4e6a56223
Travis Rich <travis.s.rich@gmail.com>
2015-11-04 10:36:41 -0500

    Tweak strategy

--- api/api.js
+++ api/api.js
@@ -26,7 +26,7 @@ var User = require('./models').User;
 
 passport.use(new LocalStrategy(
   function(username, password, done) {
-    User.findOne({ username: username }, function (err, user) {
+    User.findOne({ email: username }, function (err, user) {
       if (err) { return done(err); }
       if (!user) {
         return done(null, false, { message: 'Incorrect username.' });
