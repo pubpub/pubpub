f4bcbcaf320afa9b1fa74234f8f1c1bda67c550e
Travis Rich <travis.s.rich@gmail.com>
2015-11-05 13:45:04 -0500

    Ooops. All backend stuff broke on heroku.

--- api/routes.js
+++ api/routes.js
@@ -1,13 +1,13 @@
 var app = require('./api');
 
-app.all('/*', function(req, res, next) {
-  // res.header("Access-Control-Allow-Origin", "http://pub.media.mit.edu");
-  res.header("Access-Control-Allow-Origin", req.headers.origin);
-  res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
-  res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Credentials", true);
-  next();
-});
+// app.all('/*', function(req, res, next) {
+//   // res.header("Access-Control-Allow-Origin", "http://pub.media.mit.edu");
+//   res.header("Access-Control-Allow-Origin", req.headers.origin);
+//   res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+//   res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+//   res.header("Access-Control-Allow-Credentials", true);
+//   next();
+// });
 
 
 require('./routes/login-routes');
