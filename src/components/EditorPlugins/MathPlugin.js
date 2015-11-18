2b93378dc9783eaf36742e8ceb50ad3c7a6d7ee3
Thariq Shihipar <trq212@gmail.com>
2015-11-17 21:26:54 -0500

    Making math loading work better

--- src/components/EditorPlugins/MathPlugin.js
+++ src/components/EditorPlugins/MathPlugin.js
@@ -1,5 +1,6 @@
 import React, {PropTypes} from 'react';
 import Radium from 'radium';
+import ImageLoader from 'react-imageloader';
 
 // let styles = {};
 
@@ -8,11 +9,29 @@ const MathPlugin = React.createClass({
 		svg: PropTypes.string,
 		children: PropTypes.string
 	},
+	getInitialState: function() {
+		this.lastURL = undefined;
+		return {};
+	},
+	preloader: function() {
+		let result;
+		if (this.lastURL) {
+			result = <img src={this.lastURL}></img>;
+		} else {
+			result = <span>loading</span>;
+		}
+		return result;
+	},
+	loadedImage: function(evt) {
+		this.lastURL = evt.target.src;
+	},
 	render: function() {
 		const equation = this.props.children;
 		const urlRequest = 'https://dry-retreat-1640.herokuapp.com/mathtest?equation=' + encodeURIComponent(equation) + '&cache=true';
 		return (
-      <img src={urlRequest}></img>
+			<ImageLoader onLoad={this.loadedImage} src={urlRequest} wrapper={React.DOM.span} preloader={this.preloader}>
+				Image load failed!
+			</ImageLoader>
 		);
 	}
 });
