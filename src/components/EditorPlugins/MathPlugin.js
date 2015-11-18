8d9ec026462cf3885e62666c38ad81e3bc916caf
Thariq Shihipar <trq212@gmail.com>
2015-11-18 12:11:19 -0500

    Getting rid of cache and making the equation rendering a bit better

--- src/components/EditorPlugins/MathPlugin.js
+++ src/components/EditorPlugins/MathPlugin.js
@@ -16,21 +16,22 @@ const MathPlugin = React.createClass({
 	preloader: function() {
 		let result;
 		if (this.lastURL) {
-			result = <img src={this.lastURL}></img>;
+			result = <span>{this.lastURL}</span>;
 		} else {
 			result = <span>loading</span>;
 		}
 		return result;
 	},
 	loadedImage: function(evt) {
-		this.lastURL = evt.target.src;
+		// this.lastURL = evt.target.src;
+		this.lastURL = this.props.children;
 	},
 	render: function() {
 		const equation = this.props.children;
-		const urlRequest = 'https://dry-retreat-1640.herokuapp.com/mathtest?equation=' + encodeURIComponent(equation) + '&cache=true';
+		const urlRequest = 'https://dry-retreat-1640.herokuapp.com/mathtest?equation=' + encodeURIComponent(equation);
 		return (
 			<ImageLoader onLoad={this.loadedImage} src={urlRequest} wrapper={React.DOM.span} preloader={this.preloader}>
-				Syntax Error!
+				{equation}
 			</ImageLoader>
 		);
 	}
