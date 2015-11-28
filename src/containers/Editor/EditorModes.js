227397135f146ccf249c6f617aaea500aa197074
Thariq Shihipar <trq212@gmail.com>
2015-11-28 18:35:17 -0500

    Making editor modes work on some units

--- src/containers/Editor/EditorModes.js
+++ src/containers/Editor/EditorModes.js
@@ -1,24 +1,24 @@
 export default function() {
 	CodeMirror.defineSimpleMode("plugin", {
 		start: [
-			{regex: /.*/, token: "string"},
+			{regex: /\[abstract.*\]/, token: "meta-abstract number"},
+			{regex: /\[title.*\]/, token: "meta-abstract number"},
+			{regex: /\[asset.*\]/, token: "plugin-asset string"},
+			{regex: /\[image.*\]/, token: "plugin-image string"}
 		],
-		comment: [
-			{regex: /.*?\*\//, token: "comment", next: "start"},
-			{regex: /.*/, token: "comment"}
-		],
-		meta: {
-			dontIndentStates: ["comment"],
-			lineComment: "//"
-		}
+		asset: [
+			{regex: /.*/, token: "plugin-asset"},
+			{regex: /\]/, token: "plugin-asset",next:'start'}
+		]
 	});
 
 	CodeMirror.defineMode("pubpubmarkdown", function(config) {
 		return CodeMirror.multiplexingMode(
 			CodeMirror.getMode(config, "markdown"),
-			{open: "::", close: "::",
+			{open: "[", close: "]",
 			 mode: CodeMirror.getMode(config, "plugin"),
-			 delimStyle: "plugindelimit"}
+			 innerStyle: 'test',
+		 	 parseDelimiters: true}
 		);
 	});
 }
