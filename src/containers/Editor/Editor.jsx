190038f0262882b21cac06800c5e36a738971244
Travis Rich <travis.s.rich@gmail.com>
2015-11-29 11:34:50 -0500

    Splitting up some more of Editor.jsx and cleaning up commented code, etc

--- src/containers/Editor/Editor.jsx
+++ src/containers/Editor/Editor.jsx
@@ -4,15 +4,19 @@ import {connect} from 'react-redux';
 import { pushState } from 'redux-router';
 import Radium, {Style} from 'radium';
 import DocumentMeta from 'react-document-meta';
+
 import PureRenderMixin from 'react-addons-pure-render-mixin';
+import ReactFireMixin from 'reactfire';
+
 import {LoaderDeterminate, EditorPluginPopup} from '../../components';
-import {loadCss} from '../../utils/loadingFunctions';
 import {EditorModalAssets, EditorModalCollaborators, EditorModalPublish, EditorModalReferences, EditorModalSettings} from '../../components/EditorModals';
 import {getPubEdit, toggleEditorViewMode, toggleFormatting, toggleTOC, unmountEditor, closeModal, openModal, publishVersion, saveCollaboratorsToPub, saveSettingsPubPub} from '../../actions/editor';
 import {saveSettingsUser} from '../../actions/login';
-import ReactFireMixin from 'reactfire';
-import EditorModes from './EditorModes';
-import {styles, codeMirrorStyles, animateListItemStyle} from './EditorStyles';
+import {loadCss} from '../../utils/loadingFunctions';
+import EditorModes from './editorCodeMirrorMode';
+import {styles, codeMirrorStyles, animateListItemStyle} from './editorStyles';
+import {insertText, createFocusDoc} from './editorCodeFunctions';
+
 
 import markLib from '../../modules/markdown/markdown';
 import markdownExtensions from '../../components/EditorPlugins';
@@ -214,110 +218,28 @@ const Editor = React.createClass({
 	insertFormatting: function(formatting) {
 		return ()=>{
 			const cm = this.getActiveCodemirrorInstance();
-
-
 			const currentSelection = cm.getSelection();
-			const insertText = currentSelection !== '' ? currentSelection : 'example';
-
+			const baseText = currentSelection !== '' ? currentSelection : 'example';
+			insertText(cm, formatting, baseText);
 			this.toggleFormatting();
-
-			switch (formatting) {
-			case 'H1':
-				return cm.replaceSelection('# ' + insertText + '\n');
-			case 'H2':
-				return cm.replaceSelection('## ' + insertText + '\n');
-			case 'H3':
-				return cm.replaceSelection('### ' + insertText + '\n');
-			case 'Bold':
-				return cm.replaceSelection('**' + insertText + '**');
-			case 'Italic':
-				return cm.replaceSelection('*' + insertText + '*');
-			case '# List':
-				return cm.replaceSelection('\n 1. ' + insertText + '\n');
-			case '- List':
-				return cm.replaceSelection('\n -  ' + insertText + '\n');
-			case 'Image':
-				return cm.replaceSelection('::image::refName::');
-			case 'Video':
-				return cm.replaceSelection('::video::refName::');
-			default:
-				return null;
-			}
-
-
 		};
 	},
 
 	// focusEditor: function(title, index) {
+	// TODO: use the index variable that's passed in to accomodate the case
+	// where a document has more than one identical header title.
+	// Right now, no matter which is clicked, the focus will focus on the first instance of it.
 	focusEditor: function(title) {
-		// TODO: use the index variable that's passed in to accomodate the case
-		// where a document has more than one identical header title.
-		// Right now, no matter which is clicked, the focus will focus on the first instance of it.
 		return ()=>{
-
-
-			// If the focus button clicked is the same as the activeFocus,
-			// turn off the focusing
+			// If the focus button clicked is the same as the activeFocus, turn off the focusing
 			if (this.state.activeFocus === title) {
 				this.setState({ activeFocus: ''});
-
-				// Erase the existing focus CodeMirror
-				document.getElementById('codemirror-focus-wrapper').innerHTML = '';
-
+				document.getElementById('codemirror-focus-wrapper').innerHTML = ''; // Erase the existing focus CodeMirror
 			} else {
-				// Get main codemirror doc
-				const cm = document.getElementsByClassName('CodeMirror')[0].CodeMirror;
-
-				// Erase the existing focus CodeMirror
-				document.getElementById('codemirror-focus-wrapper').innerHTML = '';
-
-				let startLine = undefined;
-				let endLine = undefined;
-
-				// Iterate over all lines in the doc
-				cm.eachLine(function(line) {
-					// Proceed if either startLine or endLine is undefined
-					if (typeof(startLine) === 'undefined' || typeof(endLine) === 'undefined') {
-
-						// If we have a startline, but no endline, check to see if the line is a header
-						// We wish to set endline to the first #H1 header after startline
-						if (typeof(endLine) === 'undefined' && typeof(startLine) !== 'undefined' && line.stateAfter.header === 1 && line.text !== '') {
-							endLine = cm.getLineNumber(line);
-						}
-
-						// If we don't yet have a startline, see if the current line matches the format of the selected title
-						if (typeof(startLine) === 'undefined' && line.text.indexOf('# ' + title) > -1) {
-							startLine = cm.getLineNumber(line);
-						}
-					}
-				});
-
-				// Create new linked doc from startline and endLine
-				const newFocus = cm.linkedDoc({
-					from: startLine,
-					to: endLine,
-					sharedHist: true,
-				});
-
-				// Create new codemirror inside of the focus-wrapper
-				const cmFocus = CodeMirror(document.getElementById('codemirror-focus-wrapper'), cmOptions);
-
-				// Insert the new focus doc
-				cmFocus.swapDoc(newFocus);
-
-				// Scroll to top.
-				// We had a weird bug where the focus was defaulting to the bottom of the div
-				document.getElementById('editor-text-wrapper').scrollTop = 0;
-
-				// Update the activeFocus state
-				this.setState({
-					activeFocus: title,
-				});
-
-				// Hide the TOC if we were in live-preview mode and it was expanded
-				this.toggleTOC();
+				createFocusDoc(title, cmOptions);
+				this.setState({ activeFocus: title }); // Update the activeFocus state
+				this.toggleTOC(); // Hide the TOC if we were in live-preview mode and it was expanded
 			}
-
 		};
 	},
 
---
+++ src/containers/Editor/editorCodeFunctions.js
@@ -0,0 +1,74 @@
+/* global CodeMirror */
+export function insertText(cm, formatting, baseText) {
+
+	switch (formatting) {
+	case 'H1':
+		return cm.replaceSelection('# ' + baseText + '\n');
+	case 'H2':
+		return cm.replaceSelection('## ' + baseText + '\n');
+	case 'H3':
+		return cm.replaceSelection('### ' + baseText + '\n');
+	case 'Bold':
+		return cm.replaceSelection('**' + baseText + '**');
+	case 'Italic':
+		return cm.replaceSelection('*' + baseText + '*');
+	case '# List':
+		return cm.replaceSelection('\n 1. ' + baseText + '\n');
+	case '- List':
+		return cm.replaceSelection('\n -  ' + baseText + '\n');
+	case 'Image':
+		return cm.replaceSelection('[image]');
+	case 'Video':
+		return cm.replaceSelection('[video]');
+	default:
+		return null;
+	}
+
+}
+
+// focusEditor: function(title, index) {
+export function createFocusDoc(title, cmOptions) {
+	// Get main codemirror doc
+	const cm = document.getElementsByClassName('CodeMirror')[0].CodeMirror;
+
+	// Erase the existing focus CodeMirror
+	document.getElementById('codemirror-focus-wrapper').innerHTML = '';
+
+	let startLine = undefined;
+	let endLine = undefined;
+
+	// Iterate over all lines in the doc
+	cm.eachLine(function(line) {
+		// Proceed if either startLine or endLine is undefined
+		if (typeof(startLine) === 'undefined' || typeof(endLine) === 'undefined') {
+
+			// If we have a startline, but no endline, check to see if the line is a header
+			// We wish to set endline to the first #H1 header after startline
+			if (typeof(endLine) === 'undefined' && typeof(startLine) !== 'undefined' && line.stateAfter.header === 1 && line.text !== '') {
+				endLine = cm.getLineNumber(line);
+			}
+
+			// If we don't yet have a startline, see if the current line matches the format of the selected title
+			if (typeof(startLine) === 'undefined' && line.text.indexOf('# ' + title) > -1) {
+				startLine = cm.getLineNumber(line);
+			}
+		}
+	});
+
+	// Create new linked doc from startline and endLine
+	const newFocus = cm.linkedDoc({
+		from: startLine,
+		to: endLine,
+		sharedHist: true,
+	});
+
+	// Create new codemirror inside of the focus-wrapper
+	const cmFocus = CodeMirror(document.getElementById('codemirror-focus-wrapper'), cmOptions);
+
+	// Insert the new focus doc
+	cmFocus.swapDoc(newFocus);
+
+	// Scroll to top.
+	// We had a weird bug where the focus was defaulting to the bottom of the div
+	document.getElementById('editor-text-wrapper').scrollTop = 0;
+}
