512aa25e72ba03d7cd6be3059f5da041e3e3836a
Thariq Shihipar <trq212@gmail.com>
2015-11-30 19:09:53 -0500

    moving parsing plugins into helper functions

--- src/utils/parsePlugins.js
+++ src/utils/parsePlugins.js
@@ -7,3 +7,35 @@ export function parsePluginString(str) {
 	}
 	return propDict;
 }
+
+export function createPluginString(pluginType, content, refs) {
+	let outputVariables = '';
+	for (const key in content) {
+		// Generate an output string based on the key, values in the object
+		if (Object.prototype.hasOwnProperty.call(content, key)) {
+			const val = refs['pluginInput-' + key].value;
+
+			if (val && val.length) {
+				outputVariables += key + '=' + val + ', ';
+			}
+		}
+	}
+	outputVariables = outputVariables.slice(0, -2); // Remove the last comma and space
+	const mergedString = outputVariables.length ? pluginType + ': ' + outputVariables : pluginType;
+	return mergedString;
+}
+
+/*
+export function getAssetInformation(propDict, assets, assetType) {
+	const refName = propDict.src || 'none';
+	const asset = assets.find(asst => (asst.refName === refName));
+	let url = null;
+	if (asset && asset.assetType === 'image') {
+		url = asset.url_s3;
+	} else if (asset) {
+		url = 'error:type';
+	}
+	propDict.url = url;
+	return propDict;
+}
+*/
