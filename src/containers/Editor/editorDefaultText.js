34858edbfc9d68d07719ca2ce867524250f55c02
Thariq Shihipar <trq212@gmail.com>
2015-12-10 15:24:17 -0500

    Updating defautl text to include title & abstract

--- src/containers/Editor/editorDefaultText.js
+++ src/containers/Editor/editorDefaultText.js
@@ -1,4 +1,8 @@
-const defaultText = `# Welcome to your new Pub!
+const defaultText = `
+[title: Your first pub!]
+[abstract: Here is some abstract text!]
+
+# Welcome to your new Pub!
 Here you can find lots of useful things to do.
 
 ## Like create subheaders
