173bcd92462b3b36b92b2216ebf010dfcc909444
Travis Rich <travis.s.rich@gmail.com>
2015-12-28 18:46:41 -0500

    Some heavier error handling for /handleNewFile

--- api/routes/test-routes.js
+++ api/routes/test-routes.js
@@ -334,28 +334,50 @@ app.get('/handleNewFile', function(req,res){
 		// There's a 10mb limit for cloudinary images. Gotta fix that.
 		// This shoudl eventually be written to handle discovery and processing of all file types.
 		if(req.query.contentType.indexOf('image') > -1){
-			const delay = (req.query.contentType.indexOf('image/gif') > -1) ? 1500 : 0;
-			// Gifs through a 403 for some reason.
+			const delay = (req.query.contentType.indexOf('image/gif') > -1) ? 1500 : 1000;
+			// Gifs throw a 403 for some reason.
 			// A delay seems to make that 403 go away.
 			// Perhaps S3 is taking time to process? That seems strange, why would it finish to begin with 
 			// if it had more to do?
 			// Probably need a cleaner solution, but this'll work for now.
 			setTimeout(function(){		
-				cloudinary.uploader.upload(req.query.url, function(result) { 
-			        result.thumbnail = result.url.replace('/upload', '/upload/c_limit,h_50,w_50');
-			        result.assetType = 'image';
+				try {
+					cloudinary.uploader.upload(req.query.url, function(result) { 
+				        result.thumbnail = result.url.replace('/upload', '/upload/c_limit,h_50,w_50');
+				        result.assetType = 'image';
+						return res.status(201).json(result);
+					});		
+				} catch (err) {
+					console.log('In catch on cloudinary image upload. '); console.log('req.body', req.body); console.log('err', err);
+					// If cloudinary fails - just set the thumbnail url to the full sized amazon version url. We can go through and check all the
+					// items that have .amazon urls in their thumbnail fields.
+					result = {
+						url: req.query.url,
+						thumbnail: req.query.url,
+						assetType: 'image',
+					};
 					return res.status(201).json(result);
-				});	
+				}
+				
 			}, delay);
 
 		} else if (req.query.contentType.indexOf('video') > -1){
-			cloudinary.uploader.upload(req.query.url, function(result) { 
-				console.log(result) 
-				
-				result.thumbnail = 'https://res.cloudinary.com/pubpub/video/upload/t_media_lib_thumb/c_limit,h_50,w_50/' + result.public_id + '.jpg';
-				result.assetType = 'video';
-				return res.status(201).json(result);
-			}, { resource_type: "video" });
+			try {
+				cloudinary.uploader.upload(req.query.url, function(result) { 
+					// console.log(result) 
+					result.thumbnail = 'https://res.cloudinary.com/pubpub/video/upload/t_media_lib_thumb/c_limit,h_50,w_50/' + result.public_id + '.jpg';
+					result.assetType = 'video';
+					return res.status(201).json(result);
+				}, { resource_type: "video" });	
+			} catch (err) {
+					console.log('In catch on cloudinary video upload.'); console.log('req.body', req.body); console.log('err', err);
+					result = {
+						url: req.query.url,
+						thumbnail: '/thumbnails/file.png',
+						assetType: 'video',
+					};
+					return res.status(201).json(result);
+			
 		} else {
 			return res.status(201).json({
 				thumbnail: '/thumbnails/data.png',
