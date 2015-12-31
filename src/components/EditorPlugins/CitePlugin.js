3e1ca38e21165474c0f3e98673fd8bbf2bce564b
Travis Rich <travis.s.rich@gmail.com>
2015-12-31 17:06:11 -0500

    Hovering on citations a bit cleaner now

--- src/components/EditorPlugins/CitePlugin.js
+++ src/components/EditorPlugins/CitePlugin.js
@@ -2,6 +2,7 @@ import React, {PropTypes} from 'react';
 import Radium from 'radium';
 import {srcRef} from './pluginProps';
 import ErrorMsg from './ErrorPlugin';
+import {Reference} from '../';
 
 export const citeOptions = {srcRef};
 
@@ -16,47 +17,68 @@ const CitePlugin = React.createClass({
 		count: PropTypes.number
 	},
 	getInitialState: function() {
-		this.hoverTimeout = null;
-		return {hover: false, expanded: false};
+		return {
+			hover: false, 
+			// expanded: false
+		};
 	},
 	mouseOver: function() {
-		this.hoverTimeout = setTimeout(this.startHover.bind(this), 100);
+		// this.hoverTimeout = setTimeout(this.startHover.bind(this), 100);
+		this.setState({hover: true});
 	},
 	mouseOut: function() {
 		this.setState({hover: false});
 	},
-	onClick: function() {
-		this.setState({expanded: !this.state.expanded, hover: !this.state.expanded});
-	},
-	cancelHover: function() {
-		if (this.hoverTimeout) {
-			clearTimeout(this.hoverTimeout);
-			this.hoverTimeout = null;
-		}
-	},
-	startHover: function() {
-		this.setState({hover: true});
-	},
-	close: function() {
-		this.setState({expanded: false, hover: false});
-	},
+	// onClick: function() {
+	// 	this.setState({expanded: !this.state.expanded, hover: !this.state.expanded});
+	// },
+	// cancelHover: function() {
+	// 	if (this.hoverTimeout) {
+	// 		clearTimeout(this.hoverTimeout);
+	// 		this.hoverTimeout = null;
+	// 	}
+	// },
+	// startHover: function() {
+	// 	this.setState({hover: true});
+	// },
+	// close: function() {
+	// 	this.setState({expanded: false, hover: false});
+	// },
 	render: function() {
-		let html;
-
+		// let html;
 		if (this.props.error === 'empty') {
-			html = <span></span>;
-		} else if (this.props.error === 'type') {
-			html = <ErrorMsg>Could not find reference.</ErrorMsg>;
-		}	else {
-			const title = this.props.reference.title;
-			const author = this.props.reference.author;
-			const year = this.props.reference.year;
+			return <span></span>;
+		}
+		return (
+			<span>
+				{this.props.error === 'type'
+					? <ErrorMsg>Could not find reference.</ErrorMsg>
+					: <span style={[styles.ref]} onMouseOver={this.mouseOver} onMouseOut={this.mouseOut}>
 
-			const show = this.state.expanded || this.state.hover;
-			const titleStyle = show && ((this.state.expanded && styles.expanded) || (this.state.hover && styles.hover));
+						[{this.props.count}]
+						<span style={[styles.hoverRef, this.state.hover && styles.hoverRefVisible]}>
+							<Reference citationObject={this.props.reference} mode={'mla'} />
+						</span>
+						
+					</span> 
+				}
+			</span>
+		);
 
-			let expandedElem = null;
-			const fakeElem = null;
+		// if (this.props.error === 'empty') {
+		// 	html = <span></span>;
+		// } else if (this.props.error === 'type') {
+		// 	html = 
+		// }	else {
+		// 	const title = this.props.reference.title;
+		// 	const author = this.props.reference.author;
+		// 	const year = this.props.reference.year;
+
+		// 	const show = this.state.expanded || this.state.hover;
+		// 	const titleStyle = show && ((this.state.expanded && styles.expanded) || (this.state.hover && styles.hover));
+
+		// 	let expandedElem = null;
+		// 	const fakeElem = null;
 
 			/*
 			const fakeElem = (<span><img style={[styles.img]} src="https://pbs.twimg.com/media/CWLFL0UUsAEtuij.png"></img>
@@ -65,107 +87,125 @@ const CitePlugin = React.createClass({
 								<a target="_blank" href="http://physics.gu.se/~frtbm/joomla/media/mydocs/Kramers.pdf" >Copy Citation</a></span>);
 			*/
 
-			if (show) {
-				expandedElem = (
-				<span>
-				<span style={[styles.show, titleStyle]} onClick={this.onClick} onMouseOut={this.mouseOut} >
-					<span style={[styles.author]}>({author} , {year})</span> <br/>
-					<span style={[styles.title]}>{title}</span>
-					{fakeElem}
-				</span>
-				<span style={[styles.cover]} onClick={this.close}></span>
-				</span>
-				);
-			}
+			// if (show) {
+			// 	expandedElem = (
+			// 	<span>
+			// 	<span style={[styles.show, titleStyle]} onClick={this.onClick} onMouseOut={this.mouseOut} >
+			// 		<span style={[styles.author]}>({author} , {year})</span> <br/>
+			// 		<span style={[styles.title]}>{title}</span>
+			// 		{fakeElem}
+			// 	</span>
+			// 	<span style={[styles.cover]} onClick={this.close}></span>
+			// 	</span>
+			// 	);
+			// }
 
-			html = (
-				<span style={[styles.ref]} onClick={this.onClick} onMouseOver={this.mouseOver} onMouseOut={this.cancelHover}>
-					[{this.props.count}]
-					{expandedElem}
-				</span>
-			);
-		}
-		return html;
+			// html = (
+			// 	<span style={[styles.ref]} onClick={this.onClick} onMouseOver={this.mouseOver} onMouseOut={this.cancelHover}>
+			// 		[{this.props.count}]
+			// 		{expandedElem}
+			// 	</span>
+			// );
+		// }
+		// return html;
 	}
 });
 
-const expandedHeight = '150px';
-const expandedWidth = '350px';
-const hoverWidth = '150px';
-const hoverHeight = '50px';
+// const expandedHeight = '150px';
+// const expandedWidth = '350px';
+// const hoverWidth = '150px';
+// const hoverHeight = '50px';
 
 
-const pulseKeyframes = Radium.keyframes({
-	'0%': {width: '10px', height: '17px', color: 'rgba(0,0,0,0)'},
-	'90%': {width: hoverWidth, height: hoverHeight, color: 'rgba(0,0,0,0)'},
-	'100%': {width: hoverWidth, height: hoverHeight, color: 'black'},
-}, 'Spinner');
+// const pulseKeyframes = Radium.keyframes({
+// 	'0%': {width: '10px', height: '17px', color: 'rgba(0,0,0,0)'},
+// 	'90%': {width: hoverWidth, height: hoverHeight, color: 'rgba(0,0,0,0)'},
+// 	'100%': {width: hoverWidth, height: hoverHeight, color: 'black'},
+// }, 'Spinner');
 
-const expandFrames = Radium.keyframes({
-	'0%': {width: hoverWidth, height: hoverHeight},
-	'100%': {width: expandedWidth, height: expandedHeight}
-}, 'Spinner');
+// const expandFrames = Radium.keyframes({
+// 	'0%': {width: hoverWidth, height: hoverHeight},
+// 	'100%': {width: expandedWidth, height: expandedHeight}
+// }, 'Spinner');
 
 styles = {
 	ref: {
-		'cursor': 'pointer',
-		'position': 'relative',
-		'overflow': 'visible',
-		'display': 'inline-block',
-		'backgroundColor': 'white'
+		// cursor: 'pointer',
+		// position: 'relative',
+		overflow: 'visible',
+		display: 'inline-block',
+		backgroundColor: 'white'
 	},
-	img: {
-		'width': expandedWidth
-	},
-	hidden: {
+	hoverRef: {
 		display: 'none',
-		// width: '0px',
-		// color: 'rgba(0,0,0,0)',
-		// transition: 'width 0.5s ease 0.1s, color 0.5s ease 0.5s'
-	},
-	author: {
-		fontStyle: 'italic'
-	},
-	title: {
-		WebkitLineClamp: 2,
-		overflow: 'hidden'
-	},
-	cover: {
-		position: 'fixed',
-		zIndex: 99999,
-		left: 0,
-		top: 0,
-		width: '100vw',
-		height: '100vh',
-		opacity: 0
-	},
-	show: {
-		// opacity: '1',
-		// color: 'rgba(0,0,0,1.0)',
-		animation: `${pulseKeyframes} 0.5s ease 0s 1`,
-		display: 'block',
 		position: 'absolute',
-		zIndex: 100000,
 		backgroundColor: 'white',
-		borderColor: '#ddd',
-		borderStyle: 'solid',
-		borderWidth: '1.5px',
-		padding: '5px',
-		textOverflow: 'ellipsis',
-		fontSize: '0.75em',
-		overflow: 'hidden',
-		marginTop: '-17px',
-		// transition: 'width 0.5s ease 0.1s, color 0.5s ease 0.5s'
+		width: 'calc(80% - 20px)',
+		left: '10%',
+		wordWrap: 'break-word',
+		transform: 'translateY(10px)',
+		fontSize: '11px',
+		boxShadow: '0px 0px 5px rgba(0,0,0,0.7)',
+		padding: '10px',
+
+
 	},
-	expanded: {
-		height: expandedHeight,
-		width: expandedWidth,
-		animation: `${expandFrames} 0.5s ease 0s 1`,
+	hoverRefVisible: {
+		display: 'block',
 	},
-	hover: {
-		height: hoverHeight,
-		width: hoverWidth
-	}
+
+	// img: {
+	// 	'width': expandedWidth
+	// },
+	// hidden: {
+	// 	display: 'none',
+	// 	// width: '0px',
+	// 	// color: 'rgba(0,0,0,0)',
+	// 	// transition: 'width 0.5s ease 0.1s, color 0.5s ease 0.5s'
+	// },
+	// author: {
+	// 	fontStyle: 'italic'
+	// },
+	// title: {
+	// 	WebkitLineClamp: 2,
+	// 	overflow: 'hidden'
+	// },
+	// cover: {
+	// 	position: 'fixed',
+	// 	zIndex: 99999,
+	// 	left: 0,
+	// 	top: 0,
+	// 	width: '100vw',
+	// 	height: '100vh',
+	// 	opacity: 0
+	// },
+	// show: {
+	// 	// opacity: '1',
+	// 	// color: 'rgba(0,0,0,1.0)',
+	// 	// animation: `${pulseKeyframes} 0.5s ease 0s 1`,
+	// 	display: 'block',
+	// 	position: 'absolute',
+	// 	zIndex: 100000,
+	// 	backgroundColor: 'white',
+	// 	borderColor: '#ddd',
+	// 	borderStyle: 'solid',
+	// 	borderWidth: '1.5px',
+	// 	padding: '5px',
+	// 	textOverflow: 'ellipsis',
+	// 	fontSize: '0.75em',
+	// 	overflow: 'hidden',
+	// 	marginTop: '-17px',
+	// 	// transition: 'width 0.5s ease 0.1s, color 0.5s ease 0.5s'
+	// },
+	// expanded: {
+	// 	height: expandedHeight,
+	// 	width: expandedWidth,
+	// 	// animation: `${expandFrames} 0.5s ease 0s 1`,
+	// },
+	// hover: {
+	// 	height: hoverHeight,
+	// 	width: hoverWidth
+	// }
 };
 
 
