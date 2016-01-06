229451c43128bbb6dfbde150d4cf6ee62066bbdf
Thariq Shihipar <trq212@gmail.com>
2016-01-06 17:50:28 -0500

    Quote Plugin, bsaic infrastructure

---
+++ src/components/EditorPlugins/QuotePlugin.js
@@ -0,0 +1,48 @@
+import React, {PropTypes} from 'react';
+import Radium from 'radium';
+import ErrorMsg from './ErrorPlugin';
+import Media from './baseMediaPlugin';
+
+import {propSize, propAlign, propCaption, propSrcRef} from './pluginProps';
+export const quoteOptions = {caption: propCaption, size: propSize, align: propAlign, reference: propSrcRef};
+
+// let styles = {};
+
+const QuotePlugin = React.createClass({
+	propTypes: {
+		url: PropTypes.string,
+		error: PropTypes.string,
+		children: PropTypes.string,
+		size: React.PropTypes.oneOfType([React.PropTypes.oneOf(['small', 'medium', 'large']), React.PropTypes.number]),
+		align: React.PropTypes.oneOf(['left', 'right', 'full']),
+		caption: PropTypes.string,
+		attribution: PropTypes.string,
+		reference: PropTypes.object
+	},
+	getInitialState: function() {
+		return {};
+	},
+	render: function() {
+		const url = this.props.url;
+		const size = this.props.size;
+		const align = this.props.align;
+		const caption = this.props.caption;
+
+		let html;
+
+		if (this.props.error === 'empty') {
+			html = <span></span>;
+		} else if (url) {
+			html = (<Media size={size} align={align}>
+				{caption}
+			</Media>
+		);
+		} else {
+			html = <ErrorMsg>Could not find the reference.</ErrorMsg>;
+		}
+		return html;
+	}
+});
+
+
+export default Radium(QuotePlugin);
