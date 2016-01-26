7a096fa17d1bb418f1166eb9600b4c6b92550fd4
Thariq Shihipar <trq212@gmail.com>
2016-01-25 23:37:13 -0500

    captions, align and size

--- src/components/EditorPluginFields/index.js
+++ src/components/EditorPluginFields/index.js
@@ -1,9 +1,15 @@
 import assetField from './assetField';
+import alignField from './alignField';
+import sizeField from './sizeField';
+
 import baseTextField from './baseTextField';
+import baseRadioField from './baseRadioField';
 
 export default {
 	'asset': assetField,
-	'text': baseTextField
+	'text': baseTextField,
+	'align': alignField,
+	'size': sizeField,
 	// 'size' : (radio with small, medium, large and %),
 	// 'align' : (radio with left, right and full)
 	// 'caption' : (text field)
