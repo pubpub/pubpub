3760b4d71eddbf24730852ec4ac48b8e7c2f6a08
Thariq Shihipar <trq212@gmail.com>
2016-01-26 11:15:53 -0500

    Adding the video plugin

--- src/components/EditorPluginsNew/index.js
+++ src/components/EditorPluginsNew/index.js
@@ -1,7 +1,9 @@
 import ImagePlugin 	from './imagePlugin';
 import QuotePlugin 	from './quotePlugin';
+import VideoPlugin 	from './videoPlugin';
 
 export default {
 	image: ImagePlugin,
-	quote: QuotePlugin
+	quote: QuotePlugin,
+	video: VideoPlugin,
 };
