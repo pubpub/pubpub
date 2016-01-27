a8fd33d5c03820a889fe34991f65df1f7d5011c1
Travis Rich <travis.s.rich@gmail.com>
2016-01-27 14:52:06 -0500

    Adding iframe plugin

--- src/components/EditorPluginsNew/index.js
+++ src/components/EditorPluginsNew/index.js
@@ -2,10 +2,12 @@ import ImagePlugin 	from './imagePlugin';
 import QuotePlugin 	from './quotePlugin';
 import VideoPlugin 	from './videoPlugin';
 import CitePlugin 	from './citePlugin';
+import IframePlugin 	from './iframePlugin';
 
 export default {
 	image: ImagePlugin,
 	quote: QuotePlugin,
 	video: VideoPlugin,
 	cite: CitePlugin,
+	iframe: IframePlugin,
 };
