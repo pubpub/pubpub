da525d89337985cff55b3d9caab0e1aa626102b3
Thariq Shihipar <trq212@gmail.com>
2016-01-26 22:34:03 -0500

    cite plugin works!

--- src/components/EditorPluginsNew/index.js
+++ src/components/EditorPluginsNew/index.js
@@ -1,9 +1,11 @@
 import ImagePlugin 	from './imagePlugin';
 import QuotePlugin 	from './quotePlugin';
 import VideoPlugin 	from './videoPlugin';
+import CitePlugin 	from './citePlugin';
 
 export default {
 	image: ImagePlugin,
 	quote: QuotePlugin,
 	video: VideoPlugin,
+	cite: CitePlugin,
 };
