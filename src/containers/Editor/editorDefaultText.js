5cc62a8df83327742a6ecb23189b33751356755c
Travis Rich <travis.s.rich@gmail.com>
2016-01-27 14:42:26 -0500

    Pagebreak, linebreak, title, abstract, and authorsNote working with new version of markdown

--- src/containers/Editor/editorDefaultText.js
+++ src/containers/Editor/editorDefaultText.js
@@ -1,9 +1,9 @@
 export function editorDefaultText(title) {
-	return `{{title: ` + title + `}}
+	return `[[title: ` + title + `]]
 
-{{abstract: Type your abstract here! Your abstract will be used to help users search for pubs throughout the site.}}
+[[abstract: Type your abstract here! Your abstract will be used to help users search for pubs throughout the site.]]
 
-{{authorsNote: An authorsNote is placed at the top of your pub. Use this space to mark if a published piece of work is a draft in-progress or any other pertinent details you think your readers should know - or remove it all together.}}
+[[authorsNote: An authorsNote is placed at the top of your pub. Use this space to mark if a published piece of work is a draft in-progress or any other pertinent details you think your readers should know - or remove it all together.]]
 
 # Welcome to your new Pub!
 PubPub documents let you separate the style from the writing. Click live-preview in the top-right corner to view your rendered document.
@@ -12,7 +12,7 @@ Formatting actions like **bold** or *italic* can be found on the right, while a 
 
 # You can focus on sections!
 
-Rich media and content can be uploaded using the assets popup (top-left). Using plugins like {{image: }} or {{video: }} to add your assets into your pub. Available plugins can be found on the right-hand formatting bar.
+Rich media and content can be uploaded using the assets popup (top-left). Using plugins like [[image: ]] or [[video: ]] to add your assets into your pub. Available plugins can be found on the right-hand formatting bar.
 
 Markdown has great support for lists
 
--- src/markdown/PPMComponent.js
+++ src/markdown/PPMComponent.js
@@ -15,7 +15,6 @@ import InputFields from '../components/EditorPluginFields/index';
 
 import MathComponent from './MathComponent';
 
-
 const MathOptions = {
 	inlineOpen: '$$',
 	inlineClose: '$$',
@@ -35,10 +34,12 @@ const PPMComponent = React.createClass({
 		assets: PropTypes.array,
 		references: PropTypes.array,
 	},
+
 	getInitialState() {
 		this.globals = {};
 		return {};
 	},
+
 	getDefaultProps: function() {
 		return {
 			markdown: '',
@@ -46,93 +47,105 @@ const PPMComponent = React.createClass({
 			references: [],
 		};
 	},
+
 	handleIterate: function(globals, Tag, props, children) {
 
 		let Component = Tag;
 
 		switch(Tag) {
-			case 'table':
+		case 'table':
 			props.className = 'table table-striped';
 			break;
-			case 'div':
+		case 'div':
 			if (props['data-info']) {
 				props.className = props.className ? props.className + props['data-info'] : props['data-info'];
 			}
 			break;
-			case 'ppm':
+		case 'ppm':
 			props.className = 'ppm';
 			if (children.length > 1) {
 				console.log('This shouldnt happen!!');
 			}
+			
+			if (children[0] === 'pagebreak') {
+				return <div className={'pagebreak'} style={{display: 'block', borderTop: '1px dashed #ddd'}}></div>
+			}
+			if (children[0] === 'linebreak') {
+				return <div className={'linebreak p-block'} style={{display: 'block', height: '1.5em'}}></div>
+			}
+
+
 			const pluginName = children[0].split(':')[0];
 			const plugin = Plugins[pluginName];
 			if (!plugin) {
 				console.warn('Could not find a plugin');
 				return <span {...props}>{children}</span>;
-				}
+			}
 
-				Component = plugin.Component;
-				const PluginInputFields = plugin.InputFields;
-				const pluginString = children[0];
-				let pluginProps = parsePluginString(pluginString);
+			Component = plugin.Component;
+			const PluginInputFields = plugin.InputFields;
+			const pluginString = children[0];
+			let pluginProps = parsePluginString(pluginString);
 
-				for (const propName in pluginProps) {
-					const propVal = pluginProps[propName];
-					const pluginInputField = PluginInputFields.find( field => field.title === propName);
-					if (pluginInputField) {
-						let inputVal = pluginProps[propName];
-						const InputFieldType = pluginInputField.type;
-						const Field = InputFields[InputFieldType];
-						if (InputFields[InputFieldType].transform) {
-							pluginProps[propName] = InputFields[InputFieldType].transform(propVal, pluginInputField.params, this.props.assets, this.props.references);
-						}
+			for (const propName in pluginProps) {
+				const propVal = pluginProps[propName];
+				const pluginInputField = PluginInputFields.find( field => field.title === propName);
+				if (pluginInputField) {
+					let inputVal = pluginProps[propName];
+					const InputFieldType = pluginInputField.type;
+					const Field = InputFields[InputFieldType];
+					if (InputFields[InputFieldType].transform) {
+						pluginProps[propName] = InputFields[InputFieldType].transform(propVal, pluginInputField.params, this.props.assets, this.props.references);
 					}
 				}
+			}
 
-				if (plugin.Config.prerender) {
-					({globals, pluginProps} = plugin.Config.prerender(globals, pluginProps));
-				}
+			if (plugin.Config.prerender) {
+				({globals, pluginProps} = plugin.Config.prerender(globals, pluginProps));
+			}
 
-				return <Component {...pluginProps}/>;
+			return <Component {...pluginProps}/>;
+			break;
+
+		case 'code':
+			if (props['data-language']) {
+				return <Tag {...props} dangerouslySetInnerHTML={{__html: window.hljs.highlight(props['data-language'], children[0]).value}} />
+			};
+			break;
+		case 'math':
+			return <MathComponent>{children[0]}</MathComponent>;
 				break;
-				case 'code':
-				if (props['data-language']) {
-					return <Tag {...props} dangerouslySetInnerHTML={{__html: window.hljs.highlight(props['data-language'], children[0]).value}} />
-				};
+				case 'p':
+				props.className = 'p-block';
+				Component = 'div';
 				break;
-				case 'math':
-				return <MathComponent>{children[0]}</MathComponent>;
-					break;
-					case 'p':
-					props.className = 'p-block';
-					Component = 'div';
-					break;
-				}
-				return <Component {...props}>{children}</Component>;
-				},
+		}
 
-				render: function() {
-					for (const member in this.globals) delete this.globals[member];
+		return <Component {...props}>{children}</Component>;
+	},
 
-					return (
-						<MDReactComponent
-							text={this.props.markdown}
-							onIterate={this.handleIterate.bind(this, this.globals)}
-							markdownOptions={{
-								typographer: true,
-								linkify: true,
-							}}
-							plugins={[
-								abbr,
-								emoji,
-								sub,
-								sup,
-								{plugin: mathIt, args: [MathOptions]},
-								{plugin: container, args: ['blank', {validate: ()=>{return true;}}]},
-								ppm
-							]} />
-						);
-					}
-				});
+	render: function() {
+		for (const member in this.globals) delete this.globals[member];
 
-				export default PPMComponent;
+		return (
+			<MDReactComponent
+				text={this.props.markdown}
+				onIterate={this.handleIterate.bind(this, this.globals)}
+				markdownOptions={{
+					typographer: true,
+					linkify: true,
+				}}
+				plugins={[
+					abbr,
+					emoji,
+					sub,
+					sup,
+					{plugin: mathIt, args: [MathOptions]},
+					{plugin: container, args: ['blank', {validate: ()=>{return true;}}]},
+					ppm
+				]} />
+		);
+	}
+});
+
+		export default PPMComponent;
