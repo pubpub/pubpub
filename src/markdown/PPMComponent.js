5062c4d0fbf7023735391602db0bdd1a8cfd1fb2
Travis Rich <travis.s.rich@gmail.com>
2016-01-27 16:02:26 -0500

    Cleaning ppmComponent Proptypes

--- src/markdown/PPMComponent.js
+++ src/markdown/PPMComponent.js
@@ -32,9 +32,9 @@ const PPMComponent = React.createClass({
 	propTypes: {
 		markdown: PropTypes.string,
 		
-		assets: PropTypes.object,
-		references: PropTypes.object,
-		selections: PropTypes.object,
+		assets: PropTypes.array,
+		references: PropTypes.array,
+		selections: PropTypes.array,
 	},
 
 	getInitialState() {
