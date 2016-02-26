bec3f26cec1caab7059d4d2c8407ff939305326d
Travis Rich <travis.s.rich@gmail.com>
2016-02-26 13:47:31 -0500

    Trying to debug asset uploading bug

--- api/routes/test-routes.js
+++ api/routes/test-routes.js
@@ -334,13 +334,16 @@ app.get('/handleNewFile', function(req,res){
 		// There's a 10mb limit for cloudinary images. Gotta fix that.
 		// This shoudl eventually be written to handle discovery and processing of all file types.
 		if(req.query.contentType.indexOf('image') > -1) {
-			const delay = (req.query.contentType.indexOf('image/gif') > -1) ? 1500 : 1000;
+			// const delay = (req.query.contentType.indexOf('image/gif') > -1) ? 1500 : 1000;
+			const delay = (req.query.contentType.indexOf('image/gif') > -1) ? 0 : 0;
+
+
 			// Gifs throw a 403 for some reason.
 			// A delay seems to make that 403 go away.
 			// Perhaps S3 is taking time to process? That seems strange, why would it finish to begin with 
 			// if it had more to do?
 			// Probably need a cleaner solution, but this'll work for now.
-			setTimeout(function(){		
+			// setTimeout(function(){		
 					cloudinary.uploader.upload(req.query.url, function(result) { 
 						try{
 							result.thumbnail = result.url ? result.url.replace('/upload', '/upload/c_limit,h_50,w_50') : req.query.url;
@@ -365,7 +368,7 @@ app.get('/handleNewFile', function(req,res){
 					});		
 
 				
-			}, delay);
+			// }, delay);
 
 		} else if (req.query.contentType.indexOf('video') > -1) {
 			// cloudinary.uploader.upload(req.query.url, function(result) { 
