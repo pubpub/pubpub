001bc033330cffb23dbd0374320ba060ac237a4e
Travis Rich <travis.s.rich@gmail.com>
2016-03-10 18:54:34 -0500

    Wooooo. Commenting out pub-rout-tests as loading api causes us to wipe our dev database

--- api/routes/_tests_/pub-routes.test.js
+++ api/routes/_tests_/pub-routes.test.js
@@ -1,34 +1,34 @@
-import httpMocks from 'node-mocks-http';
-import {expect} from 'chai';
-import {beforeEachMongoose, afterMongoose} from '../../../tests/helpersServer';
-import {getPub} from '../pub-routes';
+// import httpMocks from 'node-mocks-http';
+// import {expect} from 'chai';
+// import {beforeEachMongoose, afterMongoose} from '../../../tests/helpersServer';
+// import {getPub} from '../pub-routes';
 
-describe('API', () => {
-	describe('pub-routes.js', () => {
+// describe('API', () => {
+// 	describe('pub-routes.js', () => {
 
-		beforeEach(beforeEachMongoose);
-		after(afterMongoose);
+// 		beforeEach(beforeEachMongoose);
+// 		after(afterMongoose);
 
-		it('should get an empty pub', function(done){
-			const req = {query: {slug: 'refactor5'}};
-			const res = httpMocks.createResponse({
-			  eventEmitter: require('events').EventEmitter
-			});
+// 		it('should get an empty pub', function(done){
+// 			const req = {query: {slug: 'refactor5'}};
+// 			const res = httpMocks.createResponse({
+// 			  eventEmitter: require('events').EventEmitter
+// 			});
 
-			getPub(req, res);
+// 			getPub(req, res);
 
-			res.on('end', function() {
-				const data = JSON.parse( res._getData() );
-				const status = res.statusCode;
-				// console.log(data);
-				// console.log(status);
+// 			res.on('end', function() {
+// 				const data = JSON.parse( res._getData() );
+// 				const status = res.statusCode;
+// 				// console.log(data);
+// 				// console.log(status);
 
-				expect(status).to.equal(201);
-				done();
-			});
+// 				expect(status).to.equal(201);
+// 				done();
+// 			});
 
-		});
+// 		});
 
-	});
-});
+// 	});
+// });
 
