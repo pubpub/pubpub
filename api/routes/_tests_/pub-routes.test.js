b9f673b92d988cd8d129988daae48216c99179ce
Travis Rich <travis.s.rich@gmail.com>
2016-03-10 18:00:09 -0500

    Adding more server tests

--- api/routes/_tests_/pub-routes.test.js
+++ api/routes/_tests_/pub-routes.test.js
@@ -1,65 +1,34 @@
+import httpMocks from 'node-mocks-http';
 import {expect} from 'chai';
-// import {configMongoTest} from '../../testConfig';
-// import {getPub} from '../pub-routes';
-
-// var mongoose = require('mongoose');
-// const dbURI    = 'mongodb://localhost:27017';
-// const clearDB  = require('mocha-mongoose')(dbURI);
+import {beforeEachMongoose, afterMongoose} from '../../../tests/helpersServer';
+import {getPub} from '../pub-routes';
 
 describe('API', () => {
 	describe('pub-routes.js', () => {
-		
 
-		// before(function(done) {
-		// 	if (mongoose.connection.db) return done();
-		// 	mongoose.connect(dbURI, done);
-		// });
-		
-		// beforeEach(function(done) {
-		// 	clearDB(done);
-		// });
+		beforeEach(beforeEachMongoose);
+		after(afterMongoose);
 
-
-
-		// configMongoTest();
-
-		// beforeEach(function (done) {
-
-		//   function clearDB() {
-		//     for (var i in mongoose.connection.collections) {
-		//       mongoose.connection.collections[i].remove(function() {});
-		//     }
-		//     return done();
-		//   }
-
-
-		//   if (mongoose.connection.readyState === 0) {
-		//     mongoose.connect(dbURI, function (err) {
-		//       if (err) {
-		//         throw err;
-		//       }
-		//       return clearDB();
-		//     });
-		//   } else {
-		//     return clearDB();
-		//   }
-		// });
-
-
-		// afterEach(function (done) {
-		//   mongoose.disconnect();
-		//   return done();
-		// });
-
-		it('should get an empty pub', () => {
+		it('should get an empty pub', function(done){
 			const req = {query: {slug: 'refactor5'}};
-			// getPub(req, function(status, json){
-			// 	console.log(status, json);
+			const res = httpMocks.createResponse({
+			  eventEmitter: require('events').EventEmitter
+			});
 
-			// 	// return res.status(status).json(json);	
-			// });
+			getPub(req, res);
+
+			res.on('end', function() {
+				const data = JSON.parse( res._getData() );
+				const status = res.statusCode;
+				// console.log(data);
+				// console.log(status);
+
+				expect(status).to.equal(201);
+				done();
+			});
 
 		});
+
 	});
 });
 
--- tests/helpersServer.js
+++ tests/helpersServer.js
@@ -1,101 +1,31 @@
+var mongoose = require('mongoose');
 
+export function clearDB() {
+	for (var i in mongoose.connection.collections) {
+		mongoose.connection.collections[i].remove(function() {});
+	}
+}
 
-// export function configMongoTest() {
-// 	var mongoose = require('mongoose');
-// 	const dbURI    = 'mongodb://localhost:27017/demo-app-clearing-db';
+export function beforeEachMongoose(done)  {
+	const dbURI    = 'mongodb://localhost:27017/pubpub_test';
 
-// 	beforeEach(function (done) {
+	if (mongoose.connection.readyState === 0) {
+		mongoose.connect(dbURI, function (err) {
+			if (err) {
+				throw err;
+			}
+			clearDB();
+			return done();
+		});
+	} else {
+		clearDB();
+		return done();
+	}
+	
+};
 
-// 	  function clearDB() {
-// 	    for (var i in mongoose.connection.collections) {
-// 	      mongoose.connection.collections[i].remove(function() {});
-// 	    }
-// 	    return done();
-// 	  }
-
-
-// 	  if (mongoose.connection.readyState === 0) {
-// 	    mongoose.connect(dbURI, function (err) {
-// 	      if (err) {
-// 	        throw err;
-// 	      }
-// 	      return clearDB();
-// 	    });
-// 	  } else {
-// 	    return clearDB();
-// 	  }
-// 	});
-
-
-// 	afterEach(function (done) {
-// 	  mongoose.disconnect();
-// 	  return done();
-// 	});
-
-// }
-
-
-
-
-
-
-// var MongoClient = require('mongodb').MongoClient
-//   , async = require('async')
-
-// var state = {
-//   db: null,
-//   mode: null,
-// }
-
-// // In the real world it will be better if the production uri comes
-// // from an environment variable, instead of being hard coded.
-// var PRODUCTION_URI = 'mongodb://127.0.0.1:27017/production'
-//   , TEST_URI = 'mongodb://127.0.0.1:27017/test'
-
-// exports.MODE_TEST = 'mode_test'
-// exports.MODE_PRODUCTION = 'mode_production'
-
-// exports.connect = function(mode, done) {
-//   if (state.db) return done()
-
-//   var uri = mode === exports.MODE_TEST ? TEST_URI : PRODUCTION_URI
-
-//   MongoClient.connect(uri, function(err, db) {
-//     if (err) return done(err)
-//     state.db = db
-//     state.mode = mode
-//     done()
-//   })
-// }
-
-// exports.getDB = function() {
-//   return state.db
-// }
-
-// exports.drop = function(done) {
-//   if (!state.db) return done()
-//   // This is faster then dropping the database
-//   state.db.collections(function(err, collections) {
-//     async.each(collections, function(collection, cb) {
-//       if (collection.collectionName.indexOf('system') === 0) {
-//         return cb()
-//       }
-//       collection.remove(cb)
-//     }, done)
-//   })
-// }
-
-// exports.fixtures = function(data, done) {
-//   var db = state.db
-//   if (!db) {
-//     return done(new Error('Missing database connection.'))
-//   }
-
-//   var names = Object.keys(data.collections)
-//   async.each(name, function(name, cb) {
-//     db.createCollection(name, function(err, collection) {
-//       if (err) return cb(err)
-//       collection.insert(data.collections[name], cb)
-//     })
-//   }, done)
-// }
+export function afterMongoose(done){
+	mongoose.models = {};
+	mongoose.modelSchemas = {};
+	done();
+}
