06f10e2b8a69630e8e5203ecb8d032352619e825
Dang Pham <dangpham@mit.edu>
2016-03-30 19:42:15 -0400

    Added util method for recommendation service

--- api/services/recommendations.js
+++ api/services/recommendations.js
@@ -1,7 +1,50 @@
 import request from 'superagent';
 
+const recURL = env.process.RECOMMENDURL;
 
-export function featurePub() {
-
-
+export function featurePub(journalID, pub, callback) {
+  request
+  .post(recURL + journalID)
+  .send({
+    pub : pub,
+    action: 'featured'
+  })
+  .end(callback);
 };
+
+export function getRecommendations(type, query, journalId, callback){
+  let recQuery = {}; 
+  
+  if (type === 'user') {
+    recQuery = {user : query}
+  } else {
+    recQuery = {pub : query}
+  }
+
+  request
+  .get(recURl + journalId)
+  .query(recQuery)
+  .end(callback);
+} 
+
+
+export function inpRecAction(journalId, pubId, userId, action, callback){ 
+  request
+  .post(recURL + journalId)
+  .send({
+    user   : userId,
+    pub    : pubId,
+    action : action
+  })
+  .end(callback);
+}
+
+export function removeAction(journalId, pubId, userId, action, callback){
+  request
+  .del(recURL + jounralId)
+  .send({
+    user   : userId,
+    pub    : pubId,
+    action : action
+  })
+  .end(callback);
