c4195065167e6d43135b5c7d9426c6d3a0ddc82f
Thariq Shihipar <trq212@gmail.com>
2016-04-05 20:38:07 -0400

    fixing editor comments

--- api/migrations/test.js
+++ api/migrations/test.js
@@ -3,7 +3,7 @@ import {migrateDiscussions} from './migrateDiscussions';
 import {migrateJournals} from './migrateJournals';
 
 // const PUBS_TO_MIGRATE = 4;
-// migrateAllPubText(1000000, true);
+migrateAllPubText(1000000, true);
 
 // migrateDiscussions(1000000);
-migrateJournals();
+// migrateJournals();
