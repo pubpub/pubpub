bcdfd9f789bd54f9de48e263b6aa0c1ca9db3c92
Travis Rich <travis.s.rich@gmail.com>
2016-04-06 01:39:41 -0400

    Removing  migration code

--- api/migrations/scripts/index.js
+++
@@ -1,3 +0,0 @@
-require('babel-core/register');
-// exports.backupFirebase = require('./backupDatabases').backupFirebase;
-require('./backupDatabases').backupFirebase({prod: true});
--- api/migrations/test.js
+++
@@ -1,9 +0,0 @@
-import {migrateAllPubText} from './migrateMarkdown';
-import {migrateDiscussions} from './migrateDiscussions';
-import {migrateJournals} from './migrateJournals';
-
-// const PUBS_TO_MIGRATE = 4;
-// migrateAllPubText(1000000, true);
-
-// migrateDiscussions(1000000);
-migrateJournals();
