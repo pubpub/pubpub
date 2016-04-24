945d98a3fa2f0bb1fac3c87d5ddd24b987c33a3b
Thariq Shihipar <trq212@gmail.com>
2016-04-24 15:12:05 -0400

    fixing safe parse typo

--- src/utils/safeParse.js
+++ src/utils/safeParse.js
@@ -1,8 +1,9 @@
 import Immutable from 'immutable';
 
+//Example: safeGetInToJS(this.props.readerData, ['pubData', 'featuredInList']) || [])
 export function safeGetInToJS(obj, props) {
 	if (obj && obj.hasOwnProperty('getIn')) {
-		const val = obj.getIn(['pubData', 'featuredInList']);
+		const val = obj.getIn(props);
 		if (Immutable.Iterable.isIterable(val) && val.hasOwnProperty('toJS')) {
 			return val.toJS();
 		}
