9becb3ce17f8fc5b050cf5243088748a653af56a
Thariq Shihipar <trq212@gmail.com>
2016-05-12 09:32:18 -0400

    Adding meta tags

--- src/components/PubBody/PubBodyRSS.jsx
+++ src/components/PubBody/PubBodyRSS.jsx
@@ -3,11 +3,12 @@ import Radium, {Style} from 'radium';
 import {IntlProvider} from 'react-intl';
 
 import {globalStyles} from 'utils/styleConstants';
-import {Markdown, SelectionPopup, Reference, License} from 'components';
-import ResizingText from 'utils/ResizingText';
-
-import {globalMessages} from 'utils/globalMessages';
 import {parsePluginString} from 'utils/parsePlugins';
+import ResizingText from 'utils/ResizingText';
+import {globalMessages} from 'utils/globalMessages';
+
+import {Markdown, SelectionPopup, Reference, License} from 'components';
+
 
 import {FormattedMessage} from 'react-intl';
 
@@ -40,15 +41,34 @@ const PubBody = React.createClass({
 	componentWillReceiveProps(nextProps) {
 	},
 
+	findFirstImage: function(markdown) {
+		const imageRegex = /\[\[{"pluginType":"image".*?\]\]/g;
+		const matches = markdown.match(imageRegex);
+		if (matches && matches.length > 0) {
+			const images = matches.map((match) => parsePluginString(match)).filter((image) => (!!image.source && !!image.source.url));
+			if (images.length > 0 && images[0].source.url) {
+				return images[0].source.url;
+			}
+			return null;
+		}
+		return null;
+	},
+
 
 	render: function() {
 
 		return (
 			<html lang="en" prefix="op: http://media.facebook.com/op#">
 				 <head>
-					 <meta charset="utf-8"/>
-					 <link rel="canonical" href={this.props.pubURL}/>
-					 <meta property="op:markup_version" content="v1.0"/>
+					 	<meta charset="utf-8"/>
+					 	<link rel="canonical" href={this.props.pubURL}/>
+					 	<meta property="op:markup_version" content="v1.0"/>
+						<meta property="og:title" content={this.props.title}/>
+						<meta property="og:description" content={this.props.abstract}/>
+						<meta property="og:image" content={this.findFirstImage(this.props.markdown) || 'https://pbs.twimg.com/profile_images/718534268373311489/nP5WoROK.jpg'}/>
+						<meta property="fb:pages" content="228105957546675" />
+
+
 				 </head>
 				 <body>
 				 <article>
