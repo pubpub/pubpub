80ee20ca766adaa54d247ce49b03fb952927ea0d
Travis Rich <travis.s.rich@gmail.com>
2016-05-13 16:46:32 -0400

    Updating and cleaning routes

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {IndexRoute, Route} from 'react-router';
-import {App, Collection, Editor, Explore, GroupCreate, GroupProfile, JournalCreate, JournalProfile, Landing, PubCreate, PubReader, ResetPassword, UserProfile} from 'containers';
+import {App, Editor, GroupCreate, GroupProfile, JournalCreate, JournalProfile, Landing, PubCreate, PubReader, ResetPassword, UserProfile} from 'containers';
 import {NotFound} from 'components';
 
 export default () => {
@@ -12,36 +12,25 @@ export default () => {
 			<IndexRoute component={Landing}/>
 
 			{ /* Routes */ }
-			<Route path="/collection/:slug" component={Collection}/>
-			<Route path="/collection/:slug/:mode" component={Collection}/>
-			<Route path="/collections" component={Explore}/>
-
-			<Route path="/explore" component={Explore}/>
-
 			<Route path="/group/:groupSlug" component={GroupProfile}/>
 			<Route path="/group/:groupSlug/:mode" component={GroupProfile}/>
-
 			<Route path="/groups/create" component={GroupCreate}/>
 
-			<Route path="/journal/:subdomain" component={JournalProfile}/>
-			<Route path="/journal/:subdomain/:mode" component={JournalProfile}/>
-
-			<Route path="/journals" component={Explore}/>
 			<Route path="/journals/create" component={JournalCreate}/>
 
-			<Route path="/pub/:slug" component={PubReader}/>
-			<Route path="/pub/:slug/draft" component={Editor}/>
-			<Route path="/pub/:slug/:meta" component={PubReader}/>
-			<Route path="/pub/:slug/:meta/:metaID" component={PubReader}/> // Used for discussions
-
-			<Route path="/pubs" component={Explore}/>
+			<Route path="/pub/:slug" component={PubReader}/> {/* /pub/designandscience?journal=jods or /pub/designandscience?journal=impacts&version=8 */}
+			<Route path="/pub/:slug/draft" component={Editor}/> {/* /pub/designandscience/draft */}
+			<Route path="/pub/:slug/:meta" component={PubReader}/> {/* /pub/designandscience/history or /pub/designandscience/source?version=8 */}
 			<Route path="/pubs/create" component={PubCreate}/>
 
 			<Route path="/resetpassword" component={ResetPassword}/>
 			<Route path="/resetpassword/:hash/:username" component={ResetPassword}/>
 
-			<Route path="/user/:username" component={UserProfile}/>
-			<Route path="/user/:username/:mode" component={UserProfile}/>
+			<Route path="/user/:username" component={UserProfile}/> {/* /user/kate?filter=unpublished */}
+			<Route path="/user/:username/:mode" component={UserProfile}/> {/* /user/kate/discussions?page=4 or /user/kate/settings */}
+			
+			<Route path="/:subdomain" component={JournalProfile}/> {/* /jods or /jods?collection=fall2015 */}
+			<Route path="/:subdomain/:mode" component={JournalProfile}/> {/* /jods/about or /jods/settings */}
 
 			{ /* Catch all route */ }
 			<Route path="*" component={NotFound} status={404} />
