d6ee5114b61d33f71759af5c413dcc2e723eff87
Femi <femi@Viral-iMac-2.local>
2016-06-10 14:27:46 -0400

    Notes

--- src/containers/Login/Login.jsx
+++ src/containers/Login/Login.jsx
@@ -1,36 +1,53 @@
-import React, {PropTypes} from 'react'; //React is the UI functionality. {PropTypes} is used to validate the type of a prop recieved. The {} are because it is not the defualt export. This is only used in devmode? 'react' is node module. 
-import {connect} from 'react-redux'; //{connect} is used to generate containers for "Dumb/Pure" components. Connect(function ReduxState->Prop)(component). React component takes props and returns UI.
-import {pushState} from 'redux-router'; //Stores router state in redux store. See differences between React-router-redux and redux-router.
-import Radium from 'radium'; //Styling tools for react. CSS free. Imports whole.
-import Helmet from 'react-helmet'; //I dont understand this but i think it has to do with global stuff.
-import {login} from './actions'; //Grabs exported {login} function from actions.js. Why action type variables and action creator functions? Login (action creator), given email/passs, returns action.
-import {Link} from 'react-router'; //React-Router maps different routes to various react componenets. Link Routes from app files.
-import {Loader} from 'components'; //How does it get in loader folder? Loader is a componenet.
+import React, {PropTypes} from 'react';
+//React is the UI functionality. {PropTypes} is used to validate the type of a prop recieved.
+//The {} are because it is not the defualt export. This is only used in devmode. 'react' is node module. 
+import {connect} from 'react-redux';
+//{connect} is used to generate containers for "Dumb/Pure" components.
+//Connect(function ReduxState->Prop)(React component). React component takes props and returns UI.
+import {pushState} from 'redux-router';
+//Stores router state in redux store. See differences between React-router-redux and redux-router.
+import Radium from 'radium';
+//Styling tools for react. CSS free. Imports whole.
+import Helmet from 'react-helmet';
+//I dont understand this but i think it has to do with global stuff.
+import {login} from './actions';
+//Grabs exported {login} function from actions.js. Login is an Action Creator, given email/passs, returns action.
+import {Link} from 'react-router';
+//React-Router maps different routes to various react componenets. Link Routes from app files.
+import {Loader} from 'components';
+//Components forlder has an index.js that exports Loader.jsx from Louder folder
+//Loader is a component.
 
 
 import {globalStyles} from 'utils/styleConstants'; //These are global styles, accessed from utilities.
 import {globalMessages} from 'utils/globalMessages'; //These are global messages, accessed from utilities.
 import {FormattedMessage} from 'react-intl'; //Used for international language functionality.
 
-let styles = {};
+let styles = {}; //local variable
 
+//React export readonly value. Value is React.CreateClass
 export const Login = React.createClass({
 	propTypes: {
 		loginData: PropTypes.object,
 		dispatch: PropTypes.func,
 		query: PropTypes.object,
 	},
+	//Checks that props are correct type. Error if otherwise.
 
 	handleLoginSubmit: function(evt) {
 		evt.preventDefault();
 		this.props.dispatch(login(this.refs.loginEmail.value, this.refs.loginPassword.value));
 	},
+	//Calls dispatch function on Login action. Dispatch is from props.
+	//Where is dispatch/logindata from?
 
 	componentWillReceiveProps(nextProps) {
-		// If there is a new username in loginData, login was a sucess, so redirect
+		//If there is a new username in loginData, login was a sucess, so redirect
+		//&& is inline conditional operator
 		const oldUsername = this.props.loginData && this.props.loginData.getIn(['userData', 'username']);
 		const newUsername = nextProps.loginData && nextProps.loginData.getIn(['userData', 'username']);
 		if (newUsername && oldUsername !== newUsername) {
+			//new username exists and is not the same as oldusername
 			const redirectRoute = this.props.query && this.props.query.redirect;
 			this.props.dispatch(pushState(null, (redirectRoute || '/')));
 		}
