e08e0f11b93092e25b770e995f79849292bf92ed
Travis Rich <travis.s.rich@gmail.com>
2016-07-11 11:09:05 -0400

    Small changes to embedWrapper

--- src/components/AtomTypes/Markdown/EmbedWrapper.jsx
+++ src/components/AtomTypes/Markdown/EmbedWrapper.jsx
@@ -1,4 +1,5 @@
 import React, {PropTypes} from 'react';
+// import {AtomViewerPane} from './AtomViewerPane';
 
 let styles;
 
@@ -8,23 +9,12 @@ export const EmbedWrapper = React.createClass({
 		className: PropTypes.string,
 	},
 
-	componentDidMount() {
-		console.log('Mounting');
-		// setInterval(function(){
-		// 	console.log('bloop');
-		// }, 500);
-	},
-
-	componentWillUnmount() {
-		console.log('Death');
-	},
-
 	render: function() {
 		return (
-			<div className={'killme ' + this.props.className} style={styles.container}>
+			<div className={'pub-embed ' + this.props.className} style={styles.container}>
 				{this.props.source}
-				{/* <img src="http://cdn1-www.dogtime.com/assets/uploads/gallery/30-impossibly-cute-puppies/impossibly-cute-puppy-2.jpg"/> */}
-				<iframe width="420" height="315" src="https://www.youtube.com/embed/6i7ycxiog40" frameborder="0" allowfullscreen></iframe>
+				
+				
 			</div>
 		);
 	}
