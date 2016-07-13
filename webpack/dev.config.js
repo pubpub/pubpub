1eb242277425b0b97231eb9291e6ec192c33e11a
Travis Rich <travis.s.rich@gmail.com>
2016-07-13 19:52:57 -0400

    Linting webpack config files

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -1,38 +1,38 @@
 require('babel-polyfill');
 
 // Webpack config for development
-var fs = require('fs');
-var path = require('path');
-var webpack = require('webpack');
-// var HappyPack = require('happypack');
-var assetsPath = path.resolve(__dirname, '../static/dist');
-var host = (process.env.HOST || 'localhost');
-var port = parseInt(process.env.PORT) + 1 || 3001;
+const fs = require('fs');
+const path = require('path');
+const webpack = require('webpack');
+// const HappyPack = require('happypack');
+const assetsPath = path.resolve(__dirname, '../static/dist');
+const host = (process.env.HOST || 'localhost');
+const port = parseInt(process.env.PORT, 10) + 1 || 3001;
 
 // https://github.com/halt-hammerzeit/webpack-isomorphic-tools
-var WebpackIsomorphicToolsPlugin = require('webpack-isomorphic-tools/plugin');
-var webpackIsomorphicToolsPlugin = new WebpackIsomorphicToolsPlugin(require('./webpack-isomorphic-tools'));
+const WebpackIsomorphicToolsPlugin = require('webpack-isomorphic-tools/plugin');
+const webpackIsomorphicToolsPlugin = new WebpackIsomorphicToolsPlugin(require('./webpack-isomorphic-tools'));
 
-var babelrc = fs.readFileSync('./.babelrc');
-var babelrcObject = {};
+const babelrc = fs.readFileSync('./.babelrc');
+let babelrcObject = {};
 
 try {
-  babelrcObject = JSON.parse(babelrc);
+	babelrcObject = JSON.parse(babelrc);
 } catch (err) {
-  console.error('==>     ERROR: Error parsing your .babelrc.');
-  console.error(err);
+	console.error('==>     ERROR: Error parsing your .babelrc.');
+	console.error(err);
 }
 
-var container_regex = /.+\/containers\/[^\/]+\/((AtomReader)|(AtomEditor)|(Editor)|(EmailVerification)|(GroupCreate)|(GroupProfile)|(JournalCreate)|(JournalProfile)|(JrnlCreate)|(JrnlProfile)|(Login)|(PubCreate)|(PubReader)|(ResetPassword)|(SignUp)|(UserProfile)|(UserSettings))\.jsx?$/;
-var component_regex = /.+\/components\/[^\/]+\/((AboutJournals)|(AboutPubs)|(AboutReviews))\.jsx?$/;
+const containerRegex = /.+\/containers\/[^\/]+\/((AtomReader)|(AtomEditor)|(Editor)|(EmailVerification)|(GroupCreate)|(GroupProfile)|(JournalCreate)|(JournalProfile)|(JrnlCreate)|(JrnlProfile)|(Login)|(PubCreate)|(PubReader)|(ResetPassword)|(SignUp)|(UserProfile)|(UserSettings))\.jsx?$/;
+const componentRegex = /.+\/components\/[^\/]+\/((AboutJournals)|(AboutPubs)|(AboutReviews))\.jsx?$/;
 
-var babelrcObjectDevelopment = babelrcObject.env && babelrcObject.env.development || {};
+const babelrcObjectDevelopment = babelrcObject.env && babelrcObject.env.development || {};
 
 // merge global and dev-only plugins
-var combinedPlugins = babelrcObject.plugins || [];
+let combinedPlugins = babelrcObject.plugins || [];
 combinedPlugins = combinedPlugins.concat(babelrcObjectDevelopment.plugins);
 
-var babelLoaderQuery = Object.assign({}, babelrcObjectDevelopment, babelrcObject, {plugins: combinedPlugins});
+const babelLoaderQuery = Object.assign({}, babelrcObjectDevelopment, babelrcObject, {plugins: combinedPlugins});
 delete babelLoaderQuery.env;
 
 // Since we use .babelrc for client and server, and we don't want HMR enabled on the server, we have to add
@@ -40,40 +40,40 @@ delete babelLoaderQuery.env;
 
 // make sure react-transform is enabled
 babelLoaderQuery.plugins = babelLoaderQuery.plugins || [];
-var reactTransform = null;
-for (var i = 0; i < babelLoaderQuery.plugins.length; ++i) {
-  var plugin = babelLoaderQuery.plugins[i];
-  if (Array.isArray(plugin) && plugin[0] === 'react-transform') {
-	reactTransform = plugin;
-  }
+let reactTransform = null;
+for (let index = 0; index < babelLoaderQuery.plugins.length; ++index) {
+	const plugin = babelLoaderQuery.plugins[index];
+	if (Array.isArray(plugin) && plugin[0] === 'react-transform') {
+		reactTransform = plugin;
+	}
 }
 
 if (!reactTransform) {
-  reactTransform = ['react-transform', {transforms: []}];
-  babelLoaderQuery.plugins.push(reactTransform);
+	reactTransform = ['react-transform', {transforms: []}];
+	babelLoaderQuery.plugins.push(reactTransform);
 }
 // babelLoaderQuery.cacheDirectory = true;
 
 if (!reactTransform[1] || !reactTransform[1].transforms) {
-  reactTransform[1] = Object.assign({}, reactTransform[1], {transforms: []});
+	reactTransform[1] = Object.assign({}, reactTransform[1], {transforms: []});
 }
 
 // make sure react-transform-hmr is enabled
 reactTransform[1].transforms.push({
-  transform: 'react-transform-hmr',
-  imports: ['react'],
-  locals: ['module']
+	transform: 'react-transform-hmr',
+	imports: ['react'],
+	locals: ['module']
 });
 
 module.exports = {
-  devtool: 'inline-source-map',
-  context: path.resolve(__dirname, '..'),
-  entry: {
-			'main': [
-					'webpack-hot-middleware/client?path=http://' + host + ':' + port + '/__webpack_hmr',
-					'./src/client.js'
-			]
-  },
+	devtool: 'inline-source-map',
+	context: path.resolve(__dirname, '..'),
+	entry: {
+		'main': [
+			'webpack-hot-middleware/client?path=http://' + host + ':' + port + '/__webpack_hmr',
+			'./src/client.js'
+		]
+	},
 	output: {
 		path: assetsPath,
 		filename: '[name]-[hash].js',
@@ -82,8 +82,8 @@ module.exports = {
 	},
 	module: {
 		loaders: [
-			{ test: /\.(js|jsx)$/, exclude: [/node_modules/, component_regex, container_regex], loaders: ['babel?' + JSON.stringify(babelLoaderQuery)]},
-			{ test: /\.(js|jsx)$/, exclude: /node_modules/, include: [component_regex, container_regex], loaders: ['../helpers/bundleLoader', 'babel?' + JSON.stringify(babelLoaderQuery)]},
+			{ test: /\.(js|jsx)$/, exclude: [/node_modules/, componentRegex, containerRegex], loaders: ['babel?' + JSON.stringify(babelLoaderQuery)]},
+			{ test: /\.(js|jsx)$/, exclude: /node_modules/, include: [componentRegex, containerRegex], loaders: ['../helpers/bundleLoader', 'babel?' + JSON.stringify(babelLoaderQuery)]},
 			{ test: /\.json$/, loader: 'json-loader' }
 		]
 	},
@@ -96,7 +96,7 @@ module.exports = {
 		extensions: ['', '.json', '.js', '.jsx']
 	},
 	node: {
-		fs: "empty"
+		fs: 'empty'
 	},
 	plugins: [
 		// new HappyPack({
--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -1,88 +1,88 @@
 require('babel-polyfill');
 
 // Webpack config for creating the production bundle.
-var path = require('path');
-var webpack = require('webpack');
-var CleanPlugin = require('clean-webpack-plugin');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var strip = require('strip-loader');
+const path = require('path');
+const webpack = require('webpack');
+const CleanPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const strip = require('strip-loader');
 
-var relativeAssetsPath = '../static/dist';
-var assetsPath = path.join(__dirname, relativeAssetsPath);
+const relativeAssetsPath = '../static/dist';
+const assetsPath = path.join(__dirname, relativeAssetsPath);
 
 // https://github.com/halt-hammerzeit/webpack-isomorphic-tools
-var WebpackIsomorphicToolsPlugin = require('webpack-isomorphic-tools/plugin');
-var webpackIsomorphicToolsPlugin = new WebpackIsomorphicToolsPlugin(require('./webpack-isomorphic-tools'));
+const WebpackIsomorphicToolsPlugin = require('webpack-isomorphic-tools/plugin');
+const webpackIsomorphicToolsPlugin = new WebpackIsomorphicToolsPlugin(require('./webpack-isomorphic-tools'));
 
-var container_regex = /.+\/containers\/[^\/]+\/((AtomReader)|(AtomEditor)|(Editor)|(EmailVerification)|(GroupCreate)|(GroupProfile)|(JournalCreate)|(JournalProfile)|(JrnlCreate)|(JrnlProfile)|(Login)|(PubCreate)|(PubReader)|(ResetPassword)|(SignUp)|(UserProfile)|(UserSettings))\.jsx?$/;
-var component_regex = /.+\/components\/[^\/]+\/((AboutJournals)|(AboutPubs)|(AboutReviews))\.jsx?$/;
+const containerRegex = /.+\/containers\/[^\/]+\/((AtomReader)|(AtomEditor)|(Editor)|(EmailVerification)|(GroupCreate)|(GroupProfile)|(JournalCreate)|(JournalProfile)|(JrnlCreate)|(JrnlProfile)|(Login)|(PubCreate)|(PubReader)|(ResetPassword)|(SignUp)|(UserProfile)|(UserSettings))\.jsx?$/;
+const componentRegex = /.+\/components\/[^\/]+\/((AboutJournals)|(AboutPubs)|(AboutReviews))\.jsx?$/;
 
 module.exports = {
-  devtool: 'source-map',
-  context: path.resolve(__dirname, '..'),
-  entry: {
-    'main': [
-      './src/client.js'
-    ]
-  },
-  output: {
-    path: assetsPath,
-    filename: '[name]-[chunkhash].js',
-    chunkFilename: '[name]-[chunkhash].js',
-    publicPath: '/dist/'
-  },
-  module: {
-    loaders: [
-      { test: /\.jsx?$/, exclude: [/node_modules/, component_regex, container_regex], loaders: [strip.loader('debug'), 'babel']},		
-				 	{ test: /\.jsx?$/, exclude: /node_modules/, include: [component_regex, container_regex], loaders: ['../helpers/bundleLoader', strip.loader('debug'), 'babel']},
-      { test: /\.json$/, loader: 'json-loader' },
-      { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
-      { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
-      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
-      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
-      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" },
-      { test: webpackIsomorphicToolsPlugin.regular_expression('images'), loader: 'url-loader?limit=10240' }
-    ]
-  },
-  progress: true,
-  resolve: {
-    root: path.resolve('src'),
-    modulesDirectories: [
-      'node_modules'
-    ],
-    extensions: ['', '.json', '.js', '.jsx']
-  },
-  node: {
-    fs: "empty"
-  },
-  plugins: [
-    new CleanPlugin([relativeAssetsPath]),
+	devtool: 'source-map',
+	context: path.resolve(__dirname, '..'),
+	entry: {
+		'main': [
+			'./src/client.js'
+		]
+	},
+	output: {
+		path: assetsPath,
+		filename: '[name]-[chunkhash].js',
+		chunkFilename: '[name]-[chunkhash].js',
+		publicPath: '/dist/'
+	},
+	module: {
+		loaders: [
+			{ test: /\.jsx?$/, exclude: [/node_modules/, componentRegex, containerRegex], loaders: [strip.loader('debug'), 'babel']},		
+					{ test: /\.jsx?$/, exclude: /node_modules/, include: [componentRegex, containerRegex], loaders: ['../helpers/bundleLoader', strip.loader('debug'), 'babel']},
+			{ test: /\.json$/, loader: 'json-loader' },
+			{ test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
+			{ test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff' },
+			{ test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
+			{ test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
+			{ test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' },
+			{ test: webpackIsomorphicToolsPlugin.regular_expression('images'), loader: 'url-loader?limit=10240' }
+		]
+	},
+	progress: true,
+	resolve: {
+		root: path.resolve('src'),
+		modulesDirectories: [
+			'node_modules'
+		],
+		extensions: ['', '.json', '.js', '.jsx']
+	},
+	node: {
+		fs: 'empty'
+	},
+	plugins: [
+		new CleanPlugin([relativeAssetsPath]),
 
-    // css files from the extract-text-plugin loader
-    new ExtractTextPlugin('[name]-[chunkhash].css', {allChunks: true}),
-    new webpack.DefinePlugin({
-      'process.env': {
-        // Useful to reduce the size of client-side libraries, e.g. react
-        NODE_ENV: JSON.stringify('production')
-      },
-      __CLIENT__: true,
-      __SERVER__: false,
-      __DEVELOPMENT__: false,
-      __DEVTOOLS__: false
-    }),
+		// css files from the extract-text-plugin loader
+		new ExtractTextPlugin('[name]-[chunkhash].css', {allChunks: true}),
+		new webpack.DefinePlugin({
+			'process.env': {
+				// Useful to reduce the size of client-side libraries, e.g. react
+				NODE_ENV: JSON.stringify('production')
+			},
+			__CLIENT__: true,
+			__SERVER__: false,
+			__DEVELOPMENT__: false,
+			__DEVTOOLS__: false
+		}),
 
-    // ignore dev config
-    new webpack.IgnorePlugin(/\.\/dev/, /\/config$/),
+		// ignore dev config
+		new webpack.IgnorePlugin(/\.\/dev/, /\/config$/),
 
-    // optimizations
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
-      }
-    }),
+		// optimizations
+		new webpack.optimize.DedupePlugin(),
+		new webpack.optimize.OccurenceOrderPlugin(),
+		new webpack.optimize.UglifyJsPlugin({
+			compress: {
+				warnings: false
+			}
+		}),
 
-    webpackIsomorphicToolsPlugin
-  ]
+		webpackIsomorphicToolsPlugin
+	]
 };
