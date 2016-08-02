f4e27a9e10822249bb8b8b4b4c2281ee50931f58
Joel Gustafson <joelg@mit.edu>
2016-08-02 13:32:26 -0400

    added some text to /pubs

--- src/components/About/AboutPubs.jsx
+++ src/components/About/AboutPubs.jsx
@@ -6,9 +6,9 @@ import Helmet from 'react-helmet';
 
 import {styles} from './aboutStyles';
 
-export const AboutJournals = React.createClass({
+export const AboutPubs = React.createClass({
 
-	render: function() {
+	render() {
 		const metaData = {
 			title: 'Pubs · PubPub',
 		};
@@ -20,21 +20,49 @@ export const AboutJournals = React.createClass({
 
 				<div className={'lightest-bg'}>
 					<div className={'section'}>
-
 						<h1>Pubs</h1>
-						<p style={[styles.headerSubtitle, styles.headerTextMax]}>Description of Pubs coming here.</p>
-						<Link style={globalStyles.link} to={'/signup'}><div className={'button'} style={styles.headerButton}>Create Pub</div></Link>
-
+						<p style={[styles.headerSubtitle, styles.headerTextMax]}>PubPub is a network of digitally native publications called Pubs. </p>
+						<p>
+							Publishing shouldn't be a lossy compression, but static PDFs are tedious to compile, 
+							obscure their own development, and capture only shallow snapshots of their context.
+							Pubs are a modern type of document designed for everyone and built in the 21st century. 
+						</p>
+						<Link style={globalStyles.link} to={'/signup'}><div className={'button'} style={styles.headerButton}>Sign Up</div></Link>
 					</div>
 				</div>
 
 				<div>
 					<div className={'section'}>
-						<h2>Features of Pubs</h2>
-						
+						<h2>Collaborative Evolution</h2>
+						<p>
+							Pubs feature real-time collaborative editing, rich inline discussions, and a transparent review process.
+							Versioned history encourages a mindset of incremental development rather than opaque publication.
+						</p>
 					</div>
 				</div>
-				
+
+				<div className={'lightest-bg'}>
+					<div className={'section'}>
+						<h2>Dynamic Media</h2>
+						<p>
+							It's the 21st century.
+							Pubs emphasize interactive visualization, direct manipulation, and live execution.
+							Everything is open source and designed to be extensible.
+						</p>
+					</div>
+				</div>
+
+				<div>
+					<div className={'section'}>
+						<h2>Idea-oriented Documents</h2>
+						<p>
+							Ideas don't come out of a vacuum.
+							Pubs can embed other pubs, reference replies, and share media.
+							A powerful transclusion model makes it easy to trace context and attribution.
+						</p>
+					</div>
+				</div>
+
 			</div>
 		);
 	}
@@ -42,4 +70,4 @@ export const AboutJournals = React.createClass({
 });
 
 
-export default Radium(AboutJournals);
+export default Radium(AboutPubs);
