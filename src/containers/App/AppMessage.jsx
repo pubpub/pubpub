99494be77ededa10ee4adb8539f6242ad2d58108
Travis Rich <travis.s.rich@gmail.com>
2016-08-03 17:12:18 -0400

    Adding new style to appMessage

--- src/containers/App/AppMessage.jsx
+++ src/containers/App/AppMessage.jsx
@@ -1,4 +1,4 @@
-import React, {PropTypes} from 'react';
+import React from 'react';
 import Radium from 'radium';
 // import { Link } from 'react-router';
 import {globalStyles} from 'utils/styleConstants';
@@ -9,23 +9,21 @@ import {globalStyles} from 'utils/styleConstants';
 let styles;
 
 export const AppMessage = React.createClass({
-  onGotIt: function() {
-    window.localStorage["migrationMsg"] = true;
-    this.forceUpdate();
-  },
+	onGotIt: function() {
+		window.localStorage.migrationMsg = true;
+		this.forceUpdate();
+	},
 
 	render: function() {
-    if (typeof window === "undefined" || typeof window.localStorage === "undefined" || window.localStorage["migrationMsg"] === "true") {
-      return null;
-    }
+		if (typeof window === 'undefined' || typeof window.localStorage === 'undefined' || window.localStorage.migrationMsg === 'true') {
+			return null;
+		}
 		return (
 			<div style={styles.container}>
 				<div style={styles.content}>
-					<div>We've made some big changes to PubPub!
-          There might be bugs or things that don't make sense, email us if you run into any:
-            <a style={styles.emailLink} href="mailto:pubpub@media.mit.edu" target="_top">pubpub@media.mit.edu</a>
-            <div onClick={this.onGotIt} className={'button'} style={styles.button}>Got it!</div>
-          </div>
+					<div>We've made some <a style={styles.emailLink} href={'https://github.com/pubpub/pubpub/blob/master/CHANGELOG.md'} target={'_blank'}>big changes</a> to PubPub!</div>
+					<div>See something you like? Or a bug perhaps? <a style={styles.emailLink} href="mailto:pubpub@media.mit.edu" target="_top">Please let us know!</a></div>
+					<div onClick={this.onGotIt} className={'button'} style={styles.button}>Got it!</div>
 				</div>
 			</div>
 
@@ -36,15 +34,15 @@ export const AppMessage = React.createClass({
 export default Radium(AppMessage);
 
 styles = {
-  button: {
-    height: '1.25em',
-    verticalAlign: 'middle',
-    lineHeight: '1.25em',
-    margin: '0px 1em',
-    ':hover': {
-      textDecoration: 'underline',
-    },
-  },
+	button: {
+		display: 'block',
+		width: '3em',
+		margin: '.5em auto 0em',
+		':hover': {
+			// textDecoration: 'underline',
+			borderColor: '#808284',
+		},
+	},
 	container: {
 		position: 'fixed',
 		zIndex: 4,
@@ -52,15 +50,12 @@ styles = {
 		left: 0,
 		right: 0,
 		textAlign: 'center',
-		height: globalStyles.headerHeight,
-		lineHeight: globalStyles.headerHeight,
-		fontSize: '0.85em',
+		fontSize: '0.95em',
 		pointerEvents: 'none',
 		'@media screen and (min-resolution: 3dppx), screen and (max-width: 767px)': {
 			borderTop: '1px solid #58585B',
-			height: globalStyles.headerHeightMobile,
-			lineHeight: globalStyles.headerHeightMobile,
 			position: 'static',
+			padding: '0em 0em',
 		}
 	},
 	content: {
@@ -72,22 +67,19 @@ styles = {
 		position: 'relative',
 		top: '-2px',
 		borderRadius: '2px',
-		padding: '2px 1em 0px 1em',
-    fontSize: '0.80em',
-    height: '4.5em',
-    lineHeight: '4.5em',
+		padding: '0.5em 1em 0.5em 1em',
+		fontSize: '0.80em',
+		boxShadow: '0px 1px 5px 1px rgba(0, 0, 0, 0.2)',
 		'@media screen and (min-resolution: 3dppx), screen and (max-width: 767px)': {
 			display: 'block',
 			top: '0px',
 			borderRadius: '0px',
 			padding: '0px 1em 0px 1em',
-      height: 'auto',
-      lineHeight: '3em',
 		}
 	},
 	emailLink: {
 		cursor: 'pointer',
-		padding: '0em 0em 0em 1em',
-    color: 'white',
+		color: 'white',
+		textDecoration: 'underline',
 	},
 };
