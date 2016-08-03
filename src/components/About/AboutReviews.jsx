800a93f4cc6de2db97b0625dfb6d5f1652853b9f
Travis Rich <travis.s.rich@gmail.com>
2016-08-03 10:59:43 -0400

    Some small bits for Reviews page

--- src/components/About/AboutReviews.jsx
+++ src/components/About/AboutReviews.jsx
@@ -22,14 +22,19 @@ export const AboutReviews = React.createClass({
 					<div className={'section'}>
 
 						<h1>Reviews</h1>
-						<p style={[styles.headerSubtitle, styles.headerTextMax]}>Description of Reviews coming here.</p>
+						<p style={[styles.headerSubtitle, styles.headerTextMax]}>Reviews in PubPub are open, iterative, and contributory.</p>
+
+						<p>When reviewing science, it can often be hard to understand the methods, analysis, and results if operating purely on the author's description of these things. For this reason, PubPub is designed to give reviewers access to the data, code, and raw results of research in a way that makes it actionably reviewable. </p>
+
 
 					</div>
 				</div>
 
 				<div>
 					<div className={'section'}>
-						<h2>Features of Reviews</h2>
+						<h2>Contributory Review</h2>
+						<p>Allow reviewers to clone, edit, and publish embedded content, data, code.</p>
+						<p>Reviews are open, show the productivity of your reviews.</p>
 						
 					</div>
 				</div>
