3b85b14468e1a0bf7d3c85ed96fd6303f259ce12
Travis Rich <travis.s.rich@gmail.com>
2016-08-03 19:30:42 -0400

    Linting mod.js and adding support for UserSnap

--- src/components/AtomTypes/Document/collab/mod.js
+++ src/components/AtomTypes/Document/collab/mod.js
@@ -44,31 +44,32 @@ export class ModCollab {
 		const allSessionIds = [];
 		const that = this;
 
-		const grouped = groupBy(participants,'id');
+		const grouped = groupBy(participants, 'id');
 		this.participants = mapObject(grouped, function(entries) {
-	 		const sessionIds = [];
-	 		// Collect all Session IDs.
-		 	entries.forEach(function(entry) {
-		 		sessionIds.push(entry.session_id);
-		 		allSessionIds.push(entry.session_id);
-		 		delete entry.session_id;
-		 	});
-	 		entries[0].sessionIds = sessionIds;
-		 	return entries[0];
+			const sessionIds = [];
+			// Collect all Session IDs.
+			entries.forEach(function(entry) {
+				sessionIds.push(entry.session_id);
+				allSessionIds.push(entry.session_id);
+				delete entry.session_id;
+			});
+			entries[0].sessionIds = sessionIds;
+			return entries[0];
 		 });
 		// Check if each of the old session IDs is still present in last update.
 		// If not, remove the corresponding marked range, if any.
-		 this.sessionIds.forEach(function(sessionId) {
-		     if (allSessionIds.indexOf(sessionId) === -1) {
-		         that.carets.removeSelection(sessionId)
-		     }
-		 })
-		 this.sessionIds = allSessionIds;
-		 if (participants.length > 1) {
-		 	this.collaborativeMode = true;
-		 } else if (participants.length === 1) {
-		 	this.collaborativeMode = false;
-		 }
+		this.sessionIds.forEach(function(sessionId) {
+			if (allSessionIds.indexOf(sessionId) === -1) {
+				that.carets.removeSelection(sessionId);
+			}
+		});
+
+		this.sessionIds = allSessionIds;
+		if (participants.length > 1) {
+			this.collaborativeMode = true;
+		} else if (participants.length === 1) {
+			this.collaborativeMode = false;
+		}
 
 		// this.participants.forEach(function(participant) {
 		//     /* We assign a color to each user. This color stays even if the user
