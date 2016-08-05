506149d6382990b775f0a2dd2747fc698bf7d6dd
Hassan Shaikley <hassan.shaikley@gmail.com>
2016-08-05 16:05:55 -0400

    inefficiently Filters atoms on own profile page

--- src/containers/UserProfile/UserProfilePubs.jsx
+++ src/containers/UserProfile/UserProfilePubs.jsx
@@ -2,28 +2,60 @@ import React, {PropTypes} from 'react';
 import Radium from 'radium';
 import {PreviewCard} from 'components';
 import dateFormat from 'dateformat';
+import Select from 'react-select';
 
 // import {globalMessages} from 'utils/globalMessages';
 // import {FormattedMessage} from 'react-intl';
+let styles = {};
 
 export const UserProfilePubs = React.createClass({
 	propTypes: {
 		profileData: PropTypes.object,
 		ownProfile: PropTypes.bool,
+		filters: PropTypes.string
 	},
 
 	getInitialState: function() {
 		return {
-			
+			filters: []
 		};
 	},
 
+	handleFilterChange: function(filters) {
+		this.setState({filters: filters});
+	},
+	// doing this asynchronously (loadOptions) instead of options because
+	// in the future we will be pulling tags from DB with a call
+	loadOptions: function(a, callback) {
+		const one = {
+			value: 'published',
+			label: 'published',
+			id: 0
+		};
+		const two = {
+			value: 'unpublished',
+			label: 'unpublished',
+			id: 0
+		};
+		callback(null, { options: [one, two] })
+	},
 	render: function() {
 		const profileData = this.props.profileData || {};
 		const atoms = profileData.atoms || [];
-		
+
 		return (
 			<div className={'firstChildNoTopMargin'}>
+				{this.props.ownProfile &&
+					<Select.Async
+						name="form-field-filter"
+						minimumInput={1}
+						value={this.state.filters}
+						loadOptions={this.loadOptions}
+						multi={true}
+						placeholder={<span>Add a filter</span>}
+						onChange={this.handleFilterChange} />
+				}
+
 				{
 					atoms.filter((item)=>{
 						return item.type === 'document' && item.title.indexOf('Reply:') === -1;
