3c51606e6751ff8e499404fb2ed4774b771a14c2
Travis Rich <travis.s.rich@gmail.com>
2016-08-25 15:37:12 -0400

    Adding publish confirmation toggle to VersionsButton

--- src/containers/Atom/AtomVersionsButton.jsx
+++ src/containers/Atom/AtomVersionsButton.jsx
@@ -14,13 +14,28 @@ export const AtomVersionsButton = React.createClass({
 		permissionType: PropTypes.string,
 	},
 
+	getInitialState() {
+		return {
+			confirmPublishIndex: undefined,
+		};
+	},
+
 	onPublish: function(id) {
 		this.props.handlePublishVersion(id);
 	},
 
+	setPublishIndex: function(index) {
+		this.setState({confirmPublishIndex: index});
+	},
+
+	clearPublishIndex: function() {
+		this.setState({confirmPublishIndex: undefined});
+	},
+
 	render: function() {
 		const versionsData = this.props.versionsData || [];
-
+		const showPublishStatus = this.props.permissionType === 'author' || this.props.permissionType === 'editor' || this.props.permissionType === 'reader';
+		const isAuthor = this.props.permissionType === 'author';
 		return (
 			<div className={'light-button arrow-down-button'} style={this.props.buttonStyle}>Versions
 				<div className={'hoverChild arrow-down-child'} style={styles.content}>
@@ -39,19 +54,27 @@ export const AtomVersionsButton = React.createClass({
 						return (
 							<div className={'testing'} key={'version-' + index} style={[styles.versionItem, index === versionsData.length - 1 && styles.versionItemLast]}>
 
-								{!item.isPublished && (this.props.permissionType === 'author' || this.props.permissionType === 'editor' || this.props.permissionType === 'reader') &&
-									<div>
-										<div style={styles.statusLabel}>Private</div>
-										<div className={'button'} onClick={this.onPublish.bind(this, item._id)} style={styles.publishButton}>Publish Version</div>	
-									</div>
-								}
+								{!item.isPublished && showPublishStatus && <div style={styles.statusLabel}>Private</div> }
+								{item.isPublished && showPublishStatus && <div style={styles.statusLabel}>Published</div> }
 
-								{item.isPublished && (this.props.permissionType === 'author' || this.props.permissionType === 'editor' || this.props.permissionType === 'reader') &&
-									<div style={styles.statusLabel}>Published</div>
-								}
 								<Link to={'/pub/' + this.props.slug + '?version=' + item._id} className={'underlineOnHover'} style={styles.versionDate}>{dateFormat(item.createDate, 'mmm dd, yyyy h:MM TT')}</Link>
 								<div style={styles.versionMessage}>{item.message}</div>
 
+								{!item.isPublished && isAuthor &&
+
+									<div style={styles.publishButtonWrapper}>
+										{this.state.confirmPublishIndex === index &&
+											<div>
+												<div className={'button'} onClick={this.onPublish.bind(this, item._id)} style={styles.publishButton}>Confirm Publish</div>	
+												<div className={'button'} onClick={this.clearPublishIndex} style={styles.publishButton}>Cancel Publish</div>	
+											</div>
+										}
+
+										{this.state.confirmPublishIndex !== index &&
+											<div className={'button'} onClick={this.setPublishIndex.bind(this, index)} style={styles.publishButton}>Publish Version</div>	
+										}
+									</div>
+								}
 								
 							</div>
 						);
@@ -88,12 +111,16 @@ styles = {
 	versionMessage: {
 		padding: '.5em 0em',
 	},
+	publishButtonWrapper: {
+		textAlign: 'right',
+	},
 	publishButton: {
 		padding: '0em .5em',
 		fontSize: '.85em',
-		position: 'absolute',
-		bottom: 'calc(1em / .85)',
-		right: 0,
+		marginLeft: '.5em',
+		// position: 'absolute',
+		// bottom: 'calc(1em / .85)',
+		// right: 0,
 	},
 	statusLabel: {
 		display: 'inline-block',
