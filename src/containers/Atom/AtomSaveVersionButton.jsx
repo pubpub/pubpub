db5b720030a4d563c74f9b8d4073e46f470249ae
Travis Rich <travis.s.rich@gmail.com>
2016-08-25 10:58:20 -0400

    Styling saveVersionButton

--- src/containers/Atom/AtomSaveVersionButton.jsx
+++ src/containers/Atom/AtomSaveVersionButton.jsx
@@ -1,6 +1,7 @@
 import React, {PropTypes} from 'react';
 import Radium from 'radium';
 import {Loader} from 'components';
+import {globalStyles} from 'utils/styleConstants';
 
 let styles;
 
@@ -16,6 +17,7 @@ export const AtomSaveVersionButton = React.createClass({
 		return {
 			message: '',
 			isPublished: false,
+			noNote: false,
 		};
 	},
 	
@@ -34,6 +36,11 @@ export const AtomSaveVersionButton = React.createClass({
 
 	onSave: function(evt) {
 		evt.preventDefault();
+		if (!this.state.message) {
+			this.setState({noNote: true});
+			return;
+		}
+		this.setState({noNote: false});
 		this.props.handleVersionSave(this.state.message, this.state.isPublished);
 	},
 
@@ -44,27 +51,39 @@ export const AtomSaveVersionButton = React.createClass({
 				<div className={'hoverChild arrow-down-child'} style={styles.content}>
 					<h2>Save Version</h2>
 					<form onSubmit={this.onSave}>
-						Save Versions to mark milestones in your document. Any individial version can be published.
+						<div style={styles.text}>
+							Save Versions to mark milestones in your document. Any individial version can be published.
+						</div>
+						
+
 						<label htmlFor={'versionNote'}>
 							Version Note
 						</label>
 						<input type="text" id={'versionNote'} name={'version note'} value={this.state.message} onChange={this.onMessageChange} style={styles.input}/>
-						<div className={'light-color inputSubtext'} to={'/resetpassword'}>
+						<div className={'light-color inputSubtext'}>
 							Describe changes or updates.
 						</div>
 
 						<div>
-							<label htmlFor={'isPublished'}>
-								Publish
+							<input type="checkbox" id={'isPublished'} value={this.state.isPublished} onChange={this.onPublishedChange} style={styles.checkbox}/>	
+							<label htmlFor={'isPublished'} style={styles.publishLabel}>
+								Publish on Save
 							</label>
-							<input type="checkbox" id={'isPublished'} value={this.state.isPublished} onChange={this.onPublishedChange} />	
+							<div className={'light-color inputSubtext'}>
+								Publishing with permanently make this version publicly available.
+							</div>
+							
 						</div>
 						
 
-						<button className={'button'} onClick={this.onSave}>
+						<button className={'button'} onClick={this.onSave} style={styles.saveVersionButton}>
 							Save Version {this.state.isPublished && 'and Publish'}
 						</button>
 						<div style={styles.loaderContainer}><Loader loading={this.props.isLoading} showCompletion={true}/></div>
+						{this.state.noNote &&
+							<div style={styles.errorMessage}>Version Note required</div>
+						}
+						
 
 					</form>
 				</div>
@@ -81,12 +100,31 @@ styles = {
 		padding: '1em',
 		zIndex: 1,
 	},
+	text: {
+		whiteSpace: 'initial',
+		marginBottom: '.5em',
+	},
 	input: {
 		width: 'calc(100% - 20px - 4px)',
+	},
+	checkbox: {
+		marginLeft: '0em',
 	},
 	loaderContainer: {
 		display: 'inline-block',
 		position: 'relative',
 		top: 15,
+	},
+	publishLabel: {
+		display: 'inline-block',
+		paddingLeft: '.5em',
+		marginBottom: '1.25em',
+	},
+	saveVersionButton: {
+		whiteSpace: 'nowrap',
+	},
+	errorMessage: {
+		padding: '10px 0px',
+		color: globalStyles.errorRed,
 	},
 };
