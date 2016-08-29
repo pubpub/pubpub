cf371224be6a69a4c86913c4ce03f2e7b8136eb2
Hassan Shaikley <hassan.shaikley@gmail.com>
2016-08-29 16:11:20 -0400

    Translated cite button

--- src/containers/Atom/AtomCiteButton.jsx
+++ src/containers/Atom/AtomCiteButton.jsx
@@ -12,7 +12,7 @@ export const AtomCiteButton = React.createClass({
 		customAuthorString: PropTypes.string,
 		versionQuery: PropTypes.string,
 		buttonStyle: PropTypes.object,
-		
+
 	},
 
 	render: function() {
@@ -38,7 +38,8 @@ export const AtomCiteButton = React.createClass({
 
 
 		return (
-			<div className={'light-button arrow-down-button'} style={this.props.buttonStyle}>Cite
+			<div className={'light-button arrow-down-button'} style={this.props.buttonStyle}>
+				<FormattedMessage {...globalMessages.Cite}/>
 				<div className={'hoverChild arrow-down-child'} style={styles.citeContent}>
 					<h3 className={'normalWeight'}>Bibtex</h3>
 					<div style={[styles.typeContent, styles.bibtexContent]}>
