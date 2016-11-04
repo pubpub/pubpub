56b6ce96d111cd35cc211d2323089cc6a04414bd
Thariq Shihipar <trq212@gmail.com>
2016-11-04 10:10:53 -0400

    fixing dom parser

--- src/components/AtomTypes/Document/proseEditor/schema.js
+++ src/components/AtomTypes/Document/proseEditor/schema.js
@@ -68,7 +68,8 @@ const Embed = {
 		tag: 'div.embed',
 		getAttrs: dom => {
 			const nodeId = dom.getAttribute('data-nodeId');
-			const nodeAttrs = ElementSchema.findNodeById(nodeId);
+      console.log('Creating Embed');
+			const nodeAttrs = ElementSchema.findNodeById(nodeId).attrs;
 			return {
 				source: nodeAttrs.source,
 				data: nodeAttrs.data,
@@ -110,7 +111,8 @@ const BlockEmbed = {
 		tag: 'div.block-embed',
 		getAttrs: dom => {
 			const nodeId = dom.getAttribute('data-nodeId');
-			const nodeAttrs = ElementSchema.findNodeById(nodeId);
+      console.log('Creating block Embed');
+			const nodeAttrs = ElementSchema.findNodeById(nodeId).attrs;
 			return {
 				source: nodeAttrs.source,
 				data: nodeAttrs.data,
@@ -184,164 +186,3 @@ const migrateDiffs = (diffs) => {
 };
 
 exports.migrateDiffs = migrateDiffs;
-
-/*
-
-class Embed extends Inline {
-	get attrs() {
-		return {
-			source: new Attribute,
-			className: new Attribute({default: ''}),
-			id: new Attribute({default: ''}),
-			nodeId: new Attribute({default: null}),
-			align: new Attribute({default: 'full'}),
-			size: new Attribute({default: ''}),
-			caption: new Attribute({default: ''}),
-			mode: new Attribute({default: 'embed'}), // mode = embed || cite
-			data: new Attribute({default: {}}),
-			selected: new Attribute({default: false}),
-			figureName: new Attribute({default: {}}),
-		};
-	}
-	get draggable() { return true; }
-
-
-	// What if this doesnt exist?
-	get matchDOMTag() {
-		return {'.embed': dom => {
-			const nodeId = dom.getAttribute('data-nodeId');
-			const nodeAttrs = ElementSchema.findNodeById(nodeId);
-			return {
-				source: nodeAttrs.source,
-				data: nodeAttrs.data,
-				align: nodeAttrs.align,
-				size: nodeAttrs.size,
-				caption: nodeAttrs.caption,
-				mode: nodeAttrs.mode,
-				className: nodeAttrs.className,
-				figureName: nodeAttrs.figureName,
-				nodeId: nodeAttrs.nodeId,
-				children: null,
-				childNodes: null,
-			};
-		}};
-	}
-
-	toDOM(node) {
-		return ElementSchema.createElementAtNode(node);
-	}
-}
-
-*/
-
-/*
-class Pointer extends Inline {
-	get attrs() {
-		return {
-			figureName: new Attribute({default: ''}),
-		};
-	}
-	get draggable() { return true; }
-
-	get matchDOMTag() {
-		return {'.pointer': dom => {
-			const figureName = dom.getAttribute('data-figureName');
-
-			return {
-				figureName: figureName,
-			};
-		}};
-	}
-
-	toDOM(node) {
-		return ElementSchema.createPointerAtNode(node);
-	}
-}
-
-exports.Pointer = Pointer;
-
-
-class BlockEmbed extends Block {
-	get attrs() {
-		return {
-			source: new Attribute,
-			className: new Attribute({default: ''}),
-			id: new Attribute({default: ''}),
-			nodeId: new Attribute({default: null}),
-			align: new Attribute({default: 'full'}),
-			size: new Attribute({default: ''}),
-			caption: new Attribute({default: ''}),
-			mode: new Attribute({default: 'embed'}), // mode = embed || cite
-			data: new Attribute({default: {}}),
-			selected: new Attribute({default: false}),
-			figureName: new Attribute({default: {}}),
-		};
-	}
-	get draggable() { return true; }
-
-
-	// What if this doesnt exist?
-	get matchDOMTag() {
-		return {'.block-embed': dom => {
-			const nodeId = dom.getAttribute('data-nodeId');
-			const nodeAttrs = ElementSchema.findNodeById(nodeId);
-			return {
-				source: nodeAttrs.source,
-				data: nodeAttrs.data,
-				align: nodeAttrs.align,
-				size: nodeAttrs.size,
-				caption: nodeAttrs.caption,
-				mode: nodeAttrs.mode,
-				className: nodeAttrs.className,
-				figureName: nodeAttrs.figureName,
-				nodeId: nodeAttrs.nodeId,
-				children: null,
-				childNodes: null,
-			};
-		}};
-	}
-
-	toDOM(node) {
-		return ElementSchema.createElementAtNode(node, true);
-	}
-}
-
-exports.BlockEmbed = BlockEmbed;
-
-*/
-
-/*
-export const schema = new Schema({
-	nodes: {
-		doc: {type: Doc, content: 'block+'},
-
-		paragraph: {type: Paragraph, content: 'inline<_>*', group: 'block'},
-		blockquote: {type: BlockQuote, content: 'block+', group: 'block'},
-		ordered_list: {type: OrderedList, content: 'list_item+', group: 'block'},
-		bullet_list: {type: BulletList, content: 'list_item+', group: 'block'},
-		horizontal_rule: {type: HorizontalRule, group: 'block'},
-		page_break: {type: PageBreak, group: 'block'},
-		heading: {type: Heading, content: 'inline<_>*', group: 'block'},
-		code_block: {type: CodeBlock, content: 'text*', group: 'block'},
-		block_embed: {type: BlockEmbed, group: 'block'},
-
-		list_item: {type: ListItem, content: 'paragraph block*'},
-
-		text: {type: Text, group: 'inline'},
-		emoji: {type: Emoji, group: 'inline'},
-		image: {type: Image, group: 'inline'},
-		embed: {type: Embed, group: 'inline'},
-		pointer: {type: Pointer, group: 'inline'},
-		hard_break: {type: HardBreak, group: 'inline'}
-	},
-	marks: {
-		em: EmMark,
-		strong: StrongMark,
-		link: LinkMark,
-		code: CodeMark,
-		sub: SubMark,
-		sup: SupMark,
-		strike: StrikeThroughMark,
-	}
-});
-*/
