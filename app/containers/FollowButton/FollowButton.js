d135d595c09f2dc5ac7e18de2d649facfdc7cdf1
Travis Rich <travis.s.rich@gmail.com>
2016-12-22 16:50:16 -0500

    Removing options from follow button

--- app/containers/FollowButton/FollowButton.js
+++ app/containers/FollowButton/FollowButton.js
@@ -1,46 +1,22 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
-import { browserHistory, Link } from 'react-router';
+import { Link } from 'react-router';
 import Radium from 'radium';
 import Helmet from 'react-helmet';
-import { Popover, Position, Menu, MenuDivider, Checkbox, Tooltip } from '@blueprintjs/core';
+import { Popover, Position, Tooltip } from '@blueprintjs/core';
 
 // import { globalStyles } from 'utils/globalStyles';
 import { globalMessages } from 'utils/globalMessages';
 import { FormattedMessage } from 'react-intl';
 
 import { 
-	postFollowsPub, putFollowsPub, deleteFollowsPub,
-	postFollowsUser, putFollowsUser, deleteFollowsUser,
-	postFollowsJournal, putFollowsJournal, deleteFollowsJournal,
-	postFollowsLabel, putFollowsLabel, deleteFollowsLabel 
+	postFollowsPub, deleteFollowsPub,
+	postFollowsUser, deleteFollowsUser,
+	postFollowsJournal, deleteFollowsJournal,
+	postFollowsLabel, deleteFollowsLabel 
 } from './actions';
 
 let styles;
-
-const followKeys = {
-	pubNotifyOnVersions: true,
-	pubNotifyOnDiscussions: true,
-	pubNotifyOnJournals: true,
-	pubNotifyOnContributors: true,
-	pubNotifyOnReviewers: true,
-	pubNotifyOnFollowers: true,
-
-	userNotifyOnPubs: true,
-	userNotifyOnJournals: true,
-	userNotifyOnDiscussions: true,
-	userNotifyOnReviews: true,
-	userNotifyOnFollows: true,
-	userNotifyOnFollowers: true,
-
-	journalNotifyOnAdmins: true,
-	journalNotifyOnFeatures: true,
-	journalNotifyOnSubmissions: true,
-	journalNotifyOnFollowers: true,
-
-	labelNotifyOnPubs: true,
-	labelNotifyOnFollowers: true,
-};
 
 export const FollowButton = React.createClass({
 	propTypes: {
@@ -56,37 +32,11 @@ export const FollowButton = React.createClass({
 		dispatch: PropTypes.func,
 	},
 
-	componentWillMount() {
-		// Populate state with journalData values if they exist
-		this.initializeState(this.props.followData);
-	},
-
-	componentWillReceiveProps(nextProps) {
-		if (!this.props.followData && !!nextProps.followData) {
-			this.initializeState(nextProps.followData);
-		}
-	},
 
 	getInitialState() {
 		return {
 			justFollowed: 0,
-			...followKeys,
 		};
-	},
-
-	initializeState: function(followData) {
-		let mode;
-		if (!followData) { return null; }
-		if (followData.pubId !== undefined) { mode = 'pub'; }
-		if (followData.journalId !== undefined) { mode = 'journal'; }
-		if (followData.userId !== undefined) { mode = 'user'; }
-		if (followData.labelId !== undefined) { mode = 'label'; }
-
-		Object.keys(followKeys).map((key)=> {
-			const newKey = key.charAt(mode.length).toLowerCase() + key.substring(mode.length + 1, key.length); // Strips the mode prefix from the state key and fixes camelCase. Needed for backend.
-			followKeys[key] = followData[newKey];
-		});
-		return this.setState(followKeys);
 	},
 
 	createFollow: function(followId, mode) {
@@ -103,28 +53,6 @@ export const FollowButton = React.createClass({
 		if (mode === 'journal') { this.props.dispatch(deleteFollowsJournal(followId)); }
 		if (mode === 'label') { this.props.dispatch(deleteFollowsLabel(followId)); }
 		this.setState({ justFollowed: -1 });
-	},
-
-	handleChange: function(followId, mode, option, evt) {
-		const nextState = { 
-			...this.state,
-			[option]: evt.target.checked
-		};
-		this.setState(nextState);
-
-		// Filter the new options and prep them for sending to backend.
-		const putOptions = {};
-		const putOptionsKeys = Object.keys(nextState).filter((key)=> {
-			return key.indexOf(mode) === 0;
-		}).map((key)=> {
-			const newKey = key.charAt(mode.length).toLowerCase() + key.substring(mode.length + 1, key.length); // Strips the mode prefix from the state key and fixes camelCase. Needed for backend.
-			putOptions[newKey] = nextState[key];
-		});
-
-		if (mode === 'pub') { this.props.dispatch(putFollowsPub(followId, putOptions)); }
-		if (mode === 'user') { this.props.dispatch(putFollowsUser(followId, putOptions)); }
-		if (mode === 'journal') { this.props.dispatch(putFollowsJournal(followId, putOptions)); }
-		if (mode === 'label') { this.props.dispatch(putFollowsLabel(followId, putOptions)); }
 	},
 
 	render() {
@@ -145,32 +73,11 @@ export const FollowButton = React.createClass({
 		const isFollowing = this.state.justFollowed !== -1 && (followData.followerId || this.state.justFollowed === 1);
 		const followerCount = this.props.followerCount;
 
-		const options = Object.keys(this.state).filter((key)=> {
-			return key.indexOf(mode) === 0;
-		});
-
-		const optionsLanguage = {
-			pubNotifyOnVersions: 'Versions are updated',
-			pubNotifyOnDiscussions: 'New Discussions made',
-			pubNotifyOnJournals: 'Journals feature this pub',
-			pubNotifyOnContributors: 'Contributors are changed',
-			pubNotifyOnReviewers: 'Reviewers are invited',
-			pubNotifyOnFollowers: 'Someone follows this pub',
-
-			userNotifyOnPubs: 'Pubs are added',
-			userNotifyOnJournals: 'Journals are joined',
-			userNotifyOnDiscussions: 'Discussions are made',
-			userNotifyOnReviews: 'Reviews invitations are made or accepted',
-			userNotifyOnFollows: 'A new follow is made',
-			userNotifyOnFollowers: 'Someone follows this user',
-
-			journalNotifyOnAdmins: 'Admins are added',
-			journalNotifyOnFeatures: 'Pubs are featured',
-			journalNotifyOnSubmissions: 'Pubs are submitted',
-			journalNotifyOnFollowers: 'Someone follows this journal',
-
-			labelNotifyOnPubs: 'Pubs are added',
-			labelNotifyOnFollowers: 'Someone follows this label',
+		const followDescription = {
+			pub: 'Receiving updates for new versions, new discussions, new contributors, and new journal features.',
+			user: 'Receiving updates for new pubs, new journals, and new follows.',
+			journal: 'Receiving updates for new features, new submissions, and new admins.',
+			label: 'Receiving updates for new pubs and new followers.',
 		};
 
 		return (
@@ -188,25 +95,16 @@ export const FollowButton = React.createClass({
 				{isFollowing &&
 					<Popover 
 						content={
-							<Menu>
-								<li className={'pt-menu-header'}><h6>Notify when:</h6></li>
-								{options.map((option, index)=> {
-									return (
-										<li style={styles.checkboxItem} key={'checkbox-' + option}>
-											<Checkbox checked={this.state[option]} onChange={this.handleChange.bind(this, followId, mode, option)}>
-												{optionsLanguage[option]}
-											</Checkbox>
-										</li>
-									);
-								})}
-
-								<MenuDivider />
-								<li className={'pt-menu-item'} style={styles.unfollowButton} onClick={this.deleteFollow.bind(this, followId, mode)}>Unfollow</li>
-							</Menu>
+							<div style={styles.followingBox}>
+								<p style={styles.followDescription}>{followDescription[mode]}</p>
+								<hr />
+								<button className={'pt-button pt-fill'} onClick={this.deleteFollow.bind(this, followId, mode)}>Unfollow</button>
+							</div>
+							
 						} 
 						position={Position.BOTTOM_RIGHT}>
 
-						<button className="pt-button">Following <span className="pt-icon-standard pt-icon-caret-down pt-align-right"/></button>
+						<button className="pt-button">Following <span className="pt-icon-standard pt-icon-caret-down pt-align-right" /> </button>
 
 					</Popover>
 				}
@@ -232,11 +130,15 @@ function mapStateToProps(state) {
 export default connect(mapStateToProps)(Radium(FollowButton));
 
 styles = {
-	checkboxItem: {
-		margin: '1em 0.5em 0em',
+	followingBox: {
+		maxWidth: '250px',
+		padding: '.5em',
 	},
 	unfollowButton: {
 		textAlign: 'center',
+	},
+	followDescription: {
+		margin: '0.5em 0em',
 	},
 	
 };
