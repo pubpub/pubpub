76d10df5039b4063b845a97363fd9f01dd28b871
Thariq Shihipar <trq212@gmail.com>
2017-05-10 14:14:39 -0400

    Finally fixing storybook webpack config

--- stories/storybookLayoutEditor.js
+++ stories/storybookLayoutEditor.js
@@ -1,13 +1,41 @@
+import React, { PropTypes } from 'react';
+
 import LayoutEditor from '../app/components/LayoutEditor/LayoutEditor';
-import React from 'react';
+import { connect } from 'react-redux';
+// import { getUserData } from './actions';
 
 require('../static/blueprint.scss');
 require('../static/style.scss');
 require('../static/pubBody.scss');
 require('../static/markdown.scss');
 
-const Layout = ({ children, onClick }) => (
-  <LayoutEditor />
-);
+const Layout = React.createClass({
+	propTypes: {
+		accountData: PropTypes.object,
+		userData: PropTypes.object,
+		params: PropTypes.object,
+		dispatch: PropTypes.func,
+	},
 
-export default Layout;
+	componentWillMount() {
+		// this.props.dispatch(getUserData(params.username));
+	},
+
+	getInitialState() {
+		return { };
+	},
+
+	render() {
+		return (
+      <LayoutEditor/>
+    );
+
+	}
+});
+
+function mapStateToProps(state) {
+	return {
+	};
+}
+
+export default connect(mapStateToProps)(Layout);
