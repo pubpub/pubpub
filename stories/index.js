c12758926f2da73f5ac881ea09f26ee16ae622a0
Thariq Shihipar <trq212@gmail.com>
2017-05-10 16:14:31 -0400

    adding ordering by slugs for the layout editor

--- stories/index.js
+++ stories/index.js
@@ -13,10 +13,12 @@ global.clientFetch = function(route, opts) {
 	const isLocalDev = window.location.hostname === 'localhost' || window.location.hostname === 'www.funky.com' || window.location.hostname === 'www.funkynocors.com';
 	const isRemoteDev = window.location.hostname === 'dev.pubpub.org' || window.location.hostname === 'test.epsx.org' || window.location.hostname === 'testnocors.epsx.org';
 
-	let urlPrefix = '';
+	let urlPrefix = 'https://dev.pubpub.org';
+  /*
 	if (window.isJournal && isLocalDev) { urlPrefix = 'http://localhost:3000'; }
 	if (window.isJournal && isRemoteDev) { urlPrefix = 'https://dev.pubpub.org'; }
 	if (window.isJournal && !isLocalDev && !isRemoteDev) { urlPrefix = 'https://www.pubpub.org'; }
+  */
 	// If we're on a journal, we need to query routes directly to pubpub.org
 	// so that cookies are included.
 	const finalRoute = urlPrefix + route;
