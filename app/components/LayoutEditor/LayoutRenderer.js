b05d2c8c3f1da66e2aeca4999846d2433819588b
Thariq Shihipar <trq212@gmail.com>
2017-05-16 01:05:37 -0400

    Fixing layout renderer bug

--- app/components/LayoutEditor/LayoutRenderer.js
+++ app/components/LayoutEditor/LayoutRenderer.js
@@ -10,31 +10,65 @@ const getSinglePub =  function({journal, slug}) {
   return pub.pub;
 };
 
-const LayoutRenderer = ({content, journal}) => {
 
-  const PubsList = (props) => {
-    return (<div>
-      <LayoutPubsList journal={this.props.journal} {...props}/>
-      </div>
-    );
+const LayoutRenderer = React.createClass({
+  getInitialState: function() {
+    return {
+      elem: null
+    };
+  },
+
+  componentDidMount: function() {
+    const {content, journal} = this.props;
+    if (content && journal) {
+      this.renderContent(content, journal);
+    }
+  },
+
+  getSinglePub: function(slug) {
+    const journal = this.props.journal;
+    const pubFeatures = journal.pubFeatures;
+    const pub = pubFeatures.find((pubFeature) => (pubFeature.pub.slug === slug));
+    return pub.pub;
+  },
+  renderContent: function(content, journal) {
+      try {
+        const PubsList = (props) => {
+          return (<div>
+            <LayoutPubsList journal={journal} {...props}/>
+            </div>
+          );
+        }
+
+        const Pub = (props) => {
+          const slug = props.slug;
+          const pub = this.getSinglePub({journal, slug});
+          return (<div>
+              <LayoutSinglePub journal={journal} pub={pub} {...props}/>
+            </div>
+          );
+        }
+
+        const createElem = React.createElement;
+        const compiled = jsx.fromString(content, {
+          factory: 'createElem'
+        });
+        const elem = eval(compiled);
+        this.setState({elem});
+      } catch (err) {
+
+      }
+  },
+	componentWillReceiveProps: function(nextProps) {
+    if (this.props.content !== nextProps.content) {
+      this.renderContent(nextProps.content);
+    }
+	},
+  render() {
+    const { elem } = this.state;
+    return (<div>{elem}</div>)
   }
 
-  const Pub = (props) => {
-    const slug = props.slug;
-    const pub = this.getSinglePub({journal, slug});
-    return (<div>
-        <LayoutSinglePub journal={this.props.journal} pub={pub} {...props}/>
-      </div>
-    );
-  }
-
-  const createElem = React.createElement;
-  const compiled = jsx.fromString(content, {
-    factory: 'createElem'
-  });
-  const elem = eval(compiled);
-  return (<div>{elem}</div>);
-};
-
+});
 
 export default LayoutRenderer;
