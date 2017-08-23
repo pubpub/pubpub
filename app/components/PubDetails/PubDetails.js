7f577fbbea2f13966c8f4aa45bb40a8e5d0187ac
Travis Rich <travis.s.rich@gmail.com>
2017-08-23 12:56:18 -0400

    PubDetails component built

--- app/components/PubDetails/PubDetails.js
+++ app/components/PubDetails/PubDetails.js
@@ -1,34 +1,110 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import dateFormat from 'dateformat';
+import Avatar from 'components/Avatar/Avatar';
+import { Popover, PopoverInteractionKind, Position, Menu, MenuItem, MenuDivider } from '@blueprintjs/core';
 
 require('./pubDetails.scss');
 
 const propTypes = {
-	title: PropTypes.string.isRequired,
-	description: PropTypes.string,
-	backgroundImage: PropTypes.string,
-};
-
-const defaultProps = {
-	description: undefined,
-	backgroundImage: undefined,
+	pubData: PropTypes.object.isRequired,
+	collaborators: PropTypes.array.isRequired,
+	versions: PropTypes.array.isRequired,
 };
 
 const PubDetails = function(props) {
-	const backgroundStyle = {};
-	if (props.backgroundImage) {
-		backgroundStyle.backgroundImage = `url("${props.backgroundImage}")`;
-		backgroundStyle.color = 'white';
-	}
+	const authors = props.collaborators.filter((collaborator)=> {
+		return collaborator.isAuthor;
+	});
+
+	const activeVersion = props.versions.reduce((prev, curr)=> {
+		if (curr.active) { return curr; }
+		return prev;
+	}, undefined);
 
 	return (
-		<div className={'pub-header'} style={backgroundStyle}>
-			<div className={props.backgroundImage ? 'background dim' : 'background'}>
-				<div className={'container'}>
-					<div className={'row'}>
-						<div className={'col-12'}>
-							<h1>{props.title}</h1>
-							<div className={'description'}>{props.description}</div>
+		<div className={'pub-details'}>
+			<div className={'container'}>
+				<div className={'row'}>
+					<div className={'col-12'}>
+						<div className={'details'}>
+							{authors.length && <span>by </span>}
+							{authors.map((author, index)=> {
+								const separator = index === authors.length - 1 ? '' : ', '; 
+								const prefix = index === authors.length - 1 ? ' and ' : ''
+								if (author.slug) {
+									return (
+										<span>
+											{prefix}
+											<Link key={`author-${author.id}`} to={`/user/${author.slug}`}>{author.fullName}</Link>
+											{separator}
+										</span>
+									);	
+								}
+								return <span key={`author-${author.id}`}>{prefix}{author.fullName}{separator}</span>
+							})}
+						</div>
+						<div className={'button'}>
+							<Popover
+								content={
+									<Menu>
+										{props.versions.sort((foo, bar)=>{
+											if (foo.date < bar.date) { return 1; }
+											if (foo.date > bar.date) { return -1; }
+											return 0;
+										}).map((version)=> {
+											return (
+												<li key={`version-${version.id}`} style={{ textAlign: 'right' }}>
+													<Link to={`/pub/${props.pubData.slug}`} className="pt-menu-item pt-popover-dismiss">
+														<span style={{ fontWeight: version.active ? '600' : 'normal' }}>
+															{dateFormat(version.date, 'mmm dd, yyyy · HH:MM')}
+														</span>
+													</Link>
+												</li>
+											);
+										})}
+									</Menu>
+								}
+								interactionKind={PopoverInteractionKind.CLICK}
+								position={Position.BOTTOM_RIGHT}
+								popoverClassName={'pt-minimal'}
+								transitionDuration={-1}
+								inheritDarkTheme={false}
+							>
+								<button className={'pt-button pt-minimal'}>
+									{dateFormat(activeVersion.date, 'mmm dd, yyyy')}
+									<span className={'pt-icon-standard pt-icon-caret-down pt-align-right'} />
+								</button>
+							</Popover>
+
+
+							
+						</div>
+					</div>
+					<div className={'col-12'}>
+						<div className={'details'}>
+							{props.pubData.numDiscussions}
+							<span className={'pt-icon-standard pt-icon-chat'} />
+							{props.pubData.numSuggestions}
+							<span className={'pt-icon-standard pt-icon-manually-entered-data'} />
+							{props.pubData.numCollaborators}
+							<span className={'pt-icon-standard pt-icon-people'} />
+							{props.collaborators.map((collaborator)=> {
+								return (
+									<Avatar
+										key={`avatar-${collaborator.id}`}
+										userInitials={collaborator.userInitials}
+										userAvatar={collaborator.userAvatar}
+										borderColor={'rgba(255, 255, 255, 0.5)'}
+										width={20}
+										doesOverlap={true}
+									/>
+								);
+							})}
+						</div>
+						<div className={'button'}>
+							<Link to={'/'} className={'pt-button pt-intent-primary'}>Collaborate</Link>
 						</div>
 					</div>
 				</div>
@@ -37,6 +113,5 @@ const PubDetails = function(props) {
 	);
 };
 
-PubDetails.defaultProps = defaultProps;
 PubDetails.propTypes = propTypes;
 export default PubDetails;
--- stories/pubDetailsStories.js
+++ stories/pubDetailsStories.js
@@ -4,22 +4,84 @@ import PubDetails from 'components/PubDetails/PubDetails';
 
 const wrapperStyle = { margin: '1em', boxShadow: '0px 0px 1px rgba(0, 0, 0, 0.25)' };
 
-const description = 'A virtual representation of the space of an event and provide tools by which a producer can draw upon images, graphics, data, and live cameras to create a video stream equivalent to a broadcast';
+const collaborators = [
+	{
+		id: 0,
+		slug: 'userslug',
+		userInitials: 'TR',
+		userAvatar: '/dev/trich.jpg',
+		fullName: 'Travis Rich',
+		isAuthor: true,
+	},
+	{
+		id: 1,
+		slug: 'userslug',
+		userInitials: 'MW',
+	},
+	{
+		id: 2,
+		slug: 'userslug',
+		userInitials: 'TW',
+		userAvatar: '/dev/tomer.jpg',
+		fullName: 'Tomer Weller',
+		isAuthor: true,
+	},
+	{
+		id: 3,
+		slug: 'userslug',
+		userInitials: 'FF',
+		userAvatar: '/dev/danny.jpg',
+		fullName: 'Danny Hillis',
+		isAuthor: true,
+	},
+	{
+		id: 4,
+		slug: 'userslug',
+		userInitials: 'WL',
+	},
+	{
+		id: 5,
+		slug: 'userslug',
+		userInitials: 'PL',
+		userAvatar: '/dev/lip.jpg',
+	},
+];
 
+const pubData = {
+	slug: 'myslug',
+	numDiscussions: '3',
+	numCollaborators: '6',
+	numSuggestions: '11',
+};
+
+const versions = [
+	{
+		id: 0,
+		date: new Date() - 1000000000,
+		active: true,
+	},
+	{
+		id: 1,
+		date: new Date() - 2000000000,
+	},
+	{
+		id: 3,
+		date: new Date() - 5000000000,
+	},
+	{
+		id: 2,
+		date: new Date() - 3000000000,
+	},
+	
+];
 storiesOf('Pub Details', module)
 .add('Default', () => (
 	<div>
 		<div style={wrapperStyle}>
 			<PubDetails
-				title={'Soundscapes'}
-				description={description}
-			/>
-		</div>
-		<div style={wrapperStyle}>
-			<PubDetails
-				title={'Soundscapes'}
-				description={description}
-				backgroundImage={'/dev/pubHeader.png'}
+				collaborators={collaborators}
+				pubData={pubData}
+				versions={versions}
 			/>
 		</div>
 	</div>
