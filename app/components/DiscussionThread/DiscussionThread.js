7331c50c36f5e8ceea5f4c877dd7f846fb3e6618
Travis Rich <travis.s.rich@gmail.com>
2017-08-27 15:47:38 -0400

    Adding buttons and dropdowns to thread

--- app/components/DiscussionThread/DiscussionThread.js
+++ app/components/DiscussionThread/DiscussionThread.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import TimeAgo from 'react-timeago';
 import { Link } from 'react-router-dom';
 import Avatar from 'components/Avatar/Avatar';
+import DropdownButton from 'components/DropdownButton/DropdownButton';
 
 require('./discussionThread.scss');
 
@@ -11,7 +12,6 @@ const propTypes = {
 };
 
 const DiscussionThread = function(props) {
-
 	const sortedDiscussions = props.discussions.sort((foo, bar)=> {
 		if (foo.date > bar.date) { return 1; }
 		if (foo.date < bar.date) { return -1; }
@@ -20,6 +20,10 @@ const DiscussionThread = function(props) {
 
 	return (
 		<div className={'discussion-thread'}>
+			<div className={'thread-buttons pt-button-group pt-minimal pt-small'}>
+				<button type={'button'} className={'pt-button pt-icon-edit2'} />
+				<button type={'button'} className={'pt-button pt-icon-compressed'} />
+			</div>
 			<div className={'title'}>{sortedDiscussions[0].title}</div>
 
 			<div>
@@ -40,7 +44,16 @@ const DiscussionThread = function(props) {
 
 								<div className={'pt-button-group pt-minimal pt-small'}>
 									<button type={'button'} className={'pt-button pt-icon-edit2'} />
-									<button type={'button'} className={'pt-button pt-icon-more'} />
+									<DropdownButton icon={'pt-icon-more'} isRightAligned={true}>
+										<div className={'pt-menu'}>
+											<div className="pt-menu-item pt-popover-dismiss">
+												Flag
+											</div>
+											<div className="pt-menu-item pt-popover-dismiss">
+												Link To...
+											</div>
+										</div>
+									</DropdownButton>
 								</div>
 							</div>
 
@@ -49,6 +62,11 @@ const DiscussionThread = function(props) {
 					);
 				})}
 			</div>
+
+			<input
+				className="pt-input"
+				placeholder={'Reply...'}
+			/>
 		</div>
 	);
 };
