293018fb03562e6a04e2066237d9f1e3500d5602
Travis Rich <travis.s.rich@gmail.com>
2017-09-01 11:29:01 -0400

    Attempting to move discussion data to real api data

--- stories/discussionPreviewPanelStories.js
+++ stories/discussionPreviewPanelStories.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import DiscussionPreviewPanel from 'components/DiscussionPreviewPanel/DiscussionPreviewPanel';
 import AccentStyle from 'components/AccentStyle/AccentStyle';
-import { accentDataDark, discussions } from './_data';
+import { accentDataDark, pubData, discussions } from './_data';
 
 const wrapperStyle = { width: '350px', minHeight: 'calc(100vh - 2em)', margin: '1em', boxShadow: '0px 0px 1px rgba(0, 0, 0, 0.25)' };
 
@@ -12,7 +12,7 @@ storiesOf('DiscussionPreviewPanel', module)
 		<AccentStyle {...accentDataDark} />
 		<div style={wrapperStyle}>
 			<DiscussionPreviewPanel
-				threads={discussions}
+				threads={pubData.discussions}
 				slug={'my-article'}
 			/>
 		</div>
