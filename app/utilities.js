bdefb01bd1764c9af847ea7d183215f533fd588f
Travis Rich <travis.s.rich@gmail.com>
2017-09-19 18:27:36 -0400

    Turn on real api

--- app/utilities.js
+++ app/utilities.js
@@ -1,6 +1,6 @@
 export const apiFetch = function(path, opts) {
-	// const urlPrefix = 'https://pubpub-api-v4-dev.herokuapp.com';
-	const urlPrefix = 'http://localhost:9876';
+	const urlPrefix = 'https://pubpub-api-v4-dev.herokuapp.com';
+	// const urlPrefix = 'http://localhost:9876';
 	const finalRoute = `${urlPrefix}${path}`;
 
 	return fetch(finalRoute, {
