487c67ef3851588533f66331f96d489be90081db
Travis Rich <travis.s.rich@gmail.com>
2017-12-29 11:27:10 -0500

    Update header

--- client/components/Header/Header.js
+++ client/components/Header/Header.js
@@ -1,52 +1,144 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
+import Avatar from 'components/Avatar/Avatar';
 import { Popover, PopoverInteractionKind, Position, Menu, MenuItem, MenuDivider } from '@blueprintjs/core';
+import { getResizedUrl } from 'utilities';
 
 if (typeof require.ensure === 'function') {
 	require('./header.scss');
 }
 
 const propTypes = {
+	userName: PropTypes.string,
+	userInitials: PropTypes.string,
+	userSlug: PropTypes.string,
+	userAvatar: PropTypes.string,
+	userIsAdmin: PropTypes.bool,
+
+	smallHeaderLogo: PropTypes.string,
+	largeHeaderLogo: PropTypes.string,
+	largeHeaderDescription: PropTypes.string,
+	largeHeaderBackground: PropTypes.string,
+
+	onLogout: PropTypes.func.isRequired,
+	isBasePubPub: PropTypes.bool,
+	isLandingPage: PropTypes.bool,
 
 };
 
 const defaultProps = {
-
+	userName: undefined,
+	userInitials: undefined,
+	userSlug: undefined,
+	userAvatar: undefined,
+	userIsAdmin: undefined,
+	smallHeaderLogo: undefined,
+	largeHeaderLogo: undefined,
+	largeHeaderDescription: undefined,
+	largeHeaderBackground: undefined,
+	isBasePubPub: false,
+	isLandingPage: false,
 };
 
-const Header = function() {
+const Header = function(props) {
+	const loggedIn = !!props.userSlug;
+	const showGradient = props.isLandingPage && !!props.largeHeaderBackground;
+	const backgroundStyle = {};
+	if (showGradient) {
+		const resizedBackground = getResizedUrl(props.largeHeaderBackground, 'fit-in', '1500x600');
+		backgroundStyle.backgroundImage = `url("${resizedBackground}")`;
+	}
+	if (props.isBasePubPub && !props.isLandingPage) {
+		backgroundStyle.boxShadow = '0 0 0 1px rgba(16, 22, 26, 0.1), 0 0 0 rgba(16, 22, 26, 0), 0 1px 1px rgba(16, 22, 26, 0.2)';
+	}
+
+	const resizedSmallHeaderLogo = getResizedUrl(props.smallHeaderLogo, 'fit-in', '0x50');
+	const resizedLargeHeaderLogo = getResizedUrl(props.largeHeaderLogo, 'fit-in', '0x200');
+	const useAccentsString = props.isBasePubPub ? '' : 'accent-background accent-color';
 	return (
-		<nav className="header-component">
-			<div className="left">My SSR Site</div>
-			<div className="right">
-				<Popover
-					content={
-						<Menu>
-							<MenuItem text="Here is a menu item! :) !" />
-							<MenuDivider />
-							<li>
-								<a href={'/'} className="pt-menu-item pt-popover-dismiss">
-									Home
-								</a>
-							</li>
-							<li>
-								<a href={'/about'} className="pt-menu-item pt-popover-dismiss">
-									About
-								</a>
-							</li>
-							<MenuItem text="Logout" />
-						</Menu>
-					}
-					interactionKind={PopoverInteractionKind.CLICK}
-					position={Position.BOTTOM_RIGHT}
-					transitionDuration={-1}
-					inheritDarkTheme={false}
-				>
-					<button className="pt-button pt-large pt-minimal">
-						Login
-					</button>
-				</Popover>
+		<nav className={`header ${useAccentsString} ${props.largeHeaderBackground && props.isLandingPage ? 'has-image' : ''}`} style={backgroundStyle} >
+			<div className={showGradient ? 'header-gradient' : ''}>
+				<div className="container">
+					<div className="row">
+						<div className="col-12">
+
+							{/* App Logo - do not show on homepage */}
+							{!props.isLandingPage &&
+								<div className="headerItems headerItemsLeft">
+									<a href="/">
+										<img alt="header logo" className="headerLogo" src={resizedSmallHeaderLogo} />
+									</a>
+								</div>
+							}
+
+							<div className="headerItems headerItemsRight">
+
+								{/* Search button */}
+								<a href="/search" className="pt-button pt-large pt-minimal pt-icon-search" />
+
+								{/* Dashboard panel button */}
+								{props.userIsAdmin &&
+									<a href="/dashboard" className="pt-button pt-large pt-minimal pt-icon-page-layout" />
+								}
+
+								{/* User avatar and menu */}
+								{loggedIn &&
+									<Popover
+										content={
+											<Menu>
+												<li>
+													<a href={`/user/${props.userSlug}`} className="pt-menu-item pt-popover-dismiss">
+														<div>{props.userName}</div>
+														<div className="subtext">View Profile</div>
+													</a>
+												</li>
+												<MenuDivider />
+												{!props.isBasePubPub &&
+													<li>
+														<a href="/pub/create" className="pt-menu-item pt-popover-dismiss">
+															Create New Pub
+														</a>
+													</li>
+												}
+												<MenuItem text="Logout" onClick={props.onLogout} />
+											</Menu>
+										}
+										interactionKind={PopoverInteractionKind.CLICK}
+										position={Position.BOTTOM_RIGHT}
+										transitionDuration={-1}
+										inheritDarkTheme={false}
+									>
+										<button className="pt-button pt-large pt-minimal avatar-button">
+											<Avatar
+												userInitials={props.userInitials}
+												userAvatar={props.userAvatar}
+												width={30}
+											/>
+										</button>
+									</Popover>
+								}
+
+								{/* Login or Signup button */}
+								{!loggedIn &&
+									<a href="/login" className="pt-button pt-large pt-minimal">Login or Signup</a>
+								}
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
+			{props.isLandingPage &&
+				<div className="community-header">
+					<div className="container">
+						<div className="row">
+							<div className="col-12">
+								<img alt="community logo" className="logo" src={resizedLargeHeaderLogo} />
+								<div className="description">{props.largeHeaderDescription}</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			}
 		</nav>
 	);
 };
---
+++ stories/components/headerStories.js
@@ -0,0 +1,127 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import Header from 'components/Header/Header';
+import AccentStyle from 'components/AccentStyle/AccentStyle';
+import { accentDataDark, accentDataLight } from '../data';
+
+const wrapperStyle = { margin: '1em', boxShadow: '0px 0px 1px rgba(0, 0, 0, 0.25)' };
+const titleStyle = { margin: '1em 1em -0.5em' };
+
+const headerBars = function(isBasePubPub) {
+	const data = {
+		userName: 'Maggie Farnkrux',
+		userInitials: 'MF',
+		userSlug: 'maggiefarn',
+		userAvatar: 'https://assets.pubpub.org/mflaxicd/11505393046254.jpg',
+		userIsAdmin: true,
+		smallHeaderLogo: isBasePubPub
+			? '/static/icon.png'
+			: 'https://assets.pubpub.org/c7cfyyz6/31507218538557.png',
+		largeHeaderLogo: 'https://assets.pubpub.org/c7cfyyz6/31507218538557.png',
+		largeHeaderDescription: 'Group publications and research docs from around the world all situated here in this little community.',
+		largeHeaderBackground: 'https://assets.pubpub.org/9s4gbj5y/51507218425793.png',
+		isBasePubPub: isBasePubPub,
+		isLandingPage: false,
+		onLogout: ()=> {},
+	};
+	return (
+		<div>
+			<h4 style={titleStyle}>Logged Out</h4>
+			<div style={wrapperStyle}>
+				<Header
+					smallHeaderLogo={data.smallHeaderLogo}
+					onLogout={data.onLogout}
+					isBasePubPub={data.isBasePubPub}
+					isLandingPage={false}
+				/>
+			</div>
+
+			<h4 style={titleStyle}>Logged In · User</h4>
+			<div style={wrapperStyle}>
+				<Header
+					userName={data.userName}
+					userInitials={data.userInitials}
+					userSlug={data.userSlug}
+					userIsAdmin={false}
+					smallHeaderLogo={data.smallHeaderLogo}
+					onLogout={data.onLogout}
+					isBasePubPub={data.isBasePubPub}
+					isLandingPage={false}
+				/>
+			</div>
+
+			<h4 style={titleStyle}>Logged In · Admin</h4>
+			<div style={wrapperStyle}>
+				<Header
+					userName={data.userName}
+					userInitials={data.userInitials}
+					userSlug={data.userSlug}
+					userAvatar={data.userAvatar}
+					userIsAdmin={true}
+					smallHeaderLogo={data.smallHeaderLogo}
+					onLogout={data.onLogout}
+					isBasePubPub={data.isBasePubPub}
+					isLandingPage={false}
+				/>
+			</div>
+
+			{!isBasePubPub &&
+				<div>
+					<h4 style={titleStyle}>Home · No Background</h4>
+					<div style={wrapperStyle}>
+						<Header
+							userName={data.userName}
+							userInitials={data.userInitials}
+							userSlug={data.userSlug}
+							userAvatar={data.userAvatar}
+							userIsAdmin={true}
+							smallHeaderLogo={data.smallHeaderLogo}
+							largeHeaderLogo={data.largeHeaderLogo}
+							largeHeaderDescription={data.largeHeaderDescription}
+							onLogout={data.onLogout}
+							isBasePubPub={data.isBasePubPub}
+							isLandingPage={true}
+						/>
+					</div>
+
+					<h4 style={titleStyle}>Home · Background (Image)</h4>
+					<div style={wrapperStyle}>
+						<Header
+							userName={data.userName}
+							userInitials={data.userInitials}
+							userSlug={data.userSlug}
+							userAvatar={data.userAvatar}
+							userIsAdmin={true}
+							smallHeaderLogo={data.smallHeaderLogo}
+							largeHeaderLogo={data.largeHeaderLogo}
+							largeHeaderDescription={data.largeHeaderDescription}
+							largeHeaderBackground={data.largeHeaderBackground}
+							onLogout={data.onLogout}
+							isBasePubPub={data.isBasePubPub}
+							isLandingPage={true}
+						/>
+					</div>		
+				</div>
+			}
+		</div>
+	);
+};
+
+storiesOf('Header', module)
+.add('PubPub', () => (
+	<div>
+		{headerBars(true)}
+	</div>
+))
+.add('Styled Dark', () => (
+	<div>
+		<AccentStyle {...accentDataDark} />
+		{headerBars()}
+	</div>
+))
+.add('Styled Light', () => (
+	<div>
+		<AccentStyle {...accentDataLight} />
+		{headerBars()}
+	</div>
+));
