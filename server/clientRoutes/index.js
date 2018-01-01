336bc40c979662c5e593332c74773abc35119f10
Travis Rich <travis.s.rich@gmail.com>
2018-01-01 16:33:51 -0500

    rename pub to pubpResentation

--- server/clientRoutes/index.js
+++ server/clientRoutes/index.js
@@ -2,7 +2,7 @@
 require('./communityCreate');	// Route: /community/create
 
 /* Routes for Communities */
-require('./pub'); 				// Route: /pub/:slug
+require('./pubPresentation');	// Route: /pub/:slug
 require('./collection'); 		// Route: / and /:slug
 require('./collectionSubmit'); 	// Route: /:slug/submit and /:slug/submit/:hash
 
