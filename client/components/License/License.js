e71926e04ce54399abde51b21d0d0eecb75ee273
Travis Rich <travis.s.rich@gmail.com>
2018-01-02 21:39:35 -0500

    Update license to new cc image

--- client/components/License/License.js
+++ client/components/License/License.js
@@ -9,7 +9,7 @@ const License = function() {
 				<div className="row">
 					<div className="col-12">
 						<a rel="license" href="https://creativecommons.org/licenses/by/4.0/">
-							<img alt="Creative Commons License" src="https://i.creativecommons.org/l/by/4.0/88x31.png" />
+							<img alt="Creative Commons License" src="https://licensebuttons.net/l/by/4.0/88x31.png" />
 						</a>
 						<div className="text">
 							This work is licensed under a <a rel="license" href="https://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.
