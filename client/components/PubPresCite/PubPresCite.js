380ece12b8af23e0f1a04b9622d7c8dafaff1e58
Travis Rich <travis.s.rich@gmail.com>
2018-02-05 16:56:41 -0500

    Style and language updates for PubPresCite

--- client/components/PubPresCite/PubPresCite.js
+++ client/components/PubPresCite/PubPresCite.js
@@ -51,32 +51,6 @@ class PubPresCite extends Component {
 				</div>
 
 				<h5 className="overlay-title">Cite</h5>
-				{this.props.loginData.isAdmin && this.props.loginData.id === 'b242f616-7aaa-479c-8ee5-3933dcf70859' &&
-					<div className="pt-callout">
-						{!pubData.doi &&
-							<Button
-								text="Assign DOI"
-								className="pt-small"
-								loading={this.props.postDoiIsLoading}
-								onClick={this.props.onAssignDoi}
-							/>
-						}
-						<h5>DOI Assignment</h5>
-						{!pubData.doi &&
-							<p>A DOI can be assigned to each pub by community admins. When assigned, the pub is given an article-level DOI and each version is assigned it's own child DOI. The article-level DOI will always point to the most recent version while each version DOI can be used to reference the specific version.</p>
-						}
-						{pubData.doi && !this.state.justSetDoi &&
-							<p>DOIs have been registered for this pub and all of it's published versions.</p>
-						}
-
-						{pubData.doi && this.state.justSetDoi &&
-							<div>
-								<p>Successfully registered DOIs for this pub and all of it's published versions!</p>
-								<p>Registration may take a few hours to complete in Crossref's system. If DOI URLs do not work immediately, the registration is likely still processing.</p>
-							</div>
-						}
-					</div>
-				}
 
 				<div className="style-wrapper">
 					<div className="style-title">APA</div>
@@ -97,6 +71,33 @@ class PubPresCite extends Component {
 					<div className="style-title">Bibtex</div>
 					<div className="style-content bibtex" dangerouslySetInnerHTML={{ __html: modeData.bibtex }} />
 				</div>
+
+				{this.props.loginData.isAdmin && this.props.loginData.id === 'b242f616-7aaa-479c-8ee5-3933dcf70859' &&
+					<div className="pt-callout">
+						{!pubData.doi &&
+							<Button
+								text="Assign DOI"
+								className="pt-small"
+								loading={this.props.postDoiIsLoading}
+								onClick={this.props.onAssignDoi}
+							/>
+						}
+						<h5>DOI Assignment</h5>
+						{!pubData.doi &&
+							<p>A DOI can be registered for each pub by community admins. When completed, the pub is assigned an article-level DOI and each version is assigned its own component DOI. The article-level DOI will always point to the most recent version while each version DOI can be used to reference earlier snapshots.</p>
+						}
+						{pubData.doi && !this.state.justSetDoi &&
+							<p>DOIs have been registered for this pub and all of its published versions.</p>
+						}
+
+						{pubData.doi && this.state.justSetDoi &&
+							<div>
+								<p>Successfully registered DOIs for this pub and all of its published versions!</p>
+								<p>Registration may take a few hours to complete in Crossref's system. If DOI URLs do not work immediately, the registration is likely still processing.</p>
+							</div>
+						}
+					</div>
+				}
 			</div>
 		);
 	}
