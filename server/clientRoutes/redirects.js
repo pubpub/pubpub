3a421e297f5e8f76961676a548d9b90b35f900be
Travis Rich <travis.s.rich@gmail.com>
2018-04-17 11:24:37 -0400

    Add resiting-reduction redirect

--- server/clientRoutes/redirects.js
+++ server/clientRoutes/redirects.js
@@ -5,3 +5,10 @@ app.get('/iai', (req, res, next)=> {
 	if (!hostIsValid(req, 'pubpub')) { return next(); }
 	return res.redirect('https://v3.pubpub.org/iai');
 });
+
+app.get('/pub/resisting-reduction', (req, res, next)=> {
+	if (req.hostname === 'pubpub.ito.com') {
+		return res.redirect('https://jods.mitpress.mit.edu/pub/resisting-reduction');
+	}
+	return next();
+});
