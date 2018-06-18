e93dca98a70e2e83a2046cd637b6da0c3546974e
Travis Rich <travis.s.rich@gmail.com>
2018-06-18 11:12:57 -0400

    Write to tmp file

--- workers/worker.js
+++ workers/worker.js
@@ -1,13 +1,26 @@
+import fs from 'fs';
+import Promise from 'bluebird';
 import nodePandoc from 'node-pandoc';
+import tmp from 'tmp-promise';
 
-const src = '<h1>Hello</h1><p>It&rsquo;s bananas</p>';
-// Arguments in either a single String or as an Array:
-const args = '-f html -t docx -o word.docx';
+// const fsReadFile = Promise.promisify(fs.readFile);
+// const fsWriteFile = Promise.promisify(fs.writeFile);
+tmp.file({ postfix: '.docx' })
+.then((tmpFile)=> {
+	const src = '<h1>Hello</h1><p>It&rsquo;s bananas</p>';
+	// Arguments in either a single String or as an Array:
+	// const args = '-f html -t docx -o word.docx';
+	const args = `-f html -t docx -o ${tmpFile.path}`;
+	console.log(tmpFile.path);
 
-
-// Call pandoc
-nodePandoc(src, args, (err, result)=> {
-	if (err) console.error('Oh Nos: ',err);
-  // Without the -o arg, the converted value will be returned.
-  return console.log(result), result;
+	// Call pandoc
+	nodePandoc(src, args, (err, result)=> {
+		if (err) console.error('Oh Nos: ',err);
+		// Without the -o arg, the converted value will be returned.
+		return console.log(result), result;
+	});
+})
+.catch((promiseErr)=> {
+	console.log('Error in promise chain', promiseErr);
 });
+
