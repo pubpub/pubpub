7040d07de6cbbc3a32fc0b0480019ce6cc861b54
Travis Rich <travis.s.rich@gmail.com>
2018-06-18 15:32:44 -0400

    Create stubs for separate task files

---
+++ workers/exportTask.js
@@ -0,0 +1,40 @@
+import fs from 'fs';
+import Promise from 'bluebird';
+import nodePandoc from 'node-pandoc';
+import tmp from 'tmp-promise';
+
+export default (pubId, versionId, format)=> {
+	const dataDir = process.env.NODE_ENV === 'production'
+		? '--data-dir=/app/.apt/usr/share/pandoc/data '
+		: '';
+	// const fsReadFile = Promise.promisify(fs.readFile);
+	// const fsWriteFile = Promise.promisify(fs.writeFile);
+	tmp.file({ postfix: '.docx' })
+	.then((tmpFile)=> {
+		const src = '<h1>Hello</h1><p>It&rsquo;s bananas</p>';
+		// Arguments in either a single String or as an Array:
+		// const args = '-f html -t docx -o word.docx';
+		const args = `${dataDir}-f html -t docx -o ${tmpFile.path}`;
+		// const args = `${dataDir}-f html -t json -o out.json`;
+		console.log(tmpFile.path);
+		console.log(args);
+
+		// Call pandoc
+		nodePandoc(src, args, (err, result)=> {
+			if (err) console.error('Oh no: ', err);
+			// Without the -o arg, the converted value will be returned.
+			console.log(result);
+			return result;
+		});
+	})
+	.catch((promiseErr)=> {
+		console.log('Error in promise chain', promiseErr);
+	});
+
+	/*
+	- get json from pubpub, convert to HTML, enter into pandoc, upload output, return url
+	- get file, enter into pandoc, get html, convert into pubpub json
+	- Have a 'check task' route and 'task' table that can be queried for task results
+
+	*/	
+};
--- workers/worker.js
+++ workers/worker.js
@@ -1,51 +1,7 @@
-import fs from 'fs';
-import Promise from 'bluebird';
-import nodePandoc from 'node-pandoc';
-import tmp from 'tmp-promise';
+import exportTask from './exportTask';
+import importTask from './importTask';
+import analyticsTask from './analyticsTask';
 
-const dataDir = process.env.NODE_ENV === 'production'
-	? '--data-dir=/app/.apt/usr/share/pandoc/data '
-	: '';
-// const fsReadFile = Promise.promisify(fs.readFile);
-// const fsWriteFile = Promise.promisify(fs.writeFile);
-tmp.file({ postfix: '.docx' })
-.then((tmpFile)=> {
-	const src = '<h1>Hello</h1><p>It&rsquo;s bananas</p>';
-	// Arguments in either a single String or as an Array:
-	// const args = '-f html -t docx -o word.docx';
-	const args = `${dataDir}-f html -t docx -o ${tmpFile.path}`;
-	// const args = `${dataDir}-f html -t json -o out.json`;
-	console.log(tmpFile.path);
-	console.log(args);
-
-	// Call pandoc
-	nodePandoc(src, args, (err, result)=> {
-		if (err) console.error('Oh no: ', err);
-		// Without the -o arg, the converted value will be returned.
-		console.log(result);
-		return result;
-	});
-})
-.catch((promiseErr)=> {
-	console.log('Error in promise chain', promiseErr);
-});
-
-
-tmp.file({ postfix: '.pdf' })
-.then((tmpFile)=> {
-	const src = '<h1>Hello</h1><p>It&rsquo;s bananas</p>';
-	// Arguments in either a single String or as an Array:
-	// const args = '-f html -t docx -o word.docx';
-	const args = `${dataDir}-f html -t latex -o ${tmpFile.path}`;
-	console.log(tmpFile.path);
-
-	// Call pandoc
-	nodePandoc(src, args, (err, result)=> {
-		if (err) console.error('Oh Nos2: ',err);
-		// Without the -o arg, the converted value will be returned.
-		return console.log(2, result), result;
-	});
-})
-.catch((promiseErr)=> {
-	console.log('Error in promise chain', promiseErr);
-});
+exportTask('resisting-reduction');
+importTask();
+analyticsTask();
