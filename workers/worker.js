f9ee411250487de1ee210844a07177f9eb2c9036
Travis Rich <travis.s.rich@gmail.com>
2018-06-18 11:59:52 -0400

    Set pandoc data dir

--- workers/worker.js
+++ workers/worker.js
@@ -3,6 +3,9 @@ import Promise from 'bluebird';
 import nodePandoc from 'node-pandoc';
 import tmp from 'tmp-promise';
 
+const dataDir = process.env.NODE_ENV === 'production'
+	? '--data-dir /app/.apt/usr/share/pandoc/'
+	: '';
 // const fsReadFile = Promise.promisify(fs.readFile);
 // const fsWriteFile = Promise.promisify(fs.writeFile);
 tmp.file({ postfix: '.docx' })
@@ -10,7 +13,7 @@ tmp.file({ postfix: '.docx' })
 	const src = '<h1>Hello</h1><p>It&rsquo;s bananas</p>';
 	// Arguments in either a single String or as an Array:
 	// const args = '-f html -t docx -o word.docx';
-	const args = `-f html -t docx -o ${tmpFile.path}`;
+	const args = `${dataDir} -f html -t docx -o ${tmpFile.path}`;
 	console.log(tmpFile.path);
 
 	// Call pandoc
@@ -24,3 +27,22 @@ tmp.file({ postfix: '.docx' })
 	console.log('Error in promise chain', promiseErr);
 });
 
+
+tmp.file({ postfix: '.pdf' })
+.then((tmpFile)=> {
+	const src = '<h1>Hello</h1><p>It&rsquo;s bananas</p>';
+	// Arguments in either a single String or as an Array:
+	// const args = '-f html -t docx -o word.docx';
+	const args = `${dataDir} -f html -t pdf -o ${tmpFile.path}`;
+	console.log(tmpFile.path);
+
+	// Call pandoc
+	nodePandoc(src, args, (err, result)=> {
+		if (err) console.error('Oh Nos2: ',err);
+		// Without the -o arg, the converted value will be returned.
+		return console.log(2, result), result;
+	});
+})
+.catch((promiseErr)=> {
+	console.log('Error in promise chain', promiseErr);
+});
