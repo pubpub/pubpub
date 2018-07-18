06e582c5dc1009fdb1d9c2109ba2a699b5438518
Travis Rich <travis.s.rich@gmail.com>
2018-07-18 15:41:35 -0400

    Update heap environment

--- client/utilities.js
+++ client/utilities.js
@@ -26,7 +26,10 @@ export const hydrateWrapper = (Component)=> {
 
 			/* Heap Code */
 			window.heap = window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=t.forceSSL||"https:"===document.location.protocol,a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src=(r?"https:":"http:")+"//cdn.heapanalytics.com/js/heap-"+e+".js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(a,n);for(var o=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],c=0;c<p.length;c++)heap[p[c]]=o(p[c])};
-			heap.load('422727431');
+			const heapEnvironment = window.location.origin.indexOf('https://dev.pubpub.org') === 0
+				? '3777990325'
+				: '422727431';
+			heap.load(heapEnvironment);
 
 			/* Matomo Code */
 			window._paq = [];
