2fe9ff108e6eea4011093027c6a493407c0c73a5
Travis Rich <travis.s.rich@gmail.com>
2018-08-06 13:58:56 -0400

    update: DashboardPage updates

--- client/components/DashboardSide/DashboardSide.js
+++ client/components/DashboardSide/DashboardSide.js
@@ -4,14 +4,11 @@ import PropTypes from 'prop-types';
 require('./dashboardSide.scss');
 
 const propTypes = {
-	pages: PropTypes.array,
-	// collections: PropTypes.array,
+	pages: PropTypes.array.isRequired,
 	activeTab: PropTypes.string,
 };
 
 const defaultProps = {
-	pages: [],
-	// collections: [],
 	activeTab: undefined,
 };
 
@@ -65,9 +62,11 @@ const DashboardSide = function(props) {
 				})}
 			</ul>
 			<ul className="pt-menu">
-
 				<li className="pt-menu-header">
-					<a href="/dashboard/page/create" className="pt-button pt-icon-plus pt-small pt-minimal title-button" />
+					<a
+						href="/dashboard/page"
+						className="pt-button pt-icon-plus pt-small pt-minimal title-button"
+					/>
 					<h6>Pages</h6>
 				</li>
 				{props.pages.sort((foo, bar)=> {
@@ -79,47 +78,20 @@ const DashboardSide = function(props) {
 					return 0;
 				}).map((page)=> {
 					const publicStatusIcon = page.isPublic ? 'pt-icon-globe' : 'pt-icon-lock';
-					const pageIcon = page.slug ? publicStatusIcon : 'pt-icon-home';
+					const pageIcon = page.slug
+						? publicStatusIcon
+						: 'pt-icon-home';
+					const isActive = page.slug
+						? props.activeTab === page.slug
+						: props.activeTab === 'pages';
 					return (
 						<li key={`side-page-${page.id}`}>
-							<a className={`pt-menu-item ${props.activeTab === page.slug && 'pt-active'} ${pageIcon}`} tabIndex="0" href={`/dashboard/pages/${page.slug}`}>
+							<a className={`pt-menu-item ${isActive ? 'pt-active' : ''} ${pageIcon}`} tabIndex="0" href={`/dashboard/pages/${page.slug}`}>
 								{page.title}
 							</a>
 						</li>
 					);
 				})}
-				{/*props.pages.length === 0 &&
-					<li>
-						<a className="pt-menu-item empty">No Pages</a>
-					</li>
-				*/}
-
-				{/*<li className="pt-menu-header2">
-					<a href="/dashboard/collection/create" className="pt-button pt-icon-plus pt-small pt-minimal title-button" />
-					<h6>Collections</h6>
-				</li>*/}
-
-				{/*props.collections.sort((foo, bar)=> {
-					const fooTitle = foo.title.toLowerCase();
-					const barTitle = bar.title.toLowerCase();
-					if (fooTitle > barTitle) { return 1; }
-					if (fooTitle < barTitle) { return -1; }
-					return 0;
-				}).map((collection)=> {
-					return (
-						<li key={`side-collection-${collection.id}`}>
-							<a className={`pt-menu-item ${props.activeTab === collection.slug && 'pt-active'} ${collection.isPublic ? 'pt-icon-globe' : 'pt-icon-lock'}`} tabIndex="0" href={`/dashboard/${collection.slug}`}>
-								{collection.title}
-							</a>
-						</li>
-					);
-				})}
-				{props.collections.length === 0 &&
-					<li>
-						<a className="pt-menu-item empty">No Collections</a>
-					</li>
-				*/}
-
 			</ul>
 		</div>
 	);
--- client/containers/Dashboard/Dashboard.js
+++ client/containers/Dashboard/Dashboard.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import PageWrapper from 'components/PageWrapper/PageWrapper';
 import DashboardSide from 'components/DashboardSide/DashboardSide';
-import DashboardCollection from 'components/DashboardCollection/DashboardCollection';
-import DashboardCollectionEdit from 'components/DashboardCollectionEdit/DashboardCollectionEdit';
+// import DashboardCollection from 'components/DashboardCollection/DashboardCollection';
+// import DashboardCollectionEdit from 'components/DashboardCollectionEdit/DashboardCollectionEdit';
 import DashboardCreateCollection from 'components/DashboardCreateCollection/DashboardCreateCollection';
 import DashboardSite from 'components/DashboardSite/DashboardSite';
 import DashboardTeam from 'components/DashboardTeam/DashboardTeam';
 import DashboardTags from 'components/DashboardTags/DashboardTags';
 import DashboardPubs from 'components/DashboardPubs/DashboardPubs';
+import DashboardPage from 'components/DashboardPage/DashboardPage';
 
 import { hydrateWrapper, apiFetch } from 'utilities';
 
@@ -26,143 +27,147 @@ class Dashboard extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			putCommunityIsLoading: false,
-			putCommunityError: undefined,
-			postCollectionIsLoading: false,
-			postCollectionError: undefined,
-			putCollectionIsLoading: false,
-			deleteCollectionIsLoading: false,
-			putCollectionError: undefined,
-			postPubIsLoading: false,
+			communityData: props.communityData,
+			collectionData: props.collectionData,
+			// putCommunityIsLoading: false,
+			// putCommunityError: undefined,
+			// postCollectionIsLoading: false,
+			// postCollectionError: undefined,
+			// putCollectionIsLoading: false,
+			// deleteCollectionIsLoading: false,
+			// putCollectionError: undefined,
+			// postPubIsLoading: false,
 		};
-		this.handleCreatePub = this.handleCreatePub.bind(this);
-		this.handleSiteSave = this.handleSiteSave.bind(this);
-		this.handleCollectionCreate = this.handleCollectionCreate.bind(this);
-		this.handleCollectionSave = this.handleCollectionSave.bind(this);
-		this.handleCollectionDelete = this.handleCollectionDelete.bind(this);
-		this.handleAddAdmin = this.handleAddAdmin.bind(this);
-		this.handleRemoveAdmin = this.handleRemoveAdmin.bind(this);
+		this.setCommunityData = this.setCommunityData.bind(this);
+		this.setCollectionData = this.setCollectionData.bind(this);
+		// this.handleCreatePub = this.handleCreatePub.bind(this);
+		// this.handleSiteSave = this.handleSiteSave.bind(this);
+		// this.handleCollectionCreate = this.handleCollectionCreate.bind(this);
+		// this.handleCollectionSave = this.handleCollectionSave.bind(this);
+		// this.handleCollectionDelete = this.handleCollectionDelete.bind(this);
+		// this.handleAddAdmin = this.handleAddAdmin.bind(this);
+		// this.handleRemoveAdmin = this.handleRemoveAdmin.bind(this);
 	}
 
-	handleCreatePub(collectionId) {
-		this.setState({ postPubIsLoading: true });
-		return apiFetch('/api/pubs', {
-			method: 'POST',
-			body: JSON.stringify({
-				collectionId: collectionId,
-				communityId: this.props.communityData.id,
-				createPubHash: undefined,
-			})
-		})
-		.then((result)=> {
-			window.location.href = result;
-		})
-		.catch((err)=> {
-			console.error(err);
-			this.setState({ postPubIsLoading: false });
-		});
+	setCommunityData(newCommunityData) {
+		this.setState({ communityData: newCommunityData });
 	}
-	handleSiteSave(siteObject) {
-		this.setState({ putCommunityIsLoading: true, putCommunityError: undefined });
-		return apiFetch('/api/communities', {
-			method: 'PUT',
-			body: JSON.stringify(siteObject)
-		})
-		.then(()=> {
-			if (!this.props.communityData.domain) {
-				window.location.replace(`https://${siteObject.subdomain}.pubpub.org/dashboard/site`);
-			} else {
-				window.location.reload();
-			}
-		})
-		.catch((err)=> {
-			console.error(err);
-			this.setState({ putCommunityIsLoading: false, putCommunityError: err });
-		});
+
+	setCollectionData(newCollectionData) {
+		this.setState({ collectionData: newCollectionData });
 	}
-	handleCollectionCreate(collectionObject) {
-		this.setState({ postCollectionIsLoading: true, postCollectionError: undefined });
-		return apiFetch('/api/collections', {
-			method: 'POST',
-			body: JSON.stringify({
-				...collectionObject,
-				communityId: this.props.communityData.id,
-			})
-		})
-		.then(()=> {
-			window.location.href = `/dashboard/${collectionObject.slug}`;
-		})
-		.catch((err)=> {
-			console.error(err);
-			this.setState({ postCollectionIsLoading: false, postCollectionError: err });
-		});
-	}
-	handleCollectionSave(collectionObject) {
-		this.setState({ putCollectionIsLoading: true, putCollectionError: undefined });
-		return apiFetch('/api/collections', {
-			method: 'PUT',
-			body: JSON.stringify({
-				...collectionObject,
-				communityId: this.props.communityData.id,
-			})
-		})
-		.then(()=> {
-			window.location.href = `/dashboard/${collectionObject.slug}`;
-		})
-		.catch((err)=> {
-			console.error(err);
-			this.setState({ putCollectionIsLoading: false, putCollectionError: err });
-		});
-	}
-	handleCollectionDelete(collectionId) {
-		this.setState({ deleteCollectionIsLoading: true });
-		return apiFetch('/api/collections', {
-			method: 'DELETE',
-			body: JSON.stringify({
-				collectionId: collectionId,
-				communityId: this.props.communityData.id,
-			})
-		})
-		.then(()=> {
-			window.location.href = '/dashboard';
-		})
-		.catch((err)=> {
-			console.error(err);
-			this.setState({ deleteCollectionIsLoading: false });
-		});
-	}
-	handleAddAdmin(adminObject) {
-		return apiFetch('/api/communityAdmins', {
-			method: 'POST',
-			body: JSON.stringify(adminObject)
-		})
-		.then(()=> {
-			window.location.reload();
-		});
-	}
-	handleRemoveAdmin(adminObject) {
-		return apiFetch('/api/communityAdmins', {
-			method: 'DELETE',
-			body: JSON.stringify(adminObject)
-		})
-		.then(()=> {
-			window.location.reload();
-		});
-	}
+
+	// handleCreatePub(collectionId) {
+	// 	this.setState({ postPubIsLoading: true });
+	// 	return apiFetch('/api/pubs', {
+	// 		method: 'POST',
+	// 		body: JSON.stringify({
+	// 			collectionId: collectionId,
+	// 			communityId: this.props.communityData.id,
+	// 			createPubHash: undefined,
+	// 		})
+	// 	})
+	// 	.then((result)=> {
+	// 		window.location.href = result;
+	// 	})
+	// 	.catch((err)=> {
+	// 		console.error(err);
+	// 		this.setState({ postPubIsLoading: false });
+	// 	});
+	// }
+	// handleSiteSave(siteObject) {
+	// 	this.setState({ putCommunityIsLoading: true, putCommunityError: undefined });
+	// 	return apiFetch('/api/communities', {
+	// 		method: 'PUT',
+	// 		body: JSON.stringify(siteObject)
+	// 	})
+	// 	.then(()=> {
+	// 		if (!this.props.communityData.domain) {
+	// 			window.location.replace(`https://${siteObject.subdomain}.pubpub.org/dashboard/site`);
+	// 		} else {
+	// 			window.location.reload();
+	// 		}
+	// 	})
+	// 	.catch((err)=> {
+	// 		console.error(err);
+	// 		this.setState({ putCommunityIsLoading: false, putCommunityError: err });
+	// 	});
+	// }
+	// handleCollectionCreate(collectionObject) {
+	// 	this.setState({ postCollectionIsLoading: true, postCollectionError: undefined });
+	// 	return apiFetch('/api/collections', {
+	// 		method: 'POST',
+	// 		body: JSON.stringify({
+	// 			...collectionObject,
+	// 			communityId: this.props.communityData.id,
+	// 		})
+	// 	})
+	// 	.then(()=> {
+	// 		window.location.href = `/dashboard/${collectionObject.slug}`;
+	// 	})
+	// 	.catch((err)=> {
+	// 		console.error(err);
+	// 		this.setState({ postCollectionIsLoading: false, postCollectionError: err });
+	// 	});
+	// }
+	// handleCollectionSave(collectionObject) {
+	// 	this.setState({ putCollectionIsLoading: true, putCollectionError: undefined });
+	// 	return apiFetch('/api/collections', {
+	// 		method: 'PUT',
+	// 		body: JSON.stringify({
+	// 			...collectionObject,
+	// 			communityId: this.props.communityData.id,
+	// 		})
+	// 	})
+	// 	.then(()=> {
+	// 		window.location.href = `/dashboard/${collectionObject.slug}`;
+	// 	})
+	// 	.catch((err)=> {
+	// 		console.error(err);
+	// 		this.setState({ putCollectionIsLoading: false, putCollectionError: err });
+	// 	});
+	// }
+	// handleCollectionDelete(collectionId) {
+	// 	this.setState({ deleteCollectionIsLoading: true });
+	// 	return apiFetch('/api/collections', {
+	// 		method: 'DELETE',
+	// 		body: JSON.stringify({
+	// 			collectionId: collectionId,
+	// 			communityId: this.props.communityData.id,
+	// 		})
+	// 	})
+	// 	.then(()=> {
+	// 		window.location.href = '/dashboard';
+	// 	})
+	// 	.catch((err)=> {
+	// 		console.error(err);
+	// 		this.setState({ deleteCollectionIsLoading: false });
+	// 	});
+	// }
+	// handleAddAdmin(adminObject) {
+	// 	return apiFetch('/api/communityAdmins', {
+	// 		method: 'POST',
+	// 		body: JSON.stringify(adminObject)
+	// 	})
+	// 	.then(()=> {
+	// 		window.location.reload();
+	// 	});
+	// }
+	// handleRemoveAdmin(adminObject) {
+	// 	return apiFetch('/api/communityAdmins', {
+	// 		method: 'DELETE',
+	// 		body: JSON.stringify(adminObject)
+	// 	})
+	// 	.then(()=> {
+	// 		window.location.reload();
+	// 	});
+	// }
 
 	render() {
-		const communityData = this.props.communityData;
-		const collectionData = this.props.collectionData;
-		// const pages = communityData.collections.filter((item)=> {
-		// 	return item.isPage;
-		// });
-		// const collections = communityData.collections.filter((item)=> {
-		// 	return !item.isPage;
-		// });
-
+		const communityData = this.state.communityData;
+		const collectionData = this.state.collectionData;
 		const activeSlug = this.props.locationData.params.slug || '';
 		const activeMode = this.props.locationData.params.mode || '';
-		console.log(activeMode, activeSlug)
 		return (
 			<div id="dashboard-container">
 				<PageWrapper
@@ -198,13 +203,11 @@ class Dashboard extends Component {
 											);
 										case 'team':
 											return (
-												<div>
-													<DashboardTeam
-														communityData={communityData}
-														onAddAdmin={this.handleAddAdmin}
-														onRemoveAdmin={this.handleRemoveAdmin}
-													/>
-												</div>
+												<DashboardTeam
+													communityData={communityData}
+													onAddAdmin={this.handleAddAdmin}
+													onRemoveAdmin={this.handleRemoveAdmin}
+												/>
 											);
 										case 'details':
 											return (
@@ -225,55 +228,45 @@ class Dashboard extends Component {
 												/>
 											);
 										case 'page':
-											if (activeMode === 'create') {
-												return (
-													<DashboardCreateCollection
-														communityData={communityData}
-														isPage={true}
-														onCreate={this.handleCollectionCreate}
-														isLoading={this.state.postCollectionIsLoading}
-														error={this.state.postCollectionError}
-														hostname={this.props.locationData.hostname}
-													/>
-												);
-											}
-											break;
-										case 'collection':
-											if (activeMode === 'create') {
-												return (
-													<DashboardCreateCollection
-														communityData={communityData}
-														isPage={false}
-														onCreate={this.handleCollectionCreate}
-														isLoading={this.state.postCollectionIsLoading}
-														error={this.state.postCollectionError}
-														hostname={this.props.locationData.hostname}
-													/>
-												);
-											}
-											break;
-										default:
-											if (activeMode === 'edit') {
-												return (
-													<DashboardCollectionEdit
-														location={this.props.locationData}
-														collectionData={collectionData}
-														putIsLoading={this.state.putCollectionIsLoading}
-														deleteIsLoading={this.state.deleteCollectionIsLoading}
-														error={this.state.putCollectionError}
-														onSave={this.handleCollectionSave}
-														onDelete={this.handleCollectionDelete}
-													/>
-												);
-											}
 											return (
-												<DashboardCollection
-													collectionData={collectionData}
-													onCreatePub={this.handleCreatePub}
-													createPubLoading={this.state.postPubIsLoading}
+												<DashboardCreateCollection
+													communityData={communityData}
+													isPage={true}
+													onCreate={this.handleCollectionCreate}
+													isLoading={this.state.postCollectionIsLoading}
+													error={this.state.postCollectionError}
 													hostname={this.props.locationData.hostname}
 												/>
 											);
+										default:
+											return (
+												<DashboardPage
+													communityData={communityData}
+													collectionData={collectionData}
+													location={this.props.locationData}
+												/>
+											);
+											// if (activeMode === 'edit') {
+											// 	return (
+											// 		<DashboardCollectionEdit
+											// 			location={this.props.locationData}
+											// 			collectionData={collectionData}
+											// 			putIsLoading={this.state.putCollectionIsLoading}
+											// 			deleteIsLoading={this.state.deleteCollectionIsLoading}
+											// 			error={this.state.putCollectionError}
+											// 			onSave={this.handleCollectionSave}
+											// 			onDelete={this.handleCollectionDelete}
+											// 		/>
+											// 	);
+											// }
+											// return (
+											// 	<DashboardCollection
+											// 		collectionData={collectionData}
+											// 		onCreatePub={this.handleCreatePub}
+											// 		createPubLoading={this.state.postPubIsLoading}
+											// 		hostname={this.props.locationData.hostname}
+											// 	/>
+											// );
 										}
 									})()}
 								</div>
