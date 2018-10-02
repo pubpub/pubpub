3997b4fac7804ea05c56b5631465bd46950ce0b4
Travis Rich <travis.s.rich@gmail.com>
2018-10-02 18:34:53 -0400

    fix: Update sideControls position on window resize

--- client/components/PubSideControls/PubSideControls.js
+++ client/components/PubSideControls/PubSideControls.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import throttle from 'lodash.throttle';
 import PubSideControlsIframe from 'components/PubSideControls/PubSideControlsIframe';
 import PubSideControlsImage from 'components/PubSideControls/PubSideControlsImage';
 import PubSideControlsVideo from 'components/PubSideControls/PubSideControlsVideo';
@@ -18,70 +19,91 @@ const propTypes = {
 	getAbsolutePosition: PropTypes.func.isRequired,
 };
 
-const PubSideControls = (props)=> {
-	const selectedNode = props.editorChangeObject.selectedNode || {};
-	const selectionBoundingBox = props.editorChangeObject.selectionBoundingBox || {};
-	const menuItems = props.editorChangeObject.menuItems || [];
-	const isTable = menuItems.reduce((prev, curr)=> {
-		if (curr.title === 'table-delete') { return true; }
-		return prev;
-	}, false);
+class PubSideControls extends Component {
+	constructor(props) {
+		super(props);
 
-	const uncontrolledNodes = ['paragraph', 'blockquote', 'horizontal_rule', 'heading', 'ordered_list', 'bullet_list', 'list_item', 'code_block'];
-	const isUncontrolledNode = selectedNode.type && uncontrolledNodes.indexOf(selectedNode.type.name) > -1;
-	if (!props.pubData.isDraft || isUncontrolledNode || (!selectedNode.attrs && !isTable)) { return null; }
+		this.triggerRedraw = this.triggerRedraw.bind(this);
+		this.handleScroll = throttle(this.triggerRedraw, 50, { leading: true, trailing: true });
+	}
 
-	const menuStyle = {
-		position: 'absolute',
-		...props.getAbsolutePosition(selectionBoundingBox.top, undefined, true)
-	};
+	componentDidMount() {
+		window.addEventListener('resize', this.handleScroll);
+	}
 
-	const attrs = selectedNode.attrs;
-	const updateFunc = props.editorChangeObject.updateNode;
-	const changeNodeFunc = props.editorChangeObject.changeNode;
-	const nodeType = selectedNode.type ? selectedNode.type.name : 'table';
+	componentWillUnmount() {
+		window.removeEventListener('resize', this.handleScroll);
+	}
 
-	// ?Inline permalink
-	const controlsProps = {
-		attrs: attrs,
-		updateAttrs: updateFunc,
-		changeNode: changeNodeFunc,
-		menuItems: menuItems,
-		threads: props.threads,
-		selectedNode: selectedNode,
-		editorChangeObject: props.editorChangeObject,
-	};
-	return (
-		<div className="pub-side-controls-component" style={menuStyle}>
-			<div className="content-wrapper">
-				{nodeType === 'iframe' &&
-					<PubSideControlsIframe {...controlsProps} />
-				}
-				{nodeType === 'image' &&
-					<PubSideControlsImage {...controlsProps} />
-				}
-				{nodeType === 'video' &&
-					<PubSideControlsVideo {...controlsProps} />
-				}
-				{nodeType === 'citation' &&
-					<PubSideControlsCitation {...controlsProps} />
-				}
-				{nodeType === 'footnote' &&
-					<PubSideControlsFootnote {...controlsProps} />
-				}
-				{nodeType === 'table' &&
-					<PubSideControlsTable {...controlsProps} />
-				}
-				{(nodeType === 'equation' || nodeType === 'block_equation') &&
-					<PubSideControlsEquation {...controlsProps} />
-				}
-				{nodeType === 'discussion' &&
-					<PubSideControlsDiscussion {...controlsProps} />
-				}
+	triggerRedraw() {
+		this.setState({});
+	}
+
+	render() {
+		const selectedNode = this.props.editorChangeObject.selectedNode || {};
+		const selectionBoundingBox = this.props.editorChangeObject.selectionBoundingBox || {};
+		const menuItems = this.props.editorChangeObject.menuItems || [];
+		const isTable = menuItems.reduce((prev, curr)=> {
+			if (curr.title === 'table-delete') { return true; }
+			return prev;
+		}, false);
+
+		const uncontrolledNodes = ['paragraph', 'blockquote', 'horizontal_rule', 'heading', 'ordered_list', 'bullet_list', 'list_item', 'code_block'];
+		const isUncontrolledNode = selectedNode.type && uncontrolledNodes.indexOf(selectedNode.type.name) > -1;
+		if (!this.props.pubData.isDraft || isUncontrolledNode || (!selectedNode.attrs && !isTable)) { return null; }
+
+		const menuStyle = {
+			position: 'absolute',
+			...this.props.getAbsolutePosition(selectionBoundingBox.top, undefined, true)
+		};
+
+		const attrs = selectedNode.attrs;
+		const updateFunc = this.props.editorChangeObject.updateNode;
+		const changeNodeFunc = this.props.editorChangeObject.changeNode;
+		const nodeType = selectedNode.type ? selectedNode.type.name : 'table';
+
+		// ?Inline permalink
+		const controlsProps = {
+			attrs: attrs,
+			updateAttrs: updateFunc,
+			changeNode: changeNodeFunc,
+			menuItems: menuItems,
+			threads: this.props.threads,
+			selectedNode: selectedNode,
+			editorChangeObject: this.props.editorChangeObject,
+		};
+		return (
+			<div className="pub-side-controls-component" style={menuStyle}>
+				<div className="content-wrapper">
+					{nodeType === 'iframe' &&
+						<PubSideControlsIframe {...controlsProps} />
+					}
+					{nodeType === 'image' &&
+						<PubSideControlsImage {...controlsProps} />
+					}
+					{nodeType === 'video' &&
+						<PubSideControlsVideo {...controlsProps} />
+					}
+					{nodeType === 'citation' &&
+						<PubSideControlsCitation {...controlsProps} />
+					}
+					{nodeType === 'footnote' &&
+						<PubSideControlsFootnote {...controlsProps} />
+					}
+					{nodeType === 'table' &&
+						<PubSideControlsTable {...controlsProps} />
+					}
+					{(nodeType === 'equation' || nodeType === 'block_equation') &&
+						<PubSideControlsEquation {...controlsProps} />
+					}
+					{nodeType === 'discussion' &&
+						<PubSideControlsDiscussion {...controlsProps} />
+					}
+				</div>
 			</div>
-		</div>
-	);
-};
+		);
+	}
+}
 
 
 PubSideControls.propTypes = propTypes;
