eb7a4eb13baa5674af143e5f20b47970dacbea84
Gabriel Stein <g@gabestein.com>
2018-10-04 17:04:07 -0400

    dev: Major landing page re-factor. Add new styles, content, and more.

--- client/containers/NewLanding/NewLanding.js
+++ client/containers/NewLanding/NewLanding.js
@@ -12,6 +12,18 @@ const propTypes = {
 	locationData: PropTypes.object.isRequired,
 };
 
+const FeatureItem = (props) => {
+	return (
+		<li>
+			<div className="icon"><Icon icon={props.icon} iconSize={30} /></div>
+			<div className="feature-description">
+				<p className="title">{props.title}</p>
+				<p className="description">{props.desc}</p>
+			</div>
+		</li>
+	)
+}
+
 class Landing extends React.Component {
 	constructor(props) {
 		super(props)
@@ -37,7 +49,9 @@ class Landing extends React.Component {
 	render() {
 		return (
 			<div id="new-landing-container">
-				<div className="header">
+				{/* BEGIN Jumbotron */}
+				<div id="jumbotron">
+					{/* BEGIN Nav */}
 					<div className="container">
 						<div className="row">
 							<div className="col-12">
@@ -56,9 +70,11 @@ class Landing extends React.Component {
 							</div>
 						</div>
 					</div>
+					{/* END Nav */}
+					{/* BEGIN Jumbotron Content */}
 					<div className="container">
-						<div className="row">
-							<div className="col-12 content">
+						<div className="row content">
+							<div className="col-12">
 									<h1>
 										<TransitionGroup className="landing-header" component='span'>
 										<CSSTransition
@@ -72,142 +88,254 @@ class Landing extends React.Component {
 										&nbsp;use PubPub to make their research stronger.
 									</h1>
 								<p className="subtitle">Collaboratively research, draft, review, and publish in a single, integrated process – as it should be!</p>
-								<button className="pt-button pt-intent-primary pt-large">Join PubPub</button>
-								<button className="pt-button pt-large">Create your Community</button>
-								<img src="/static/hero.png" />
+								<div className="buttons">
+									<button className="pt-button pt-intent-primary pt-large">Join PubPub</button>
+									<button className="pt-button pt-large">Create your Community</button>
+								</div>
+								<img src="/static/hero.png" className="hero" />
 							</div>
 						</div>
 					</div>
+					{/* END Jumbotron Content */}
 				</div>
-				<div className="bg0">
-					<div className="container">
-						<div className="row">
-							<div className="col-12">
-								<h2>One PubPub, Any Community</h2>
-								{/* Journals */}
-								<div className="type-block">
-									<div className="content">
-										<div className="title">For Journals</div>
-										<div className="description">Host and publish your entire journal on PubPub. Accept submissions, manage peer review, publish to the web with one-click DOI generation, export to print, and more.</div>
-										<button className="pt-button">Create your Journal</button>
+				{/* END Jumbotron */}
+				{/* BEGIN Main content */ }
+				<div id="main">
+					{/* BEGIN Community Block */}
+					<div className="bg" id="communities">
+						<div className="container">
+							<div className="row">
+								<div className="col-12">
+									<p className="divider">
+										<Icon icon="people" iconSize={20} />
+									</p>
+									<h2>Start Your Community</h2>
+									{/* Books */}
+									<div className="type-block">
+										<div className="content">
+											<div className="title">For Journals</div>
+											<p className="description">Host and publish your entire journal on PubPub. Accept submissions, manage peer review, publish to the web with one-click DOI generation, export to print, and more.</p>
+											<div className="buttons">
+												<button className="pt-button">Create your Journal</button>
+											</div>
+										</div>
+										<div className="image pt-elevation-3"><img src="/static/jods.png" /></div>
 									</div>
-									<div className="image pt-elevation-3"><img src="/static/jods.png" /></div>
-								</div>
 
-								{/* Books */}
-								<div className="type-block flipped">
-									<div className="content">
-										<div className="title">For Book Publishers</div>
-										<div className="description">Publish beautiful digital editions of your books on PubPub. Embed bonus multimedia content, include interactive discussions and annotations, and use analytics to make smarter publishing decisions.</div>
-										<button className="pt-button">Create your Book</button>
+									{/* Books */}
+									<div className="type-block flipped">
+										<div className="content">
+											<div className="title">For Book Publishers</div>
+											<p className="description">Publish beautiful digital editions of your books on PubPub. Embed bonus multimedia content, include interactive discussions and annotations, and use analytics to make smarter publishing decisions.</p>
+											<div className="buttons">
+												<button className="pt-button">Create your Book</button>
+											</div>
+										</div>
+										<div className="image pt-elevation-3"><img src="/static/jods.png" /></div>
 									</div>
-									<div className="image"></div>
-								</div>
 
-								{/* Research Labs */}
-								<div className="type-block">
-									<div className="content">
-										<div className="title">For Research Labs</div>
-										<div className="description">Host your lab’s online presence on PubPub. Publicly post papers and citations, publish a lab blog, host resources, and use private channels for lab notes and journal clubs.</div>
-										<button className="pt-button">Create your Lab Site</button>
+									{/* Research Labs */}
+									<div className="type-block">
+										<div className="content">
+											<div className="title">For Research Labs</div>
+											<p className="description">Host your lab’s online presence on PubPub. Publicly post papers and citations, publish a lab blog, host resources, and use private channels for lab notes and journal clubs.</p>
+											<div className="buttons">
+												<button className="pt-button">Create your Lab Site</button>
+											</div>
+										</div>
+										<div className="image pt-elevation-3"><img src="/static/jods.png" /></div>
 									</div>
-									<div className="image"></div>
-								</div>
 
-								{/* Conferences */}
-								<div className="type-block flipped">
-									<div className="content">
-										<div className="title">For Conferences</div>
-										<div className="description">Host your interactive conference website on PubPub. Solicit and review submissions, publish accepted papers, post blogs and recaps, and encourage private and public conversation before and after your gathering.</div>
-										<button className="pt-button">Create your Conference</button>
+									{/* Conferences */}
+									<div className="type-block flipped">
+										<div className="content">
+											<div className="title">For Conferences</div>
+											<p className="description">Host your interactive conference website on PubPub. Solicit and review submissions, publish accepted papers, post blogs and recaps, and encourage private and public conversation before and after your gathering.</p>
+											<div className="buttons">
+												<button className="pt-button">Create your Conference</button>
+											</div>
+										</div>
+										<div className="image pt-elevation-3"><img src="/static/jods.png" /></div>
 									</div>
-									<div className="image"></div>
+									{/*<div className="type-block centered">
+										<div className="content">
+											<h2>For You!</h2>
+											<p className="description">We designed PubPub with flexibility in mind. Use every feature, or just the ones you need. If you’re a company or community that needs to collaboratively draft, review, edit and publish documents, we want to support you.</p>
+											<div className="buttons">
+												<button className="pt-button pt-intent-primary">Get in touch</button>
+												<button className="pt-button">Explore the possibilities</button>
+											</div>
+										</div>
+								</div>*/}
 								</div>
 							</div>
 						</div>
 					</div>
-
-				</div>
-				<div className="bg1">
-					<div className="container">
-						<div className="row">
-							<div className="col-12">
-								<h2>For You!</h2>
-								<p>We designed PubPub with flexibility in mind. Use every feature, or just the ones you need. If you’re a company or community that needs to collaboratively draft, review, edit and publish documents, we want to support you.</p>
-								<button className="pt-button">Get in touch</button>
-								<button className="pt-button">Explore the possibilities</button>
+					{/* END Community Block */}
+					{/* BEGIN For You Block
+					<div className="bg" id="foryou">
+						<div className="container">
+							<div className="row">
+								<div className="col-12">
+									<h2>For You!</h2>
+									<p>We designed PubPub with flexibility in mind. Use every feature, or just the ones you need. If you’re a company or community that needs to collaboratively draft, review, edit and publish documents, we want to support you.</p>
+									<div className="buttons">
+										<button className="pt-button pt-intent-primary">Get in touch</button>
+										<button className="pt-button">Explore the possibilities</button>
+									</div>
+								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-				<div className="bg2">
-					<div className="container">
-						<div className="row">
-							<div className="col-12">
-								<h2>Rich Features for Community Publishing</h2>
-								<p className="subtitle">Everything you need to write, review, publish, and discuss, all in one place.</p>
-								<div className="type-block">
-									<ul className="content">
-										<li><Icon icon="edit" /> Real-time, collaborative writing and editing</li>
-										<li><Icon icon="media" /> Rich media embeds like videos, photos, and tables</li>
-										<li><Icon icon="changes" /> Import and export MS Word, LaTex, XML, and more</li>
-										<li><Icon icon="citation" /> Add citations, footnotes, and figures</li>
-										<li><Icon icon="function" /> Embed LaTex equations</li>
-										<li><Icon icon="badge" /> Create DOIs in one click, for free</li>
-										<li><Icon icon="book" />Include chapters and sections</li>
-										<li><Icon icon="comment" /> Host public and private discussions and annotations</li>
-										<li><Icon icon="shield" /> Manage submissions and peer review</li>
-										<li><Icon icon="page-layout" /> Create beautiful, customizable, mobile-friendly page layouts</li>
-										<li><Icon icon="people" /> Allow anyone to access your community, or just people you choose</li>
-										<li><Icon icon="more" /> And much more. Full feature list coming soon.</li>
+					*/}
+					{/* END For You Block */}
+					{/* BEGIN O/S Block */}
+					<div className="bg" id="opensource">
+						<div className="container">
+							<div className="row">
+								<div className="col-12">
+									<div className="header">
+										<h2>
+											<Icon icon="endorsed" iconSize={28} /> Free
+										</h2>
+										<h2>
+											<Icon icon="endorsed" iconSize={28} /> Open Access
+										</h2>
+										<h2>
+											<Icon icon="endorsed" iconSize={28} /> Open Source
+										</h2>
+										<h2>
+											<Icon icon="endorsed" iconSize={28} /> Sustainable
+										</h2>
+									</div>
+									<div className="content">
+										<p>A member of MIT’s <a href="https://mitpress.mit.edu/kfg" target="_blank">Knowledge Futures Group</a>, we are committed to providing PubPub for free forever, releasing open-source code, and operating under non-profit, sustainable, researcher-friendly business models.</p>
+										<div className="pubpub-links">
+											<a><Icon icon="git-repo" /> pubpub</a>
+											<a><Icon icon="git-repo" /> pubpub-editor</a>
+											<a>review-maps</a>
+										</div>
+										<div className="buttons">
+											<button className="pt-button">Our Mission</button>
+										</div>
+								</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					{/* END O/S Block */}
+					{/* BEGIN Features Block */}
+					<div className="bg" id="features">
+						<div className="container">
+							<div className="row">
+								<div className="col-12">
+									<h2>Rich Features for Community Publishing</h2>
+									<p className="subtitle">Everything you need to write, review, publish, and discuss, all in one place.</p>
+									<ul className="feature-list">
+										<FeatureItem
+											icon={'edit'}
+											title={'Collaborative Editor'}
+											desc={'Draft with colleagues around the world in real-time.'}
+											/>
+										<FeatureItem
+											icon={'media'}
+											title={'Rich Media Embeds'}
+											desc={'Add videos, photos, tables, and interactives to your publications.'}
+											/>
+										<FeatureItem
+												icon={'changes'}
+												title={'Import & Export'}
+												desc={'Support for MS Word, LaTex, XML, ePub, and more.'}
+												/>
+										<FeatureItem
+												icon={'citation'}
+												title={'Citation Management'}
+												desc={'Easily add and manage citations, footnotes, and figure notes.'}
+												/>
+										<FeatureItem
+											icon={'function'}
+											title={'Equation Editor'}
+											desc={'Embed and edit LaTex equations right in your documents.'}
+											/>
+										<FeatureItem
+											icon={'badge'}
+											title={'DOI Support'}
+											desc={'Generate CrossRef DOIs for your documents in one click, for free.'}
+											/>
+										<FeatureItem
+											icon={'book'}
+											title={'Chapters & Sections'}
+											desc={'Break long documents into browsable chapters and sections – perfect for books and dissertations.'}
+											/>
+										<FeatureItem
+											icon={'comment'}
+											title={'Discussions & Annotations'}
+											desc={'Host public and private discussions with your readers and community.'}
+											/>
+											<FeatureItem
+												icon={'shield'}
+												title={'Submissions & Review'}
+												desc={'Manage submissions and peer review right from within PubPub.'}
+												/>
+											<FeatureItem
+												icon={'page-layout'}
+												title={'Beautiful Layouts'}
+												desc={'Host your entire site on PubPub with customizable, mobile-friendly page layouts.'}
+												/>
+											<FeatureItem
+												icon={'people'}
+												title={'Access Levels'}
+												desc={'Allow anyone to access your community, or just the people you choose.'}
+												/>
+											<FeatureItem
+												icon={'lightbulb'}
+												title={'More To Come'}
+												desc={'We\'re constantly adding new features, and love hearing ideas from users like you.'}
+												/>
 									</ul>
 									<div className="image"></div>
 								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-				<div className="bg1">
-					<div className="container">
-						<div className="row">
-							<div className="col-12">
-								<h2>Free, Open Access, and Open Source</h2>
-								<p>A member of MIT’s <a href="https://mitpress.mit.edu/kfg" target="_blank">Knowledge Futures Group</a>, we are committed to providing PubPub for free forever, releasing open-source code, and operating under non-profit, researcher-friendly business models. We make money from donations and by charging for enterprise setup and features, not by charging for access or running ads.</p>
-								<div>
-									<button className="pt-button">PubPub Mission</button>
-								</div>
-								<div className="pubpub-links">
-									<a><Icon icon="git-repo" /> pubpub</a>
-									<a><Icon icon="git-repo" /> pubpub-editor</a>
-									<a>review-maps</a>
+					{/* END Features Block */}
+					{/* BEGIN For Your */}
+					<div className="bg" id="foryou">
+						<h2>Built For You</h2>
+						<p className="description">We designed PubPub with flexibility in mind. Use every feature, or just the ones you need. If you’re an individual, company, or community that needs to collaboratively draft, review, edit and publish documents, we want to support you.</p>
+						<div className="buttons">
+							<button className="pt-button pt-intent-primary">Get in touch</button>
+							<button className="pt-button">Explore the possibilities</button>
+						</div>
+					</div>
+					{/* END For You */}
+					{/* BEGIN Friends Block
+					<div className="bg" id="friends">
+						<div className="container">
+							<div className="row bg2">
+								<div className="col-12">
+									<h2>Built with a growing, open communtiy</h2>
+									<ul>
+										<li>Travis</li>
+										<li>Catherine</li>
+										<li>Gabe</li>
+										<li>SJ</li>
+										<li>Amy Brand</li>
+										<li>Joi Ito</li>
+										<li>Jess Polka</li>
+										<li>Andy Lippman</li>
+										<li>Terry</li>
+										<li>Joel</li>
+										<li>Ed Finn</li>
+										<li>Thomas</li>
+									</ul>
 								</div>
 							</div>
 						</div>
 					</div>
+					 Friends Block */}
 				</div>
-				<div className="bg2">
-					<div className="container">
-						<div className="row">
-							<div className="col-12">
-								<h2>Built with a growing, open communtiy</h2>
-								<ul>
-									<li>Travis</li>
-									<li>Catherine</li>
-									<li>Gabe</li>
-									<li>SJ</li>
-									<li>Amy Brand</li>
-									<li>Joi Ito</li>
-									<li>Jess Polka</li>
-									<li>Andy Lippman</li>
-									<li>Terry</li>
-									<li>Joel</li>
-									<li>Ed Finn</li>
-									<li>Thomas</li>
-								</ul>
-							</div>
-						</div>
-					</div>
-				</div>
+				{/* END Main content */}
 
 				<div className="footer">
 					<div className="container">
