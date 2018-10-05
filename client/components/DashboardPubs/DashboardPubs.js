b72b31ea6f802691aa5f67f0cc00989e595da8a1
Travis Rich <travis.s.rich@gmail.com>
2018-10-05 18:30:35 -0400

    update: Dashboard pubs layout

--- client/components/DashboardPubs/DashboardPubs.js
+++ client/components/DashboardPubs/DashboardPubs.js
@@ -25,15 +25,50 @@ class DashboardPubs extends Component {
 				}).map((pub)=> {
 					return (
 						<div key={`pub-${pub.id}`} className="pub-wrapper">
-							<div className="title">
-								<a href={`/pub/${pub.slug}`}>{pub.title}</a>
+							<div className="header">
+								<div className="title">
+									<a href={`/pub/${pub.slug}`}>{pub.title}</a>
+									{!!pub.versions.length && (pub.isDraftEditor || pub.isDraftViewer || pub.isManager) &&
+										<a className="draft" href={`/pub/${pub.slug}/draft`}>Go To Working Draft</a>
+									}
+								</div>
+								<div>
+									{pub.pubTags.map((pubTag)=> {
+										return <span className="pt-tag pt-minimal pt-small">{pubTag.tag.title}</span>;
+									})}
+								</div>
+								{!pub.versions.length &&
+									<div>Working Draft</div>
+								}
+								{!!pub.versions.length &&
+									<div>{pub.versions.length} Saved Version{pub.versions.length === 1 ? '' : 's'}</div>
+								}
 							</div>
-							<div>
-								{pub.pubTags.map((pubTag)=> {
-									return <span className="pt-tag pt-minimal pt-small">{pubTag.tag.title}</span>;
+							<div className="authors">
+								<span>by </span>
+								{pub.attributions.filter((attribution)=> {
+									return attribution.isAuthor;
+								}).sort((foo, bar)=> {
+									if (foo.order < bar.order) { return -1; }
+									if (foo.order > bar.order) { return 1; }
+									if (foo.createdAt < bar.createdAt) { return 1; }
+									if (foo.createdAt > bar.createdAt) { return -1; }
+									return 0;
+								}).map((attribution, index, array)=> {
+									const separator = index === array.length - 1 || array.length === 2 ? '' : ', ';
+									const prefix = index === array.length - 1 && index !== 0 ? ' and ' : '';
+									if (attribution.user.slug) {
+										return (
+											<span key={`author-${attribution.id}`}>
+												{prefix}
+												<a href={`/user/${attribution.user.slug}`}>{attribution.user.fullName}</a>
+												{separator}
+											</span>
+										);
+									}
+									return <span key={`author-${attribution.id}`}>{prefix}{attribution.user.fullName}{separator}</span>;
 								})}
 							</div>
-							<div>{pub.versions.length} versions ·</div>
 						</div>
 					);
 				})}
