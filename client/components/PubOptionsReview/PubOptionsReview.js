e711d38982d55b39d65e5c9bacdbe0c50a9ba843
Travis Rich <travis.s.rich@gmail.com>
2018-10-08 15:29:33 -0400

    dev: Add placeholder select to reviews

--- client/components/PubOptionsReview/PubOptionsReview.js
+++ client/components/PubOptionsReview/PubOptionsReview.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Button } from '@blueprintjs/core';
+import { Select } from '@blueprintjs/select';
 import { apiFetch } from 'utilities';
 
 require('./pubOptionsReview.scss');
@@ -77,6 +78,28 @@ class PubOptionsReview extends Component {
 			<div className="pub-options-review-component">
 				<h1>Review</h1>
 
+				<Select
+					items={this.props.pubData.versions}
+					filterable={false}
+					itemRenderer={(item, { handleClick, modifiers })=> {
+						return (
+							<button
+								type="button"
+								tabIndex={-1}
+								onClick={handleClick}
+								className={modifiers.active ? 'pt-menu-item pt-active' : 'pt-menu-item'}
+							>
+								{item.id}
+							</button>
+						);
+					}}
+					onItemSelect={(item)=> {
+						console.log(item);
+					}}
+					popoverProps={{ popoverClassName: 'pt-minimal' }}
+				>
+					<Button text="Select a Version" rightIcon="caret-down" />
+				</Select>
 				<Button
 					className={review.status === 'none' ? 'pt-active' : ''}
 					text="None"
