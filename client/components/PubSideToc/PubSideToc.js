a791bd4088c19abf1cfc5d9f309edccc67be1cb3
Travis Rich <travis.s.rich@gmail.com>
2018-10-09 12:28:56 -0400

    fix: Pub side options z index issues

--- client/components/PubSideToc/PubSideToc.js
+++ client/components/PubSideToc/PubSideToc.js
@@ -45,29 +45,31 @@ const PubSideToc = function(props) {
 
 	return (
 		<div className="pub-side-toc-component">
-			{headings.map((item)=> {
-				return (
-					<a
-						href={`#${item.href}`}
-						className={`subsection-${item.level - 1}`}
-						key={`subsection-${item.index}`}
-						onClick={(evt)=> {
-							/* In the working draft, don't use anchor tags for nav since we have */
-							/* a fixed header bar. Plus, the URL with an anchor tag will behave */
-							/* unexpectedly on reload given the async loading of doc. Instead, */
-							/* manually scroll to the position and offset by fixed header height. */
-							if (props.pubData.isDraft) {
-								evt.preventDefault();
-								document.getElementById(item.href).scrollIntoView();
-								const currentTop = document.body.scrollTop || document.documentElement.scrollTop;
-								document.body.scrollTop = document.documentElement.scrollTop = currentTop - 75;
-							}
-						}}
-					>
-						{item.title}
-					</a>
-				);
-			})}
+			<div className="toc">
+				{headings.map((item)=> {
+					return (
+						<a
+							href={`#${item.href}`}
+							className={`subsection-${item.level - 1}`}
+							key={`subsection-${item.index}`}
+							onClick={(evt)=> {
+								/* In the working draft, don't use anchor tags for nav since we have */
+								/* a fixed header bar. Plus, the URL with an anchor tag will behave */
+								/* unexpectedly on reload given the async loading of doc. Instead, */
+								/* manually scroll to the position and offset by fixed header height. */
+								if (props.pubData.isDraft) {
+									evt.preventDefault();
+									document.getElementById(item.href).scrollIntoView();
+									const currentTop = document.body.scrollTop || document.documentElement.scrollTop;
+									document.body.scrollTop = document.documentElement.scrollTop = currentTop - 75;
+								}
+							}}
+						>
+							{item.title}
+						</a>
+					);
+				})}
+			</div>
 		</div>
 	);
 };
