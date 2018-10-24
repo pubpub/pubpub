72673b3f3cf98b37943b5470b4d4edebe8eed229
Travis Rich <travis.s.rich@gmail.com>
2018-10-24 14:20:22 -0400

    fix: Add Object.values to polyfill for Safari 10.2

--- server/Html.js
+++ server/Html.js
@@ -47,7 +47,7 @@ const Html = (props) => {
 				<div id="root">
 					{props.children}
 				</div>
-				<script src="https://cdn.polyfill.io/v2/polyfill.min.js?features=fetch,default,HTMLCanvasElement.prototype.toBlob,Object.entries" />
+				<script src="https://cdn.polyfill.io/v2/polyfill.min.js?features=fetch,default,HTMLCanvasElement.prototype.toBlob,Object.entries,Object.values" />
 				<script id="initial-data" type="text/plain" data-json={JSON.stringify(props.initialData)} />
 				<script src={getPath('vendor', 'js')} />
 				<script src={getPath(props.chunkName, 'js')} />
