a9b11f919ecd42a21f7f8bcdde9ad8be7b8ccbc2
Travis Rich <travis.s.rich@gmail.com>
2018-10-26 15:29:13 -0400

    fix: discussionChannel filtering edge case

--- server/queryHelpers.js
+++ server/queryHelpers.js
@@ -118,17 +118,19 @@ export const formatAndAuthenticatePub = (pub, loginData, communityAdminData, req
 		}).filter((item)=> {
 			return !item.tag || item.tag.isPublic || communityAdminData;
 		}),
-		discussionChannels: pub.discussionChannels.filter((channel)=> {
-			const isPrivate = channel.permissions === 'private';
-			const isParticipant = channel.participants.reduce((prev, curr)=> {
-				if (curr.userId === loginData.id) { return true; }
-				return prev;
-			}, false);
-			const permissionAsAdmin = channel.isCommunityAdminModerated && loginData.isAdmin;
+		discussionChannels: pub.discussionChannels
+			? pub.discussionChannels.filter((channel)=> {
+				const isPrivate = channel.permissions === 'private';
+				const isParticipant = channel.participants.reduce((prev, curr)=> {
+					if (curr.userId === loginData.id) { return true; }
+					return prev;
+				}, false);
+				const permissionAsAdmin = channel.isCommunityAdminModerated && loginData.isAdmin;
 
-			if (!isPrivate || isParticipant || permissionAsAdmin) { return true; }
-			return false;
-		}),
+				if (!isPrivate || isParticipant || permissionAsAdmin) { return true; }
+				return false;
+			})
+			: undefined,
 		isManager: isManager,
 		isDraftEditor: isDraftEditor,
 		isDraftViewer: isDraftViewer,
