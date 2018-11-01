43991260a49d1ccbb5c878a3c202a56440c1424f
Travis Rich <travis.s.rich@gmail.com>
2018-11-01 18:24:30 -0400

    update: Page container layout

--- client/containers/Page/Page.js
+++ client/containers/Page/Page.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Button, NonIdealState } from '@blueprintjs/core';
 import PageWrapper from 'components/PageWrapper/PageWrapper';
 import LayoutPubs from 'components/LayoutPubs/LayoutPubs';
 import LayoutHtml from 'components/LayoutHtml/LayoutHtml';
 import LayoutBanner from 'components/LayoutBanner/LayoutBanner';
-// import LayoutCreatePub from 'components/LayoutCreatePub/LayoutCreatePub';
-// import LayoutDrafts from 'components/LayoutDrafts/LayoutDrafts';
 import LayoutText from 'components/LayoutText/LayoutText';
-import { hydrateWrapper, apiFetch, getDefaultLayout, generateRenderLists } from 'utilities';
+import { hydrateWrapper, getDefaultLayout, generateRenderLists } from 'utilities';
 
 require('./page.scss');
 
@@ -20,34 +17,6 @@ const propTypes = {
 };
 
 class Page extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			createPubIsLoading: false,
-		};
-		this.handleCreatePub = this.handleCreatePub.bind(this);
-	}
-
-	handleCreatePub() {
-		this.setState({ createPubIsLoading: true });
-		return apiFetch('/api/pubs', {
-			method: 'POST',
-			body: JSON.stringify({
-				collectionId: this.props.pageData.id,
-				communityId: this.props.communityData.id,
-				createPubHash: undefined,
-			})
-		})
-		.then((result)=> {
-			// this.setState({ createPubIsLoading: false });
-			window.location.href = result;
-		})
-		.catch((err)=> {
-			console.error(err);
-			this.setState({ createPubIsLoading: false });
-		});
-	}
-
 	render() {
 		const pageData = this.props.pageData;
 		const slug = this.props.locationData.params.slug;
@@ -57,18 +26,8 @@ class Page extends Component {
 			return prev;
 		}, undefined);
 		if (!title) { return <h1>Nothing</h1>; }
-		const publicDrafts = pageData.pubs.filter((item)=> {
-			return !item.firstPublishedAt;
-		}).sort((foo, bar)=> {
-			if (foo.updatedAt > bar.updatedAt) { return -1; }
-			if (foo.updatedAt < bar.updatedAt) { return 1; }
-			return 0;
-		});
+
 		const layout = pageData.layout || getDefaultLayout();
-		const hasTextLayoutComponent = layout.reduce((prev, curr)=> {
-			if (curr.type === 'text') { return true; }
-			return prev;
-		}, false);
 		const pubRenderLists = generateRenderLists(layout, this.props.pageData.pubs);
 		return (
 			<div id="page-container">
@@ -77,55 +36,6 @@ class Page extends Component {
 					communityData={this.props.communityData}
 					locationData={this.props.locationData}
 				>
-					{/* <div className="container create-pub-wrapper">
-							<div className="row">
-								<div className="col-12">
-									<Button
-										type="button"
-										className="pt-button pt-intent-primary"
-										loading={this.state.createPubIsLoading}
-										onClick={this.handleCreatePub}
-										text="Create Pub in Collection"
-									/>
-								</div>
-							</div>
-						</div>
-					*/}
-					{/* ((!pageData.isPage && pageData.isOpenSubmissions) || (title && title !== 'Home')) &&
-						<div className="row">
-							<div className="col-12">
-								{!pageData.isPage && pageData.isOpenSubmissions &&
-									<div className="create-pub-wrapper">
-										{this.props.loginData.id &&
-											<Button
-												type="button"
-												className="pt-button pt-intent-primary"
-												loading={this.state.createPubIsLoading}
-												onClick={this.handleCreatePub}
-												text="Create Pub in Collection"
-											/>
-										}
-										{!this.props.loginData.id &&
-											<a
-												href={`/login?redirect=${this.props.locationData.path}`}
-												className="pt-button pt-intent-primary"
-											>
-												Login to Create Pub
-											</a>
-										}
-										{pageData.createPubMessage &&
-											<a href={`/${pageData.slug}/submit`} className="instructions-link">
-												Submission Instructions
-											</a>
-										}
-									</div>
-								}
-								{title && title !== 'Home' &&
-									<h1 className="collection-title">{title}</h1>
-								}
-							</div>
-						</div>
-					*/}
 					{layout.map((item, index)=> {
 						const validType = ['pubs', 'text', 'html', 'banner'].indexOf(item.type) > -1;
 						if (!validType) { return null; }
@@ -160,26 +70,9 @@ class Page extends Component {
 										locationData={this.props.locationData}
 									/>
 								}
-								{/* item.type === 'createPub' &&
-									<LayoutCreatePub
-										key={`item-${item.id}`}
-										content={item.content}
-										communityData={this.props.communityData}
-										loginData={this.props.loginData}
-										locationData={this.props.locationData}
-									/>
-								*/}
 							</div>
 						);
 					})}
-
-					{!publicDrafts.length && !!pageData.id && !pageData.isPage && !hasTextLayoutComponent &&
-						<NonIdealState
-							title="Empty Collection"
-							description="This collection has no Pubs."
-							visual="pt-icon-applications"
-						/>
-					}
 				</PageWrapper>
 			</div>
 		);
