135f96af53dcfacc5d61399b433e22528aa90b1a
Gabriel Stein <g@gabestein.com>
2018-11-12 12:40:47 -0500

    update: Cleanup footer and make subscribe form more interactive.

--- server/apiRoutes/subscribe.js
+++ server/apiRoutes/subscribe.js
@@ -6,7 +6,7 @@ app.post('/api/subscribe', (req, res)=> {
 	const list = req.body.list || '2847d5271c';
 	subscribeUser(email, list)
 	.then(() => {
-		return res.status(200).json();
+		return res.status(200).json(true);
 	})
 	.catch((err) => {
 		return res.status(500).json(err.message);
