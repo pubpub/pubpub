d01228130f9b20e8cf754b48ac4b0d8047899109
Gabriel Stein <g@gabestein.com>
2018-12-06 17:02:57 -0500

    update: Return 404 if user not logged in / admin.

--- server/clientRoutes/adminDashboard.js
+++ server/clientRoutes/adminDashboard.js
@@ -8,6 +8,10 @@ app.get('/admin', (req, res, next)=> {
 	if (!hostIsValid(req, 'pubpub')) { return next(); }
 	return getInitialData(req)
 	.then((initialData)=> {
+		const user = req.user || {};
+		const users = ['b242f616-7aaa-479c-8ee5-3933dcf70859', '5d9d63b3-6990-407c-81fb-5f87b9d3e360',
+			'807f3604-4223-4495-b576-861d04d2f39e', '237fe275-0618-4a8f-bd40-ea9065836e67'];
+		if (!users.includes(user.id)) { throw new Error('Page Not Found'); }
 		return renderToNodeStream(res,
 			<Html
 				chunkName="AdminDashboard"
