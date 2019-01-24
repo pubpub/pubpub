646b065e9d941404991acfde4b833c8e641af693
Travis Rich <travis.s.rich@gmail.com>
2019-01-23 22:22:56 -0500

    fix: Rotate AWS keys

--- client/utilities.js
+++ client/utilities.js
@@ -328,7 +328,7 @@ export function s3Upload(file, progressEvent, finishEvent, index) {
 		const formData = new FormData();
 
 		formData.append('key', filename);
-		formData.append('AWSAccessKeyId', 'AKIAJQ5MNLCTIMY2ZF7Q');
+		formData.append('AWSAccessKeyId', 'AKIAIUXFM6YJQGAV7GRQ');
 		formData.append('acl', 'public-read');
 		formData.append('policy', JSON.parse(this.responseText).policy);
 		formData.append('signature', JSON.parse(this.responseText).signature);
