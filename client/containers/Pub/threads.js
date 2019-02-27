3593703614f3b448291e8432fdd3536e95ba5f6a
Ian Reynolds <idreyn@gmail.com>
2019-02-27 14:06:08 -0800

    moving back to utilities for now

--- client/containers/Pub/threads.js
+++ client/containers/Pub/threads.js
@@ -1,32 +1,4 @@
-const nestDiscussionsToThreads = (discussions) => {
-	const maxThreadNumber = discussions.reduce((prev, curr) => {
-		if (curr.threadNumber > prev) {
-			return curr.threadNumber;
-		}
-		return prev;
-	}, 0);
-
-	const tempThreads = new Array(maxThreadNumber).fill([]);
-	discussions.forEach((item) => {
-		tempThreads[item.threadNumber - 1] = [...tempThreads[item.threadNumber - 1], item];
-	});
-
-	return tempThreads
-		.filter((thread) => {
-			return thread.length;
-		})
-		.map((thread) => {
-			return thread.sort((foo, bar) => {
-				if (foo.createdAt > bar.createdAt) {
-					return 1;
-				}
-				if (foo.createdAt < bar.createdAt) {
-					return -1;
-				}
-				return 0;
-			});
-		});
-};
+import { nestDiscussionsToThreads } from 'utilities';
 
 export const getActiveDiscussionChannel = (pubData, locationData) => {
 	return pubData.discussionChannels.reduce((prev, curr) => {
--- client/utilities.js
+++ client/utilities.js
@@ -136,6 +136,36 @@ export const getResizedUrl = function(url, type, dimensions) {
 	return `https://resize.pubpub.org/${prefix}${dimensions}/${filepath}`;
 };
 
+export const nestDiscussionsToThreads = (discussions) => {
+	const maxThreadNumber = discussions.reduce((prev, curr) => {
+		if (curr.threadNumber > prev) {
+			return curr.threadNumber;
+		}
+		return prev;
+	}, 0);
+
+	const tempThreads = new Array(maxThreadNumber).fill([]);
+	discussions.forEach((item) => {
+		tempThreads[item.threadNumber - 1] = [...tempThreads[item.threadNumber - 1], item];
+	});
+
+	return tempThreads
+		.filter((thread) => {
+			return thread.length;
+		})
+		.map((thread) => {
+			return thread.sort((foo, bar) => {
+				if (foo.createdAt > bar.createdAt) {
+					return 1;
+				}
+				if (foo.createdAt < bar.createdAt) {
+					return -1;
+				}
+				return 0;
+			});
+		});
+};
+
 export const populateNavigationIds = function(collections, navigation) {
 	const collectionsObject = {};
 	collections.forEach((item) => {
