b2858ca29d5b549dab51e63f97760a87cf3212fa
Travis Rich <travis.s.rich@gmail.com>
2019-03-01 23:51:25 -0500

    fix: PubPub non-landing css header fix

--- client/components/Header/Header.js
+++ client/components/Header/Header.js
@@ -65,17 +65,19 @@ class Header extends Component {
 		let dynamicComponentClasses = '';
 
 		const isLanding = this.props.locationData.path === '/';
+		const isBasePubPub = this.props.locationData.isBasePubPub;
 		const backgroundColorChange =
 			this.props.communityData.accentColor !== this.props.communityData.heroBackgroundColor;
 		const textColorChange =
 			this.props.communityData.accentTextColor !== this.props.communityData.heroTextColor;
-		if ((!this.props.locationData.isBasePubPub && !backgroundColorChange) || !isLanding) {
+
+		if ((!isBasePubPub && !backgroundColorChange) || !isLanding) {
 			dynamicComponentClasses += ' accent-background';
 		}
-		if ((!this.props.locationData.isBasePubPub && !textColorChange) || !isLanding) {
+		if ((!isBasePubPub && !textColorChange) || (!isBasePubPub && !isLanding)) {
 			dynamicComponentClasses += ' accent-color';
 		}
-		if (this.props.locationData.isBasePubPub && this.props.locationData.path === '/') {
+		if (isBasePubPub && this.props.locationData.path === '/') {
 			dynamicComponentClasses += ' bp3-dark';
 		}
 		if (hideHero) {
