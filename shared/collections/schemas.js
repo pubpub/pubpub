e153f1963287f9b3b03d29c715d8868d1f4bcfdd
Ian Reynolds <idreyn@gmail.com>
2019-03-26 18:16:09 -0700

    pull in all collection schemas

--- shared/collections/schemas.js
+++ shared/collections/schemas.js
@@ -1,6 +1,26 @@
 import { communityUrl } from 'shared/util/canonicalUrls';
 
+const sharedFields = {
+	doi: {
+		name: 'doi',
+		label: 'DOI',
+		defaultDerivedFrom: ({ collection }) => collection && collection.doi,
+		disabled: true,
+	},
+	url: {
+		name: 'url',
+		label: 'URL',
+		defaultDerivedFrom: ({ community }) => community && communityUrl(community),
+	},
+};
+
 const schemas = [
+	{
+		kind: 'tag',
+		label: { singular: 'tag', plural: 'tags' },
+		bpDisplayIcon: 'tag',
+		metadata: [],
+	},
 	{
 		kind: 'book',
 		label: { singular: 'book', plural: 'books' },
@@ -11,27 +31,42 @@ const schemas = [
 			{ value: 'chapter', label: 'Chapter', default: true },
 		],
 		metadata: [
-			{
-				name: 'doi',
-				label: 'DOI',
-				defaultDerivedFrom: ({ collection }) => collection && collection.doi,
-				disabled: true,
-			},
-			{
-				name: 'url',
-				label: 'URL',
-				defaultDerivedFrom: ({ community }) => community && communityUrl(community),
-			},
+			sharedFields.doi,
+			sharedFields.url,
 			{ name: 'isbn', label: 'ISBN' },
 			{ name: 'copyrightYear', label: 'Copyright year', pattern: '^[0-9]*$' },
 			{ name: 'edition', label: 'Edition no.', pattern: '^[0-9]*$' },
 		],
 	},
 	{
-		kind: 'tag',
-		label: { singular: 'tag', plural: 'tags' },
-		bpDisplayIcon: 'tag',
-		metadata: [],
+		kind: 'issue',
+		label: { singular: 'issue', plural: 'issues' },
+		bpDisplayIcon: 'manual',
+		contextHints: [{ value: 'article', label: 'Article', default: true }],
+		metadata: [
+			sharedFields.doi,
+			sharedFields.url,
+			{ name: 'printIssn', label: 'Print ISSN' },
+			{ name: 'electronicIssn', label: 'Electronic ISSN' },
+			{ name: 'volume', label: 'Volume' },
+			{ name: 'issue', label: 'Issue' },
+			{ name: 'printPublicationDate', label: 'Print publication date' },
+			{ name: 'electronicPublicationDate', label: 'Electronic publication date' },
+		],
+	},
+	{
+		kind: 'conference',
+		label: { singular: 'conference', plural: 'conferences' },
+		bpDisplayIcon: 'presentation',
+		contextHints: [{ value: 'paper', label: 'Paper', default: true }],
+		metadata: [
+			sharedFields.doi,
+			sharedFields.url,
+			{ name: 'theme', label: 'Theme' },
+			{ name: 'acronym', label: 'Acronym' },
+			{ name: 'location', label: 'Location' },
+			{ name: 'date', label: 'Date' },
+		],
 	},
 ];
 
