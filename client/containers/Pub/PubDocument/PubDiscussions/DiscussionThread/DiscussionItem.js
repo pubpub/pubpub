34f4c3139b1392b1cd4fe4dd444573a83b00f9af
Travis Rich <travis.s.rich@gmail.com>
2019-04-24 10:30:33 -0400

    update: Editable discussions functional

--- client/containers/Pub/PubDocument/PubDiscussions/DiscussionThread/DiscussionItem.js
+++ client/containers/Pub/PubDocument/PubDiscussions/DiscussionThread/DiscussionItem.js
@@ -1,19 +1,60 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import PropTypes from 'prop-types';
 import TimeAgo from 'react-timeago';
-import Editor from '@pubpub/editor';
+import classNames from 'classnames';
+import Editor, { getText, getJSON } from '@pubpub/editor';
 import { Button, Intent } from '@blueprintjs/core';
 import { PageContext } from 'components/PageWrapper/PageWrapper';
 import { Avatar, Icon } from 'components';
+import { apiFetch } from 'utils';
 
 const propTypes = {
 	discussionData: PropTypes.object.isRequired,
+	pubData: PropTypes.object.isRequired,
 	updateLocalData: PropTypes.func.isRequired,
 };
 
 const DiscussionItem = (props) => {
-	const { discussionData } = props;
+	const { discussionData, pubData, updateLocalData } = props;
 	const { loginData, communityData } = useContext(PageContext);
+	const [isEditing, setIsEditing] = useState(false);
+	const [changeObject, setChangeObject] = useState({});
+	const [isLoading, setIsLoading] = useState(false);
+
+	const handlePutDiscussion = () => {
+		setIsLoading(true);
+		return apiFetch('/api/discussions', {
+			method: 'PUT',
+			body: JSON.stringify({
+				discussionId: discussionData.id,
+				userId: loginData.id,
+				pubId: pubData.id,
+				branchId: pubData.activeBranch.id,
+				communityId: communityData.id,
+				content: getJSON(changeObject.view),
+				text: getText(changeObject.view) || '',
+			}),
+		})
+			.then((updatedDiscussionData) => {
+				updateLocalData('pub', {
+					...pubData,
+					discussions: pubData.discussions.map((discussion) => {
+						if (discussion.id !== discussionData.id) {
+							return discussion;
+						}
+						return {
+							...discussion,
+							...updatedDiscussionData,
+						};
+					}),
+				});
+			})
+			.then(() => {
+				setIsEditing(false);
+				setChangeObject({});
+				setIsLoading(false);
+			});
+	};
 
 	return (
 		<div className="discussion-item">
@@ -46,22 +87,47 @@ const DiscussionItem = (props) => {
 							<span> (edited)</span>
 						)}
 					</span>
-					<span className="actions">
-						<Button
-							icon={<Icon icon="edit2" iconSize={14} />}
-							minimal={true}
-							small={true}
-						/>
-						<Button
-							icon={<Icon icon="flag" iconSize={14} />}
-							minimal={true}
-							small={true}
-						/>
-					</span>
+					{loginData.id === discussionData.userId && (
+						<span className="actions">
+							<Button
+								icon={isEditing ? undefined : <Icon icon="edit2" iconSize={14} />}
+								text={isEditing ? 'Cancel Edit' : undefined}
+								minimal={true}
+								small={true}
+								onClick={() => {
+									setIsEditing(!isEditing);
+								}}
+							/>
+						</span>
+					)}
 				</div>
-				<div className="discussion-body-wrapper">
-					<Editor isReadOnly={true} initialContent={discussionData.content} />
+				<div
+					className={classNames({
+						'discussion-body-wrapper': true,
+						editable: isEditing,
+					})}
+				>
+					<Editor
+						key={`${isEditing}-${discussionData.text}`}
+						isReadOnly={!isEditing}
+						initialContent={discussionData.content}
+						onChange={(editorChangeObject) => {
+							if (isEditing) {
+								setChangeObject(editorChangeObject);
+							}
+						}}
+					/>
 				</div>
+				{isEditing && (
+					<Button
+						className="discussion-primary-button"
+						intent={Intent.PRIMARY}
+						text="Update Discussion"
+						loading={isLoading}
+						disabled={!getText(changeObject.view)}
+						onClick={handlePutDiscussion}
+					/>
+				)}
 			</div>
 		</div>
 	);
