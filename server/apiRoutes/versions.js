a19ec149de14e5153ed98e22a4bc00d4bb2dd7bc
Ian Reynolds <idreyn@gmail.com>
2019-05-20 14:10:34 -0400

    update: Don't submit DOI data on version creation

--- server/apiRoutes/versions.js
+++ server/apiRoutes/versions.js
@@ -69,14 +69,7 @@ app.post('/api/versions', (req, res) => {
 					},
 				},
 			);
-			const updateDoiData = setDoiData(
-				{
-					pubId: req.body.pubId,
-					communityId: req.body.communityId,
-				},
-				'pub',
-			);
-			return Promise.all([updateDiscussion, updateDoiData]);
+			return updateDiscussion;
 		})
 		.then(() => {
 			setPubSearchData(req.body.pubId);
