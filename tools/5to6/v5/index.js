88d0a96b8403755b8884f09aa692da42e2bf61ae
Travis Rich <travis.s.rich@gmail.com>
2019-06-19 13:20:32 -0400

    dev: Add pub to blacklist for migration

--- tools/5to6/v5/index.js
+++ tools/5to6/v5/index.js
@@ -9,6 +9,7 @@ const getPipedPubIds = require('../util/getPipedPubIds');
 const blacklist = [
 	'0f21f44a-dc5e-4e8d-83b8-d1194e38d755', // Frankenbook
 	// '8f7503e4-6e51-4e13-8204-708ad04067ec', // Resisting Reduction
+	'c7212765-f563-4659-83a3-df8ef826663d', // Enlightened - go in and manually set this after migration
 ];
 
 const main = async () => {
