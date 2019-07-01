746344d444a0b00050d7a9bd3054f25c7eb365a7
Ian Reynolds <idreyn@gmail.com>
2019-07-01 10:59:01 -0400

    update/hack: enable MITP Crossref login

--- server/doi/handler.js
+++ server/doi/handler.js
@@ -89,8 +89,23 @@ const findCommunity = (communityId) =>
 		attributes: ['id', 'title', 'issn', 'domain', 'subdomain'],
 	});
 
-const submitDoiData = (json, timestamp) => {
-	const { DOI_SUBMISSION_URL, DOI_LOGIN_ID, DOI_LOGIN_PASSWORD } = process.env;
+const getDoiLogin = (communityId) => {
+	const {
+		DOI_LOGIN_ID,
+		DOI_LOGIN_PASSWORD,
+		MITP_DOI_LOGIN_ID,
+		MITP_DOI_LOGIN_PASSWORD,
+	} = process.env;
+	if (communityId === '99608f92-d70f-46c1-a72c-df272215f13e') {
+		// HDSR
+		return { login: MITP_DOI_LOGIN_ID, password: MITP_DOI_LOGIN_PASSWORD };
+	}
+	return { login: DOI_LOGIN_ID, password: DOI_LOGIN_PASSWORD };
+};
+
+const submitDoiData = (json, timestamp, communityId) => {
+	const { DOI_SUBMISSION_URL } = process.env;
+	const { login, password } = getDoiLogin(communityId);
 	const xmlObject = xmlbuilder.create(json, { headless: true }).end({ pretty: true });
 	const readStream = new Readable();
 	// eslint-disable-next-line no-underscore-dangle
@@ -102,8 +117,8 @@ const submitDoiData = (json, timestamp) => {
 		method: 'POST',
 		url: DOI_SUBMISSION_URL,
 		formData: {
-			login_id: DOI_LOGIN_ID,
-			login_passwd: DOI_LOGIN_PASSWORD,
+			login_id: login,
+			login_passwd: password,
 			fname: readStream,
 		},
 		headers: {
@@ -150,7 +165,7 @@ export const setDoiData = ({ communityId, collectionId, pubId }, doiTarget) =>
 		{ communityId: communityId, collectionId: collectionId, pubId: pubId },
 		doiTarget,
 	).then(({ deposit, timestamp, dois }) =>
-		submitDoiData(deposit, timestamp)
+		submitDoiData(deposit, timestamp, communityId)
 			.then(() => persistDoiData({ collectionId: collectionId, pubId: pubId }, dois))
 			.then(() => {
 				return { deposit: deposit, dois: dois };
