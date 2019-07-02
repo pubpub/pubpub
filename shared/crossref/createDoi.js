d43bbb275248752299416ad2e665d9d02a553d3c
Ian Reynolds <idreyn@gmail.com>
2019-07-01 20:44:02 -0400

    Revert "update: Use correct DOI prefix for MITP"
    This reverts commit 6f04bd7532c6c86ddfd7cced7988e8edc629940f.

--- shared/crossref/createDoi.js
+++ shared/crossref/createDoi.js
@@ -1,17 +1,8 @@
 const PUBPUB_DOI_PREFIX = '10.21428';
-const MITP_DOI_PREFIX = '10.1162';
 
 const withPubPubDoiPrefix = (component) => `${PUBPUB_DOI_PREFIX}/${component}`;
-const withMitpPrefix = (component) => `${MITP_DOI_PREFIX}/${component}`;
 
 const splitId = (item) => item.id.split('-')[0];
-
-const choosePrefixerForCommunity = (community) => {
-	if (community.id === '99608f92-d70f-46c1-a72c-df272215f13e') {
-		return withMitpPrefix;
-	}
-	return withPubPubDoiPrefix;
-};
 
 export const makeComponentId = (community, collection, pub) =>
 	[community && splitId(community), collection && splitId(collection), pub && splitId(pub)]
@@ -21,5 +12,5 @@ export const makeComponentId = (community, collection, pub) =>
 export default ({ community, target }) => {
 	const communityPart = splitId(community);
 	const targetPart = target ? `.${splitId(target)}` : '';
-	return choosePrefixerForCommunity(community)(communityPart + targetPart);
+	return withPubPubDoiPrefix(communityPart + targetPart);
 };
