d03cc13375e511285059197c0aa1184fdc6d86c6
Ian Reynolds <idreyn@gmail.com>
2019-07-01 20:43:53 -0400

    Revert "update/hack: enable MITP Crossref login"
    This reverts commit 746344d444a0b00050d7a9bd3054f25c7eb365a7.

--- server/doi/handler.js
+++ server/doi/handler.js
@@ -89,23 +89,8 @@ const findCommunity = (communityId) =>
 		attributes: ['id', 'title', 'issn', 'domain', 'subdomain'],
 	});
 
-const getDoiLogin = (communityId) => {
-	const {
-		DOI_LOGIN_ID,
-		DOI_LOGIN_PASSWORD,
-		MITP_DOI_LOGIN_ID,
-		MITP_DOI_LOGIN_PASSWORD,
-	} = process.env;
-	if (communityId === '99608f92-d70f-46c1-a72c-df272215f13e') {
-		// HDSR
-		return { login: MITP_DOI_LOGIN_ID, password: MITP_DOI_LOGIN_PASSWORD };
-	}
-	return { login: DOI_LOGIN_ID, password: DOI_LOGIN_PASSWORD };
-};
-
-const submitDoiData = (json, timestamp, communityId) => {
-	const { DOI_SUBMISSION_URL } = process.env;
-	const { login, password } = getDoiLogin(communityId);
+const submitDoiData = (json, timestamp) => {
+	const { DOI_SUBMISSION_URL, DOI_LOGIN_ID, DOI_LOGIN_PASSWORD } = process.env;
 	const xmlObject = xmlbuilder.create(json, { headless: true }).end({ pretty: true });
 	const readStream = new Readable();
 	// eslint-disable-next-line no-underscore-dangle
@@ -117,8 +102,8 @@ const submitDoiData = (json, timestamp, communityId) => {
 		method: 'POST',
 		url: DOI_SUBMISSION_URL,
 		formData: {
-			login_id: login,
-			login_passwd: password,
+			login_id: DOI_LOGIN_ID,
+			login_passwd: DOI_LOGIN_PASSWORD,
 			fname: readStream,
 		},
 		headers: {
@@ -165,7 +150,7 @@ export const setDoiData = ({ communityId, collectionId, pubId }, doiTarget) =>
 		{ communityId: communityId, collectionId: collectionId, pubId: pubId },
 		doiTarget,
 	).then(({ deposit, timestamp, dois }) =>
-		submitDoiData(deposit, timestamp, communityId)
+		submitDoiData(deposit, timestamp)
 			.then(() => persistDoiData({ collectionId: collectionId, pubId: pubId }, dois))
 			.then(() => {
 				return { deposit: deposit, dois: dois };
