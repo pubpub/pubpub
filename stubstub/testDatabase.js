0de332cb855cb099f0e4fbede9961f07e9fb5afd
Ian Reynolds <idreyn@gmail.com>
2019-08-02 19:26:19 -0400

    automatically start a Postgres server to run tests

---
+++ stubstub/testDatabase.js
@@ -0,0 +1,76 @@
+import { exec as execWithCallback, spawn } from 'child_process';
+import fs from 'fs';
+import hasbin from 'hasbin';
+
+const requiredBinaries = ['createdb', 'dropuser', 'dropdb', 'postgres', 'psql'];
+
+const pgDataPath = process.env.TEST_DB_PGSQL_PATH || '~/.pubpub-pgsql/data';
+
+const testDbConfig = {
+	username: 'testuser',
+	password: 'testpwd',
+	name: 'pubpub-testdb',
+};
+
+const exec = (command) =>
+	new Promise((resolve, reject) =>
+		execWithCallback(
+			command,
+			{
+				env: {
+					...process.env,
+					PGDATA: pgDataPath,
+				},
+			},
+			(err, res) => {
+				if (err) {
+					return reject(err);
+				}
+				return resolve(res);
+			},
+		),
+	);
+
+const createDbUrl = ({ name, username, password }) => {
+	return `postgres://${username}:${password}@localhost:5432/${name}`;
+};
+
+const throwBinariesWarning = () => {
+	console.warn(
+		'This script relies on these Postgres utilities to work: ' +
+			requiredBinaries.join(', ') +
+			". If you're using a Mac you might try 'brew install postgres'.",
+	);
+	throw new Error('Missing some binaries to handle test database');
+};
+
+export const initTestDatabase = async () => {
+	if (!fs.existsSync(pgDataPath)) {
+		await exec('initdb');
+	}
+};
+
+export const setupTestDatabase = async (config = testDbConfig) => {
+	if (!hasbin.all.sync(requiredBinaries)) {
+		throwBinariesWarning();
+	}
+	await exec(`dropdb --if-exists ${config.name}`);
+	await exec(`dropuser --if-exists ${config.username}`);
+	await exec(
+		`psql postgres -c "CREATE USER ${config.username} WITH PASSWORD '${config.password}';"`,
+	);
+	await exec(`createdb ${config.name} '--no-password' '--owner' ${config.username}`);
+	return createDbUrl(config);
+};
+
+export const startTestDatabaseServer = () => {
+	if (!hasbin.all.sync(requiredBinaries)) {
+		throwBinariesWarning();
+	}
+	return spawn('postgres', {
+		env: {
+			...process.env,
+			PGDATA: pgDataPath,
+		},
+	});
+};
