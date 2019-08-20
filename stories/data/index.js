785af14b4a4e2f2880394a5ea22510d46557c605
Ian Reynolds <idreyn@gmail.com>
2019-08-20 15:14:11 -0400

    everything for now

--- stories/data/index.js
+++ stories/data/index.js
@@ -4,4 +4,5 @@ export const { locationData, loginData } = initialData;
 export { default as communityData } from './dataCommunity.js';
 export { default as collectionData } from './dataCollection.js';
 export { default as pubData } from './dataPub.js';
+export { default as discussionsData } from './dataDiscussions.js'
 export { plainDoc, imageDoc, fullDoc } from './dataDocs.js';
