9a8bc044b7ca7a366392d662c01020d3ed2245f1
Ian Reynolds <idreyn@gmail.com>
2019-10-24 12:53:36 -0400

    currently working

--- client/components/Menu/Menu.js
+++ client/components/Menu/Menu.js
@@ -9,56 +9,53 @@ import {
 	useMenuState,
 } from 'reakit/Menu';
 
+require('./menu.scss');
+
 const MenuContext = React.createContext(null);
 
 export const Menu = (props) => {
+	const { children, disclosure } = props;
 	const menu = useMenuState();
 	return (
 		<React.Fragment>
-			<RKMenuDisclosure {...menu}>Hello there!</RKMenuDisclosure>
+			<RKMenuDisclosure {...menu}>
+				{(disclosureProps) => disclosure(disclosureProps)}
+			</RKMenuDisclosure>
 			<RKMenu {...menu} as="ul" className="bp3-menu bp3-elevation-1">
-				<MenuContext.Provider value={menu}>
-					<MenuItem text="Cut" icon="cut" />
-					<MenuItem text="Copy" icon="clipboard" />
-					<MenuItem text="Paste" icon="duplicate" />
-					<MenuItem text="Stuff" icon="cube" />
-				</MenuContext.Provider>
+				<MenuContext.Provider value={menu}>{children}</MenuContext.Provider>
 			</RKMenu>
 		</React.Fragment>
 	);
 };
 
 const DisplayMenuItem = React.forwardRef((props, ref) => {
-	const { text, ...restProps } = props;
+	const { children, ...restProps } = props;
 	return (
-		<li ref={ref} {...restProps}>
-			<a className="bp3-menu-item">{text}</a>
+		<li {...restProps} ref={ref}>
+			<a className="bp3-menu-item">{children}</a>
 		</li>
 	);
 });
 
 export const MenuItem = React.forwardRef((props, ref) => {
-	const { children, ...restProps } = props;
+	const { children, text, ...restProps } = props;
 	const parentMenu = useContext(MenuContext);
-	const subMenu = useMenuState();
-	console.log(parentMenu);
 	if (children) {
 		return (
-			<React.Fragment>
-				<RKMenuDisclosure ref={ref} {...subMenu} {...parentMenu} {...restProps}>
-					{(disclosureProps) => <DisplayMenuItem {...props} {...disclosureProps} />}
-				</RKMenuDisclosure>
-				<MenuContext.Provider value={subMenu}>
-					<RKMenu {...subMenu} as="ul" className="bp3-menu bp3-elevation-1">
-						{children}
-					</RKMenu>
-				</MenuContext.Provider>
-			</React.Fragment>
+			<Menu
+				disclosure={(dProps) => (
+					<RKMenuItem as={DisplayMenuItem} {...dProps} {...parentMenu}>
+						{text}
+					</RKMenuItem>
+				)}
+			>
+				{children}
+			</Menu>
 		);
 	}
 	return (
-		<RKMenuItem ref={ref} {...parentMenu} {...restProps}>
-			{(menuItemProps) => <DisplayMenuItem {...props} {...menuItemProps} />}
+		<RKMenuItem as={DisplayMenuItem} ref={ref} {...parentMenu} {...restProps}>
+			{text}
 		</RKMenuItem>
 	);
 });
--- stories/components/menuStories.js
+++ stories/components/menuStories.js
@@ -1,5 +1,17 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { Menu } from 'components/Menu/Menu';
+import { Menu, MenuItem } from 'components/Menu/Menu';
+import { Button } from '@blueprintjs/core';
 
-storiesOf('components/Menu', module).add('default', () => <Menu />);
+storiesOf('components/Menu', module).add('default', () => {
+	return (
+		<Menu disclosure={(props) => <Button {...props}>Hello!</Button>}>
+			<MenuItem text="hello" />
+			<MenuItem text="hi" />
+			<MenuItem text="hey">
+				<MenuItem text="hola" />
+				<MenuItem text="ni hao" />
+			</MenuItem>
+		</Menu>
+	);
+});
