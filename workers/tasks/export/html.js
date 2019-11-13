eedf0093933ed59e1f75b00ce39ff1ee2a6270fa
Ian Reynolds <idreyn@gmail.com>
2019-11-13 16:24:36 -0500

    exporter getting there

--- workers/tasks/export/html.js
+++ workers/tasks/export/html.js
@@ -1,60 +1,144 @@
+import path from 'path';
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
+import sass from 'node-sass';
 import { renderStatic, buildSchema } from '@pubpub/editor';
 
 import { SimpleNotesList } from 'components';
 
 const nonExportableNodeTypes = ['discussion'];
+const katexCdnPrefix = 'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.11.1/';
+const citationPrefix = 'cite';
+const footnotePrefix = 'fn';
+
+const createCss = () => {
+	const nodeModulesPath = path.join(process.env.PWD, 'node_modules');
+	const clientPath = path.join(process.env.PWD, 'client');
+	return (
+		sass
+			.renderSync({
+				file: path.join(__dirname, 'styles', 'print-document.scss'),
+				includePaths: [nodeModulesPath, clientPath],
+				importer: (url) => {
+					if (url.startsWith('~')) {
+						return { file: path.join(nodeModulesPath, url.slice(1)) };
+					}
+					return null;
+				},
+			})
+			.css.toString()
+			// Find all things like url(fonts/KaTeX_whatever) and replace them with a version that
+			// is loaded from an external CDN.
+			.replace(
+				/url\((fonts\/KaTeX_(?:[A-z0-9\-_]*?).(?:[A-z0-9]+))\)/g,
+				(_, fontPath) => `url(${katexCdnPrefix + fontPath})`
+			)
+	);
+};
+
+const staticCss = createCss();
 
 const filterNonExportableNodes = (nodes) =>
 	nodes.filter((n) => !nonExportableNodeTypes.includes(n.type));
 
-const hideEquationChildren = (nodes) =>
+const addAttrsToNodes = (newAttrs, matchNodeTypes, nodes) =>
 	nodes.map((node) => {
-		if (node.type === 'equation' || node.type === 'block_equation') {
+		if (matchNodeTypes.includes(node.type)) {
 			return {
 				...node,
 				attrs: {
 					...node.attrs,
-					renderForPandoc: true,
+					...(typeof newAttrs === 'function' ? newAttrs(node) || {} : newAttrs),
 				},
 			};
 		}
 		if (node.content) {
 			return {
 				...node,
-				content: hideEquationChildren(node.content),
+				content: addAttrsToNodes(newAttrs, matchNodeTypes, node.content),
 			};
 		}
 		return node;
 	});
 
-export const createStaticHtml = async ({ prosemirrorDoc, pubMetadata, citations, footnotes }) => {
+const renderEquationsAsScripts = (nodes) =>
+	addAttrsToNodes({ renderForPandoc: true }, ['equation', 'block_equation'], nodes);
+
+const addHrefsToNotes = (nodes) =>
+	addAttrsToNodes(
+		(node) => {
+			const { count } = node.attrs;
+			if (node.type === 'citation') {
+				return {
+					href: `#${citationPrefix}-${count}`,
+					id: `${citationPrefix}-${count}-return`,
+				};
+			}
+			if (node.type === 'footnote') {
+				return {
+					href: `#${footnotePrefix}-${count}`,
+					id: `${footnotePrefix}-${count}-return`,
+				};
+			}
+			return {};
+		},
+		['citation', 'footnote'],
+		nodes,
+	);
+
+export const createStaticHtml = async (
+	{ prosemirrorDoc, pubMetadata, citations, footnotes },
+	targetPandoc,
+) => {
 	const { title, doi, publishedDateString, updatedDateString } = pubMetadata;
 	const showUpdatedDate = updatedDateString && updatedDateString !== publishedDateString;
+
+	const renderableNodes = [
+		filterNonExportableNodes,
+		targetPandoc && renderEquationsAsScripts,
+		!targetPandoc && addHrefsToNotes,
+	]
+		.filter((x) => x)
+		.reduce((nodes, fn) => fn(nodes), prosemirrorDoc.content);
+
+	const docContent = renderStatic(buildSchema(), renderableNodes, {});
+
 	return ReactDOMServer.renderToStaticMarkup(
 		<html lang="en">
 			<head>
 				<title>{title}</title>
+				{!targetPandoc && (
+					<style type="text/css" dangerouslySetInnerHTML={{ __html: staticCss }} />
+				)}
 			</head>
 			<body>
-				{showUpdatedDate && (
-					<div>
-						<strong>Updated on:</strong> {updatedDateString}
+				<div className="pub-body-component">
+					<div className="editor Prosemirror">
+						{showUpdatedDate && (
+							<div>
+								<strong>Updated on:</strong> {updatedDateString}
+							</div>
+						)}
+						{doi && (
+							<div>
+								<strong>DOI:</strong> {doi}
+							</div>
+						)}
+						{docContent}
+						<div className="pub-notes">
+							<SimpleNotesList
+								title="Footnotes"
+								notes={footnotes}
+								prefix={footnotePrefix}
+							/>
+							<SimpleNotesList
+								title="Citations"
+								notes={citations}
+								prefix={citationPrefix}
+							/>
+						</div>
 					</div>
-				)}
-				{doi && (
-					<div>
-						<strong>DOI:</strong> {doi}
-					</div>
-				)}
-				{renderStatic(
-					buildSchema(),
-					hideEquationChildren(filterNonExportableNodes(prosemirrorDoc.content)),
-					{},
-				)}
-				<SimpleNotesList title="Footnotes" notes={footnotes} />
-				<SimpleNotesList title="Citations" notes={citations} />
+				</div>
 			</body>
 		</html>,
 	);
