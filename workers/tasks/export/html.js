d7d1b56ac8aa9644927144b32e3daf6681b47077
Ian Reynolds <idreyn@gmail.com>
2019-11-14 15:18:12 -0500

    working on front matter

--- workers/tasks/export/html.js
+++ workers/tasks/export/html.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/no-array-index-key */
+/* eslint-disable react/prop-types */
 import path from 'path';
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
@@ -10,6 +12,7 @@ const nonExportableNodeTypes = ['discussion'];
 const katexCdnPrefix = 'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.11.1/';
 const citationPrefix = 'cite';
 const footnotePrefix = 'fn';
+const bullet = ' • ';
 
 const createCss = () => {
 	const nodeModulesPath = path.join(process.env.PWD, 'node_modules');
@@ -17,7 +20,7 @@ const createCss = () => {
 	return (
 		sass
 			.renderSync({
-				file: path.join(__dirname, 'styles', 'print-document.scss'),
+				file: path.join(__dirname, 'styles', 'printDocument.scss'),
 				includePaths: [nodeModulesPath, clientPath],
 				importer: (url) => {
 					if (url.startsWith('~')) {
@@ -31,7 +34,7 @@ const createCss = () => {
 			// is loaded from an external CDN.
 			.replace(
 				/url\((fonts\/KaTeX_(?:[A-z0-9\-_]*?).(?:[A-z0-9]+))\)/g,
-				(_, fontPath) => `url(${katexCdnPrefix + fontPath})`
+				(_, fontPath) => `url(${katexCdnPrefix + fontPath})`,
 			)
 	);
 };
@@ -86,12 +89,103 @@ const addHrefsToNotes = (nodes) =>
 		nodes,
 	);
 
+const renderFrontMatterForPandoc = ({ updatedDateString, publishedDateString, doi }) => {
+	const showUpdatedDate = updatedDateString && updatedDateString !== publishedDateString;
+	return (
+		<React.Fragment>
+			{showUpdatedDate && (
+				<div>
+					<strong>Updated on:</strong> {updatedDateString}
+				</div>
+			)}
+			{doi && (
+				<div>
+					<strong>DOI:</strong> {doi}
+				</div>
+			)}
+		</React.Fragment>
+	);
+};
+
+const renderFrontMatterForHtml = ({
+	updatedDateString,
+	publishedDateString,
+	primaryCollectionTitle,
+	doi,
+	title,
+	communityTitle,
+	attributions,
+}) => {
+	const showUpdatedDate = updatedDateString && updatedDateString !== publishedDateString;
+	const affiliations = [...new Set(attributions.map((attr) => attr.affiliation))];
+	return (
+		<section className="cover">
+			<h2>
+				<span className="community">{communityTitle}</span>
+				{primaryCollectionTitle && (
+					<React.Fragment>
+						{bullet}
+						<span className="collection">{primaryCollectionTitle}</span>
+					</React.Fragment>
+				)}
+			</h2>
+			<h1 className="title">{title}</h1>
+			<div className="byline">
+				<h3>
+					{attributions.map((attr, index) => {
+						const {
+							user: { fullName },
+							affiliation,
+						} = attr;
+						const affiliationNumber =
+							affiliation && affiliations.includes(affiliation)
+								? 1 + affiliations.indexOf(affiliation)
+								: null;
+						return (
+							<span key={index}>
+								{fullName}
+								{affiliationNumber && <sup>{affiliationNumber}</sup>}
+								{index < attributions.length - 1 && ', '}
+							</span>
+						);
+					})}
+				</h3>
+				{affiliations.length > 0 && (
+					<h5>
+						{affiliations.map((aff, index) => (
+							<span key={index}>
+								{<sup>{index + 1}</sup>}
+								{aff}
+								{index < affiliations.length - 1 && ', '}
+							</span>
+						))}
+					</h5>
+				)}
+			</div>
+			{publishedDateString && (
+				<div>
+					<strong>Published on: </strong> {publishedDateString}
+				</div>
+			)}
+			{showUpdatedDate && (
+				<div>
+					<strong>Updated on:</strong> {updatedDateString}
+				</div>
+			)}
+			{doi && (
+				<div>
+					<strong>DOI:</strong> {doi}
+				</div>
+			)}
+		</section>
+	);
+};
+
 export const createStaticHtml = async (
 	{ prosemirrorDoc, pubMetadata, citations, footnotes },
 	targetPandoc,
 ) => {
-	const { title, doi, publishedDateString, updatedDateString } = pubMetadata;
-	const showUpdatedDate = updatedDateString && updatedDateString !== publishedDateString;
+	const { title } = pubMetadata;
 
 	const renderableNodes = [
 		filterNonExportableNodes,
@@ -112,18 +206,11 @@ export const createStaticHtml = async (
 				)}
 			</head>
 			<body>
+				{targetPandoc
+					? renderFrontMatterForPandoc(pubMetadata)
+					: renderFrontMatterForHtml(pubMetadata)}
 				<div className="pub-body-component">
 					<div className="editor Prosemirror">
-						{showUpdatedDate && (
-							<div>
-								<strong>Updated on:</strong> {updatedDateString}
-							</div>
-						)}
-						{doi && (
-							<div>
-								<strong>DOI:</strong> {doi}
-							</div>
-						)}
 						{docContent}
 						<div className="pub-notes">
 							<SimpleNotesList
--- workers/tasks/export/metadata.js
+++ workers/tasks/export/metadata.js
@@ -2,12 +2,30 @@ import { getPubPublishedDate, getPubUpdatedDate } from 'shared/pub/pubDates';
 import dateFormat from 'dateformat';
 
 import ensureUserForAttribution from 'shared/utils/ensureUserForAttribution';
-import { Branch, Community, Pub, PubAttribution, User } from '../../../server/models';
+import {
+	Branch,
+	Collection,
+	CollectionPub,
+	Community,
+	Pub,
+	PubAttribution,
+	User,
+} from '../../../server/models';
 
 export const getPubMetadata = async (pubId) => {
 	const pubData = await Pub.findOne({
 		where: { id: pubId },
 		include: [
+			{
+				model: CollectionPub,
+				as: 'collectionPubs',
+				include: [
+					{
+						model: Collection,
+						as: 'collection',
+					},
+				],
+			},
 			{ model: Community, as: 'community' },
 			{ model: Branch, as: 'branches' },
 			{
@@ -27,13 +45,18 @@ export const getPubMetadata = async (pubId) => {
 	const publishedDateString = publishedDate && dateFormat(publishedDate, 'mmm dd, yyyy');
 	const updatedDateString = updatedDate && dateFormat(updatedDate, 'mmm dd, yyyy');
 	return {
+		title: pubData.title,
 		doi: pubData.doi,
 		publishedDateString: publishedDateString,
 		updatedDateString: updatedDateString,
-		communityTitle: pubData.communityTitle,
+		communityTitle: pubData.community.title,
+		primaryCollectionTitle: [pubData.collectionPubs.find((cp) => cp.isPrimary)]
+			.filter((x) => x)
+			.map((cp) => cp.collection.title)
+			.pop(),
 		attributions: pubData.attributions
 			.concat()
 			.sort((a, b) => a.order - b.order)
-			.map((attr) => ensureUserForAttribution(attr).user.fullName),
+			.map((attr) => ensureUserForAttribution(attr)),
 	};
 };
