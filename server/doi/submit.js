dfc8cefa2293c8fccd294a54566980aaea6095e2
Ian Reynolds <idreyn@gmail.com>
2019-11-25 15:14:17 -0500

    dev: Prettier

--- server/doi/submit.js
+++ server/doi/submit.js
@@ -12,9 +12,8 @@ const getDoiLogin = (communityId) => {
 		IASTATE_DOI_LOGIN_PASSWORD,
 	} = process.env;
 	if (
-		communityId === '99608f92-d70f-46c1-a72c-df272215f13e' //HDSR
-		||
-		communityId === '00c13b77-f067-4b53-8f11-c97aa4b024ff'//Projections
+		communityId === '99608f92-d70f-46c1-a72c-df272215f13e' || // HDSR
+		communityId === '00c13b77-f067-4b53-8f11-c97aa4b024ff' // Projections
 	) {
 		return { login: MITP_DOI_LOGIN_ID, password: MITP_DOI_LOGIN_PASSWORD };
 	}
