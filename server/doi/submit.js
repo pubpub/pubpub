9f6cbc0a188b80fe4d7872dc71891fa0926149c7
Ian Reynolds <idreyn@gmail.com>
2020-01-13 21:01:47 -0500

    fix: Swapping lines to remove unreachable code

--- server/doi/submit.js
+++ server/doi/submit.js
@@ -10,6 +10,10 @@ const getDoiLogin = (communityId) => {
 		MITP_DOI_LOGIN_PASSWORD,
 		IASTATE_DOI_LOGIN_ID,
 		IASTATE_DOI_LOGIN_PASSWORD,
+		AAS_DOI_LOGIN_ID,
+		AAS_DOI_LOGIN_PASSWORD,
+		MEDIASTUDIES_DOI_LOGIN_ID,
+		MEDIASTUDIES_DOI_LOGIN_PASSWORD,
 	} = process.env;
 	if (
 		communityId === '99608f92-d70f-46c1-a72c-df272215f13e' || // HDSR
@@ -19,6 +23,15 @@ const getDoiLogin = (communityId) => {
 	}
 	if (communityId === '3d9ea6a4-25b9-42d3-8ceb-22459c649096') {
 		return { login: IASTATE_DOI_LOGIN_ID, password: IASTATE_DOI_LOGIN_PASSWORD };
+	}
+	if (communityId === '25c2cfeb-dc5b-4dd7-9c82-19146688a931') {
+		return { login: AAS_DOI_LOGIN_ID, password: AAS_DOI_LOGIN_PASSWORD };
+	}
+	if (
+		communityId === '3f8575cb-cdf7-48bb-8985-f4eee1b6c624' ||
+		communityId === 'd895a0ea-f471-4730-9c4a-3d5de1733ccb'
+	) {
+		return { login: MEDIASTUDIES_DOI_LOGIN_ID, password: MEDIASTUDIES_DOI_LOGIN_PASSWORD };
 	}
 	return { login: DOI_LOGIN_ID, password: DOI_LOGIN_PASSWORD };
 };
