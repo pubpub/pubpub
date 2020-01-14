90534c72e3b28214c6907204be712564372d3b0d
Ian Reynolds <idreyn@gmail.com>
2020-01-14 10:18:47 -0500

    update: Refactor handling of per-community DOI prefixes and logins

--- server/doi/submit.js
+++ server/doi/submit.js
@@ -2,38 +2,21 @@ import { Readable } from 'stream';
 import request from 'request-promise';
 import xmlbuilder from 'xmlbuilder';
 
+import { getDoiOverrideByCommunityId } from 'shared/crossref/communities';
+
 const getDoiLogin = (communityId) => {
-	const {
-		DOI_LOGIN_ID,
-		DOI_LOGIN_PASSWORD,
-		MITP_DOI_LOGIN_ID,
-		MITP_DOI_LOGIN_PASSWORD,
-		IASTATE_DOI_LOGIN_ID,
-		IASTATE_DOI_LOGIN_PASSWORD,
-		AAS_DOI_LOGIN_ID,
-		AAS_DOI_LOGIN_PASSWORD,
-		MEDIASTUDIES_DOI_LOGIN_ID,
-		MEDIASTUDIES_DOI_LOGIN_PASSWORD,
-	} = process.env;
-	if (
-		communityId === '99608f92-d70f-46c1-a72c-df272215f13e' || // HDSR
-		communityId === '00c13b77-f067-4b53-8f11-c97aa4b024ff' // Projections
-	) {
-		return { login: MITP_DOI_LOGIN_ID, password: MITP_DOI_LOGIN_PASSWORD };
+	const doiOverride = getDoiOverrideByCommunityId(communityId);
+	if (doiOverride) {
+		const { key } = doiOverride;
+		return {
+			login: process.env[`${key}_DOI_LOGIN_ID`],
+			password: process.env[`${key}_DOI_LOGIN_PASSWORD`],
+		};
 	}
-	if (communityId === '3d9ea6a4-25b9-42d3-8ceb-22459c649096') {
-		return { login: IASTATE_DOI_LOGIN_ID, password: IASTATE_DOI_LOGIN_PASSWORD };
-	}
-	if (communityId === '25c2cfeb-dc5b-4dd7-9c82-19146688a931') {
-		return { login: AAS_DOI_LOGIN_ID, password: AAS_DOI_LOGIN_PASSWORD };
-	}
-	if (
-		communityId === '3f8575cb-cdf7-48bb-8985-f4eee1b6c624' ||
-		communityId === 'd895a0ea-f471-4730-9c4a-3d5de1733ccb'
-	) {
-		return { login: MEDIASTUDIES_DOI_LOGIN_ID, password: MEDIASTUDIES_DOI_LOGIN_PASSWORD };
-	}
-	return { login: DOI_LOGIN_ID, password: DOI_LOGIN_PASSWORD };
+	return {
+		login: process.env.DOI_LOGIN_ID,
+		password: process.env.DOI_LOGIN_PASSWORD,
+	};
 };
 
 export const submitDoiData = (json, timestamp, communityId) => {
---
+++ shared/crossref/communities.js
@@ -0,0 +1,54 @@
+// Some overrides for specific communities.
+// TODO(ian): Handle this with the database instead.
+const PUBPUB_DOI_PREFIX = '10.21428';
+const MITP_DOI_PREFIX = '10.1162';
+const IASTATE_DOI_PREFIX = '10.31274';
+const AAS_DOI_PREFIX = '10.3847';
+const MEDIASTUDIES_DOI_PREFIX = '10.32376';
+
+export const communityDoiOverrides = [
+	{
+		communityIds: [
+			'99608f92-d70f-46c1-a72c-df272215f13e', // HDSR
+			'00c13b77-f067-4b53-8f11-c97aa4b024ff', // Projections
+		],
+		prefix: MITP_DOI_PREFIX,
+		key: 'MITP',
+	},
+	{
+		communityIds: [
+			'3d9ea6a4-25b9-42d3-8ceb-22459c649096', // SUS-RURI
+		],
+		prefix: IASTATE_DOI_PREFIX,
+		key: 'IASTATE',
+	},
+	{
+		communityIds: [
+			'25c2cfeb-dc5b-4dd7-9c82-19146688a931', // Bulletin of the AAS
+		],
+		prefix: AAS_DOI_PREFIX,
+		key: 'AAS',
+	},
+	{
+		communityIds: [
+			'3f8575cb-cdf7-48bb-8985-f4eee1b6c624', // mediastudies.press
+			'd895a0ea-f471-4730-9c4a-3d5de1733ccb', // History of Media Studies
+		],
+		prefix: MEDIASTUDIES_DOI_PREFIX,
+		key: 'MEDIASTUDIES',
+	},
+];
+
+export const getDoiOverrideByCommunityId = (communityId) => {
+	return communityDoiOverrides.find((override) =>
+		override.communityIds.some((id) => id === communityId),
+	);
+};
+
+export const choosePrefixByCommunityId = (communityId) => {
+	const matchingOverride = getDoiOverrideByCommunityId(communityId);
+	if (matchingOverride) {
+		return matchingOverride.prefix;
+	}
+	return PUBPUB_DOI_PREFIX;
+};
