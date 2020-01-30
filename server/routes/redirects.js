fe1f363c21e79787b77ba5dde88ac3d0ace78bf0
Travis Rich <travis.s.rich@gmail.com>
2020-01-30 18:54:42 +0000

    dev: Cleaner redirect code

--- server/routes/redirects.js
+++ server/routes/redirects.js
@@ -1,50 +1,41 @@
 import app from '../server';
-import { hostIsValid } from '../utils';
 
-app.get('/iai', (req, res, next) => {
-	if (!hostIsValid(req, 'pubpub')) {
-		return next();
-	}
-	return res.redirect('https://v3.pubpub.org/iai');
-});
+app.use((req, res, next) => {
+	const redirectHosts = {
+		'www.pubpub.org': {
+			'/iai': 'https://v3.pubpub.org/iai',
+		},
+		'pubpub.ito.com': {
+			'/pub/resisting-reduction': 'https://jods.mitpress.mit.edu/pub/resisting-reduction',
+		},
+		'bookbook.pubpub.org': {
+			'/pub/oki': 'https://wip.mitpress.mit.edu/pub/oki',
+			'/oki': 'https://wip.mitpress.mit.edu/oki',
+		},
+		'kfg.mit.edu': {
+			'/': 'https://www.knowledgefutures.org',
+			'*': 'https://notes.knowledgefutures.org$1',
+		},
+		'blog.pubpub.org': {
+			'*': 'https://notes.knowledgefutures.org$1',
+		},
+		'commonplace.pubpub.org': {
+			'/pub/y6zaxybl': 'https://notes.knowledgefutures.org/pub/y6zaxybl',
+			'/pub/zmxeo3dv': 'https://notes.knowledgefutures.org/pub/zmxeo3dv',
+			'/pub/ek9zpak0': 'https://notes.knowledgefutures.org/pub/ek9zpak0',
+		},
+	};
 
-const setRedirect = (oldHostname, oldPath, newUrl) => {
-	app.get(oldPath, (req, res, next) => {
-		if (req.hostname === oldHostname) {
-			return res.redirect(newUrl);
+	const hostValues = redirectHosts[req.hostname];
+	if (hostValues) {
+		const pathValue = hostValues[req.path];
+		if (pathValue) {
+			return res.redirect(pathValue);
 		}
-		return next();
-	});
-};
-
-setRedirect(
-	'pubpub.ito.com',
-	'/pub/resisting-reduction',
-	'https://jods.mitpress.mit.edu/pub/resisting-reduction',
-);
-setRedirect('bookbook.pubpub.org', '/pub/oki', 'https://wip.mitpress.mit.edu/pub/oki');
-setRedirect('bookbook.pubpub.org', '/oki', 'https://wip.mitpress.mit.edu/oki');
-setRedirect('kfg.mit.edu', '/', 'https://www.knowledgefutures.org');
-setRedirect('blog.pubpub.org', '/', 'https://notes.knowledgefutures.org');
-setRedirect('kfg.mit.edu', '/pub/3jhmyfk5', 'https://notes.knowledgefutures.org/pub/3jhmyfk5');
-setRedirect(
-	'commonplace.pubpub.org',
-	'/pub/y6zaxybl',
-	'https://notes.knowledgefutures.org/pub/y6zaxybl',
-);
-setRedirect(
-	'commonplace.pubpub.org',
-	'/pub/zmxeo3dv',
-	'https://notes.knowledgefutures.org/pub/zmxeo3dv',
-);
-setRedirect(
-	'commonplace.pubpub.org',
-	'/pub/ek9zpak0',
-	'https://notes.knowledgefutures.org/pub/ek9zpak0',
-);
-setRedirect('blog.pubpub.org', '/pub/ek9zpak0', 'https://notes.knowledgefutures.org/pub/ek9zpak0');
-setRedirect(
-	'blog.pubpub.org',
-	'/pub/kzujjdx8',
-	'https://commonplace.knowledgefutures.org/pub/kzujjdx8',
-);
+		const catchAllValue = hostValues['*'];
+		if (catchAllValue) {
+			return res.redirect(catchAllValue.replace('$1', req.path));
+		}
+	}
+	return next();
+});
