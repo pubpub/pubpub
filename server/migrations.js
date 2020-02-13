b2a7f6df88c9fc30642788459ba4e9c75ba6cff8
Ian Reynolds <idreyn@gmail.com>
2020-02-13 16:39:36 -0500

    migration to new fields

--- server/migrations.js
+++ server/migrations.js
@@ -684,11 +684,17 @@ new Promise((resolve) => {
 	// 	// Handle addition of Export model and Branch.exports field
 	// 	return sequelize.sync();
 	// })
-	.then(() => {
+	.then(async () => {
 		sequelize.queryInterface.changeColumn('Pubs', 'headerStyle', {
 			type: Sequelize.ENUM('white-blocks', 'black-blocks', 'dark', 'light'),
-			allowNull: false
 		});
+		sequelize.queryInterface.removeColumn('Pubs', 'headerBackgroundType');
+		// There are no pubs with headerBackgroundColor set yet...set them all to "light", which
+		// represents a light and inoffensive grey color.
+		await Pub.update({headerBackgroundColor: 'light'}, {where: {}});
+		// Any pubs without a header style will get set to "dark".
+		await Pub.update({headerStyle: "dark"}, {where: {headerStyle: null}});
+
 	})
 	.catch((err) => {
 		console.log('Error with Migration', err);
