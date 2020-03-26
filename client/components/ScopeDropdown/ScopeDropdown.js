d4dadaedeea3751bb96ebe24459d9251b71ff65f
Deepak Jagdish <deepak.jagdish@gmail.com>
2020-03-26 12:35:26 +0530

    fix: minor typo in dropdown copy

--- client/components/ScopeDropdown/ScopeDropdown.js
+++ client/components/ScopeDropdown/ScopeDropdown.js
@@ -46,7 +46,7 @@ const ScopeDropdown = () => {
 
 	return (
 		<div className="scope-dropdown-component">
-			<div className="intro">Select Dashbord to view:</div>
+			<div className="intro">Select Dashboard to view:</div>
 			<div className="scopes">
 				{scopes.map((scope, index) => {
 					return (
