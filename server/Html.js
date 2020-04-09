a8d45ea192aef54908723de534f44cb2eca498b2
Gabriel Stein <g@gabestein.com>
2020-04-09 13:46:24 -0400

    new: Adds heap tracking bug for dev

--- server/Html.js
+++ server/Html.js
@@ -72,6 +72,12 @@ const Html = (props) => {
 					title={props.initialData.communityData.title}
 					href="/opensearch.xml"
 				/>
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cdn.heapanalytics.com/js/heap-"+e+".js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a);for(var n=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],o=0;o<p.length;o++)heap[p[o]]=n(p[o])};
+	  heap.load("3777990325");`,
+					}}
+				/>
 			</head>
 			<body>
 				<div id="root">{props.children}</div>
