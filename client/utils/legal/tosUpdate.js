a8f5b184be52f0d2ebf0753d80bb17b900ae9ee6
Ian Reynolds <idreyn@gmail.com>
2020-05-13 14:08:57 -0400

    update: Hide the TOS banner

--- client/utils/legal/tosUpdate.js
+++ client/utils/legal/tosUpdate.js
@@ -3,13 +3,14 @@ import { getCookieOptions } from './cookieOptions';
 
 const cookieKey = 'tos-update';
 const tosLastUpdated = Date.parse('2020-01-01');
+const isBannerDisplayed = false;
 
 export const shouldShowTosUpdate = () => {
 	const cookieValue = Cookie.get(cookieKey);
 	if (cookieValue && parseInt(cookieValue, 10) > tosLastUpdated) {
 		return false;
 	}
-	return true;
+	return isBannerDisplayed;
 };
 
 export const markTosUpdateSeen = () => {
