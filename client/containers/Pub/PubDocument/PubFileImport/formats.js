f4053e1538b99e6072064bc743c2f273f6f09718
Ian Reynolds <idreyn@gmail.com>
2020-06-02 15:34:30 -0400

    wip

--- client/containers/Pub/PubDocument/PubFileImport/formats.js
+++ client/containers/Pub/PubDocument/PubFileImport/formats.js
@@ -26,27 +26,27 @@ export const bibliographyFormats = [
 
 export const exclusiveFileLabels = ['document', 'bibliography'];
 
-const extensionOf = (fileName) => {
+export const extensionFor = (fileName) => {
 	const pieces = fileName.split('.');
 	return pieces[pieces.length - 1];
 };
 
 const fileIsValidDocument = (file) =>
-	Object.keys(extensionToPandocFormat).includes(extensionOf(file.localPath));
+	Object.keys(extensionToPandocFormat).includes(extensionFor(file.localPath));
 
 export const getPotentialLabelsForFile = (file) => {
 	const isDoc = fileIsValidDocument(file);
 	return [
 		isDoc && 'document',
 		isDoc && 'supplement',
-		bibliographyFormats.includes(extensionOf(file.localPath)) && 'bibliography',
+		bibliographyFormats.includes(extensionFor(file.localPath)) && 'bibliography',
 	].filter((x) => x);
 };
 
 export const labelFiles = (files) => {
 	const bibliography =
 		files.find((file) => file.label === 'bibliography') ||
-		files.find((file) => extensionOf(file.localPath) === 'bib' && !file.label);
+		files.find((file) => extensionFor(file.localPath) === 'bib' && !file.label);
 	const doc =
 		files.find((file) => file.label === 'document') ||
 		files.find((file) => fileIsValidDocument(file) && !file.label);
---
+++ workers/tasks/import/images.js
@@ -0,0 +1,25 @@
+import { execSync } from 'child_process';
+import tmp from 'tmp-promise';
+import sharp from 'sharp';
+
+import { extensionFor } from './util';
+
+export const convertFileTypeIfNecessary = async (tmpFilePath) => {
+	const extension = extensionFor(tmpFilePath);
+	if (extension === 'pdf') {
+		// Convert with pdftoppm
+		const { path: pngPath } = await tmp.file({ postfix: '.png' });
+		execSync(`pdftoppm ${tmpFilePath} ${pngPath} -png -rx 300 -ry 300`);
+		return pngPath;
+	}
+	if (extension === 'tiff' || extension === 'tif') {
+		const { path: pngPath } = await tmp.file({ postfix: '.png' });
+		await sharp(tmpFilePath).toFile(pngPath);
+		return pngPath;
+	}
+	return tmpFilePath;
+};
+
+export const convertImagesInSourceFiles = (sourceFiles) => {
+    return Promise.all(sourceFiles.map(
+}
