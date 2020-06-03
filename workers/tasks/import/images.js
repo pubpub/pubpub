e16ffea3ef914bd58a56017e35e3099885161b28
Ian Reynolds <idreyn@gmail.com>
2020-06-03 16:09:11 -0400

    okay, try converting with imagemagick instead

--- workers/tasks/import/images.js
+++ workers/tasks/import/images.js
@@ -1,8 +1,6 @@
-import { exec } from 'child_process';
 import tmp from 'tmp-promise';
-import sharp from 'sharp';
 
-import { extensionFor } from './util';
+import { extensionFor, exec } from './util';
 
 export const convertFileTypeIfNecessary = async (tmpFilePath) => {
 	const extension = extensionFor(tmpFilePath);
@@ -11,21 +9,14 @@ export const convertFileTypeIfNecessary = async (tmpFilePath) => {
 		const { path: pngPath } = await tmp.file({ postfix: '.png' });
 		// pdftoppm tacks the extension onto the output file name. Whatever.
 		const pathWithoutExtension = pngPath.slice(0, -4);
-		return new Promise((resolve, reject) =>
-			exec(
-				`pdftoppm ${tmpFilePath} ${pathWithoutExtension} -png -singlefile -rx 300 -ry 300`,
-				(err) => {
-					if (err) {
-						return reject(err);
-					}
-					return resolve(pngPath);
-				},
-			),
+		await exec(
+			`pdftoppm ${tmpFilePath} ${pathWithoutExtension} -png -singlefile -rx 300 -ry 300`,
 		);
+		return pngPath;
 	}
 	if (extension === 'tiff' || extension === 'tif') {
 		const { path: pngPath } = await tmp.file({ postfix: '.png' });
-		await sharp(tmpFilePath).toFile(pngPath);
+		await exec(`convert ${tmpFilePath} ${pngPath}`);
 		return pngPath;
 	}
 	return tmpFilePath;
