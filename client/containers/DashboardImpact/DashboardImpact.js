62546eb1c4ac0c82f462e528d17e8b4c22c6fe07
Gabriel Stein <g@gabestein.com>
2020-06-26 11:58:10 -0400

    add historical user benchmark

--- client/containers/DashboardImpact/DashboardImpact.js
+++ client/containers/DashboardImpact/DashboardImpact.js
@@ -9,23 +9,33 @@ const propTypes = {};
 
 const DashboardImpact = () => {
 	const [metabaseUrl, setMetabaseUrl] = useState(null);
+	const [metabaseBenchmarkUrl, setMetabaseBenchmarkUrl] = useState(null);
+
 	const { scopeData } = usePageContext();
 	const {
 		elements: { activeTargetType },
 		activePermissions: { canView },
 	} = scopeData;
 	const displayDataWarning = scopeData.elements.activeTarget.createdAt < '2020-04-29';
-	let apiUrl = `/api/metabase/`;
+	const apiPathBase = `/api/metabase/`;
+	let activeId;
+	let dashApiPath;
+	let benchmarkApiPath;
+
 	if (activeTargetType === 'community') {
-		apiUrl += `community/${scopeData.elements.activeIds.communityId}`;
+		activeId = scopeData.elements.activeIds.communityId;
+		dashApiPath = `${apiPathBase}community/${activeId}`;
+		benchmarkApiPath = `${apiPathBase}communityBenchmark/${activeId}`;
 	}
 	if (activeTargetType === 'pub') {
-		apiUrl += `pub/${scopeData.elements.activeIds.pubId}`;
+		activeId = scopeData.elements.activeIds.pubId;
+		dashApiPath = `${apiPathBase}pub/${activeId}`;
+		benchmarkApiPath = `${apiPathBase}pubBenchmark/${activeId}`;
 	}
 	const isCollection = activeTargetType === 'collection';
 
 	useEffect(() => {
-		apiFetch(apiUrl, {
+		apiFetch(dashApiPath, {
 			method: 'GET',
 		})
 			.then(({ token }) => {
@@ -36,6 +46,19 @@ const DashboardImpact = () => {
 			.catch((err) => {
 				console.error(err);
 			});
+		if (displayDataWarning) {
+			apiFetch(benchmarkApiPath, {
+				method: 'GET',
+			})
+				.then(({ token }) => {
+					setMetabaseBenchmarkUrl(
+						`https://metabase.pubpub.org/embed/dashboard/${token}#bordered=false&titled=false`,
+					);
+				})
+				.catch((err) => {
+					console.error(err);
+				});
+		}
 	});
 
 	return (
@@ -49,13 +72,42 @@ const DashboardImpact = () => {
 					Analytics data collected before April 30, 2020 used a different analytics system
 					than data collected after April 30, 2020. As a result, the Users metric will be
 					inconsistent between these two periods, and should not be used for direct
-					comparisons.
+					comparisons. For more information, see our{' '}
+					<a href="https://discourse.knowledgefutures.org/t/user-metric-discrepancies-between-july-12-2019-and-april-30-2020/259">
+						explanatory Discourse post
+					</a>{' '}
+					and the <a href="#historical_benchmark">Historical Benchmark Dashboard</a>,
+					below.
 				</Callout>
 			)}
 			{canView ? (
-				<iframe className="metabase" src={metabaseUrl} title="Analytics" frameBorder="0" />
+				<iframe
+					className="metabase main"
+					src={metabaseUrl}
+					title="Analytics"
+					frameBorder="0"
+				/>
 			) : (
 				<p>Login or ask the community administrator for access to impact data.</p>
+			)}
+			{canView && displayDataWarning && (
+				<React.Fragment>
+					<h3 id="historical_benchmark">Historical User Benchmark</h3>
+					<Callout>
+						This dashboard shows the historical user metric (prior to April 30, 2020)
+						graphed against the new user metric during an overlap period, for making
+						comparisons between the two. For more information, see our{' '}
+						<a href="https://discourse.knowledgefutures.org/t/user-metric-discrepancies-between-july-12-2019-and-april-30-2020/259">
+							explanatory Discourse post
+						</a>
+					</Callout>
+					<iframe
+						className="metabase benchmark"
+						src={metabaseBenchmarkUrl}
+						title="Benchmark"
+						frameBorder="0"
+					/>
+				</React.Fragment>
 			)}
 		</div>
 	);
--- server/metabase/api.js
+++ server/metabase/api.js
@@ -1,22 +1,40 @@
 import jwt from 'jsonwebtoken';
 import app from '../server';
 
-app.get(['/api/metabase/community/:community', '/api/metabase/pub/:pub'], (req, res) => {
-	// This is the metabase dashboard ID. Eventually we may need a map of this. Defaults to community.
-	let dashboard = 2;
-	if (req.params.pub) {
-		dashboard = 3;
-	}
-	const payload = {
-		resource: { dashboard: dashboard },
-		params: {
-			...req.params,
-		},
-		exp: Math.round(Date.now() / 1000) + 10 * 60, // 10 minute expiration
-	};
-	const token = jwt.sign(payload, process.env.METABASE_SECRET_KEY);
+app.get(
+	[
+		'/api/metabase/community/:community',
+		'/api/metabase/communityBenchmark/:community',
+		'/api/metabase/pub/:pub',
+		'/api/metabase/pubBenchmark/:pub',
+	],
+	(req, res) => {
+		// This is the metabase dashboard ID. Eventually we may need a map of this.
+		// Default of 2 = community.
+		// 3 = pub
+		// 8 = communityBenchmark
+		// 9 = pubBenchmark
+		let dashboard = 2;
+		if (req.params.pub) {
+			dashboard = 3;
+		}
+		if (req.originalUrl.includes('communityBenchmark')) {
+			dashboard = 8;
+		}
+		if (req.originalUrl.includes('pubBenchmark')) {
+			dashboard = 9;
+		}
+		const payload = {
+			resource: { dashboard: dashboard },
+			params: {
+				...req.params,
+			},
+			exp: Math.round(Date.now() / 1000) + 10 * 60, // 10 minute expiration
+		};
+		const token = jwt.sign(payload, process.env.METABASE_SECRET_KEY);
 
-	return res.status(200).json({
-		token: token,
-	});
-});
+		return res.status(200).json({
+			token: token,
+		});
+	},
+);
