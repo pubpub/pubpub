819380b6d59c3778f51c38aab624fc8841768e1c
Ian Reynolds <idreyn@gmail.com>
2020-07-09 14:52:12 -0400

    wip

--- client/components/PubEdge/constants.js
+++ client/components/PubEdge/constants.js
@@ -1,25 +1,27 @@
 import PropTypes from 'prop-types';
 
+export const externalPublicationType = PropTypes.shape({
+	title: PropTypes.string.isRequired,
+	url: PropTypes.string.isRequired,
+	contributors: PropTypes.oneOfType([
+		PropTypes.string,
+		PropTypes.arrayOf(
+			PropTypes.oneOfType([
+				PropTypes.shape({
+					name: PropTypes.string,
+				}),
+				PropTypes.string,
+			]),
+		),
+	]),
+	doi: PropTypes.string,
+	description: PropTypes.string,
+	avatar: PropTypes.string,
+	publicationDate: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+});
+
 export const pubEdgeType = PropTypes.shape({
-	externalPublication: PropTypes.shape({
-		title: PropTypes.string.isRequired,
-		url: PropTypes.string.isRequired,
-		contributors: PropTypes.oneOfType([
-			PropTypes.string,
-			PropTypes.arrayOf(
-				PropTypes.oneOfType([
-					PropTypes.shape({
-						name: PropTypes.string,
-					}),
-					PropTypes.string,
-				]),
-			),
-		]),
-		doi: PropTypes.string,
-		description: PropTypes.string,
-		avatar: PropTypes.string,
-		publicationDate: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
-	}),
+	externalPublication: externalPublicationType,
 	targetPub: PropTypes.shape({
 		title: PropTypes.string,
 		id: PropTypes.string,
