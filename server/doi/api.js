23f2ef20d03c086e4dbe79be8392e84a52770765
Eric McDaniel <eric.g.mcdaniel@gmail.com>
2020-07-20 10:16:35 -0400

    feature: add deposit preview endpoint (#922)
    * feature: add deposit preview endpoint
    
    * chore: remove console.log
    
    * feature: PR feedback

--- server/doi/api.js
+++ server/doi/api.js
@@ -1,40 +1,59 @@
 import app, { wrap } from 'server/server';
 import { ForbiddenError } from 'server/utils/errors';
+import xmlbuilder from 'xmlbuilder';
 
-import { setDoiData } from './queries';
+import { getDoiData, setDoiData } from './queries';
 import { getPermissions } from './permissions';
 
-const getRequestIds = (req) => {
+const previewOrDepositDoi = async (req, options = { deposit: false }) => {
+	const { deposit } = options;
 	const user = req.user || {};
-	return {
+	const { target, communityId, collectionId, pubId } = deposit ? req.body : req.query;
+	const requestIds = {
 		userId: user.id,
-		communityId: req.body.communityId,
-		collectionId: req.body.collectionId || null,
-		pubId: req.body.pubId || null,
+		communityId: communityId,
+		collectionId: collectionId || null,
+		pubId: pubId || null,
 	};
+	const permissions = await getPermissions(requestIds);
+	const isAuthenticated =
+		(target === 'pub' && permissions.pub) ||
+		(target === 'collection' && permissions.collection);
+
+	if (!isAuthenticated) {
+		throw new ForbiddenError();
+	}
+
+	const doiJson = await (deposit ? setDoiData : getDoiData)(
+		{
+			communityId: communityId,
+			collectionId: collectionId,
+			pubId: pubId,
+		},
+		target,
+	);
+
+	return doiJson;
 };
 
 app.post(
-	'/api/doi/:target',
+	'/api/doi',
 	wrap(async (req, res) => {
-		const requestIds = getRequestIds(req);
-		const { pubId, collectionId, communityId } = req.body;
-		const { target } = req.params;
-		const permissions = await getPermissions(requestIds);
-		const isAuthenticated =
-			(target === 'pub' && permissions.pub) ||
-			(target === 'collection' && permissions.collection);
-		if (!isAuthenticated) {
-			throw new ForbiddenError();
-		}
-		const doiJson = await setDoiData(
-			{
-				communityId: communityId,
-				collectionId: collectionId,
-				pubId: pubId,
-			},
-			target,
-		);
+		const doiJson = await previewOrDepositDoi(req, { deposit: true });
+
 		return res.status(201).json(doiJson);
+	}),
+);
+
+app.get(
+	'/api/doiPreview',
+	wrap(async (req, res) => {
+		const doiJson = await previewOrDepositDoi(req);
+		const depositXml = xmlbuilder.create(doiJson, { headless: true }).end({ pretty: true });
+
+		return res.status(201).json({
+			depositJson: doiJson,
+			depositXml: depositXml,
+		});
 	}),
 );
