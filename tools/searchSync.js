fea9058d78329cd2fd9935e087285b61e65650da
Ian Reynolds <idreyn@gmail.com>
2020-08-11 17:23:27 -0400

    dev: Clean up the searchsync utility

--- tools/searchSync.js
+++ tools/searchSync.js
@@ -1,29 +1,37 @@
 /* eslint-disable no-console */
 import Promise from 'bluebird';
 import algoliasearch from 'algoliasearch';
-import { Op } from 'sequelize';
+
 import { Pub, Page } from '../server/models';
 import { getPubSearchData, getPageSearchData } from '../workers/utils/searchUtils';
 
+const concurrency = 1;
 const client = algoliasearch(process.env.ALGOLIA_ID, process.env.ALGOLIA_KEY);
 const pubsIndex = client.initIndex('pubs');
 const pagesIndex = client.initIndex('pages');
 
 console.log('Beginning search sync');
 
-let records = 0;
-const findAndIndexPubs = (pubIds) => {
-	return getPubSearchData(pubIds).then((pubSyncData) => {
-		records += pubSyncData.length;
-		return pubsIndex.saveObjects(pubSyncData, { autoGenerateObjectIDIfNotExist: true });
-	});
-};
+const findAndIndexPubs = (pubIds) =>
+	getPubSearchData(pubIds).then((pubSyncData) =>
+		pubsIndex.saveObjects(pubSyncData, { autoGenerateObjectIDIfNotExist: true }),
+	);
 
-const findAndIndexPages = (pageIds) => {
-	return getPageSearchData(pageIds).then((pageSyncData) => {
-		records += pageSyncData.length;
-		return pagesIndex.saveObjects(pageSyncData, { autoGenerateObjectIDIfNotExist: true });
-	});
+const findAndIndexPages = (pageIds) =>
+	getPageSearchData(pageIds).then((pageSyncData) =>
+		pagesIndex.saveObjects(pageSyncData, { autoGenerateObjectIDIfNotExist: true }),
+	);
+
+const batch = (array, count) => {
+	const batches = [];
+	while (array.length) {
+		batches.push(
+			array.splice(0, count).map((item) => {
+				return item.id;
+			}),
+		);
+	}
+	return batches;
 };
 
 const syncPubs = async () => {
@@ -31,97 +39,35 @@ const syncPubs = async () => {
 	const pubIds = await Pub.findAll({
 		attributes: ['id'],
 	});
-	const smallArrays = [];
-	while (pubIds.length) {
-		smallArrays.push(
-			pubIds.splice(0, 100).map((item) => {
-				return item.id;
-			}),
-		);
-	}
-	await Promise.each(smallArrays, (idArray, index) => {
-		console.log('Starting pub batch ', index + 1, ' of ', smallArrays.length);
-		return findAndIndexPubs(idArray);
-	});
+	const pubBatches = batch(pubIds, 100);
+	await Promise.map(
+		pubBatches,
+		(idArray, index) => {
+			console.log(`Syncing Pub batch ${index + 1} of ${pubBatches.length}`);
+			return findAndIndexPubs(idArray).catch((err) => console.error('Sync error', err));
+		},
+		{ concurrency: concurrency },
+	);
 };
 
 const syncPages = async () => {
-	
-}
-
-new Promise((resolve) => {
-	return resolve();
-	// return reject('Fail-safe reject');
-})
-	.then(() => {
-		return Pub.findAll({
-			attributes: ['id'],
-			// limit: 10,
-			where: {
-				id: { [Op.ne]: '5dea7a72-330d-4fbf-8a88-c4723e201b39' },
-			},
-		});
-	})
-	.then((pubIds) => {
-		const smallArrays = [];
-		while (pubIds.length) {
-			smallArrays.push(
-				pubIds.splice(0, 100).map((item) => {
-					return item.id;
-				}),
-			);
-		}
-		return Promise.each(smallArrays, (idArray, index) => {
-			console.log('Starting pub batch ', index + 1, ' of ', smallArrays.length);
-			return findAndIndexPubs(idArray);
-		});
-	})
-	.then(() => {
-		return pagesIndex.setSettings({
-			unretrievableAttributes: ['pageAccessIds', 'content'],
-			searchableAttributes: [
-				'title',
-				'description',
-				'slug',
-				'content',
-				'communityTitle',
-				'communityDomain',
-			],
-			distinct: true,
-			attributeForDistinct: 'pageId',
-			attributesForFaceting: [
-				'filterOnly(isPublic)',
-				'filterOnly(pageId)',
-				'filterOnly(communityId)',
-				'filterOnly(pageAccessIds)',
-			],
-		});
-	})
-	.then(() => {
-		return Page.findAll({
-			attributes: ['id'],
-			// limit: 10,
-		});
-	})
-	.then((pageIds) => {
-		const smallArrays = [];
-		while (pageIds.length) {
-			smallArrays.push(
-				pageIds.splice(0, 100).map((item) => {
-					return item.id;
-				}),
-			);
-		}
-		return Promise.each(smallArrays, (idArray, index) => {
-			console.log('Starting page batch ', index + 1, ' of ', smallArrays.length);
-			return findAndIndexPages(idArray);
-		});
-	})
-	.catch((err) => {
-		console.log('Error with search sync', err);
-	})
-	.finally(() => {
-		console.log('Num Pub Records: ', records);
-		console.log('Ending search sync');
-		process.exit();
+	const pageIds = await Page.findAll({
+		attributes: ['id'],
 	});
+	const pageBatches = batch(pageIds, 100);
+	return Promise.map(
+		pageBatches,
+		(idArray, index) => {
+			console.log(`Syncing Page batch ${index + 1} of ${pageBatches.length}`);
+			return findAndIndexPages(idArray);
+		},
+		{ concurrency: concurrency },
+	);
+};
+
+const main = async () => {
+	await syncPubs();
+	await syncPages();
+};
+
+main();
