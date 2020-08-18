33e35b7257a904826a4b555727c81bbeb8d04ec0
Eric McDaniel <eric.g.mcdaniel@gmail.com>
2020-08-18 15:05:42 -0400

    feature: serve base robots.txt if host is invalid

--- server/routes/robots.js
+++ server/routes/robots.js
@@ -4,21 +4,27 @@ import { getInitialData } from 'server/utils/initData';
 import { hostIsValid } from 'server/utils/routes';
 import { communityUrl } from 'utils/canonicalUrls';
 
-const getCommunityRobots = (community) => `User-agent: *
-Disallow:
-Sitemap: ${communityUrl(community)}/sitemap-index.xml
-`;
+const BASE_ROBOTS = `User-agent: *
+Disallow:`;
+
+const buildRobotsFile = (community) => {
+	if (community) {
+		return `${BASE_ROBOTS}
+Sitemap: ${communityUrl(community)}/sitemap-index.xml`;
+	}
+	return BASE_ROBOTS;
+};
 
 app.get('/robots.txt', async (req, res, next) => {
 	if (!hostIsValid(req, 'community')) {
-		return next();
+		return buildRobotsFile();
 	}
 
 	try {
 		const { communityData } = await getInitialData(req, true);
 
 		res.header('Content-Type', 'text/plain');
-		return res.send(getCommunityRobots(communityData));
+		return res.send(buildRobotsFile(communityData));
 	} catch (err) {
 		return handleErrors(req, res, next)(err);
 	}
