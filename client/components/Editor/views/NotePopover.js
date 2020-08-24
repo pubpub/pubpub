846d68308c6d0fed3153b4cc9e406ecdc28b7aea
Ian Reynolds <idreyn@gmail.com>
2020-08-24 18:20:55 -0400

    better mouse event handling to allow mouse interaction with tooltips

--- client/components/Editor/views/NotePopover.js
+++ client/components/Editor/views/NotePopover.js
@@ -6,6 +6,21 @@ require('./notePopover.scss');
 
 const normalizePopoverString = (string) => string.split('\n').join('');
 
+const rectUnion = (a, b) => {
+	return {
+		right: Math.max(a.right, b.right),
+		left: Math.min(a.left, b.left),
+		x: Math.min(a.x, b.x),
+		bottom: Math.max(a.bottom, b.bottom),
+		top: Math.min(a.top, b.top),
+		y: Math.min(a.y, b.y),
+	};
+};
+
+const rectContainsPoint = ({ left, right, top, bottom }, { x, y }) => {
+	return x >= left && x <= right && y >= top && y <= bottom;
+};
+
 class NotePopover {
 	constructor(node, view, unstructuredAttrKey) {
 		const { dom, contentDOM } = DOMSerializer.renderSpec(document, node.type.spec.toDOM(node));
@@ -16,9 +31,10 @@ class NotePopover {
 		this.unstructuredAttrKey = unstructuredAttrKey;
 		this.setupTooltip = this.setupTooltip.bind(this);
 		this.teardownTooltip = this.teardownTooltip.bind(this);
+		this.handleMouseMove = this.handleMouseMove.bind(this);
 		this.dom.setAttribute('tabindex', '0');
-		this.dom.addEventListener('mouseover', this.setupTooltip);
-		this.dom.addEventListener('mouseout', this.teardownTooltip);
+		this.dom.addEventListener('mouseenter', this.setupTooltip);
+		this.dom.addEventListener('mousemove', this.handleMouseMove);
 		this.dom.addEventListener('focus', this.setupTooltip);
 		this.dom.addEventListener('blur', this.teardownTooltip);
 	}
@@ -33,6 +49,7 @@ class NotePopover {
             ${unstructuredValue ? normalizePopoverString(unstructuredValue) : ''}
         `;
 		document.body.appendChild(tooltip);
+		document.addEventListener('mousemove', this.handleMouseMove);
 		this.tooltip = tooltip;
 		this.popper = new Popper(this.dom, tooltip, {
 			placement: 'top-start',
@@ -47,6 +64,18 @@ class NotePopover {
 		});
 	}
 
+	handleMouseMove(evt) {
+		if (this.tooltip && this.dom && document.activeElement !== this.dom) {
+			const tooltipRect = this.tooltip.getBoundingClientRect();
+			const domRect = this.dom.getBoundingClientRect();
+			const union = rectUnion(tooltipRect, domRect);
+			const containsMouse = rectContainsPoint(union, { x: evt.clientX, y: evt.clientY });
+			if (!containsMouse) {
+				this.teardownTooltip();
+			}
+		}
+	}
+
 	teardownTooltip() {
 		if (this.tooltip) {
 			this.tooltip.remove();
@@ -56,8 +85,8 @@ class NotePopover {
 
 	destroy() {
 		this.teardownTooltip();
-		this.dom.removeEventListener('mouseover', this.setupTooltip);
-		this.dom.removeEventListener('mouseout', this.teardownTooltip);
+		this.dom.removeEventListener('mouseenter', this.setupTooltip);
+		document.removeEventListener('mousemove', this.handleMouseMove);
 		this.dom.removeEventListener('focus', this.setupTooltip);
 		this.dom.removeEventListener('blur', this.teardownTooltip);
 	}
