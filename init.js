6ab7721ace707a053caefe5a6d4ee1bac36afa11
Ian Reynolds <idreyn@gmail.com>
2020-08-27 20:07:18 -0400

    update: Restore throng as an uptime helper with n=1

--- init.js
+++ init.js
@@ -2,6 +2,7 @@
 const {
 	argv: { watch },
 } = require('yargs');
+const throng = require('throng');
 require('@babel/register')({ extensions: ['.js', '.jsx', '.ts', '.tsx'] });
 
 const watchables = watch && (Array.isArray(watch) ? watch : [watch]).filter((x) => x);
@@ -10,13 +11,15 @@ if (process.env.NODE_ENV === 'production') {
 	require('newrelic');
 }
 
-const loadServer = () => {
-	return require('./server/server.js').startServer();
-};
+throng({ workers: 1, lifetime: Infinity }, () => {
+	const loadServer = () => {
+		return require('./server/server.js').startServer();
+	};
 
-if (watchables) {
-	const hotReloadServer = require('./hotReloadServer');
-	hotReloadServer(loadServer, watchables);
-} else {
-	loadServer();
-}
+	if (watchables) {
+		const hotReloadServer = require('./hotReloadServer');
+		hotReloadServer(loadServer, watchables);
+	} else {
+		loadServer();
+	}
+});
