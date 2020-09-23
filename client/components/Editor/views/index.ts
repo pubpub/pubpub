e829c3c9999ff7281fdf2f63ad82a8745c904dc4
Eric McDaniel <eric.g.mcdaniel@gmail.com>
2020-09-23 12:22:04 -0400

    update: move reference view into own module

--- client/components/Editor/views/index.ts
+++ client/components/Editor/views/index.ts
@@ -1,44 +1,5 @@
-import { Node } from 'prosemirror-model';
-import { EditorView } from 'prosemirror-view';
 import NotePopover from './NotePopover';
-
-class ReferenceView {
-	view: EditorView;
-	dom: HTMLAnchorElement;
-	contentDOM: HTMLAnchorElement;
-
-	constructor(node: Node, view: EditorView) {
-		const { targetId, label } = node.attrs;
-
-		this.view = view;
-		this.dom = this.contentDOM = document.createElement('a');
-
-		this.dom.textContent = label || `(ref?)`;
-		this.dom.classList.add('reference');
-		this.dom.classList.add('testy');
-		this.dom.setAttribute('data-node-type', 'reference');
-		this.dom.setAttribute('data-target-id', targetId);
-		this.dom.setAttribute('href', `#${targetId}`);
-
-		this.onClick = this.onClick.bind(this);
-
-		this.dom.addEventListener('click', this.onClick);
-	}
-
-	onClick(e: MouseEvent) {
-		if (this.view.editable) {
-			e.preventDefault();
-		}
-	}
-
-	stopEvent() {
-		return this.view.editable;
-	}
-
-	destroy() {
-		this.dom.removeEventListener('click', this.onClick);
-	}
-}
+import ReferenceView from './ReferenceView';
 
 export default {
 	citation: (node, view) => new NotePopover(node, view, 'unstructuredValue'),
