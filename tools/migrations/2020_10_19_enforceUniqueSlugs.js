c6f601dc55c638a9fa7e7075c9a21487d41231ca
Ian Reynolds <ian@knowledgefutures.org>
2020-10-22 13:08:24 -0400

    dev: Data migration to enforce Collection <=> Page slug uniqueness within Communities (#1095)
    * wrote migration

---
+++ tools/migrations/2020_10_19_enforceUniqueSlugs.js
@@ -0,0 +1,30 @@
+import Bluebird from 'bluebird';
+import { findAcceptableSlug } from 'server/utils/slugs';
+
+const updateSlugForCollection = async (collectionModel) => {
+	const { communityId, slug } = collectionModel;
+	const newSlug = await findAcceptableSlug(`${slug}-collection`, communityId);
+	// eslint-disable-next-line no-console
+	console.log(`${slug} -> ${newSlug}`);
+	collectionModel.slug = newSlug;
+	await collectionModel.save();
+};
+
+const updateCollidingSlugsInCommunity = async (communityId, models) => {
+	const { Collection, Page } = models;
+	const [collections, pages] = await Promise.all([
+		Collection.findAll({ where: { communityId: communityId } }),
+		Page.findAll({ where: { communityId: communityId } }),
+	]);
+	const collidingCollections = collections.filter((c) => pages.some((p) => p.slug === c.slug));
+	await Promise.all(collidingCollections.map(updateSlugForCollection));
+};
+
+module.exports = async ({ models }) => {
+	const { Community } = models;
+	await Bluebird.map(
+		await Community.findAll(),
+		(community) => updateCollidingSlugsInCommunity(community.id, models),
+		{ concurrency: 10 },
+	);
+};
