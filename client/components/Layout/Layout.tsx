e5eb4607f7e2bab0dcdc0bd8eaecff9244397382
Ian Reynolds <ian@knowledgefutures.org>
2020-10-26 13:10:19 -0700

    update: Add the readingCollection URL param to Collection layout PubPreviews (#1104)
    * ensure Collection layout Pub previews have readingCollection param
    
    * move some keys to the right place

--- client/components/Layout/Layout.tsx
+++ client/components/Layout/Layout.tsx
@@ -28,8 +28,12 @@ const Layout = (props: Props) => {
 	const renderBlock = (block: LayoutBlock, index: number) => {
 		if (block.type === 'pubs') {
 			return (
-				<div className="layout-pubs-block">
-					<LayoutPubs key={index} content={block.content} pubs={pubBlocksLists[index]} />
+				<div className="layout-pubs-block" key={index}>
+					<LayoutPubs
+						content={block.content}
+						pubs={pubBlocksLists[index]}
+						collectionId={collectionId}
+					/>
 				</div>
 			);
 		}
@@ -52,9 +56,8 @@ const Layout = (props: Props) => {
 		}
 		if (block.type === 'pages' || block.type === 'collections-pages') {
 			return (
-				<div className="layout-pages-block">
+				<div className="layout-pages-block" key={index}>
 					<LayoutPagesCollections
-						key={index}
 						content={block.content}
 						pages={communityData.pages}
 						collections={communityData.collections}
