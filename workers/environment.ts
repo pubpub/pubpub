555da91fb8b687ecea9284b695c6d64ace5a16f5
Eric McDaniel <eric.g.mcdaniel@gmail.com>
2020-12-03 08:06:26 -0500

    dev: use serverModuleOverwrite in workers

--- workers/environment.ts
+++ workers/environment.ts
@@ -1,15 +1,4 @@
-const Module = require('module');
-
-/* Since we are server-rendering components, we 	*/
-/* need to ensure we don't require things intended 	*/
-/* for webpack. Namely, .scss files 				*/
-const originalRequire = Module.prototype.require;
-Module.prototype.require = function(...args) {
-	if (args[0].indexOf('.scss') > -1) {
-		return () => {};
-	}
-	return originalRequire.apply(this, args);
-};
+require('server/utils/serverModuleOverwrite');
 
 const { setEnvironment, setAppCommit } = require('utils/environment');
 
