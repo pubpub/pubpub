2a514f4b8eb3785daf7e1f97c2fa6722f79009ff
Gabriel Stein <g@gabestein.com>
2020-12-11 13:50:52 -0800

    Update pricing page

--- client/containers/Pricing/Pricing.tsx
+++ client/containers/Pricing/Pricing.tsx
@@ -1,6 +1,4 @@
-/* eslint-disable jsx-curly-brace-presence */
 import React from 'react';
-import { Popover, PopoverInteractionKind } from '@blueprintjs/core';
 import { GridWrapper } from 'components';
 
 require('./pricing.scss');
@@ -9,84 +7,84 @@ const Pricing = () => {
 	return (
 		<div id="pricing-container">
 			<GridWrapper>
-				<h1>Pricing</h1>
-				<p className="description">
+				<h1>
+					Simple Pricing
+					<br />
+					to Support Nonprofit Infrastructure
+				</h1>
+				<p className="description" id="top">
 					PubPub's goal is to provide an affordable, high-quality, open-source,
-					institution-led, hosted alternative to commercial publishing software. We offer
-					a Free tier with no user or publishing limits, as well as a Pro tier for
-					individual communities who need more advanced features (or just want to support
-					our mission), and an Organization tier for those who need to manage multiple
-					communities.
+					institution-led, hosted non-profit publishing software. We offer plans with no
+					user or publishing limits for everyone from individual communities to larger
+					organizations.
 				</p>
 				<div className="pricing-tiers">
-					<div className="option bp3-card bp3-elevation-1">
-						<h2>Free</h2>
-						<p className="subtitle">For publishing communities of all kinds.</p>
-						<p className="pricing">Free, Forever</p>
-						<ul className="features">
-							<li>
-								All of PubPub&apos;s core drafting, review, publishing, and
-								discussion features
-							</li>
-							<li>Your own pubpub.org subdomain</li>
-							<li>Unlimited publications</li>
-							<li>Unlimited users</li>
-							<li>Custom site design</li>
-							<li>Publish with any Creative Commons license</li>
-							<li>
-								Up to 10 free DOIs per year ($1 after, or free if you have a
-								crossref membership)
-							</li>
-							<li>
-								Community support on our{' '}
-								<a
-									href="https://discourse.knowledgefutures.org"
-									target="_blank"
-									rel="noopener noreferrer"
-								>
-									Discourse forums
-								</a>
-							</li>
-						</ul>
+					<div className="option bp3-card bp3-elevation-1" id="independent">
+						<h2>Independent</h2>
+						<p className="subtitle">
+							For individuals, grassroots communities, and experimenters.
+						</p>
+						<h3 className="pricing">
+							Contribute What You Want
+							<br />
+							<span className="subhead">(including $0!)</span>
+						</h3>
 						<a
 							href="/community/create"
 							className="bp3-button bp3-large bp3-intent-primary"
 						>
 							Create your Community
 						</a>
-					</div>
-					<div className="option bp3-card bp3-elevation-1">
-						<h2>Pro</h2>
-						<p className="subtitle">For individual communities with advanced needs.</p>
-						<p className="pricing">
-							Monthly or Yearly Fee Per{' '}
-							<Popover
-								interactionKind={PopoverInteractionKind.HOVER}
-								popoverClassName="bp3-popover-content-sizing"
-							>
-								<em>Seat</em>
-								<div>
-									<p>
-										Seats are calculated as the number of users added to the
-										Member tab at the Community or Collection level with
-										permissions of edit or above. View-only Members and Members
-										added to single Pubs (often used for authors or reviews) are
-										free.
-									</p>
-								</div>
-							</Popover>
-						</p>
 						<ul className="features">
-							<li>Everything in the free version, plus:</li>
-							<li>Use your own custom domain name</li>
-							<li>Publish with custom licenses</li>
+							<li>Single PubPub Community</li>
+							<li>Forum-Based support</li>
+							<li>Unlimited Pubs and Users</li>
+							<li>Creative Commons Licenses</li>
+							<li>Dedicated pubpub.org subdomain</li>
+							<li>Custom domain available with contributions over $5/month</li>
 							<li>
-								Up to 25 free DOIs per year ($1 each after, or free if you have a
-								crossref membership).
+								10 free DOIs per year<span className="star">*</span>
 							</li>
-							<li>Initial community setup support</li>
-							<li>Priority email support</li>
 						</ul>
+					</div>
+					<div className="option bp3-card bp3-elevation-1" id="starter">
+						<h2>
+							Starter <span className="subhead">(beta)</span>
+						</h2>
+						<p className="subtitle">
+							For smaller, independent, and unconventional publishers.
+						</p>
+						<h3 className="pricing">
+							$175<span className="subhead">/month</span>
+						</h3>
+						<a
+							href="mailto:partnerships@pubpub.org?subject=PubPub%20Starter%20Inquiry"
+							target="_blank"
+							rel="noopener noreferrer"
+							className="bp3-button bp3-large bp3-intent-primary"
+						>
+							Get In Touch
+						</a>
+						<ul className="features">
+							<li>Up to 5 Communities</li>
+							<li>Email support</li>
+							<li>Unlimited Pubs and Users</li>
+							<li>Custom licenses</li>
+							<li>Dedicated pubpub.org subdomain</li>
+							<li>Custom domains</li>
+							<li>
+								25 free DOIs per year<span className="star">*</span>
+							</li>
+						</ul>
+					</div>
+					<div className="option bp3-card bp3-elevation-1" id="pro">
+						<h2>
+							Pro <span className="subhead">(beta)</span>
+						</h2>
+						<p className="subtitle">For larger publishers with professional needs.</p>
+						<h3 className="pricing">
+							$425<span className="subhead">/month</span>
+						</h3>
 						<a
 							href="mailto:partnerships@pubpub.org?subject=PubPub%20Pro%20Inquiry"
 							target="_blank"
@@ -95,62 +93,59 @@ const Pricing = () => {
 						>
 							Get In Touch
 						</a>
-					</div>
-					<div className="option bp3-card bp3-elevation-1">
-						<h2>Organization</h2>
-						<p className="subtitle">For managing multiple communities.</p>
-						<p className="pricing">
-							Monthly or Yearly Fee Per{' '}
-							<Popover
-								interactionKind={PopoverInteractionKind.HOVER}
-								popoverClassName="bp3-popover-content-sizing"
-							>
-								<em>Seat</em>
-								<div>
-									<p>
-										Seats are calculated as the number of users added to the
-										Member tab at the Community or Collection level with
-										permissions of edit or above. View-only Members and Members
-										added to single Pubs (often used for authors or reviews) are
-										free. Members are only counted once across the entire
-										organization, no matter how many communities they belong to.
-									</p>
-								</div>
-							</Popover>
-						</p>
 						<ul className="features">
-							<li>Everything in the Pro version, plus:</li>
-							<li>Unlimited Communities</li>
+							<li>Up to 15 Communities</li>
+							<li>Priority email support</li>
+							<li>Unlimited Pubs and Users</li>
+							<li>Custom licenses</li>
+							<li>Dedicated pubpub.org subdomain</li>
+							<li>Custom domains</li>
 							<li>
-								Organization Dashboard for managing users, branding, and settings
-								across multiple Communities
+								50 free DOIs per year<span className="star">*</span>
 							</li>
-							<li>
-								Organization Landing Page for displaying all of the content and
-								Communities in your Organization
-							</li>
-							<li>
-								Up to 50 free DOIs per year ($1 each after, or free if you have a
-								crossref membership).
-							</li>
-							<li>Initial organization setup support</li>
-							<li>Dedicated email support</li>
 						</ul>
+					</div>
+					<div className="option bp3-card bp3-elevation-1" id="enterprise">
+						<h2>Enterprise</h2>
+						<p className="subtitle">
+							For institutions & organizations who need custom support & scale.
+						</p>
+						<h3 className="pricing">Custom</h3>
 						<a
-							href="mailto:partnerships@pubpub.org?subject=PubPub%20Org%20Inquiry"
+							href="mailto:partnerships@pubpub.org?subject=PubPub%20Enterprise%20Inquiry"
 							target="_blank"
 							rel="noopener noreferrer"
 							className="bp3-button bp3-large bp3-intent-primary"
 						>
 							Get In Touch
 						</a>
+						<ul className="features">
+							<li>Custom # of Communities</li>
+							<li>Dedicated email support</li>
+							<li>Unlimited Pubs and Users</li>
+							<li>Custom licenses</li>
+							<li>Dedicated pubpub.org subdomain</li>
+							<li>Custom domains</li>
+							<li>Custom # of DOIs</li>
+						</ul>
 					</div>
 				</div>
 				<div className="pricing-footer">
 					<p className="description">
-						Need advanced features, but unsure you can afford PubPub Pro or
-						Organization? We offer substantial discounts for small, mission-driven
-						communities. For more information,{' '}
+						Our plans are based on the cost savings PubPub offers publishing communities
+						and the cost of providing support for Communities and Organizations as they
+						scale. We developed our plans in partnership with our Communities with the
+						goal of making PubPub sustainable without excluding Communities or punishing
+						them for success.{' '}
+						<a href="https://notes.knowledgefutures.org/pub/ik8lltas">
+							Read more about the thinking behind our plans
+						</a>
+						.
+					</p>
+					<p className="description">
+						Need advanced features, but unsure you can afford our plans? We offer
+						substantial discounts for small, mission-driven communities. For more
+						information,{' '}
 						<a href="mailto:partnerships@pubpub.org?subject=Discounts">
 							please reach out and tell us about your work and needs
 						</a>
@@ -164,6 +159,14 @@ const Pricing = () => {
 							please get in touch
 						</a>
 						.
+					</p>
+
+					<p className="description" id="doi">
+						* The free DOI limit applies to DOIs published via PubPub's Crossref
+						membership. Once the limit is reached, PubPub passes on the Crossref fee of
+						$1 per DOI registered to the Community. For Communities with their own
+						Crossref membership, there is no additional fee for creating or depositing
+						DOIs.
 					</p>
 				</div>
 			</GridWrapper>
