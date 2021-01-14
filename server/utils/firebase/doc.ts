67f72abdd7641784d9687ff739140373935ddfcf
Ian Reynolds <idreyn@gmail.com>
2021-01-14 11:33:02 -0500

    remove obsolete server/utils/firebase directory

--- server/utils/firebase/doc.ts
+++
@@ -1,7 +0,0 @@
-import { Node } from 'prosemirror-model';
-
-import { editorSchema } from '../firebaseAdmin';
-
-export const getDocFromJson = (docJson: {}): Node => {
-	return Node.fromJSON(editorSchema, docJson);
-};
