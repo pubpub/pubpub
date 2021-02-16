aa19e1452b42c0b6c32caadcb4b931a2282f4471
Ian Reynolds <ian@knowledgefutures.org>
2021-02-16 10:05:40 -0500

    fix: Fix the 'Show anchored comments' toggle (#1283)

--- client/components/Editor/plugins/discussions/index.ts
+++ client/components/Editor/plugins/discussions/index.ts
@@ -1,3 +1,8 @@
-export { default, discussionsPluginKey, addDiscussionToView } from './plugin';
+export {
+	default,
+	discussionsPluginKey,
+	addDiscussionToView,
+	getAnchoredDiscussionIds,
+} from './plugin';
 export { mapDiscussionSelectionThroughSteps } from './util';
 export { DiscussionSelection } from './types';
