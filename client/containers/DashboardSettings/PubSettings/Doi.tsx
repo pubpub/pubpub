390a9777e5b0cb7e4e7204856357ad0612be9611
Gabriel Stein <gabe@pubpub.org>
2021-03-11 10:41:53 -0500

    update: Add warning if Pub is assigned DOI and released but not submitted to Crossref (#1309)
    * update: Add warning if Pub is assigned DOI and released but not submitted to Crossref
    
    * add success intent to update suffix button

--- client/containers/DashboardSettings/PubSettings/Doi.tsx
+++ client/containers/DashboardSettings/PubSettings/Doi.tsx
@@ -312,6 +312,14 @@ class Doi extends Component<Props, State> {
 		return (
 			<>
 				{!pubData.doi && <p>A DOI can be set for each Pub by admins of this community.</p>}
+				{pubData.doi &&
+					!pubData.crossrefDepositRecordId &&
+					!this.disabledDueToNoReleases() && (
+						<Callout intent="warning">
+							A DOI has been assigned to this Pub, but it has not yet been deposited
+							to Crossref.
+						</Callout>
+					)}
 				{pubData.crossrefDepositRecordId && <p>This Pub has been deposited to Crossref.</p>}
 				{this.isDoiEditableWithoutRelations() && this.findSupplementTo() && (
 					<Callout intent="warning">
@@ -421,6 +429,7 @@ class Doi extends Component<Props, State> {
 						text="Update Suffix"
 						loading={updating}
 						onClick={this.handleUpdateDoiClick}
+						intent="success"
 					/>
 					<Button
 						disabled={invalidDoi || deleting || updating}
