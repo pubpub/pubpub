c02bec2ba574053154dfa74bd13fe28ec4b2e043
Ian Reynolds <idreyn@gmail.com>
2021-03-31 14:31:26 -0400

    dev: Migration to move layout block alignment into Prosemirror

--- client/components/Layout/LayoutText.tsx
+++ client/components/Layout/LayoutText.tsx
@@ -16,27 +16,22 @@ const LayoutText = function(props) {
 	if (!props.content.text) {
 		return null;
 	}
-	const wrapperStyle = {
-		textAlign: props.content.align || 'left',
-	};
 	return (
 		<div className="layout-text-component">
 			<div className="block-content">
 				<GridWrapper>
-					<div style={wrapperStyle}>
-						<Editor
-							nodeOptions={{
-								image: {
-									onResizeUrl: (url) => {
-										return getResizedUrl(url, 'fit-in', '1200x0');
-									},
-									linkToSrc: false,
+					<Editor
+						nodeOptions={{
+							image: {
+								onResizeUrl: (url) => {
+									return getResizedUrl(url, 'fit-in', '1200x0');
 								},
-							}}
-							initialContent={props.content.text || undefined}
-							isReadOnly={true}
-						/>
-					</div>
+								linkToSrc: false,
+							},
+						}}
+						initialContent={props.content.text || undefined}
+						isReadOnly={true}
+					/>
 				</GridWrapper>
 			</div>
 		</div>
