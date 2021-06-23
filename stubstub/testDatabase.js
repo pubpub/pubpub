3075c2aa59e4a765c0efd7d9fbbaada7900f5594
ben howe <howebenhowe@gmail.com>
2021-06-23 11:51:09 -0500

    Remove unreachable postgres test dir specification

--- stubstub/testDatabase.js
+++ stubstub/testDatabase.js
@@ -5,8 +5,7 @@ import hasbin from 'hasbin';
 
 const requiredBinaries = ['createdb', 'dropuser', 'dropdb', 'pg_ctl', 'psql'];
 
-const pgDataPath =
-	path.join(process.cwd(), 'pubpub-postgres-test') || '/usr/local/var/pubpub-postgres-test';
+const pgDataPath = path.join(process.cwd(), 'pubpub-postgres-test');
 
 const testDbConfig = {
 	username: 'testuser',
