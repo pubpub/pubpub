b9e61ae5828a415d9eefaa3034eb87396b4e1c3e
Ian Reynolds <idreyn@gmail.com>
2021-06-23 16:26:51 -0400

    update: Derive UserNotifications from ActivityItems

--- server/threadComment/hooks.ts
+++ server/threadComment/hooks.ts
@@ -1,11 +1,41 @@
 import * as types from 'types';
 import { ThreadComment } from 'server/models';
-import { createUserThreadSubscription } from 'server/userSubscription/queries';
+import { defer } from 'server/utils/deferred';
+import { getParentModelForThread } from 'server/thread/queries';
+import {
+	createPubDiscussionCommentAddedActivityItem,
+	createPubReviewCommentAddedActivityItem,
+} from 'server/activityItem/queries';
+import { setUserSubscriptionStatus } from 'server/userSubscription/shared/queries';
+
+const createActivityItems = async (threadComment: types.ThreadComment) => {
+	const parent = await getParentModelForThread(threadComment.threadId);
+	if (parent) {
+		if (parent.type === 'discussion') {
+			const { value: discussion } = parent;
+			const numberOfCommentsInThread = await ThreadComment.count({
+				where: { threadId: threadComment.threadId },
+			});
+			const isReply = numberOfCommentsInThread > 1;
+			await createPubDiscussionCommentAddedActivityItem(
+				discussion.id,
+				threadComment.id,
+				isReply,
+			);
+		}
+		if (parent.type === 'review') {
+			const { value: review } = parent;
+			await createPubReviewCommentAddedActivityItem(review.id, threadComment.id);
+		}
+	}
+};
 
 ThreadComment.afterCreate(async (threadComment: types.ThreadComment) => {
-	await createUserThreadSubscription({
+	await setUserSubscriptionStatus({
 		userId: threadComment.userId,
 		threadId: threadComment.threadId,
-		createdAutomatically: true,
+		setAutomatically: true,
+		status: 'unchanged',
 	});
+	defer(() => createActivityItems(threadComment));
 });
---
+++ server/userNotification/create.ts
@@ -0,0 +1,98 @@
+import { Op } from 'sequelize';
+
+import * as types from 'types';
+import { UserNotification, UserSubscription, UserNotificationPreferences } from 'server/models';
+import { indexByProperty, splitArrayOn } from 'utils/arrays';
+import { filterUsersWhoCanSeeThread } from 'server/thread/queries';
+
+type ActivityItemResponder<Kind extends types.ActivityItemKind> = (
+	item: types.ActivityItemOfKind<Kind>,
+) => Promise<void>;
+
+const createNotificationsForThreadComment = async (
+	item: types.ActivityItemOfKind<'pub-discussion-comment-added' | 'pub-review-comment-added'>,
+	includePubLevelSubscribers: boolean,
+) => {
+	const {
+		actorId,
+		pubId,
+		payload: { threadId },
+	} = item;
+
+	const subscriptionWhereQueries = includePubLevelSubscribers
+		? [{ pubId }, { threadId }]
+		: [{ threadId }];
+
+	const subscriptions: types.UserSubscription[] = await UserSubscription.findAll({
+		where: {
+			[Op.or]: subscriptionWhereQueries,
+			userId: { [Op.not]: actorId },
+			status: { [Op.not]: 'inactive' },
+		},
+	});
+
+	const notificationPreferencesOptingOutOfNotifications: types.UserNotificationPreferences[] = await UserNotificationPreferences.findAll(
+		{
+			where: {
+				userId: { [Op.in]: [...new Set(subscriptions.map((s) => s.userId))] },
+				receiveNotifications: false,
+			},
+		},
+	);
+	const userIdsWhoDoNotWantNotifications = notificationPreferencesOptingOutOfNotifications.map(
+		(pref) => pref.userId,
+	);
+
+	const [mutedThreadSubscriptions, unmutedThreadSubscriptions] = splitArrayOn(
+		subscriptions.filter((sub) => !!sub.threadId),
+		(s) => s.status === 'muted',
+	);
+
+	const unmutedPubSubscriptionsNotSupersededByThreadMute = subscriptions.filter(
+		(sub) =>
+			sub.pubId &&
+			sub.status !== 'muted' &&
+			!mutedThreadSubscriptions.some((mutedSub) => mutedSub.userId === sub.userId),
+	);
+
+	const subscriptionsThatMayProduceNotifications = [
+		...unmutedThreadSubscriptions,
+		...unmutedPubSubscriptionsNotSupersededByThreadMute,
+	];
+
+	const subscriptionsByUserId = indexByProperty(
+		subscriptionsThatMayProduceNotifications,
+		'userId',
+	);
+
+	const userIdsToNotifty = await filterUsersWhoCanSeeThread({
+		threadId,
+		userIds: subscriptionsThatMayProduceNotifications
+			.map((sub) => sub.userId)
+			.filter((userId) => !userIdsWhoDoNotWantNotifications.includes(userId)),
+	});
+
+	await UserNotification.bulkCreate(
+		userIdsToNotifty.map((userId) => {
+			return {
+				userId,
+				userSubscriptionId: subscriptionsByUserId[userId].id,
+				activityItemId: item.id,
+			};
+		}),
+	);
+};
+
+const notificationCreatorsByKind: Partial<
+	{ [Kind in types.ActivityItemKind]: ActivityItemResponder<Kind> }
+> = {
+	'pub-discussion-comment-added': (item) => createNotificationsForThreadComment(item, true),
+	'pub-review-comment-added': (item) => createNotificationsForThreadComment(item, false),
+};
+
+export const createNotificationsForActivityItem = async (item: types.ActivityItem) => {
+	const creator = notificationCreatorsByKind[item.kind] as ActivityItemResponder<any>;
+	if (creator) {
+		await creator(item);
+	}
+};
