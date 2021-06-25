f63ab29b8e81d1437455f8bd0046cf78983d07fa
Gabriel Stein <gabe@pubpub.org>
2021-06-25 08:42:35 -0400

    dev: Remove keen for good and update admin dashboard (#1479)
    * remove keen callsites
    
    * update admin dashboard and access)
    
    * remove admindash api route
    
    * switch unneeded func to const

--- server/routes/adminDashboard.tsx
+++ server/routes/adminDashboard.tsx
@@ -6,6 +6,7 @@ import { handleErrors } from 'server/utils/errors';
 import { getInitialData } from 'server/utils/initData';
 import { hostIsValid } from 'server/utils/routes';
 import { generateMetaComponents, renderToNodeStream } from 'server/utils/ssr';
+import { generateMetabaseToken } from 'server/utils/metabase';
 
 app.get('/admin', (req, res, next) => {
 	if (!hostIsValid(req, 'pubpub')) {
@@ -20,10 +21,20 @@ app.get('/admin', (req, res, next) => {
 				'5d9d63b3-6990-407c-81fb-5f87b9d3e360',
 				'807f3604-4223-4495-b576-861d04d2f39e',
 				'237fe275-0618-4a8f-bd40-ea9065836e67',
+				'06f7d120-391e-4d71-8725-dcd3e9b9af44',
+				'408952a0-58a6-42df-86b8-d7fe3ab6ba43',
+				'c575d640-4d72-4197-bc32-0cf383a34a4f',
+				'af262062-bb64-402c-8cfc-165c27c2788a',
+				'4d506b24-80b6-462c-85f0-f237e08010d5',
+				'6ddd7a7b-c542-4127-b403-33a67eb40ff3',
+				'd29eed4d-d754-4f8c-975e-b34826e6b8d2',
 			];
 			if (!users.includes(user.id)) {
 				throw new Error('Page Not Found');
 			}
+			const impactData = {
+				baseToken: generateMetabaseToken('pubpub', null, 'base'),
+			};
 			return renderToNodeStream(
 				res,
 				<Html
@@ -34,6 +45,7 @@ app.get('/admin', (req, res, next) => {
 						title: `Admin Dashboard · ${initialData.communityData.title}`,
 						description: initialData.communityData.description,
 					})}
+					viewData={{ impactData }}
 				/>,
 			);
 		})
