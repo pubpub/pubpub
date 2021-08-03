723fb2320bcb4e78d40b4f62ef9c40737a627c24
Ian Reynolds <idreyn@gmail.com>
2021-08-03 16:06:57 -0400

    dev: Update customScripts permission list

--- server/customScript/permissions.ts
+++ server/customScript/permissions.ts
@@ -5,6 +5,7 @@ const communitiesWithCustomScriptEnabled = [
 	'eb361ef0-d557-4cac-a949-8bd196e096e7', // Staging-place
 	'e164d5cb-585e-4170-bad8-10d09e08d1bc', // Contours
 	'0417b0c0-cd38-48bd-8a84-b0b95da98813', // Demo
+	'36a3e2c8-74ed-417f-b616-4b57b85b9338', // digitalpubsummit
 ];
 
 export const communityCanUseCustomScripts = (communityId: string) => {
