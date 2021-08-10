eeceeb3d577936dad60630c5bcce77c80166c206
Qwelian D. Tanner <dmtorcode@gmail.com>
2021-08-10 09:49:30 -0400

    feature: Image hyperlink (#1525)
    * edited prosemirror schema for href capability +5.6 points 🎉
    
    * linking works. TODO: figure why input jumps to end when editing link, image resizes for what?????
    
    * Added UI, still needs css fix for <a>-wrapped imgs
    
    * Verbose, but functional, toDom anchor tag wrapping for linked images
    
    * undo
    
    * Fix css for link-wrapped img tags in figures
    
    * address comment on typing of props
    
    * put img tag for ps schema in variable
    
    * remove small from anchor button
    
    * change AnchorCdonrtol to UrlControl...remove the www., who cares about namespaces. they are arbitrary. and we must do our part to rid the culture of its reliance on specifying the default www. this is the kind of behaviour Foucault would admonish. no normalizing trivial stuffs
    
    * IVE DONE IT, BY GOD IV'E DONE IT
    
    Co-authored-by: ben howe <howebenhowe@gmail.com>

--- client/components/FormattingBar/controlComponents/ControlsMedia/ControlsMedia.tsx
+++ client/components/FormattingBar/controlComponents/ControlsMedia/ControlsMedia.tsx
@@ -9,6 +9,7 @@ import { getCurrentNodeLabels, isResizeableFormat } from 'client/components/Edit
 
 import { ControlsButton, ControlsButtonGroup } from '../ControlsButton';
 import AlignmentControl from './AlignmentControl';
+import UrlControl from './UrlControl';
 import SourceControls from './SourceControls';
 import { ControlsReferenceSettingsLink } from '../ControlsReference';
 import { EditorChangeObjectWithNode } from '../../types';
@@ -37,7 +38,16 @@ const ControlsMedia = (props: Props) => {
 		updateAttrs,
 		attrs: { altText },
 	} = pendingAttrs;
-	const { size, align, height, caption, url, fullResolution, hideLabel } = selectedNode.attrs;
+	const {
+		size,
+		align,
+		href,
+		height,
+		caption,
+		url,
+		fullResolution,
+		hideLabel,
+	} = selectedNode.attrs;
 	const nodeSupportsAltText = !!selectedNode.type.spec.attrs?.altText;
 	const canEditHeight = getCanEditNodeHeight(selectedNode);
 	const itemName = getItemName(selectedNode);
@@ -143,6 +153,7 @@ const ControlsMedia = (props: Props) => {
 					value={align}
 					onChange={(nextAlignment) => updateNode({ align: nextAlignment })}
 				/>
+				<UrlControl onChange={(nextHref) => updateNode({ href: nextHref })} value={href} />
 				<SourceControls
 					// @ts-expect-error ts-migrate(2322) FIXME: Type 'ProsemirrorNode<any> & { attrs?: { size: num... Remove this comment to see the full error message
 					selectedNode={selectedNode}
