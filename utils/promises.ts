c82cbea1d83bbceaca488958350db5d63217161b
Ian Reynolds <idreyn@gmail.com>
2021-10-06 13:39:54 -0400

    cleaning up

--- utils/promises.ts
+++ utils/promises.ts
@@ -1,17 +1,14 @@
-type FnOf<Arguments extends any[], ReturnType> = (...args: Arguments) => ReturnType;
-
 type ExpiringPromiseOptions<ReturnType> = { timeout: number } & (
 	| { fallback: ReturnType }
 	| { throws: () => Error }
 );
 
 export const expiringPromise = <Arguments extends any[], ReturnType>(
-	fn: FnOf<Arguments, Promise<ReturnType>>,
+	fn: (...args: Arguments) => Promise<ReturnType>,
 	options: ExpiringPromiseOptions<ReturnType>,
 ) => {
-	const { timeout } = options;
-	return (...args: Arguments): Promise<ReturnType> =>
-		new Promise((resolve, reject) => {
+	return (...args: Arguments): Promise<ReturnType> => {
+		return new Promise((resolve, reject) => {
 			fn(...args)
 				.then(resolve)
 				.catch(reject);
@@ -21,6 +18,7 @@ export const expiringPromise = <Arguments extends any[], ReturnType>(
 				} else if ('throws' in options) {
 					reject(options.throws());
 				}
-			}, timeout);
+			}, options.timeout);
 		});
+	};
 };
