cd6acc2e4b6118705f8f2c52f02cd39b17ea0257
qweliant <dmtorcode@gmail.com>
2021-10-06 11:54:20 -0400

    add metadat block type, metadat block menue item, begin metadata component for layout

--- utils/layout/types.ts
+++ utils/layout/types.ts
@@ -101,12 +101,37 @@ export type LayoutBlockCollectionHeader = {
 	};
 };
 
+export type LayoutBlockMetadata = {
+	type: 'collection-metadata';
+	id: string;
+	content: {
+		doi?: string;
+		url?: string;
+		isbn?: string;
+		copyrightYear?: string;
+		publicationDate?: string;
+		edition?: string;
+		printIssn?: string;
+		electronicIssn?: string;
+		volume?: string;
+		issue?: string;
+		printPublicationDate?: string;
+		// why aren't they shared in the metadata schema?
+		// publicationDate?: string;
+		theme?: string;
+		acronym?: string;
+		location?: string;
+		date?: string;
+	};
+};
+
 export type LayoutBlock =
 	| LayoutBlockBanner
 	| LayoutBlockCollectionsPages
 	| LayoutBlockHtml
 	| LayoutBlockPubs
 	| LayoutBlockText
-	| LayoutBlockCollectionHeader;
+	| LayoutBlockCollectionHeader
+	| LayoutBlockMetadata;
 
 export type CollectionLayout = LayoutOptions & { blocks: LayoutBlock[] };
