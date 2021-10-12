b13de1ff60ca41bd9f06765af387390ca9860216
Ian Reynolds <idreyn@gmail.com>
2021-10-12 19:44:55 -0400

    bang bang

--- client/utils/featureFlags.ts
+++ client/utils/featureFlags.ts
@@ -2,5 +2,5 @@ import { getClientInitialData } from './initialData';
 
 export const getFeatureFlag = (name: string): boolean => {
 	const initialData = getClientInitialData();
-	return initialData.featureFlags[name];
+	return !!initialData.featureFlags[name];
 };
