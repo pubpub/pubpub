b4977fa58f9354af67d444a7cd8490f9cf256d6b
qweliant <dmtorcode@gmail.com>
2021-10-18 16:54:41 -0400

    changes made with eric suggestions

--- utils/collections/getConferenceMetadata.ts
+++ utils/collections/getConferenceMetadata.ts
@@ -1,34 +1,11 @@
 import { Collection } from 'types';
 
-const getConferenceTheme = (collection) => {
-	if (collection.metadata.theme) {
-		return collection.metadata.theme;
-	}
-	return collection.metadata && collection.metadata.theme;
-};
+const getConferenceTheme = (collection) => collection.metadata?.theme;
+const getConferenceAcronym = (collection) => collection.metadata?.acronym;
+const getConferenceLocation = (collection) => collection.metadata?.location;
+const getConferenceDate = (collection) => collection.metadata?.date;
 
-const getConferenceAcronym = (collection) => {
-	if (collection.metadata.acronym) {
-		return collection.metadata.acronym;
-	}
-	return collection.metadata && collection.metadata.acronym;
-};
-
-const getConferenceLocation = (collection) => {
-	if (collection.metadata.location) {
-		return collection.metadata.location;
-	}
-	return collection.metadata && collection.metadata.location;
-};
-
-const getConferenceDate = (collection) => {
-	if (collection.metadata.date) {
-		return collection.metadata.date;
-	}
-	return collection.metadata && collection.metadata.date;
-};
-
-export const ConferenceMetadata = (collection: Collection) => {
+export const conferenceMetadata = (collection: Collection) => {
 	const theme = getConferenceTheme(collection);
 	const acronym = getConferenceAcronym(collection);
 	const location = getConferenceLocation(collection);
