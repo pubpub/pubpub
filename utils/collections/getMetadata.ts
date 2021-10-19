9aa806cb363cd17fe59355c73365a1d4e6c8006b
qweliant <dmtorcode@gmail.com>
2021-10-19 09:22:22 -0400

    use existing functions

---
+++ utils/collections/getMetadata.ts
@@ -0,0 +1,42 @@
+import { Collection } from 'types';
+import { deserializeMetadata } from 'utils/collections/metadata';
+
+export const issueMetadata = (collection: Collection) => {
+	const metadata = deserializeMetadata({
+		metadata: collection.metadata,
+		kind: collection.kind,
+		fallback: null,
+	});
+
+	const {
+		printIssn,
+		electronicIssn,
+		volume,
+		issue,
+		printPublicationDate,
+		publicationDate,
+	} = metadata;
+	return { printIssn, electronicIssn, volume, issue, printPublicationDate, publicationDate };
+};
+
+export const bookMetadata = (collection: Collection) => {
+	const metadata = deserializeMetadata({
+		metadata: collection.metadata,
+		kind: collection.kind,
+		fallback: null,
+	});
+
+	const { isbn, copyright, published, edition } = metadata;
+	return { isbn, copyright, published, edition };
+};
+
+export const conferenceMetadata = (collection: Collection) => {
+	const metadata = deserializeMetadata({
+		metadata: collection.metadata,
+		kind: collection.kind,
+		fallback: null,
+	});
+
+	const { theme, location, acronym, date } = metadata;
+	return { theme, location, acronym, date };
+};
