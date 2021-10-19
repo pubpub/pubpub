6bafbb55897ca80be1c4ca12603291b795891295
qweliant <dmtorcode@gmail.com>
2021-10-19 13:07:29 -0400

    rem function

--- client/components/LayoutEditor/LayoutEditorCollectionHeader/getMetaDataChecklist.tsx
+++ client/components/LayoutEditor/LayoutEditorCollectionHeader/getMetaDataChecklist.tsx
@@ -5,7 +5,6 @@ import { LayoutBlockCollectionHeader } from 'utils/layout';
 import { Collection } from 'types';
 import { issueMetadata, bookMetadata, conferenceMetadata } from 'utils/collections/getMetadata';
 import getCollectionDoi from 'utils/collections/getCollectionDoi';
-import { getSchemaForKind } from 'utils/collections/schemas';
 
 type Content = LayoutBlockCollectionHeader['content'];
 
