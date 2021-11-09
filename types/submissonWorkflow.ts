64c6d800c2ee5fa660e96f9155311afabc5a5d9e
qweliant <dmtorcode@gmail.com>
2021-11-09 15:42:30 -0500

    add type but better

--- types/submissonWorkflow.ts
+++ types/submissonWorkflow.ts
@@ -1,10 +1,15 @@
+import { DocJson } from 'types';
+import { LayoutBlockSubmissionBanner } from 'utils/layout';
+
 export type EnabledStatus = true | false;
 
 export type SubmissionWorkflow = {
 	id: string;
-	enabled: EnabledStatus;
-	instructions?: any;
-	afterSubmittedText?: any;
-	email?: any;
-	layoutBlock?: any;
+	createdAt: string;
+	updatedAt: string;
+	enabled: boolean;
+	instructionsText: DocJson;
+	emailText: DocJson;
+	targetEmailAddress: string;
+	bannerContent: LayoutBlockSubmissionBanner['content'];
 };
