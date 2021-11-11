b93f39c7a5af3a53fa6f52222e5a80b835f48d26
qweliant <dmtorcode@gmail.com>
2021-11-11 12:25:45 -0500

    changed test since attacker cannot use submissionwkflw ID

--- server/submissionWorkflow/__tests__/api.test.ts
+++ server/submissionWorkflow/__tests__/api.test.ts
@@ -3,7 +3,7 @@ import { setup, teardown, login, modelize } from 'stubstub';
 import { SubmissionWorkflow } from '../../models';
 
 const models = modelize`
-	Community community {
+	Community {
 		Member {
 			permissions: "admin"
 			User admin {}
@@ -23,12 +23,12 @@ const models = modelize`
 			SubmissionWorkflow destroyThisSubmissionWorkflow {enabled: false}
 		}
 	}
-	Community community2{
+	Community {
 		Member {
 			permissions: "admin"
 			User anotherAdmin {}
 		}
-		Collection collection2 {}
+			
 	}
 	User guest {}
 	User anotherGuest {}
@@ -40,7 +40,7 @@ setup(beforeAll, async () => {
 });
 
 it('allows a Community manager to create a new submission workflow', async () => {
-	const { admin, community, collection } = models;
+	const { admin, collection } = models;
 	const agent = await login(admin);
 	const {
 		body: { enabled, targetEmailAddress, collectionId },
@@ -48,7 +48,6 @@ it('allows a Community manager to create a new submission workflow', async () =>
 		.post('/api/submissionWorkflows')
 		.send({
 			collectionId: collection.id,
-			communityId: community.id,
 			enabled: true,
 			targetEmailAddress: 'finnandjakeforwvwer@adventuretime.com',
 		})
@@ -60,13 +59,13 @@ it('allows a Community manager to create a new submission workflow', async () =>
 });
 
 it('forbids a different Community manager from creating a new submission workflow', async () => {
-	const { community, collection, anotherAdmin } = models;
+	const { collection, anotherAdmin } = models;
 	const agent = await login(anotherAdmin);
 	await agent
 		.post('/api/submissionWorkflows')
 		.send({
 			collectionId: collection.id,
-			communityId: community.id,
+
 			enabled: true,
 			targetEmailAddress: 'finnandjakeforwvwer@adventuretime.com',
 		})
@@ -74,13 +73,13 @@ it('forbids a different Community manager from creating a new submission workflo
 });
 
 it('forbids a random user from creating a submission workflow', async () => {
-	const { community, collection, guest, anotherGuest } = models;
+	const { collection, guest, anotherGuest } = models;
 	const agent = await login(guest);
 	await agent
 		.post('/api/submissionWorkflows')
 		.send({
 			collectionId: collection.id,
-			communityId: community.id,
+
 			enabled: true,
 			targetEmailAddress: 'finnandjakeforwvwer@adventuretime.com',
 		})
@@ -91,7 +90,7 @@ it('forbids a random user from creating a submission workflow', async () => {
 		.post('/api/submissionWorkflows')
 		.send({
 			collectionId: collection.id,
-			communityId: community.id,
+
 			enabled: true,
 			targetEmailAddress: 'finnandjakeforwvwer@adventuretime.com',
 		})
@@ -99,13 +98,13 @@ it('forbids a random user from creating a submission workflow', async () => {
 });
 
 it('forbids a non mananger from creating a submission workflow', async () => {
-	const { community, collection, collectionMember } = models;
+	const { collection, collectionMember } = models;
 	const agent = await login(collectionMember);
 	await agent
 		.post('/api/submissionWorkflows')
 		.send({
 			collectionId: collection.id,
-			communityId: community.id,
+
 			enabled: true,
 			targetEmailAddress: 'finnandjakeforwvwer@adventuretime.com',
 		})
@@ -113,14 +112,14 @@ it('forbids a non mananger from creating a submission workflow', async () => {
 });
 
 it('allows a Community manager to update an existing workflow', async () => {
-	const { collectionManager, collection, community } = models;
+	const { collectionManager, collection } = models;
 	const agent = await login(collectionManager);
 	const enabled = true;
 	await agent
 		.put('/api/submissionWorkflows')
 		.send({
 			collectionId: collection.id,
-			communityId: community.id,
+
 			enabled,
 		})
 		.expect(200);
@@ -132,14 +131,13 @@ it('allows a Community manager to update an existing workflow', async () => {
 });
 
 it('forbids a non-Community manager or attacker from updating a workflow', async () => {
-	const { collectionMember, collection, community, anotherAdmin, collection2 } = models;
+	const { collectionMember, collection, anotherAdmin } = models;
 	const agent = await login(collectionMember);
 	const enabled = true;
 	await agent
 		.put('/api/submissionWorkflows')
 		.send({
 			collectionId: collection.id,
-			communityId: community.id,
 			enabled,
 		})
 		.expect(403);
@@ -148,35 +146,32 @@ it('forbids a non-Community manager or attacker from updating a workflow', async
 	await anotherAgent
 		.put('/api/submissionWorkflows')
 		.send({
-			collectionId: collection2.id,
-			communityId: community.id,
+			collectionId: collection.id,
 			enabled,
 		})
 		.expect(403);
 });
 
 it('allows a Community manager to delete a workflow', async () => {
-	const { admin, collection, community, destroyThisSubmissionWorkflow } = models;
+	const { admin, collection, destroyThisSubmissionWorkflow } = models;
 	const agent = await login(admin);
 	await agent
 		.delete('/api/submissionWorkflows')
 		.send({
 			id: destroyThisSubmissionWorkflow.id,
 			collectionId: collection.id,
-			communityId: community.id,
 		})
 		.expect(200);
 });
 
 it('forbids a Community manager from deleting a workflow', async () => {
-	const { collectionMember, collection, community, destroyThisSubmissionWorkflow } = models;
+	const { collectionMember, collection, destroyThisSubmissionWorkflow } = models;
 	const agent = await login(collectionMember);
 	await agent
 		.delete('/api/submissionWorkflows')
 		.send({
 			id: destroyThisSubmissionWorkflow.id,
 			collectionId: collection.id,
-			communityId: community.id,
 		})
 		.expect(403);
 });
