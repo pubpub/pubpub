2569e3cf18838dd4d267522a5a44bfc45fad1039
qweliant <dmtorcode@gmail.com>
2021-12-08 06:11:12 -0500

    just looking at examples of records, had an implementation question, then made a new line, like a jackass

--- client/components/LayoutEditor/LayoutEditorPubs/PreviewElements.tsx
+++ client/components/LayoutEditor/LayoutEditorPubs/PreviewElements.tsx
@@ -25,7 +25,6 @@ const labelsForPreviewElementFields: Record<PreviewElementField, string> = {
 	hideEdges: 'Connections',
 };
 
-
 const deriveFieldStatus = (content: Content, field: PreviewElementField) => {
 	const { pubPreviewType } = content;
 	const fieldValue = content[field];
