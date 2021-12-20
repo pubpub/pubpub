cc2258f79cafa4baf763ea10e5060350f106eb2c
Ian Reynolds <idreyn@gmail.com>
2021-12-20 13:01:33 -0500

    dev: Add indexes for ActivityItem table

--- server/activityItem/model.ts
+++ server/activityItem/model.ts
@@ -1,16 +1,27 @@
 export default (sequelize, dataTypes) => {
-	return sequelize.define('ActivityItem', {
-		id: sequelize.idType,
-		kind: { type: dataTypes.TEXT, allowNull: false },
-		pubId: { type: dataTypes.UUID },
-		payload: { type: dataTypes.JSONB },
-		timestamp: {
-			type: dataTypes.DATE,
-			defaultValue: sequelize.Sequelize.NOW,
-			allowNull: false,
+	return sequelize.define(
+		'ActivityItem',
+		{
+			id: sequelize.idType,
+			kind: { type: dataTypes.TEXT, allowNull: false },
+			pubId: { type: dataTypes.UUID },
+			payload: { type: dataTypes.JSONB },
+			timestamp: {
+				type: dataTypes.DATE,
+				defaultValue: sequelize.Sequelize.NOW,
+				allowNull: false,
+			},
+			communityId: { type: dataTypes.UUID, allowNull: false },
+			actorId: { type: dataTypes.UUID },
+			collectionId: { type: dataTypes.UUID },
 		},
-		communityId: { type: dataTypes.UUID, allowNull: false },
-		actorId: { type: dataTypes.UUID },
-		collectionId: { type: dataTypes.UUID },
-	});
+		{
+			indexes: [
+				{ fields: ['communityId'], method: 'BTREE' },
+				{ fields: ['collectionId'], method: 'BTREE' },
+				{ fields: ['pubId'], method: 'BTREE' },
+				{ fields: ['actorId'], method: 'BTREE' },
+			],
+		},
+	);
 };
