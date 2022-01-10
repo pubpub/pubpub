051fda6770a1260b4b0f4c04889597a7e5c3c5cd
qweliant <dmtorcode@gmail.com>
2022-01-10 18:08:22 +0100

    3

---
+++ client/containers/Pub/PubSubmissionHeader/TitleDescriptionAbstract.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const TitleDescriptionAbstract = () => {
+	return (
+		<>
+			The information you enter in this form and pub body below will be used to create a
+			submission pub, which you can preview at any time before making your final submission.
+			<br />
+			Title of your submission pub Enter pub title here...
+			<br />
+			Abstract Description Enter description text here...
+			<br />
+			Pub Content Enter your primary submission content in the pub body below by typing or by
+			importing files in which you already have content prepared.
+		</>
+	);
+};
+
+export default TitleDescriptionAbstract;
