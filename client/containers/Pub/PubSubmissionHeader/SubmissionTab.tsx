b4793067e4b310d9b82763ff9cd83affa1c3bce9
qweliant <dmtorcode@gmail.com>
2022-01-10 18:39:28 +0100

    clearly i dont know how to overwrite css selctors

--- client/containers/Pub/PubSubmissionHeader/SubmissionTab.tsx
+++ client/containers/Pub/PubSubmissionHeader/SubmissionTab.tsx
@@ -5,19 +5,16 @@ import TitleDescriptionAbstract from './TitleDescriptionAbstract';
 import Contributors from './Contributors';
 import SpubSettings from './SpubSettings';
 
-require('./spubHeader.scss');
-
 const SubmissionTab = () => {
 	const [selectedTab, setSelectedTab] = useState('title');
 
 	return (
-		<>
+		<div>
 			<Tabs
 				id="TabsExample"
 				// @ts-expect-error ts-migrate(2322) FIXME: Type 'Dispatch<SetStateAction<string>>' is not ass... Remove this comment to see the full error message
 				onChange={setSelectedTab}
 				selectedTabId={selectedTab}
-				className="bp3-large"
 			>
 				<Tab
 					id="title"
@@ -27,7 +24,7 @@ const SubmissionTab = () => {
 				<Tab id="contributors" title="Contributors" panel={<Contributors />} />
 				<Tab id="spubsettings" title="Pub Settings" panel={<SpubSettings />} />
 			</Tabs>
-		</>
+		</div>
 	);
 };
 
