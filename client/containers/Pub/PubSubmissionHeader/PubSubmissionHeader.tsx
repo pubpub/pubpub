8d57446761592649498882832412d4eedf707f68
qweliant <dmtorcode@gmail.com>
2022-01-10 13:06:55 +0100

    love is all you need for coding, because if youre like me you may find yourself in a fugue state wondering why css is a thing, then you may read a 4 month old paper about 3d graphics recommended by gabe because 'understanding' only to have the bp3-tab-list bp3-large classname to not work...why.....because get good

--- client/containers/Pub/PubSubmissionHeader/PubSubmissionHeader.tsx
+++ client/containers/Pub/PubSubmissionHeader/PubSubmissionHeader.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Tab, Tabs, Icon, IconName } from '@blueprintjs/core';
 
+import { DocJson } from 'types';
+
 import InstructionTab from './InstructionTab';
 import SubmissionTab from './SubmissionTab';
 import PreviewTab from './PreviewTab';
@@ -21,6 +23,41 @@ const PubSubmissionHeader = () => {
 	const instructions = renderInstructionTabTitle('align-left', 'Instructions');
 	const submission = renderInstructionTabTitle('manually-entered-data', 'Submission');
 	const preview = renderInstructionTabTitle('eye-open', 'Preview & Edit');
+	const instructionNode: DocJson = {
+		type: 'doc',
+		attrs: 'What Is This',
+		content: [
+			<>
+				<h1>This is the Title for Submissions Set by the Person Calling for Submissions</h1>
+				<br />
+				<br />
+				<p>
+					You gotta do a little dance to get into a pocket dimension. You know, like in
+					Fringe. First go to the building from the tape, go to apartment 413, its
+					dangerous though. careful footing. In the middle of the room walk three steps
+					forwad, step to the left. step to the right, walk backwards three steps, step
+					left, turn left 270 degrees. step forward
+				</p>
+				<br />
+				<li>One</li>
+				<li>Two</li>
+				<li>Three</li>
+				<li>Onto</li>
+				<li>Four</li>
+				<br />
+				<br />
+				<p>
+					If you have any questions, you can email the community admin at
+					hello@institution.com, and could even contain a story about civiizations that
+					have come and gone in the time it takes for a species to evolve past its own
+					technological limtations. Do we really know what exists around us? There are
+					phenomena that exists outside the visible spectrum which we may have no clear
+					perception of.{' '}
+				</p>
+				Alll above will be be forgetten, and what is left?
+			</>,
+		],
+	};
 
 	return (
 		<div className="pub-header-component">
@@ -30,7 +67,11 @@ const PubSubmissionHeader = () => {
 				onChange={setSelectedTab}
 				selectedTabId={selectedTab}
 			>
-				<Tab id="instructions" title={instructions} panel={<InstructionTab />} />
+				<Tab
+					id="instructions"
+					title={instructions}
+					panel={<InstructionTab instructions={instructionNode} />}
+				/>
 				<Tab id="submission" title={submission} panel={<SubmissionTab />} />
 				<Tab id="preview" title={preview} panel={<PreviewTab />} />
 			</Tabs>
