af49eb4cbab833d436133758bc7a81a32c49fa9d
qweliant <dmtorcode@gmail.com>
2022-01-12 19:10:53 +0100

    pass real workflow to spub header

--- client/containers/Pub/Pub.tsx
+++ client/containers/Pub/Pub.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 
 import { usePageContext } from 'utils/hooks';
-import { SubmissionWorkflow } from 'types';
+// import { SubmissionWorkflow } from 'types';
 
 import PubSyncManager from './PubSyncManager';
 import PubHeader from './PubHeader';
@@ -10,68 +10,6 @@ import PubDocument from './PubDocument';
 import { PubSuspendWhileTypingProvider, PubSuspendWhileTyping } from './PubSuspendWhileTyping';
 
 require('./pub.scss');
-
-const workflow: SubmissionWorkflow = {
-	id: '12089e31h3f23bf-f23f23f23f2-f23f32bu234v23-f2323f',
-	createdAt: '',
-	updatedAt: '',
-	enabled: true,
-	title: 'A Submission Work-work-work-work-work-work-flow',
-	collectionId: '',
-	targetEmailAddress: '',
-	emailText: {
-		type: 'doc',
-		attrs: {
-			meta: {},
-		},
-		content: [],
-	},
-	introText: {
-		type: 'doc',
-		attrs: {
-			meta: {},
-		},
-		content: [],
-	},
-	instructionsText: {
-		type: 'doc',
-		attrs: {
-			meta: {},
-		},
-		content: [
-			{
-				type: 'heading',
-				attrs: {
-					level: 1,
-				},
-				content: [
-					{
-						type: 'text',
-						text: 'Cartographie 2022: Call for Proposals',
-					},
-				],
-			},
-			{
-				type: 'paragraph',
-				content: [
-					{
-						type: 'text',
-						text: 'You gotta do a little dance to get into a pocket dimension. You know, like in Fringe. First go to the building from the tape, go to apartment 413, itsdangerous though. careful footing. In the middle of the room walk three steps forwad, step to the left. step to the right, walk backwards three steps, step	left, turn left 270 degrees. step forward',
-					},
-				],
-			},
-			{
-				type: 'paragraph',
-				content: [
-					{
-						type: 'text',
-						text: 'If you have any questions, you can email the community admin at hello@institution.com, and could even contain a story about civiizations that have come and gone in the time it takes for a species to evolve past its own technological limtations. Do we really know what exists around us? There are phenomena that exists outside the visible spectrum which we may have no clear perception of.',
-					},
-				],
-			},
-		],
-	},
-};
 
 type Props = {
 	pubData: any;
@@ -117,8 +55,7 @@ const Pub = (props: Props) => {
 	const { loginData, locationData, communityData } = usePageContext();
 	const hasSubmission =
 		'submission' in props.pubData && props.pubData.submission?.status === 'incomplete';
-
-	console.log(props.pubData, { hasSubmission });
+	const workflow = props.pubData.submission.submissionWorkflow;
 
 	useEffect(() => {
 		const { hash } = window.location;
