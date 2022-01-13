4e150ed1cc83dceec5246e24c794dbc2ae0e2de0
Ian Reynolds <ian@knowledgefutures.org>
2022-01-13 12:17:35 -0500

    feature: Send emails for Submissions (#1748)
    * wip
    
    * code written no idea if it works
    
    * gosh I guess it does
    
    * oops forgot something very important
    
    * forgot something else very important

--- server/submission/queries.ts
+++ server/submission/queries.ts
@@ -1,25 +1,46 @@
-import { createPub } from 'server/pub/queries';
+import * as types from 'types';
 import { Collection, Submission, SubmissionWorkflow } from 'server/models';
-import {
-	Submission as SubmissionType,
-	SequelizeModel,
-	managerStatuses,
-	submitterStatuses,
-} from 'types';
+import { createPub } from 'server/pub/queries';
+import { defer } from 'server/utils/deferred';
+import { getPub } from 'server/utils/queryHelpers';
 
-const updateToStatuses = [...managerStatuses, ...submitterStatuses] as const;
+import { sendSubmissionEmail } from './emails';
+
+const updateToStatuses = [...types.managerStatuses, ...types.submitterStatuses] as const;
 
 type CreateOptions = {
 	userId: string;
 	submissionWorkflowId: string;
 };
 type UpdateToStatus = typeof updateToStatuses;
-type UpdateOptions = Partial<SubmissionType> & { status: UpdateToStatus };
+
+type UpdateOptions = Partial<types.Submission> & {
+	id: string;
+	status: UpdateToStatus;
+	skipEmail?: boolean;
+	customEmailText?: types.DocJson;
+};
+
+export const getSubmissionById = async (
+	id: string,
+): Promise<null | (types.Submission & { pub: types.DefinitelyHas<types.Pub, 'members'> })> => {
+	const submission: null | types.SequelizeModel<types.Submission> = await Submission.findOne({
+		where: { id },
+	});
+	if (submission) {
+		const pub = await getPub({ id: submission.pubId }, { getMembers: true });
+		return {
+			...submission.toJSON(),
+			pub,
+		};
+	}
+	return null;
+};
 
 export const createSubmission = async ({
 	userId,
 	submissionWorkflowId,
-}: CreateOptions): Promise<SequelizeModel<SubmissionType>> => {
+}: CreateOptions): Promise<types.SequelizeModel<types.Submission>> => {
 	const { collection } = await SubmissionWorkflow.findOne({
 		where: { id: submissionWorkflowId },
 		include: [{ model: Collection, as: 'collection' }],
@@ -41,29 +62,35 @@ export const createSubmission = async ({
 	);
 };
 
-export const updateSubmission = async (
-	patch: UpdateOptions,
-	actorId: string,
-	sendEmail = false,
-) => {
-	const { id, status } = patch;
-	const submission: SequelizeModel<SubmissionType> = await Submission.findOne({ where: { id } });
+export const updateSubmission = async (options: UpdateOptions, actorId: string) => {
+	const { id, status, customEmailText, skipEmail } = options;
+	const submission = (await getSubmissionById(id))!;
 	const previousStatus = submission.status;
 	const isBeingSubmitted = previousStatus === 'incomplete' && status === 'pending';
-	if (isBeingSubmitted && sendEmail) {
-		// Send an email here
-	}
-	return Submission.update(
+
+	await Submission.update(
 		{
-			status: patch.status,
+			status,
 			...(isBeingSubmitted && { submittedAt: new Date().toISOString() }),
 		},
 		{
-			where: { id: patch.id },
+			where: { id: options.id },
 			individualHooks: true,
 			actorId,
 		},
-	).then(() => patch);
+	);
+
+	defer(async () => {
+		if (!skipEmail) {
+			await sendSubmissionEmail({
+				previousStatus,
+				submission: (await getSubmissionById(id))!,
+				customText: customEmailText,
+			});
+		}
+	});
+
+	return { status };
 };
 
 export const destroySubmission = async ({ id }: { id: string }, actorId: string) =>
