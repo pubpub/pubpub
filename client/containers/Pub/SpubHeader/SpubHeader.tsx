364423a000472b68b2ac97c61b3c368c7e9f4775
qweliant <dmtorcode@gmail.com>
2022-03-09 10:43:55 -0600

    undo changes to SpubHeader

--- client/containers/Pub/SpubHeader/SpubHeader.tsx
+++ client/containers/Pub/SpubHeader/SpubHeader.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { TabId } from '@blueprintjs/core';
+import { Tab, Tabs, TabId, Icon, IconName } from '@blueprintjs/core';
 
 import { DocJson, DefinitelyHas, PubHistoryState, PubPageData } from 'types';
 import { assert } from 'utils/assert';
@@ -7,8 +7,9 @@ import { apiFetch } from 'client/utils/apiFetch';
 import { getEmptyDoc } from 'components/Editor';
 
 import { usePubContext } from '../pubHooks';
-
-import SpubHeaderToolbar from './SpubHeaderToolBar/SpubHeaderToolbar';
+import InstructionsTab from './InstructionsTab';
+import SubmissionTab from './SubmissionTab';
+import PreviewTab from './PreviewTab';
 
 require('./spubHeader.scss');
 
@@ -20,6 +21,12 @@ type Props = {
 	) => unknown;
 	pubData: DefinitelyHas<PubPageData, 'submission'>;
 };
+
+export const renderTabTitle = (icon: IconName, title: string) => (
+	<>
+		<Icon icon={icon} /> {title}
+	</>
+);
 
 const SpubHeader = (props: Props) => {
 	const [selectedTab, setSelectedTab] = useState<TabId>('instructions');
@@ -53,28 +60,60 @@ const SpubHeader = (props: Props) => {
 	const updateHistoryData = (newHistoryData: Partial<PubHistoryState>) => {
 		return props.updateLocalData('history', newHistoryData);
 	};
+	const instructionTabTitle = renderTabTitle('align-left', 'Instructions');
+	const submissionTabTitle = renderTabTitle('manually-entered-data', 'Submission');
+	const previewTabTitle = renderTabTitle('eye-open', 'Preview & Submit');
+	const maybeActiveClass = (tabId: string) => `${tabId === selectedTab ? 'active' : 'inactive'}`;
 
 	useEffect(() => {
-		if (updatePubData) {
-			updatePubData({ isReadOnly: selectedTab === 'preview' });
-		}
+		updatePubData({ isReadOnly: selectedTab === 'preview' });
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [selectedTab]);
 
 	return (
-		<>
-			<SpubHeaderToolbar
-				selectedTab={selectedTab}
-				onSelectTab={setSelectedTab}
-				submissionWorkflow={props.pubData.submission.submissionWorkflow}
-				pubData={props.pubData}
-				abstract={abstract}
-				onUpdatePub={updateAndSavePubData}
-				onUpdateAbstract={updateAbstract}
-				updateHistoryData={updateHistoryData}
-				historyData={props.historyData}
+		<Tabs
+			id="spubHeader"
+			onChange={(t) => setSelectedTab(t)}
+			selectedTabId={selectedTab}
+			className="spub-header-component tabs bp3-large"
+		>
+			<Tab
+				id="instructions"
+				title={instructionTabTitle}
+				className={`tab-panel ${maybeActiveClass('instructions')}`}
+				panel={
+					<InstructionsTab
+						submissionWorkflow={props.pubData.submission.submissionWorkflow}
+					/>
+				}
 			/>
-		</>
+
+			<Tab
+				id="submission"
+				title={submissionTabTitle}
+				className={`tab-panel ${maybeActiveClass('submission')}`}
+				panel={
+					<SubmissionTab
+						abstract={abstract}
+						onUpdatePub={updateAndSavePubData}
+						onUpdateAbstract={updateAbstract}
+						pubData={props.pubData}
+					/>
+				}
+			/>
+			<Tab
+				id="preview"
+				title={previewTabTitle}
+				className={`${maybeActiveClass('preview')}`}
+				panel={
+					<PreviewTab
+						updateHistoryData={updateHistoryData}
+						historyData={props.historyData}
+						pubData={props.pubData}
+					/>
+				}
+			/>
+		</Tabs>
 	);
 };
 
--- client/containers/Pub/SpubHeader/SpubHeaderToolBar/SpubHeaderToolbar.tsx
+++ client/containers/Pub/SpubHeader/SpubHeaderToolBar/SpubHeaderToolbar.tsx
@@ -2,12 +2,6 @@ import React, { Dispatch, SetStateAction } from 'react';
 import { Tab, Tabs, TabId, Icon, IconName } from '@blueprintjs/core';
 
 import { GridWrapper } from 'components';
-import { DefinitelyHas, DocJson, PubHistoryState, PubPageData, SubmissionWorkflow } from 'types';
-import { assert } from 'utils/assert';
-
-import InstructionsTab from '../InstructionsTab';
-import SubmissionTab from '../SubmissionTab';
-import PreviewTab from '../PreviewTab';
 
 const renderTabTitle = (icon: IconName, title: string) => (
 	<>
@@ -18,27 +12,18 @@ const renderTabTitle = (icon: IconName, title: string) => (
 type Props = {
 	selectedTab: TabId;
 	onSelectTab: Dispatch<SetStateAction<TabId>>;
-	submissionWorkflow: SubmissionWorkflow;
-	pubData: DefinitelyHas<PubPageData, 'submission'>;
-	abstract: DocJson;
-	onUpdatePub: (pub: Partial<PubPageData>) => unknown;
-	onUpdateAbstract: (abstract: DocJson) => Promise<unknown>;
-	updateHistoryData: (patch: Partial<PubHistoryState>) => unknown;
-	historyData: PubHistoryState;
 };
 
 const SpubHeaderToolbar = (props: Props) => {
-	assert(props.pubData.submission.submissionWorkflow !== undefined);
-
 	const instructionTabTitle = renderTabTitle('align-left', 'Instructions');
 	const submissionTabTitle = renderTabTitle('manually-entered-data', 'Submission');
+	const contributorsTabTitle = renderTabTitle('people', 'Contributors');
 	const previewTabTitle = renderTabTitle('eye-open', 'Preview & Submit');
 	const maybeActiveClass = (tabId: string) =>
 		`${tabId === props.selectedTab ? 'active' : 'inactive'}`;
 
 	return (
 		<GridWrapper>
-			{' '}
 			<Tabs
 				id="spubHeader"
 				onChange={props.onSelectTab}
@@ -49,37 +34,23 @@ const SpubHeaderToolbar = (props: Props) => {
 					id="instructions"
 					title={instructionTabTitle}
 					className={`tab-panel ${maybeActiveClass('instructions')}`}
-					panel={
-						<InstructionsTab
-							submissionWorkflow={props.pubData.submission.submissionWorkflow}
-						/>
-					}
 				/>
 
 				<Tab
 					id="submission"
 					title={submissionTabTitle}
 					className={`tab-panel ${maybeActiveClass('submission')}`}
-					panel={
-						<SubmissionTab
-							abstract={props.abstract}
-							onUpdatePub={props.onUpdatePub}
-							onUpdateAbstract={props.onUpdateAbstract}
-							pubData={props.pubData}
-						/>
-					}
+				/>
+
+				<Tab
+					id="contributors"
+					title={contributorsTabTitle}
+					className={`tab-panel ${maybeActiveClass('contributors')}`}
 				/>
 				<Tab
 					id="preview"
 					title={previewTabTitle}
 					className={`${maybeActiveClass('preview')}`}
-					panel={
-						<PreviewTab
-							updateHistoryData={props.updateHistoryData}
-							historyData={props.historyData}
-							pubData={props.pubData}
-						/>
-					}
 				/>
 			</Tabs>
 		</GridWrapper>
