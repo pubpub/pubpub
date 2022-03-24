a49d33d03ae16654bdf6eed761a2a3a6f2241e85
qweliant <dmtorcode@gmail.com>
2022-03-24 18:14:10 -0400

    rmv member import

--- server/submission/__tests__/api.test.ts
+++ server/submission/__tests__/api.test.ts
@@ -1,7 +1,7 @@
 /* global describe, it, expect, beforeAll, afterAll, beforeEach, jest */
 import { setup, teardown, login, modelize, expectCreatedActivityItem, stub } from 'stubstub';
 import * as types from 'types';
-import { Member, Submission, SubmissionWorkflow } from 'server/models';
+import { Submission, SubmissionWorkflow } from 'server/models';
 import { finishDeferredTasks } from 'server/utils/deferred';
 
 const models = modelize`
