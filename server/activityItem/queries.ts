d65bd9e843cf05a3ab251da8a747f395c614d190
qweliant <dmtorcode@gmail.com>
2022-03-24 00:42:20 -0400

    fixed activity items

--- server/activityItem/queries.ts
+++ server/activityItem/queries.ts
@@ -557,38 +557,7 @@ export const createPubDiscussionCommentAddedActivityItem = async (
 	});
 };
 
-export const createSubmissionActivityItem = async (
-	kind: 'submission-deleted' | 'submission-updated',
-	actorId: null | string,
-	submissionId: string,
-) => {
-	const {
-		pubId,
-		pub: { title, communityId },
-	}: types.DefinitelyHas<types.Submission, 'pub'> = await Submission.findOne({
-		where: { id: submissionId },
-		include: [
-			{
-				model: Pub,
-				as: 'pub',
-			},
-		],
-	});
-	return createActivityItem({
-		actorId,
-		communityId,
-		pubId,
-		kind,
-		payload: {
-			submissionId,
-			pub: {
-				title,
-			},
-		},
-	});
-};
-
-export const createSubmissionStatusChangedActivityItem = async (
+export const createSubmissionStatusUpdatedActivityItem = async (
 	actorId: null | string,
 	submissionId: string,
 	oldSubmission: types.Submission,
@@ -606,6 +575,7 @@ export const createSubmissionStatusChangedActivityItem = async (
 	return createActivityItem({
 		actorId,
 		communityId: submission.pub.communityId,
+		collectionId: submission.submissionWorkflow?.collectionId || '',
 		pubId: submission.pub.id,
 		kind: 'submission-updated' as const,
 		payload: {
--- server/submission/hooks.ts
+++ server/submission/hooks.ts
@@ -1,8 +1,8 @@
 import { Submission } from 'server/models';
 import { createActivityHooks } from 'server/utils/activityHooks';
-import { createSubmissionStatusChangedActivityItem } from 'server/activityItem/queries';
+import { createSubmissionStatusUpdatedActivityItem } from 'server/activityItem/queries';
 
 createActivityHooks({
 	Model: Submission,
-	onModelUpdated: createSubmissionStatusChangedActivityItem,
+	onModelUpdated: createSubmissionStatusUpdatedActivityItem,
 });
