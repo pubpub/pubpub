b7293aabe3fe226f53ff474af49e3b848848bd1a
Eric McDaniel <eric.g.mcdaniel@gmail.com>
2022-03-28 15:02:36 -0400

    feat: add pub word count button to formatting bar (#1890)
    * feat: add pub word count button to formatting bar
    
    * feat: ensure counts are calculated once; remove unused export
    
    * fix: character count should include spaces
    
    * fix: add `doc` to dependency array for word/char count
    
    * fix: just remove counts when popover closes
    
    * fix: re-add toLocaleString

---
+++ client/containers/Pub/PubDocument/PubWordCountButton.tsx
@@ -0,0 +1,59 @@
+import React, { useMemo, useState } from 'react';
+import { Popover, Button, Icon } from '@blueprintjs/core';
+
+import { Node } from 'prosemirror-model';
+
+require('./pubWordCountButton.scss');
+
+type Props = {
+	doc: Node;
+};
+
+const getWordAndCharacterCountsFromDoc = (node: Node) => {
+	const text = node.textBetween(0, node.content.size, ' ', ' ');
+	const words = text.split(' ').filter((word) => word !== '');
+	return [words.length, text.length];
+};
+
+const PubHeaderFormattingWordCountButton = (props: Props) => {
+	const { doc } = props;
+	const [open, setOpen] = useState(false);
+	const counts = useMemo(() => {
+		if (open) {
+			return getWordAndCharacterCountsFromDoc(doc);
+		}
+		return null;
+	}, [open, doc]);
+	const content = (
+		<div className="pub-word-count-button-popover-content">
+			<dl>
+				<dt>Words</dt>
+				<dd>{counts?.[0].toLocaleString()}</dd>
+				<dt>Characters</dt>
+				<dd>{counts?.[1].toLocaleString()}</dd>
+			</dl>
+		</div>
+	);
+
+	return (
+		<Popover
+			content={content}
+			isOpen={open}
+			minimal
+			position="bottom-right"
+			popoverClassName="pub-word-count-button-popover"
+			targetClassName="pub-word-count-button-target"
+		>
+			<Button
+				role="button"
+				className="bp3-button bp3-minimal"
+				onClick={() => setOpen(!open)}
+				onBlur={() => setOpen(false)}
+			>
+				<Icon icon="timeline-line-chart" />
+			</Button>
+		</Popover>
+	);
+};
+
+export default PubHeaderFormattingWordCountButton;
