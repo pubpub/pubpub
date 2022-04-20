b4b1cc2f4be09c88549553e06803701734530870
ben howe <howebenhowe@gmail.com>
2022-04-20 15:11:48 -0400

    change to destroy single pub rather than bulk, to trigger hooks

--- server/pub/queries.ts
+++ server/pub/queries.ts
@@ -106,12 +106,11 @@ export const updatePub = (inputValues, updatePermissions, actorId) => {
 	});
 };
 
-export const destroyPub = (pubId: string, actorId: null | string = null) => {
-	return Pub.destroy({
+export const destroyPub = async (pubId: string, actorId: null | string = null) => {
+	const pub = await Pub.deleteOne({
 		where: { id: pubId },
-		individualHooks: true,
-		actorId,
-	}).then(() => {
+	});
+	return pub.destroy({ actorId }).then(() => {
 		deletePubSearchData(pubId);
 		return true;
 	});
