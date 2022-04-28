36ff17d89e6f28f4cae871a17cf1ba3cd84b0273
qweliant <dmtorcode@gmail.com>
2022-04-28 15:40:10 -0400

    rmv log

--- server/utils/ssr.tsx
+++ server/utils/ssr.tsx
@@ -275,7 +275,6 @@ export const generateMetaComponents = (metaProps: MetaProps) => {
 				/>
 			);
 		});
-		console.log(contributorRoleTags);
 		outputComponents = [
 			...outputComponents,
 			citationAuthorTags,
