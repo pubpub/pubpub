6cec16d9238bd8393f00eb8f157edf9c866ecd83
Eric McDaniel <eric.g.mcdaniel@gmail.com>
2022-05-03 12:04:26 -0400

    fix: wsod when removing abstract from submitted submission; add warning

--- client/containers/Pub/SpubHeader/AbstractEditor.tsx
+++ client/containers/Pub/SpubHeader/AbstractEditor.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React, { useEffect, useMemo } from 'react';
 
 import { Callback, DocJson } from 'types';
 import { MinimalEditor } from 'components';
@@ -30,6 +30,12 @@ const AbstractEditor = (props: Props) => {
 		return { readOnlyAbstractDoc: null, readOnlyEditorKey: Date.now() };
 	}, [isReadOnly, editorChangeObject, initialDoc]);
 
+	useEffect(() => {
+		if (isReadOnly) {
+			onUpdateAbstract(readOnlyAbstractDoc as DocJson);
+		}
+	}, [isReadOnly, readOnlyAbstractDoc]);
+
 	const sharedProps = {
 		customNodes: { doc: { content: 'paragraph' } },
 		constrainHeight: true,
@@ -58,5 +64,4 @@ const AbstractEditor = (props: Props) => {
 		/>
 	);
 };
-
 export default AbstractEditor;
