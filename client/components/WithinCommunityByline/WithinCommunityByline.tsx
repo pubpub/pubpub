66061efb449e709ae8d9a7d04a0210dd45b8ad8a
Gabriel Stein <g@gabestein.com>
2022-05-12 16:48:29 -0400

    don't mutate array and handle last name collissions

--- client/components/WithinCommunityByline/WithinCommunityByline.tsx
+++ client/components/WithinCommunityByline/WithinCommunityByline.tsx
@@ -10,25 +10,38 @@ type Props = Omit<BylineProps, 'contributors'> & {
 	contributors: AttributionWithUser[];
 };
 
+const sortContributorsByLastName = (contributors) => {
+	return contributors.sort((last, next) => {
+		if (last.user.lastName === next.user.lastName) {
+			return last.user.firstName - next.user.firstName;
+		}
+		if (last.user.lastName > next.user.lastName) {
+			return 1;
+		}
+		return -1;
+	});
+};
+
 const WithinCommunityByline = (props: Props) => {
 	const { communityData } = usePageContext();
 	const { contributors } = props;
 	let { bylinePrefix } = props;
 	// TODO: Add function to get the sort option from community settings, and add other sort options
-	if (communityData.id === '1a71ef4d-f6fe-40d3-8379-42fa2141db58') {
-		contributors.sort((last, next) => {
-			if (last.user.lastName > next.user.lastName) {
-				return -1;
-			}
-			return 1;
-		});
-		bylinePrefix = (
-			<Tooltip
-				content="Contributors (A-Z)"
-				children={<Icon iconSize={12} icon="sort-alphabetical" className="byline-icon" />}
-			/>
-		);
-	}
-	return <Byline {...props} bylinePrefix={bylinePrefix} contributors={contributors} />;
+	const sortedContributors =
+		communityData.id === '1a71ef4d-f6fe-40d3-8379-42fa2141db58'
+			? sortContributorsByLastName(contributors)
+			: contributors;
+	bylinePrefix =
+		communityData.id === '1a71ef4d-f6fe-40d3-8379-42fa2141db58'
+			? (bylinePrefix = (
+					<Tooltip
+						content="Contributors (A-Z)"
+						children={
+							<Icon iconSize={12} icon="sort-alphabetical" className="byline-icon" />
+						}
+					/>
+			  ))
+			: bylinePrefix;
+	return <Byline {...props} bylinePrefix={bylinePrefix} contributors={sortedContributors} />;
 };
 export default WithinCommunityByline;
