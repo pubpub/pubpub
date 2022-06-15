2e8e0891ab785c9fe27499530884b3896a1047b5
ben howe <howebenhowe@gmail.com>
2022-06-15 12:50:19 -0400

    Flip order to successfully walk the tree with correct positions

--- client/components/Editor/plugins/equationsToMath.ts
+++ client/components/Editor/plugins/equationsToMath.ts
@@ -1,30 +1,43 @@
+import { Node } from 'prosemirror-model';
 import { EditorState, Plugin } from 'prosemirror-state';
 import { EditorView } from 'prosemirror-view';
 
+type Replacement = {
+	pos: number;
+	replacementNode: Node;
+};
+
 const getEquationTranslationTransactionForState = (editorState: EditorState) => {
 	const transaction = editorState.tr;
+	const replacements: Replacement[] = [];
 	let mustReturnTransaction = false;
 	const mathInlineType = editorState.schema.nodes.math_inline;
 	const mathDisplayType = editorState.schema.nodes.math_display;
 	editorState.doc.descendants((node, pos) => {
-		if (node.type.name === 'equation') {
-			const replacementInlineMath = mathInlineType.create(
+		const replacementNodeType =
+			node.type.name === 'equation'
+				? mathInlineType
+				: node.type.name === 'block_equation'
+				? mathDisplayType
+				: null;
+		if (replacementNodeType) {
+			const replacementNode = replacementNodeType.create(
 				null,
 				editorState.schema.text(node.attrs.value),
 			);
-			transaction.replaceWith(pos, pos + 1, replacementInlineMath);
-			mustReturnTransaction = true;
-		}
-		if (node.type.name === 'block_equation') {
-			const replacementDisplayMath = mathDisplayType.create(
-				null,
-				editorState.schema.text(node.attrs.value),
-			);
-			transaction.replaceWith(pos, pos + 1, replacementDisplayMath);
+			replacements.push({ pos, replacementNode });
 			mustReturnTransaction = true;
 		}
 	});
-	return mustReturnTransaction ? transaction : null;
+	if (mustReturnTransaction) {
+		replacements
+			.reverse()
+			.forEach(({ pos, replacementNode }) =>
+				transaction.replaceWith(pos, pos + 1, replacementNode),
+			);
+		return transaction;
+	}
+	return null;
 };
 
 export default (_, props) => {
