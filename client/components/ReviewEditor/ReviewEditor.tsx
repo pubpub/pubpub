3249768aa831284af8496685bffb70a05a0210b6
qweliant <dmtorcode@gmail.com>
2022-07-11 18:05:44 -0400

    sidebar is not sticky

---
+++ client/components/ReviewEditor/ReviewEditor.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { MinimalEditor } from 'components';
+
+require('./reviewEditor.scss');
+
+const ReviewEditor = () => {
+	const sharedProps = {
+		customNodes: { doc: { content: 'paragraph' } },
+		constrainHeight: true,
+		noMinHeight: true,
+	};
+
+	return (
+		<div className="review-editor">
+			<MinimalEditor
+				{...sharedProps}
+				getButtons={(buttons) => buttons.minimalButtonSet}
+				onEdit={() => {
+					console.log('it has been done');
+				}}
+				debounceEditsMs={300}
+				useFormattingBar
+			/>
+		</div>
+	);
+};
+
+export default ReviewEditor;
