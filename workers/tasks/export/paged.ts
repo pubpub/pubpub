8ca529ecfe88d05209c4d2029324ab3b008aafb6
Eric McDaniel <eric.g.mcdaniel@gmail.com>
2022-07-14 16:00:30 -0400

    feat: pubstash access key

--- workers/tasks/export/paged.ts
+++ workers/tasks/export/paged.ts
@@ -2,7 +2,15 @@ export const exportWithPaged = async (html: string) => {
 	const response = await fetch(`${process.env.PUBSTASH_URL}/convert?format=pdf`, {
 		method: 'POST',
 		body: html,
-		headers: { 'Content-Type': 'text/plain' },
+		headers: {
+			Authorization: process.env.PUBSTASH_ACCESS_KEY ?? '',
+			'Content-Type': 'text/plain',
+		},
 	});
+
+	if (!response.ok) {
+		throw new Error(`PDF export failed: ${await response.text()}`);
+	}
+
 	return response.json();
 };
