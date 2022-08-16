45fcfad72faf0cfc976e57ea7c493de18de44cef
qweliant <dmtorcode@gmail.com>
2022-08-16 15:05:59 -0500

    rmv story

--- client/containers/Pub/SpubHeader/SpubHeader.stories.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-
-import SpubHeader from './SpubHeader';
-import './SpubHeader.stories.scss';
-
-storiesOf('containers/Pub/SpubHeader', module).add('default', () => <SpubHeader />);
