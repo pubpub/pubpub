f4402c6c4c712e21de7c362be09848dff2c2c838
Eric McDaniel <eric.g.mcdaniel@gmail.com>
2022-11-17 15:51:13 -0500

    feat: cleanup

--- deposit/datacite/index.tsx
+++ deposit/datacite/index.tsx
@@ -1,29 +1,17 @@
 /** @jsx x */
 /** @jsxFrag null */
-import { renderXml, x } from '@pubpub/deposit-utils/datacite';
-import { isFullResource, sanitizePubEdges } from 'deposit/utils';
-import { getCommunityDepositTarget } from 'server/depositTarget/queries';
-import { Collection, Community, Doc, Pub } from 'server/models';
-import { buildPubOptions } from 'server/utils/queryHelpers';
-import * as types from 'types';
-import { assert, expect } from 'utils/assert';
-import { getDois } from 'utils/crossref/createDeposit';
-import { getLicenseForPub } from 'utils/licenses';
-import { getTextAbstract, getWordAndCharacterCountsFromDoc } from 'utils/pub/metadata';
-import { getPubPublishedDate } from 'utils/pub/pubDates';
-import { RelationType } from 'utils/pubEdge';
-import { editorSchema } from 'components/Editor';
-import { Node } from 'prosemirror-model';
+import { x } from '@pubpub/deposit-utils/datacite';
 import {
-	AnyResource,
 	Resource,
 	ResourceContribution,
 	ResourceDescriptor,
 	ResourceKind,
 	ResourceSummaryKind,
 } from 'deposit/types';
+import { Collection, Community, Pub } from 'server/models';
+import { buildPubOptions } from 'server/utils/queryHelpers';
+import * as types from 'types';
 
-type DepositableCollection = types.Collection;
 type DepositablePub = types.PubWithConnections & {
 	collectionPubs: types.DefinitelyHas<types.CollectionPub, 'collection'>[];
 };
@@ -156,123 +144,6 @@ export async function createDeposit(resource: Resource, doi: string) {
 			)}
 		</resource>
 	);
-	// const context = await getCreateDepositContext(ids);
-	// assert('pub' in context);
-	// const { pub, parentPubEdge: pubEdge } = sanitizePubEdges(context.pub, true);
-	// const dois = getDois(
-	// 	pubEdge && pubEdge.relationType === RelationType.Supplement
-	// 		? { ...context, pubEdge }
-	// 		: context,
-	// 	'pub',
-	// 	getCommunityDepositTarget(ids.communityId, 'datacite'),
-	// );
-	// const release = pub.releases[pub.releases.length - 1];
-	// const releaseDoc = await Doc.findOne({ where: { id: release.docId } });
-	// const abstract = getTextAbstract(releaseDoc.content);
-	// const creators = pub.attributions.filter((attribution) => attribution.isAuthor);
-	// const contributors = pub.attributions.filter((attribution) => !attribution.isAuthor);
-	// const license = getLicenseForPub(pub, context.community);
-	// const [wordCount] = getWordAndCharacterCountsFromDoc(
-	// 	Node.fromJSON(editorSchema, releaseDoc.content),
-	// );
-	// return renderXml(
-	// 	<resource xmlns="http://datacite.org/schema/kernel-4">
-	// 		<resourceType resourceTypeGeneral="Journal">Some additional text</resourceType>
-	// 		<descriptions>
-	// 			{pub.description ? (
-	// 				<description xml:lang="en-US" descriptionType="Other">
-	// 					{pub.description}
-	// 				</description>
-	// 			) : undefined}
-	// 			{abstract.length > 0 ? (
-	// 				<description xml:lang="en-US" descriptionType="Abstract">
-	// 					{abstract}
-	// 				</description>
-	// 			) : undefined}
-	// 		</descriptions>
-	// 		<subjects>
-	// 			<subject schemeURI=""></subject>
-	// 		</subjects>
-	// 		<titles>
-	// 			<title xml:lang="en-US">{pub.title}</title>
-	// 		</titles>
-	// 		<creators>
-	// 			{creators.map((attribution) => {
-	// 				return (
-	// 					<creator>
-	// 						<creatorName>
-	// 							{attribution.user?.fullName ?? attribution.name}
-	// 						</creatorName>
-	// 						{attribution.affiliation ? (
-	// 							// @ts-expect-error
-	// 							<affiliation>{attribution.affiliation}</affiliation>
-	// 						) : undefined}
-	// 						{attribution.orcid ? (
-	// 							// @ts-expect-error
-	// 							<nameIdentifier
-	// 								nameIdentifierScheme="ORCID"
-	// 								schemeURI="https://orcid.org"
-	// 							>
-	// 								{attribution.orcid}
-	// 								{/* @ts-expect-error */}
-	// 							</nameIdentifier>
-	// 						) : undefined}
-	// 						{attribution.user ? (
-	// 							<>
-	// 								{/* @ts-expect-error */}
-	// 								<givenName>{attribution.user.firstName}</givenName>
-	// 								{/* @ts-expect-error */}
-	// 								<familyName>{attribution.user.lastName}</familyName>
-	// 							</>
-	// 						) : undefined}
-	// 					</creator>
-	// 				);
-	// 			})}
-	// 		</creators>
-	// 		<contributors>
-	// 			{contributors.map((attribution) => {
-	// 				return (
-	// 					<contributor contributorType="Editor">
-	// 						<contributorName xml:lang="en-US">
-	// 							{attribution.user?.fullName ?? attribution.name}
-	// 						</contributorName>
-	// 						{attribution.user ? (
-	// 							<>
-	// 								{/* @ts-expect-error */}
-	// 								<givenName>{attribution.user.firstName}</givenName>
-	// 								{/* @ts-expect-error */}
-	// 								<familyName>{attribution.user.lastName}</familyName>
-	// 							</>
-	// 						) : undefined}
-	// 					</contributor>
-	// 				);
-	// 			})}
-	// 		</contributors>
-	// 		<identifier identifierType="DOI">{dois.pub}</identifier>
-	// 		<publisher>PubPub</publisher>
-	// 		<publicationYear>{expect(getPubPublishedDate(pub)).getFullYear()}</publicationYear>
-	// 		<dates>
-	// 			<date dateType="Available">
-	// 				{getPubPublishedDate(pub)!.toISOString().slice(0, 10)}
-	// 			</date>
-	// 			{pub.releases.length > 0 ? (
-	// 				<date dateType="Updated">
-	// 					{(release.createdAt as unknown as Date).toISOString().slice(0, 10)}
-	// 				</date>
-	// 			) : undefined}
-	// 		</dates>
-	// 		<relatedItems></relatedItems>
-	// 		<rightsList>
-	// 			<rights xml:lang="en-US" rightsURI={license.link ?? undefined}>
-	// 				{license.full}
-	// 			</rights>
-	// 		</rightsList>
-	// 		<sizes>
-	// 			<size>${wordCount} words</size>
-	// 		</sizes>
-	// 	</resource>,
-	// 	// false,
-	// );
 }
 
 export async function submitDeposit(metadataXml: string, doi: string) {
