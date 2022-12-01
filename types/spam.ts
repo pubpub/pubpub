69fd553b4c897ab73e399b045ab554efb7b76bcc
Ian Reynolds <idreyn@gmail.com>
2022-12-01 16:07:59 -0500

    zoop

--- types/spam.ts
+++ types/spam.ts
@@ -13,21 +13,3 @@ export type SpamVerdict = Pick<
 	SpamTag,
 	'spamScore' | 'spamScoreVersion' | 'spamScoreComputedAt' | 'fields'
 >;
-
-export type SpamCommunityQueryOrderingField =
-	| 'community-created-at'
-	| 'spam-score'
-	| 'spam-tag-updated-at';
-
-export type SpamCommunityQueryOrdering = {
-	direction: 'ASC' | 'DESC';
-	field: SpamCommunityQueryOrderingField;
-};
-
-export type SpamCommunityQuery = {
-	status: SpamStatus[];
-	offset?: number;
-	limit?: number;
-	searchTerm?: string;
-	ordering: SpamCommunityQueryOrdering;
-};
