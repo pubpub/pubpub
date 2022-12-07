21b43feb48751d36ec80f856ce345abe3cc66e3f
Ian Reynolds <idreyn@gmail.com>
2022-12-07 11:37:14 -0500

    feature: Community search (for spam dashboard) (#2378)
    * feature: Superadmin dash and LandingPageFeature manager
    
    * stub out spam stuff for a hot second
    
    * ever prettier
    
    * feature: SpamTag model, queries, etc.
    
    * zoop
    
    * add some basic API tests
    
    * add a statusUpdatedAt field
    
    * feature: Community search (for spam dashboard)
    
    * add the API route
    
    * add types too
    
    * rename ordering field
    
    * rename a fn

--- server/spamTag/api.ts
+++ server/spamTag/api.ts
@@ -3,6 +3,7 @@ import { ForbiddenError } from 'server/utils/errors';
 
 import { canManipulateSpamTags } from './permissions';
 import { updateSpamTagForCommunity } from './queries';
+import { queryCommunitiesForSpamManagement } from './communities';
 
 app.put(
 	'/api/spamTags',
@@ -16,3 +17,19 @@ app.put(
 		return res.status(200).send({});
 	}),
 );
+
+app.post('/api/spamTags/queryCommunitiesForSpam', async (req, res) => {
+	const { offset, limit, searchTerm, status, ordering } = req.body;
+	const canQuery = await canManipulateSpamTags({ userId: req.user?.id });
+	if (!canQuery) {
+		throw new ForbiddenError();
+	}
+	const queryResult = await queryCommunitiesForSpamManagement({
+		offset: offset && parseInt(offset, 10),
+		limit: limit && parseInt(limit, 10),
+		ordering,
+		searchTerm,
+		status,
+	});
+	return res.status(200).send(queryResult);
+});
--- types/spam.ts
+++ types/spam.ts
@@ -14,3 +14,21 @@ export type SpamVerdict = Pick<
 	SpamTag,
 	'spamScore' | 'spamScoreVersion' | 'spamScoreComputedAt' | 'fields'
 >;
+
+export type SpamCommunityQueryOrderingField =
+	| 'community-created-at'
+	| 'spam-score'
+	| 'spam-status-updated-at';
+
+export type SpamCommunityQueryOrdering = {
+	direction: 'ASC' | 'DESC';
+	field: SpamCommunityQueryOrderingField;
+};
+
+export type SpamCommunityQuery = {
+	status: SpamStatus[];
+	offset?: number;
+	limit?: number;
+	searchTerm?: string;
+	ordering: SpamCommunityQueryOrdering;
+};
