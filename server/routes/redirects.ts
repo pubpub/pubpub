5bf6d4e4f55300407845f53a4467b6987d7a39cf
Gabriel Stein <g@gabestein.com>
2022-12-09 11:26:32 -0500

    try this fwd

--- server/routes/redirects.ts
+++ server/routes/redirects.ts
@@ -72,7 +72,8 @@ const redirectHosts = {
 			'https://research.arcadiascience.com/pub/perspective-publishing-round-one',
 		'/pub/publishing-round-one/release/1':
 			'https://research.arcadiascience.com/pub/perspective-publishing-round-one/release/1',
-		'/pub/*/comment/*': 'https://research.arcadiascience.com/pub/$1/$2',
+		'/pub/idea-defining-actin/comment/release/1*':
+			'https://research.arcadiascience.com/pub/idea-defining-actin$1',
 	},
 };
 
