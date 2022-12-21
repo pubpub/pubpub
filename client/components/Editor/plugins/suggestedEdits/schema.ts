edd1c7d65362869dacf87a5cc174a987a2c744a7
Ian Reynolds <idreyn@gmail.com>
2022-12-21 11:27:54 -0500

    in the end we just needed parseDOM

--- client/components/Editor/plugins/suggestedEdits/schema.ts
+++ client/components/Editor/plugins/suggestedEdits/schema.ts
@@ -3,12 +3,40 @@ import { Mark, MarkSpec } from 'prosemirror-model';
 export const marks: Record<string, MarkSpec> = {
 	suggested_edits_addition: {
 		inclusive: false,
+		parseDOM: [
+			{
+				tag: 'span',
+				getAttrs: (node: string | HTMLElement) => {
+					if (
+						node instanceof HTMLElement &&
+						node.hasAttribute('data-suggested-edits-addition')
+					) {
+						return {};
+					}
+					return false;
+				},
+			},
+		],
 		toDOM: (_: Mark, inline: boolean) => {
 			return [inline ? 'span' : 'div', { 'data-suggested-edits-addition': true }];
 		},
 	},
 	suggested_edits_removal: {
 		inclusive: false,
+		parseDOM: [
+			{
+				tag: 'span',
+				getAttrs: (node: string | HTMLElement) => {
+					if (
+						node instanceof HTMLElement &&
+						node.hasAttribute('data-suggested-edits-removal')
+					) {
+						return {};
+					}
+					return false;
+				},
+			},
+		],
 		toDOM: (_: Mark, inline: boolean) => {
 			return [inline ? 'span' : 'div', { 'data-suggested-edits-removal': true }];
 		},
