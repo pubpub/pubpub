758ef7af83482bff45582791213ae1f93a091e8c
Gabriel Stein <g@gabestein.com>
2023-01-19 12:24:28 -0500

    Add custom JS for eLife

--- utils/customScripts.ts
+++ utils/customScripts.ts
@@ -4,6 +4,7 @@ const communitiesWithCustomScriptEnabled = [
 	'e164d5cb-585e-4170-bad8-10d09e08d1bc', // Contours
 	'0417b0c0-cd38-48bd-8a84-b0b95da98813', // Demo
 	'36a3e2c8-74ed-417f-b616-4b57b85b9338', // digitalpubsummit
+	'2a12f668-ff6b-4403-aec7-d7438e5f9fb4', // elife-container
 ];
 
 export const communityCanUseCustomScripts = (communityId: string) => {
