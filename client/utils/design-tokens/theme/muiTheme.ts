59b61955095c3169e09d900e8777cd032b99b3fa
qweliant <dmtorcode@gmail.com>
2023-02-28 15:29:02 -0500

    push

--- client/utils/design-tokens/theme/muiTheme.ts
+++ client/utils/design-tokens/theme/muiTheme.ts
@@ -1,16 +1,16 @@
-import { createTheme } from '@mui/material/styles';
-import palette from './colors';
-import shadows from './shadows';
-import typography from './typography';
-import misc from './misc';
+// import { createTheme } from '@mui/material/styles';
+// import palette from './colors';
+// import shadows from './shadows';
+// import typography from './typography';
+// import misc from './misc';
 
-const theme = createTheme({
-	palette,
-	typography: {
-		...typography,
-		...misc,
-	},
-	shadows,
-});
+// const theme = createTheme({
+// 	palette,
+// 	typography: {
+// 		...typography,
+// 		...misc,
+// 	},
+// 	shadows,
+// });
 
-export default theme;
+// export default theme;
