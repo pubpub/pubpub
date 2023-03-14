e6241291309991d9be0a4a8977780db58bfc908a
ben howe <howebenhowe@gmail.com>
2023-03-14 11:10:48 -0400

    add zotero json support for citatatin-js

--- server/citation/api.ts
+++ server/citation/api.ts
@@ -1,14 +1,17 @@
 import zoteroClient from 'zotero-api-client';
+import Cite from 'citation-js';
 
 import app from 'server/server';
 
 import { ZoteroIntegration, IntegrationDataOAuth1 } from '../models';
 
+require('@citation-js/plugin-zotero-translation-server');
+
 // the parameters which can be passed to the get() method
 // can be found at https://www.zotero.org/support/dev/web_api/v3/basics
 app.get('/api/citations/zotero', (req, res) => {
 	const userId = req.user?.id;
-	const { q } = req.query;
+	const { q, include } = req.query;
 	if (!userId) return new Error('Log in to request citations');
 	return ZoteroIntegration.findOne({
 		where: { userId },
@@ -24,9 +27,16 @@ app.get('/api/citations/zotero', (req, res) => {
 			}
 			const zoteroId = parseInt(zoteroIntegration.zoteroUserId, 10);
 			const myApi = zoteroClient(integrationDataOAuth1.accessToken).library('user', zoteroId);
-			return myApi.items().get({ q });
+			return myApi.items().get({ q, include });
 		})
-		.then((items) => {
-			return res.status(200).json({ items });
+		.then((results) => {
+			const keyed = results.raw.reduce((memo, entry) => {
+				const structured = new Cite(entry.data).format('bibtex');
+				return {
+					...memo,
+					[entry.key]: { ...entry, structured },
+				};
+			}, {});
+			return res.status(200).json({ items: keyed });
 		});
 });
