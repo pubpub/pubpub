9d5be04a5fd47ac2d9751bcc3cf26d3b3ed9ffa3
ben howe <howebenhowe@gmail.com>
2023-03-21 10:43:46 -0400

    WIP, dummy location + test review app deploy

--- server/Html.tsx
+++ server/Html.tsx
@@ -81,6 +81,19 @@ const Html = (props: Props) => {
 					name="google-site-verification"
 					content="jmmJFnkSOeIEuS54adOzGMwc0kwpsa8wQ-L4GyPpPDg"
 				/>
+				<meta name="twitter:card" content="summary_large_image" />
+				<meta name="twitter:site" content="@nytimes" />
+				<meta name="twitter:creator" content="@SarahMaslinNir" />
+				<meta name="twitter:title" content="Parade of Fans for Houston’s Funeral" />
+				<meta
+					name="twitter:description"
+					content="NEWARK - The guest list and parade of limousines with celebrities emerging from them seemed more suited to a red carpet event in Hollywood or New York than than a gritty stretch of Sussex Avenue near the former site of the James M. Baxter Terrace public housing project here."
+				/>
+				<meta
+					name="twitter:image"
+					content="http://graphics8.nytimes.com/images/2012/02/19/us/19whitney-span/19whitney-span-articleLarge.jpg"
+				/>
+
 				<link rel="stylesheet" type="text/css" href={getPath('vendor', 'css')} />
 				<link rel="stylesheet" type="text/css" href={getPath('main', 'css')} />
 				{customScripts?.css && (
