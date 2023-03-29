59855decfce8d1edceec50711e8eda3e766a43fa
qweliant <dmtorcode@gmail.com>
2023-03-29 10:53:39 -0400

    start

--- client/components/FormattingBar/controlComponents/ControlsLink.tsx
+++ client/components/FormattingBar/controlComponents/ControlsLink.tsx
@@ -1,7 +1,8 @@
 import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { useDebounce } from 'use-debounce';
+
 import { moveToEndOfSelection } from 'components/Editor';
-import { Button, AnchorButton, InputGroup } from '@blueprintjs/core';
+import { Button, AnchorButton, InputGroup, Checkbox, Icon } from '@blueprintjs/core';
 import { usePubContext } from 'containers/Pub/pubHooks';
 import { pubUrl } from 'utils/canonicalUrls';
 import { usePageContext } from 'utils/hooks';
@@ -23,6 +24,7 @@ const ControlsLink = (props: Props) => {
 	const { communityData } = usePageContext();
 	const { inPub, pubData } = usePubContext();
 	const [href, setHref] = useState(activeLink.attrs.href);
+	// const [openInNewTab, setOpenInNewTab] = useState(true); // this must change mark attrs on link node
 	const [debouncedHref] = useDebounce(href, 250);
 	const inputRef = useRef();
 
@@ -34,6 +36,14 @@ const ControlsLink = (props: Props) => {
 		}
 		setHref(value);
 	};
+
+	// in actuality you would be updating mark attrs for the activeLink
+	// const target = openInNewTab ? '_blank' : '_self';
+
+	// check link to see mark attr for tyarget
+	// derive check in check box from this
+	// ex: if activeLink.attrs.target === _blank checked is true
+	// useEffect(()=>{if(activeLink.attrs) }, [target])
 
 	// eslint-disable-next-line react-hooks/exhaustive-deps
 	useEffect(() => activeLink.updateAttrs({ href: debouncedHref }), [debouncedHref]);
@@ -59,8 +69,20 @@ const ControlsLink = (props: Props) => {
 		}
 	};
 
+	function ControlsLinkPopover() {
+		return (
+			<div>
+				<Checkbox label="Open in new tab" checked={true} />
+				<Checkbox label="Create a pub connection for this url" />
+				<div>Type: connection type dropdown</div>
+				<div>Direction: direction dropdown</div>
+				<Icon icon="info-sign" /> Preview
+			</div>
+		);
+	}
+
 	return (
-		<div className="controls-link-component">
+		<div className="controls-link-component" style={{ flexDirection: 'column' }}>
 			<InputGroup
 				placeholder="Enter a URL"
 				value={href}
@@ -69,9 +91,28 @@ const ControlsLink = (props: Props) => {
 				// @ts-expect-error ts-migrate(2322) FIXME: Type 'MutableRefObject<undefined>' is not assignab... Remove this comment to see the full error message
 				inputRef={inputRef}
 			/>
-			<AnchorButton small minimal title="Visit" icon="share" href={href} target="_blank" />
-			<Button small minimal title="Remove" icon="disable" onClick={activeLink.removeLink} />
+			<div>
+				<AnchorButton small minimal title="Visit" icon="chevron-up" />
+				<AnchorButton
+					small
+					minimal
+					title="Optiona"
+					icon="share"
+					href={href}
+					target="_blank"
+					// target={target}
+				/>
+				<Button
+					small
+					minimal
+					title="Remove"
+					icon="disable"
+					onClick={activeLink.removeLink}
+				/>
+			</div>
+			<ControlsLinkPopover />
 		</div>
 	);
 };
+
 export default ControlsLink;
