b86c89d41415c01680d4d923808644af757050ad
ben howe <howebenhowe@gmail.com>
2023-03-30 12:56:12 -0400

    transate our citation-style into zotero-recognized names

--- server/citation/api.ts
+++ server/citation/api.ts
@@ -1,14 +1,35 @@
 import zoteroClient from 'zotero-api-client';
 
 import app from 'server/server';
+import { CitationStyleKind } from 'utils/citations';
+
+import { ZoteroStyleKind } from 'types';
 
 import { ZoteroIntegration, IntegrationDataOAuth1 } from '../models';
+
+// mapping our citation style keys to zotero's keys
+const zoteroStyleKindMap: Record<CitationStyleKind, ZoteroStyleKind> = {
+	'acm-siggraph': 'acm-siggraph',
+	'american-anthro': 'american-anthropological-association',
+	apa: 'apa-6th-edition',
+	'apa-7': 'apa',
+	'arcadia-science': 'arcadia-science',
+	cell: 'cell',
+	chicago: 'chicago-note-bibliography',
+	harvard: 'harvard-cite-them-right',
+	elife: 'elife',
+	frontiers: 'frontiers',
+	mla: 'modern-language-association',
+	vancouver: 'vancouver',
+	ama: 'american-medical-association',
+};
 
 // the parameters which can be passed to the get() method
 // can be found at https://www.zotero.org/support/dev/web_api/v3/basics
 app.get('/api/citations/zotero', (req, res) => {
 	const userId = req.user?.id;
-	const { q, include, style } = req.query;
+	const { q, include, style = 'apa' } = req.query;
+	const zoteroStyle = zoteroStyleKindMap[style as CitationStyleKind];
 	if (!userId) return new Error('Log in to request citations');
 	return ZoteroIntegration.findOne({
 		where: { userId },
@@ -27,7 +48,7 @@ app.get('/api/citations/zotero', (req, res) => {
 				'user',
 				zoteroId,
 			);
-			return zoteroAPI.items().get({ q, include, style });
+			return zoteroAPI.items().get({ q, include, style: zoteroStyle });
 		})
 		.then((results) => res.status(200).json({ items: results.raw }));
 });
--- types/zotero.ts
+++ types/zotero.ts
@@ -16,3 +16,19 @@ export type ZoteroCSLJSON = {
 	structured: string;
 	key: string;
 };
+
+// citations style keys as recognized by Zotero
+export type ZoteroStyleKind =
+	| 'acm-siggraph'
+	| 'american-anthropological-association'
+	| 'apa-6th-edition'
+	| 'apa'
+	| 'arcadia-science'
+	| 'cell'
+	| 'chicago-note-bibliography'
+	| 'harvard-cite-them-right'
+	| 'elife'
+	| 'frontiers'
+	| 'modern-language-association'
+	| 'vancouver'
+	| 'american-medical-association';
