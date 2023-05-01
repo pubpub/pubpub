c9fd8acb6377b0819361a57e75c3b61554e9da62
Gabriel Stein <g@gabestein.com>
2023-05-01 17:14:18 -0400

    add domain to cookie

--- server/logout/api.ts
+++ server/logout/api.ts
@@ -2,7 +2,10 @@ import app from 'server/server';
 
 app.get('/api/logout', (req, res) => {
 	res.cookie('gdpr-consent-survives-login', 'no');
-	res.cookie('pp-cache', 'pp-cache');
+	res.cookie('pp-cache', 'pp-cache', {
+		...(req.get('host')?.includes('pubpub.org') && { domain: '.pubpub.org' }),
+		...(req.get('host')?.includes('duqduq.org') && { domain: '.duqduq.org' }),
+	});
 	// @ts-expect-error
 	req.logout();
 	return res.status(200).json('success');
