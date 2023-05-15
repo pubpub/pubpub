5310f8787ad4b833c9b814906225e141b31db40d
qweliant <dmtorcode@gmail.com>
2023-05-15 11:58:51 -0400

    todo: pare down toolbar so its accept reject components

--- client/components/FormattingBar/FormattingBarSuggestedEdits.tsx
+++ client/components/FormattingBar/FormattingBarSuggestedEdits.tsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import { useToolbarState, ToolbarItem } from 'reakit';
 
 import { EditorChangeObject } from 'components/Editor';
+import { useRefMap } from 'client/utils/useRefMap';
+import { usePageContext } from 'utils/hooks';
+
 import { FormattingBarButtonData, ControlsConfiguration } from './types';
+// import { usePendingAttrs } from './hooks/usePendingAttrs';
+import FormattingBarButton from './FormattingBarButton';
+import { ButtonState, useControlsState } from './hooks/useControlsState';
+import { getButtonPopoverComponent } from './utils';
 
 type Props = {
 	editorChangeObject: EditorChangeObject;
@@ -12,7 +20,81 @@ type Props = {
 const FormattingBarSuggestedEdits = (props: Props) => {
 	const { editorChangeObject, buttons, controlsConfiguration } = props;
 	console.log(editorChangeObject, buttons, controlsConfiguration);
-	return <div>"Hello New World!"</div>;
+	const buttonElementRefs = useRefMap();
+	const wrapperRef = useRef<null | HTMLDivElement>(null);
+	const toolbar = useToolbarState({ loop: true });
+	// const pendingAttrs = usePendingAttrs(editorChangeObject);
+
+	const { communityData } = usePageContext();
+
+	const {
+		openedButton,
+		// setOpenedButton,
+		// selectedNodeId,
+		buttonStates,
+		// resolvedControlsConfiguration,
+	} = useControlsState({
+		buttons,
+		editorChangeObject,
+		controlsConfiguration: {
+			kind: 'anchored',
+			container: wrapperRef.current!,
+			isAbsolutelyPositioned: false,
+			isFullScreenWidth: false,
+			...controlsConfiguration,
+		},
+	});
+
+	useEffect(() => {
+		if (openedButton) {
+			const ref = buttonElementRefs.getRef(openedButton.key);
+			if (ref && ref.current && typeof ref.current.scrollIntoView === 'function') {
+				const buttonElement = ref.current;
+				const paddingPx = 5;
+				buttonElement.parentNode.scrollLeft = buttonElement.offsetLeft - paddingPx;
+			}
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [openedButton]);
+	const renderButtonState = (buttonState: ButtonState) => {
+		const { button, isOpen, isActive, isIndicated, isDisabled, isDetached, onClick } =
+			buttonState;
+		const maybeEditorView = button.key === 'media' && { view: editorChangeObject.view };
+		const PopoverComponent = getButtonPopoverComponent(button, isDisabled);
+
+		return (
+			<ToolbarItem
+				{...toolbar}
+				outerRef={buttonElementRefs.getRef(button.key)}
+				as={button.component || FormattingBarButton}
+				key={button.key}
+				label={button.label}
+				formattingItem={button}
+				disabled={isDisabled}
+				isActive={isActive}
+				isIndicated={isIndicated && !isOpen}
+				isOpen={isOpen}
+				isDetached={isDetached}
+				popoverContent={PopoverComponent && <PopoverComponent />}
+				accentColor={communityData.accentColorDark}
+				onClick={onClick}
+				{...maybeEditorView}
+			/>
+		);
+	};
+
+	const renderButtonGroup = (states: ButtonState[], index: number) => {
+		return (
+			<React.Fragment key={index}>
+				{index > 0 && <div className="separator" />}
+				{states.map(renderButtonState)}
+			</React.Fragment>
+		);
+	};
+	const renderControls = () => {
+		return <>{buttonStates.map(renderButtonGroup)}</>;
+	};
+	return renderControls();
 };
 
 export default FormattingBarSuggestedEdits;
