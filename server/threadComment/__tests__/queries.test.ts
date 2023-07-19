8100fa45aa2ac56f9b343282ebf33c3af3e0c69b
Thomas F. K. Jorna <hello@tefkah.com>
2023-07-19 19:36:30 +0200

    dev: await deferred tasks to prevent logging error in tests

--- server/threadComment/__tests__/queries.test.ts
+++ server/threadComment/__tests__/queries.test.ts
@@ -4,6 +4,7 @@ import { UserSubscription } from 'server/models';
 
 import { DocJson } from 'types';
 import { createThreadComment } from '../queries';
+import { finishDeferredTasks } from 'server/utils/deferred';
 
 const models = modelize`
     User willBeSubscribed {
@@ -33,7 +34,9 @@ const models = modelize`
 setup(beforeAll, async () => {
 	await models.resolve();
 });
-teardown(afterAll);
+teardown(afterAll, async () => {
+	await finishDeferredTasks();
+});
 
 describe('createThreadComment()', () => {
 	it('subscribes a user to a thread when they comment in it, according to their notification preferences', async () => {
