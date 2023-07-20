8ae4668e2d7c6c2a88ee505e17fd57efe8201402
Thomas F. K. Jorna <hello@tefkah.com>
2023-07-20 15:59:35 +0200

    dev: exchange submission types for model types
    280 errors in 84 files

--- types/submissionWorkflow.ts
+++ types/submissionWorkflow.ts
@@ -1,19 +1,22 @@
-import { Collection, DocJson } from 'types';
+import { Attributes } from 'sequelize';
+import { SubmissionWorkflow as SubmissionWorkflowModel } from 'server/models';
+// import { Collection, DocJson } from 'types';
 
-export type SubmissionWorkflow = {
-	id: string;
-	createdAt: string;
-	updatedAt: string;
-	enabled: boolean;
-	title: string;
-	introText: DocJson;
-	instructionsText: DocJson;
-	acceptedText: DocJson;
-	declinedText: DocJson;
-	receivedEmailText: DocJson;
-	targetEmailAddresses: string[];
-	requireAbstract: boolean;
-	requireDescription: boolean;
-	collectionId: string;
-	collection?: Collection;
-};
+export type SubmissionWorkflow = Attributes<SubmissionWorkflowModel>;
+// {
+// 	id: string;
+// 	createdAt: string;
+// 	updatedAt: string;
+// 	enabled: boolean;
+// 	title: string;
+// 	introText: DocJson;
+// 	instructionsText: DocJson;
+// 	acceptedText: DocJson;
+// 	declinedText: DocJson;
+// 	receivedEmailText: DocJson;
+// 	targetEmailAddresses: string[];
+// 	requireAbstract: boolean;
+// 	requireDescription: boolean;
+// 	collectionId: string;
+// 	collection?: Collection;
+// };
