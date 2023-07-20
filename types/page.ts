a1f49c110d145bbe73558399f4e51d77cd2b743f
Thomas F. K. Jorna <hello@tefkah.com>
2023-07-20 16:01:41 +0200

    dev: exchange page types for model types. 279 errors in 87 files

--- types/page.ts
+++ types/page.ts
@@ -1,15 +1,4 @@
-import { LayoutBlock } from 'utils/layout/types';
+import { Attributes } from 'sequelize';
+import { Page as PageModel } from 'server/models';
 
-export type Page = {
-	id: string;
-	title: string;
-	slug: string;
-	communityId: string;
-	description: null | string;
-	avatar?: string;
-	isPublic: boolean;
-	isNarrowWidth?: boolean;
-	viewHash?: string;
-	layout: LayoutBlock[];
-	layoutAllowsDuplicatePubs: boolean;
-};
+export type Page = Attributes<PageModel>;
