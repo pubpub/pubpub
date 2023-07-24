98846879fd0b07d9b20d13daae70eb31b01d292a
Thomas F. K. Jorna <hello@tefkah.com>
2023-07-24 13:54:42 +0200

    dev: exchange Visibility and VisibilityUser types for model types -> 481 erros in 167 files. (+0, +0)

--- types/visibility.ts
+++ types/visibility.ts
@@ -1,19 +1,16 @@
+import { Attributes } from 'sequelize';
+import {
+	Visibility as VisibilityModel,
+	VisibilityUser as VisibilityUserModel,
+} from 'server/models';
 import { Discussion } from './discussion';
 import { Review } from './review';
 
 export type VisibilityAccess = 'private' | 'members' | 'public';
 
-export type VisibilityUser = {
-	id: string;
-	visibilityId: string;
-	userId: string;
-};
+export type VisibilityUser = Attributes<VisibilityUserModel>;
 
-export type Visibility = {
-	id: string;
-	access: VisibilityAccess;
-	users: VisibilityUser[];
-};
+export type Visibility = Attributes<VisibilityModel>;
 
 export type TaggedVisibilityParent =
 	| { type: 'discussion'; value: Discussion }
