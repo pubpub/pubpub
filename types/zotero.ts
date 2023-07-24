156b4b9549880312554a5e258395800f0588de2f
Thomas F. K. Jorna <hello@tefkah.com>
2023-07-24 13:55:43 +0200

    dev: exchange ZoteroIntegration types for model types -> 481 erros in 167 files. (+0, +0)

--- types/zotero.ts
+++ types/zotero.ts
@@ -1,10 +1,7 @@
-export type ZoteroIntegration = {
-	id: string;
-	userId: string;
-	zoteroUsername: string;
-	zoteroUserId: string;
-	integrationDataOAuth1Id: string;
-};
+import { Attributes } from 'sequelize';
+import { ZoteroIntegration as ZoteroIntegrationModel } from 'server/models';
+
+export type ZoteroIntegration = Attributes<ZoteroIntegrationModel>;
 
 export type ZoteroCSLJSON = {
 	meta: {
