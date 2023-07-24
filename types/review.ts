619c275bc5714b34d60d06de7973e94b2cf2c496
Thomas F. K. Jorna <hello@tefkah.com>
2023-07-24 13:21:34 +0200

    dev: exchange review and reviewer types for model types -> 485 erros in 168 files. (+7, +1)

--- types/review.ts
+++ types/review.ts
@@ -1,30 +1,6 @@
-import { Visibility } from './visibility';
-import { Thread } from './thread';
-import { Pub } from './pub';
-import { DocJson } from './doc';
-import { User } from './user';
+import { Attributes } from 'sequelize';
+import { ReviewNew as ReviewNewModel, Reviewer as ReviewerModel } from 'server/models';
 
-export type Review = {
-	id: string;
-	author: User;
-	createdAt: string;
-	updatedAt: string;
-	title: string;
-	number: number;
-	status: 'open' | 'closed' | 'completed';
-	releaseRequested: boolean;
-	threadId: string;
-	thread: Thread;
-	visibilityId: string;
-	visibility?: Visibility;
-	userId: string;
-	pubId: string;
-	pub?: Pub;
-	reviewContent?: DocJson;
-	reviewers?: Reviewer[];
-};
+export type Review = Attributes<ReviewNewModel>;
 
-export type Reviewer = {
-	id: string;
-	name: string;
-};
+export type Reviewer = Attributes<ReviewerModel>;
