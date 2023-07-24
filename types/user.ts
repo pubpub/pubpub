315e2aa7f58f61b7fefaa868ef951df6da158da0
Thomas F. K. Jorna <hello@tefkah.com>
2023-07-24 13:49:56 +0200

    dev: exchange User types for model types -> 484 erros in 168 files. (-4, -2)

--- types/user.ts
+++ types/user.ts
@@ -1,40 +1,90 @@
-export type MinimalUser = {
-	id: string;
-	slug?: string;
-	initials: string;
-	fullName: string;
-	firstName: string;
-	lastName: string;
-	avatar?: string;
-	title?: string;
-	orcid?: string;
-	isShadowUser?: boolean;
-	publicEmail?: string;
-	feedback?: string;
-};
+import { Attributes } from 'sequelize';
+import { User as UserModel } from 'server/models';
 
-export type User = MinimalUser & {
-	bio: string;
-	location: string;
-	website: string;
-	facebook: string;
-	twitter: string;
-	github: string;
-	googleScholar: string;
-	authRedirectHost?: string;
-};
+type UserType = Attributes<UserModel>;
 
-export type UserWithPrivateFields = User & {
-	isSuperAdmin: boolean;
-	passwordDigest: string;
-	hash: string;
-	salt: string;
-	email: string;
-	resetHash: string;
-	resetHashExpiration: number;
-};
+export type MinimalUser = Pick<
+	UserType,
+	| 'id'
+	| 'slug'
+	| 'initials'
+	| 'fullName'
+	| 'firstName'
+	| 'lastName'
+	| 'avatar'
+	| 'title'
+	| 'orcid'
+	| 'isShadowUser'
+	| 'publicEmail'
+	| 'feedback'
+>;
 
-export type UserWithPrivateFieldsAndHashedPassword = UserWithPrivateFields & {
-	sha3hashedPassword: string;
-};
+// export type MinimalUser = {
+// 	id: string;
+// 	slug?: string;
+// 	initials: string;
+// 	fullName: string;
+// 	firstName: string;
+// 	lastName: string;
+// 	avatar?: string;
+// 	title?: string;
+// 	orcid?: string;
+// 	isShadowUser?: boolean;
+// 	publicEmail?: string;
+// 	feedback?: string;
+// };
+
+// export type User = MinimalUser & {
+// 	bio: string;
+// 	location: string;
+// 	website: string;
+// 	facebook: string;
+// 	twitter: string;
+// 	github: string;
+// 	googleScholar: string;
+// 	authRedirectHost?: string;
+// };
+
+export type User = Pick<
+	UserType,
+	| 'bio'
+	| 'location'
+	| 'website'
+	| 'facebook'
+	| 'twitter'
+	| 'github'
+	| 'googleScholar'
+	| 'authRedirectHost'
+	| keyof MinimalUser
+>;
+export type UserWithPrivateFields = Pick<
+	UserType,
+	| keyof User
+	| 'isSuperAdmin'
+	| 'passwordDigest'
+	| 'hash'
+	| 'salt'
+	| 'email'
+	| 'resetHash'
+	| 'resetHashExpiration'
+>;
+
+export type UserWithPrivateFieldsAndHashedPassword = Pick<
+	UserType,
+	keyof UserWithPrivateFields | 'sha3hashedPassword'
+>;
+
+// export type UserWithPrivateFields = User & {
+// 	isSuperAdmin: boolean;
+// 	passwordDigest: string;
+// 	hash: string;
+// 	salt: string;
+// 	email: string;
+// 	resetHash: string;
+// 	resetHashExpiration: number;
+// };
+
+// export type UserWithPrivateFieldsAndHashedPassword = UserWithPrivateFields & {
+// 	sha3hashedPassword: string;
+// };
 export type SuggestedEditsUser = Pick<MinimalUser, 'avatar' | 'initials' | 'fullName'>;
