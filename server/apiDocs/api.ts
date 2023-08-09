1161b8e362e2435528e4afa1d2383707fa2b61dd
Thomas F. K. Jorna <hello@tefkah.com>
2023-08-09 21:11:24 +0200

    chore: remove some extranous stuff

--- server/apiDocs/api.ts
+++ server/apiDocs/api.ts
@@ -1,42 +1,13 @@
 import app from 'server/server';
 import { openApiMiddleware } from 'utils/api';
-import swaggerUi from 'swagger-ui-express';
-import 'swagger-ui-dist';
-import { expressAppToApiSchema } from 'utils/api/express-retrospective-to-openapi-schema';
 
 app.use(
 	'/apidocs',
 	openApiMiddleware({
 		info: {
-			title: 'hi',
-			version: '1.0.0',
+			title: 'PubPub API',
+			version: '6.0.0',
 		},
 		redoc: true,
 	}),
-);
-
-let swaggerDoc = {} as ReturnType<typeof expressAppToApiSchema>;
-// ashtasht
-
-app.use(
-	'/api-docs',
-	swaggerUi.serveWithOptions({
-		redirect: false,
-	}),
-	(req, res, next) => {
-		console.log('AAAAAAAAA');
-		const doc = expressAppToApiSchema(
-			{
-				info: {
-					title: 'hi',
-					version: '1.0.0',
-				},
-			},
-			req.app,
-		);
-		swaggerDoc = swaggerDoc || doc;
-
-		next();
-	},
-	swaggerUi.setup(swaggerDoc),
 );
