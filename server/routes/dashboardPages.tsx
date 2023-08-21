a56bfccf316dccad36cfa52647841cee34784281
Thomas F. K. Jorna <hello@tefkah.com>
2023-08-21 22:23:19 +0200

    fix: correctly throw on /dash/pages/bad-name

--- server/routes/dashboardPages.tsx
+++ server/routes/dashboardPages.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Html from 'server/Html';
 import app from 'server/server';
-import { NotFoundError, handleErrors } from 'server/utils/errors';
+import { handleErrors } from 'server/utils/errors';
 import { getInitialData } from 'server/utils/initData';
 import { hostIsValid } from 'server/utils/routes';
 import { generateMetaComponents, renderToNodeStream } from 'server/utils/ssr';
@@ -14,9 +14,6 @@ app.get(['/dash/pages'], async (req, res, next) => {
 			return next();
 		}
 		const initialData = await getInitialData(req, { isDashboard: true });
-		if (!initialData.scopeData.elements.activeTarget) {
-			throw new NotFoundError();
-		}
 		// const pagesData = await getPages(initialData);
 		return renderToNodeStream(
 			res,
--- server/utils/queryHelpers/pageGet.ts
+++ server/utils/queryHelpers/pageGet.ts
@@ -5,25 +5,23 @@ import { InitialData } from 'types';
 import { expect } from 'utils/assert';
 
 export default async ({ query, initialData }: { query: any; initialData: InitialData }) => {
-	const pageData = expect(
-		await Page.findOne({
-			where: {
-				...query,
-				communityId: initialData.communityData.id,
-			},
-		}),
-	);
+	const pageData = await Page.findOne({
+		where: {
+			...query,
+			communityId: initialData.communityData.id,
+		},
+	});
 
 	const layoutPubsByBlock = await getLayoutPubsByBlock({
-		allowDuplicatePubs: pageData.layoutAllowsDuplicatePubs,
-		blocks: pageData.layout || [],
+		allowDuplicatePubs: pageData?.layoutAllowsDuplicatePubs!,
+		blocks: pageData?.layout || [],
 		initialData,
 	});
 
 	return {
-		...pageData.toJSON(),
+		...pageData?.toJSON(),
 		layout: enrichLayoutBlocksWithPubTokens({
-			blocks: pageData.layout,
+			blocks: pageData?.layout,
 			initialData,
 		} as any),
 		layoutPubsByBlock,
