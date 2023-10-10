96dc07858b4c376e587613f5e6ea53b8cfbbf732
Thomas F. K. Jorna <hello@tefkah.com>
2023-10-10 22:21:31 +0200

    fix(#2867): update zod to allow more complex email domains (#2870)
    * fix: update zod to allow hyphens and subdomains in email, and add tests to prevent regression
    
    * chore: fix eslint error

---
+++ server/login/__tests__/api.test.ts
@@ -0,0 +1,31 @@
+import { setup, login, modelize } from 'stubstub';
+
+const models = modelize`
+	Community community {
+		Member {
+			permissions: "admin"
+			User admin {
+                email: "normal@email.com"
+            }
+		}
+        Member {
+            permissions: "admin"
+            User hyphen {
+                email: "weird@something.with-hyphen.com"
+            }
+        }
+    }`;
+
+setup(beforeAll, async () => {
+	await models.resolve();
+});
+
+describe('/api/login', () => {
+	it('allows you to login normally', async () => {
+		await login(models.admin);
+	});
+
+	it('allows you to login with an email with hyphens and subdomains', async () => {
+		await login(models.hyphen);
+	});
+});
