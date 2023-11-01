4a9f27399a3d1baf5a7852fe0c3bfb66bb09165e
Thomas F. K. Jorna <hello@tefkah.com>
2023-11-01 19:38:46 +0100

    chore: improve documentation for upload endpoint

--- utils/api/contracts/upload.ts
+++ utils/api/contracts/upload.ts
@@ -5,8 +5,9 @@ import { uploadSchema } from '../schemas/upload';
 export const uploadRoute = {
 	path: '/api/upload',
 	method: 'POST',
-	description: 'Upload a file to PubPub',
 	summary: 'Upload a file',
+	description:
+		'Upload a file to PubPub.\n Make sure you include the file _last_ in the formdata, fields included after the file field are ignored.\n\nYou can forgo explictly passing the filename and mimetype if you upload a file with proper file information.',
 	body: uploadSchema,
 	contentType: 'multipart/form-data',
 	responses: {
