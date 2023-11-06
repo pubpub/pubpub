cfa9d081dc0e10fef6953268d7d7ad13074dcaa5
Thomas F. K. Jorna <hello@tefkah.com>
2023-11-06 18:32:53 +0100

    fix: remove permission check for get user endpoint

--- server/user/api.ts
+++ server/user/api.ts
@@ -1,7 +1,7 @@
 import passport from 'passport';
 
 import app, { wrap } from 'server/server';
-import { ForbiddenError, NotFoundError } from 'server/utils/errors';
+import { NotFoundError } from 'server/utils/errors';
 
 import { isProd, isDuqDuq } from 'utils/environment';
 import { getPermissions } from './permissions';
@@ -47,22 +47,17 @@ app.post('/api/users', (req, res) => {
 app.get(
 	'/api/users',
 	wrap(async (req, res) => {
-		const requestIds = getRequestIds(req);
-		const permissions = await getPermissions(requestIds);
-		if (!permissions.read) {
-			throw new ForbiddenError();
-		}
 		const { suggestionUserId } = req.query;
 		if (!suggestionUserId) {
 			throw new NotFoundError();
 		}
 
 		const userInfo = await getSuggestedEditsUserInfo(suggestionUserId);
-		if (userInfo) {
-			return res.status(201).json(userInfo);
+		if (!userInfo) {
+			throw new NotFoundError();
 		}
 
-		throw new NotFoundError();
+		return res.status(201).json(userInfo);
 	}),
 );
 
