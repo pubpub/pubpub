473ea7c9e095695c20c97471173ea9e3d70fdb5c
Thomas F. K. Jorna <hello@tefkah.com>
2023-12-19 21:07:25 +0100

    fix: its only pubattributions, not collections

--- server/member/hooks.ts
+++ server/member/hooks.ts
@@ -7,21 +7,9 @@ import {
 } from 'server/activityItem/queries';
 import { setUserSubscriptionStatus } from 'server/userSubscription/queries';
 import { getOrCreateUserNotificationPreferences } from 'server/userNotificationPreferences/queries';
-import { schedulePurge } from 'server/server';
-import { defer } from 'server/utils/deferred';
-
-function purgeUser(userId: string) {
-	defer(async () => {
-		await schedulePurge(userId);
-	});
-}
 
 Member.afterCreate(async (member) => {
-	const { userId, pubId, collectionId } = member;
-
-	if (pubId || collectionId) {
-		purgeUser(userId);
-	}
+	const { userId, pubId } = member;
 
 	if (pubId) {
 		const userNotificationPreferences = await getOrCreateUserNotificationPreferences(userId);
@@ -33,12 +21,6 @@ Member.afterCreate(async (member) => {
 				setAutomatically: true,
 			});
 		}
-	}
-});
-
-Member.afterDestroy(async ({ userId, collectionId, pubId }) => {
-	if (pubId || collectionId) {
-		purgeUser(userId);
 	}
 });
 
--- server/pubAttribution/hooks.ts
+++ server/pubAttribution/hooks.ts
@@ -1,10 +1,21 @@
 import { PubAttribution } from 'server/models';
 import { setUserSubscriptionStatus } from 'server/userSubscription/queries';
 import { getOrCreateUserNotificationPreferences } from 'server/userNotificationPreferences/queries';
+import { schedulePurge } from 'server/server';
+import { defer } from 'server/utils/deferred';
 
 PubAttribution.afterCreate(async (attribution) => {
 	const { userId, pubId } = attribution;
-	if (userId) {
+	if (!userId) {
+		return
+	}
+
+	defer(async () => {
+		// refresh all the user pages that have this pub
+		await schedulePurge(userId);
+	})
+
+
 		const userNotificationPreferences = await getOrCreateUserNotificationPreferences(userId);
 		if (userNotificationPreferences.subscribeToPubsAsContributor) {
 			await setUserSubscriptionStatus({
