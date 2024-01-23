87749fb514163a553d94e1ecf1147d1844bc9d5d
Thomas F. K. Jorna <hello@tefkah.com>
2024-01-23 10:58:23 +0100

    refactor(analytics/api.ts): add comments and remove userId from fingerprint

--- server/analytics/api.ts
+++ server/analytics/api.ts
@@ -1,3 +1,5 @@
+/** This should all be moved to an AWS lambda */
+
 import { initServer } from '@ts-rest/express';
 import { Request } from 'express';
 
@@ -5,16 +7,18 @@ import { contract } from 'utils/api/contract';
 
 const s = initServer();
 
+/**
+ * We just use the user agent and ip address to create a unique identifier for the user.
+ *
+ * We cannot use the session cookie or the userId that follows from this, this would require a
+ * cookie banner (TODO: check this)
+ */
 const fingerPrint = (req: Request) => {
 	const {
 		headers: { 'user-agent': userAgent, 'x-forwarded-for': forwardedFor },
 	} = req;
-	const { id: userId } = req.user || {};
 
-	if (userId) {
-		return userId;
-	}
-
+	/** TODO: Hash this in some way + add fallback for forwardedFor */
 	return `${forwardedFor}-${userAgent}`;
 };
 
