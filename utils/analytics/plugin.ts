9603fbdd93242c8cb879076fea5e4920ed65a3b8
Thomas F. K. Jorna <hello@tefkah.com>
2024-01-23 10:48:28 +0100

    refactor(analytics/plugin.ts): add comments

--- utils/analytics/plugin.ts
+++ utils/analytics/plugin.ts
@@ -2,10 +2,13 @@
 import { type AnalyticsPlugin } from 'analytics';
 
 const sendData = ({ payload, route }: { payload: any; route: 'page' | 'track' | 'identify' }) => {
+	// we don't want to track page renders on the server, as we cache most pages
 	if (!globalThis.navigator || typeof globalThis?.navigator?.sendBeacon !== 'function') {
 		return;
 	}
 
+	// we use navigator.sendBeacon to make sure the request is sent even if the user navigates away from the page
+	// and doesn't block the rest of the page
 	navigator.sendBeacon(`/api/analytics/${route}`, JSON.stringify(payload));
 };
 
