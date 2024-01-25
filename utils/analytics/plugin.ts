7ec7c2b0faeb58cf56a2f87e3f871246c394e491
Thomas F. K. Jorna <hello@tefkah.com>
2024-01-25 13:03:28 +0100

    feat: add referrer and referrer navigation info to tracking payload for unique visit tracking

--- utils/analytics/plugin.ts
+++ utils/analytics/plugin.ts
@@ -1,15 +1,53 @@
 /* eslint-disable no-undef, import/no-unresolved */
 import { type AnalyticsPlugin } from 'analytics';
 
+const ANALYTICS_ENDPOINT = '/api/analytics/track' as const;
+
+/**
+ * Adds referrer information to the payload. If there is no referrer, sets referrer to null and
+ * sameReferrer to false.
+ *
+ * @param payload - The payload object containing properties.
+ * @returns The updated payload object with referrer and sameReferrer properties.
+ */
+const updatePayloadWithReferrer = <P extends { properties: Record<string, any> }>(payload: P) => {
+	const referrer = window.document.referrer;
+
+	if (!referrer) {
+		return {
+			...payload,
+			properties: {
+				...payload.properties,
+				referrer: null,
+				isSameOriginReferrer: false,
+			},
+		};
+	}
+
+	const referrerUrl = new URL(referrer);
+	const currentUrl = new URL(window.location.href);
+
+	return {
+		...payload,
+		properties: {
+			...payload.properties,
+			referrer,
+			isSameOriginReferrer: referrerUrl.origin === currentUrl.origin,
+		},
+	};
+};
+
 const sendData = ({ payload }: { payload: any }) => {
 	// we don't want to track page renders on the server, as we cache most pages
 	if (!globalThis.navigator || typeof globalThis?.navigator?.sendBeacon !== 'function') {
 		return;
 	}
 
+	const payloadWithReferrer = updatePayloadWithReferrer(payload);
+
 	// we use navigator.sendBeacon to make sure the request is sent even if the user navigates away from the page
 	// and doesn't block the rest of the page
-	navigator.sendBeacon(`/api/analytics/track`, JSON.stringify(payload));
+	navigator.sendBeacon(ANALYTICS_ENDPOINT, JSON.stringify(payloadWithReferrer));
 };
 
 export const analyticsPlugin = () => {
