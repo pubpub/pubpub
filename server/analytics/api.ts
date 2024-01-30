a19bc2e9ce28bb0a6504bc0382c20c9fb60a958d
Thomas F. K. Jorna <hello@tefkah.com>
2024-01-30 17:03:20 +0100

    feat: track unique visits without fingerprinting (#3000)

--- server/analytics/api.ts
+++ server/analytics/api.ts
@@ -1,26 +1,11 @@
 /** This should all be moved to an AWS lambda */
 
 import { initServer } from '@ts-rest/express';
-import express, { Request } from 'express';
+import express from 'express';
 
 import { contract } from 'utils/api/contract';
 
 const s = initServer();
-
-/**
- * We just use the user agent and ip address to create a unique identifier for the user.
- *
- * We cannot use the session cookie or the userId that follows from this, this would require a
- * cookie banner (TODO: check this)
- */
-const fingerPrint = (req: Request) => {
-	const {
-		headers: { 'user-agent': userAgent, 'x-forwarded-for': forwardedFor },
-	} = req;
-
-	/** TODO: Hash this in some way + add fallback for forwardedFor */
-	return `${forwardedFor}-${userAgent}`;
-};
 
 const sendToStitch = async (payload: any) => {
 	if (!process.env.STITCH_WEBHOOK_URL) {
@@ -55,12 +40,9 @@ export const analyticsServer = s.router(contract.analytics, {
 				next();
 			},
 		],
-		handler: async ({ body: payload, req }) => {
-			const userFingerPrint = fingerPrint(req);
-
+		handler: async ({ body: payload }) => {
 			await sendToStitch({
-				...payload,
-				fingerprint: userFingerPrint,
+				payload,
 			});
 
 			return {
