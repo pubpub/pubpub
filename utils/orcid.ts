0a1381600606153b6a5998a6d5ed4d19247224fd
Thomas F. K. Jorna <hello@tefkah.com>
2024-02-19 11:42:55 +0100

    dev: add tests

--- utils/orcid.ts
+++ utils/orcid.ts
@@ -1,4 +1,4 @@
 export const ORCID_PATTERN = /(\d{4}-){3}\d{3}(\d|X)/g;
 
 export const ORCID_ID_OR_URL_PATTERN =
-	/^(?:https?:\/\/(?:www\.)?orcid\.org\/)?(\d{4}-){3}\d{3}(\d|X)$/g;
+	/^(?:(?:https?:\/\/)?(?:www\.)?orcid\.org\/)?(\d{4}-){3}\d{3}(\d|X)$/g;
