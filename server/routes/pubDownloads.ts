e4def7d6ade7734b8f30dd1fc050e211ff0680d7
Thomas F. K. Jorna <hello@tefkah.com>
2024-03-21 12:11:29 +0100

    fix: wrap it before you get it

--- server/routes/pubDownloads.ts
+++ server/routes/pubDownloads.ts
@@ -3,7 +3,7 @@ import { pipeline } from 'stream';
 import { promisify } from 'util';
 
 import app, { wrap } from 'server/server';
-import { ForbiddenError, NotFoundError } from 'server/utils/errors';
+import { ForbiddenError, NotFoundError, handleErrors } from 'server/utils/errors';
 import { getInitialData } from 'server/utils/initData';
 import { getPubForRequest } from 'server/utils/queryHelpers';
 import { getBestDownloadUrl } from 'utils/pub/downloads';
@@ -17,31 +17,35 @@ app.get(
 		if (!hostIsValid(req, 'community')) {
 			return next();
 		}
-		const initialData = await getInitialData(req);
-		const { pubSlug, format = null } = req.params;
+		try {
+			const initialData = await getInitialData(req);
+			const { pubSlug, format = null } = req.params;
 
-		const pubData = await getPubForRequest({ slug: pubSlug, initialData });
-		const bestPubDownloadUrl = getBestDownloadUrl(pubData, format);
+			const pubData = await getPubForRequest({ slug: pubSlug, initialData });
+			const bestPubDownloadUrl = getBestDownloadUrl(pubData, format);
 
-		if (!pubData || pubData.releases.length === 0) {
-			throw new ForbiddenError();
-		}
-
-		if (bestPubDownloadUrl) {
-			res.attachment(bestPubDownloadUrl);
-			const downloadResponse = await fetch(bestPubDownloadUrl);
-			if (downloadResponse.ok) {
-				return promisify(pipeline)(downloadResponse.body, res);
+			if (!pubData || pubData.releases.length === 0) {
+				throw new ForbiddenError();
 			}
+
+			if (bestPubDownloadUrl) {
+				res.attachment(bestPubDownloadUrl);
+				const downloadResponse = await fetch(bestPubDownloadUrl);
+				if (downloadResponse.ok) {
+					return promisify(pipeline)(downloadResponse.body, res);
+				}
+			}
+
+			// If there's no download URL then we'll want to regenerate the Pub's exports to produce
+			// an auto-generated PDF. We generally shouldn't get into this state, since those are
+			// generated when a Pub is released -- and this route only works for released Pubs.
+			defer(async () => {
+				await createPubExportsForLatestRelease(pubData.id);
+			});
+
+			throw new NotFoundError();
+		} catch (err) {
+			return handleErrors(req, res, next)(err);
 		}
-
-		// If there's no download URL then we'll want to regenerate the Pub's exports to produce
-		// an auto-generated PDF. We generally shouldn't get into this state, since those are
-		// generated when a Pub is released -- and this route only works for released Pubs.
-		defer(async () => {
-			await createPubExportsForLatestRelease(pubData.id);
-		});
-
-		throw new NotFoundError();
 	}),
 );
