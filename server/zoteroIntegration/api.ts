c4778ce301f8a7b00ae061b8991661a229cf4dd6
Thomas F. K. Jorna <hello@tefkah.com>
2024-03-26 13:58:11 +0100

    fix: fewer 500 for bad zotero integration

--- server/zoteroIntegration/api.ts
+++ server/zoteroIntegration/api.ts
@@ -1,14 +1,19 @@
 import app, { wrap } from 'server/server';
+import { ForbiddenError } from 'server/utils/errors';
 
 import { getZoteroIntegration } from './queries';
 
 app.get(
 	'/api/zoteroIntegration',
-	wrap((req, res) =>
-		getZoteroIntegration(req.user.id).then((integration) =>
+	wrap((req, res) => {
+		if (!req.user?.id) {
+			throw new ForbiddenError(new Error('User is not logged in'));
+		}
+
+		return getZoteroIntegration(req.user.id).then((integration) =>
 			integration
-				? res.status(201).json({ id: integration.id })
+				? res.status(200).json({ id: integration.id })
 				: res.status(404).json({ message: 'no zotero integration present for user' }),
-		),
-	),
+		);
+	}),
 );
