7ac5c9890aae9659c2134e50cb41c74d77f853fe
gabestein <gabe@ghscommunications.com>
2024-09-10 18:19:34 -0400

    fix race condition that resulted in no final download

--- tools/exportCollection.js
+++ tools/exportCollection.js
@@ -27,25 +27,30 @@ const getPubExports = async (pubId, dest) => {
 	const pdfUrl = getBestDownloadUrl(pubData, 'pdf');
 	const jatsUrl = getBestDownloadUrl(pubData, 'jats');
 	const finalDest = `${dest}/${pubData.slug}`;
-
+	const promises = [];
 	if (!pdfUrl || !jatsUrl) {
 		console.log('Missing:', pubData.slug);
-		await createPubExportsForLatestRelease(pubId);
-		await getPubExports(pubId, dest);
-	} else {
-		fs.mkdirSync(finalDest);
-		if (jatsUrl) {
-			await fetch(jatsUrl).then((res) => {
-				res.body.pipe(fs.createWriteStream(`${finalDest}/${pubData.slug}.xml`));
-			});
-		}
-		if (pdfUrl) {
-			await fetch(pdfUrl).then((res) => {
-				res.body.pipe(fs.createWriteStream(`${finalDest}/${pubData.slug}.pdf`));
-			});
-		}
+		promises.push(createPubExportsForLatestRelease(pubId));
+		promises.push(getPubExports(pubId, dest));
+		console.log('resolve recursive call', promises);
+		return Promise.all(promises);
 	}
-	return Promise.resolve();
+	// Make the empty dir
+	fs.mkdirSync(finalDest);
+	promises.push(
+		fetch(jatsUrl).then(async (res) => {
+			console.log('getting JATS...', jatsUrl);
+			res.body.pipe(fs.createWriteStream(`${finalDest}/${pubData.slug}.xml`));
+		}),
+	);
+	promises.push(
+		fetch(pdfUrl).then(async (res) => {
+			console.log('getting PDF...', pdfUrl);
+			res.body.pipe(fs.createWriteStream(`${finalDest}/${pubData.slug}.pdf`));
+		}),
+	);
+	console.log('resolving internal loop...', promises);
+	return Promise.all(promises);
 };
 
 const main = async () => {
