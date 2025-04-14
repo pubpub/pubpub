0da7d1578b9921b79a089cded7aaaf5f8843a516
Thomas F. K. Jorna <hello@tefkah.com>
2025-04-14 14:28:39 +0200

    fix: make ux of exporting a bit nicer

--- client/containers/DashboardSettings/CommunitySettings/ExportCommunityDataButton.tsx
+++ client/containers/DashboardSettings/CommunitySettings/ExportCommunityDataButton.tsx
@@ -1,4 +1,4 @@
-import { Button } from '@blueprintjs/core';
+import { Button, Classes } from '@blueprintjs/core';
 import { apiFetch } from 'client/utils/apiFetch';
 import { pingTask } from 'client/utils/pingTask';
 import React, { useState } from 'react';
@@ -6,37 +6,52 @@ import React, { useState } from 'react';
 export const ExportCommunityDataButton = ({ disabled }: { disabled?: boolean }) => {
 	const [isExporting, setIsExporting] = useState(false);
 	const [exportUrl, setExportUrl] = useState<string | null>(null);
+	const [error, setError] = useState<string | null>(null);
 
 	return (
 		<div>
+			<div>
+				<Button
+					disabled={isExporting || disabled}
+					onClick={() => {
+						setExportUrl(null);
+						setError(null);
+						setIsExporting(true);
+						apiFetch('/api/communities/archive', {
+							method: 'POST',
+							body: JSON.stringify({ dontWait: true }),
+						})
+							.then(({ workerTaskId }) => {
+								return pingTask(workerTaskId, 1000);
+							})
+							.then((output) => {
+								if (typeof output !== 'string') {
+									console.error('Invalid output', output);
+									setError('Invalid output');
+									return;
+								}
+
+								setExportUrl(output);
+								setIsExporting(false);
+							})
+							.catch((e) => {
+								setIsExporting(false);
+								console.error(e);
+							});
+					}}
+				>
+					{isExporting ? 'Exporting...' : 'Export Community Data'}
+				</Button>
+			</div>
+
 			{exportUrl && (
-				<a href={exportUrl} target="_blank" rel="noopener noreferrer">
-					Download
-				</a>
+				<p>
+					<a href={exportUrl} target="_blank" rel="noopener noreferrer">
+						Download
+					</a>
+				</p>
 			)}
-			<Button
-				disabled={isExporting || disabled}
-				onClick={() => {
-					setIsExporting(true);
-					apiFetch('/api/communities/archive', {
-						method: 'POST',
-						body: JSON.stringify({ dontWait: true }),
-					})
-						.then(({ url, workerTaskId }) => {
-							setExportUrl(url);
-							return pingTask(workerTaskId, 1000);
-						})
-						.then(() => {
-							setIsExporting(false);
-						})
-						.catch((e) => {
-							setIsExporting(false);
-							console.error(e);
-						});
-				}}
-			>
-				{isExporting ? 'Exporting...' : 'Export Community Data'}
-			</Button>
+			{error && <p className={Classes.INTENT_DANGER}>{error}</p>}
 		</div>
 	);
 };
